dcb6cacb6b820a6d2641fc4a17974f27bcc5b355
Andre Kohn <kohna@in.tum.de>
2019-07-09 20:15:05 +0200

    Show logo

--- tigon-app/src/components/NavigationBar.tsx
+++ tigon-app/src/components/NavigationBar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as Store from '../store';
 import TabBar from './TabBar';
+import { Logo } from '../svg/Logo'
 import { connect } from 'react-redux';
 
 import './NavigationBar.css';
@@ -13,6 +14,9 @@ class NavigationBar extends React.Component<INavigationBarProps> {
     public render() {
         return (
             <div className="NavigationBar">
+                <div className="NavigationBar-Logo">
+                    <Logo />
+                </div>
                 <div className="NavigationBar-Tabs">
                     <TabBar onViewChanged={this.props.navigateRoot} />
                 </div>
