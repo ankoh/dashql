1657e424095a7e65d134af679f17cf357c2052dd
Andre Kohn <kohna@in.tum.de>
2019-07-09 19:02:01 +0200

    Print some text in the terminal

--- tigon-app/src/components/Terminal.tsx
+++ tigon-app/src/components/Terminal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as Store from '../store';
 import * as xterm from 'xterm';
+import * as fit from 'xterm/lib/addons/fit/fit';
 import { connect } from 'react-redux';
 
 import 'xterm/dist/xterm.css';
@@ -10,27 +11,39 @@ interface ITerminalProps {
 }
 
 class Terminal extends React.Component<ITerminalProps> {
-    protected xtermContainer: React.RefObject<HTMLDivElement>;
-    protected xtermTerminal: xterm.Terminal;
+    protected termContainer: React.RefObject<HTMLDivElement>;
+    protected term: xterm.Terminal;
 
     constructor(props: ITerminalProps) {
         super(props);
-        this.xtermContainer = React.createRef();
-        this.xtermTerminal = new xterm.Terminal();
+
+        xterm.Terminal.applyAddon(fit);
+
+        this.termContainer = React.createRef();
+        this.term = new xterm.Terminal();
     }
 
     public render() {
         return (
             <div className="Terminal">
-                <div ref={this.xtermContainer} className="Terminal-XTerm">
+                <div ref={this.termContainer} className="Terminal-XTerm">
                 </div>
             </div>
         );
     }
 
     public componentDidMount() {
-        if (this.xtermContainer.current != null) {
-            this.xtermTerminal.open(this.xtermContainer.current);
+        if (this.termContainer.current != null) {
+            this.term.open(this.termContainer.current);
+            fit.fit(this.term);
+
+            this.term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+            this.term.on('key', (key, ev) => {
+                console.log(key.charCodeAt(0));
+                if (key.charCodeAt(0) === 13)
+                    this.term.write('\n');
+                this.term.write(key);
+            });
         }
     }
 
@@ -38,7 +51,7 @@ class Terminal extends React.Component<ITerminalProps> {
     }
 
     public componentWillUnmount() {
-        this.xtermTerminal.dispose();
+        this.term.dispose();
     }
 }
 
