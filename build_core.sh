5830356b42fe7a1ae49b5fba848e2478f9b4b5c1
Andre Kohn <kohna@in.tum.de>
2019-07-11 15:05:33 +0200

    Add build script for core

---
+++ build_core.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+CORE_SOURCE_DIR="${PROJECT_ROOT}/tigon-core"
+CORE_BUILD_DIR="${PROJECT_ROOT}/tigon-core/build/emscripten"
+APP_SOURCE_DIR="${PROJECT_ROOT}/tigon-app"
+APP_LIB_DIR="${APP_SOURCE_DIR}/public/lib"
+
+[ ! -z "${EMSDK}" ] \
+    && { echo "[ OK  ] Environment: EMSDK"; } \
+    || { echo "[ ERR ] Environment: EMSDK"; exit 1; }
+[ -x "$(command -v emconfigure)" ] \
+    && { echo "[ OK  ] Command: emconfigure"; } \
+    || { echo "[ ERR ] Command: emconfigure"; exit 1; }
+[ -x "$(command -v emmake)" ] \
+    && { echo "[ OK  ] Command: emmake"; } \
+    || { echo "[ ERR ] Command: emmake"; exit 1; }
+
+set -x
+
+cd ${CORE_BUILD_DIR}
+
+emconfigure cmake ${CORE_SOURCE_DIR} \
+    && { echo "[ OK  ] Build configuration"; } \
+    || { echo "[ ERR ] Build configuration"; exit 1; }
+
+emmake make -j$(nproc) \
+    && { echo "[ OK  ] Build project"; } \
+    || { echo "[ ERR ] Build project"; exit 1; }
+
+cp ${CORE_BUILD_DIR}/tigon.{wasm,js}  ${APP_LIB_DIR}
+
+cd ${CORE_SOURCE_DIR}
