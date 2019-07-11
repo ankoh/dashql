4537be8ea51ce5dbf3d5fdd1e87befb26a203833
Andre Kohn <kohna@in.tum.de>
2019-07-11 19:52:47 +0200

    Try to get rid of uppercase files

--- tigon-app/src/util/Time.ts
+++
@@ -1,9 +0,0 @@
-export function formatDurationMS(totalMS: number): string {
-    const m = Math.floor(totalMS / 1000 / 60);
-    const s = Math.floor((totalMS - m) / 1000);
-    const ms = Math.floor(totalMS - m - s);
-    const mS = ('00' + m).slice(-2);
-    const sS = ('00' + s).slice(-2);
-    const msS = ('000' + ms).slice(-3);
-    return `${mS}:${sS}.${msS}`;
-}
