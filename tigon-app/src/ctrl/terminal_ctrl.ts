e1e25290bf213e8d9c0d6d341bb282ae3a1b2a2b
Andre Kohn <kohna@in.tum.de>
2019-07-13 13:27:13 +0200

    Reorder file

--- tigon-app/src/ctrl/terminal_ctrl.ts
+++ tigon-app/src/ctrl/terminal_ctrl.ts
@@ -58,59 +58,6 @@ class HistoryBuffer {
     }
 }
 
-// Detects all the word boundaries on the given input
-function wordBoundaries(input: string, leftSide: boolean = true): Array<number> {
-    let match;
-    let words = new Array<number>();
-    let rx = /\w+/g;
-    while ((match = rx.exec(input))) {
-        if (leftSide) {
-            words.push(match.index);
-        } else {
-            words.push(match.index + match[0].length);
-        }
-    }
-    return words;
-}
-
-// The closest left word boundary of the given input at the given offset
-function closestLeftBoundary(input: string, offset: number) {
-    let found = wordBoundaries(input, true)
-        .reverse()
-        .find(x => x < offset);
-    return found == null ? 0 : found;
-}
-
-// The closest right word boundary of the given input at the given offset
-function closestRightBoundary(input: string, offset: number) {
-    let found = wordBoundaries(input, false).find(x => x > offset);
-    return found == null ? input.length : found;
-}
-
-// Convert offset at the given input to position.
-// TODO: Maintain the line breaks?
-function offsetToPos(input: string, offset: number, maxCols: number) {
-    let row = 0, col = 0;
-    for (let i = 0; i < offset; ++i) {
-        if (input.charAt(i) === "\n") {
-            col = 0;
-            row += 1;
-        } else {
-            col += 1;
-            if (col > maxCols) {
-                col = 0;
-                row += 1;
-            }
-        }
-    }
-    return { row, col };
-}
-
-// Counts the lines in the given input
-function countLines(input: string, maxCols: number) {
-    return offsetToPos(input, input.length, maxCols).row + 1;
-}
-
 // A terminal
 export class TerminalController {
     protected term: xterm.Terminal;
@@ -138,6 +85,93 @@ export class TerminalController {
         this.input = "";
         this.history = new HistoryBuffer();
         this.cursor = 0;
+    }
+
+    // ------------------
+    // Public API
+    // ------------------
+
+    // Return a promise that will resolve when the user has completed typing a single line
+    public read(inputPrompt: string, continuationPrompt: string = "> ") {
+        let t = this;
+        return new Promise(function (resolve: ResolveInputFunc, reject: RejectInputFunc): void {
+            t.term.write(inputPrompt);
+            t.activePrompt = {
+                inputPrompt,
+                continuationPrompt,
+                resolve,
+                reject
+            };
+            t.input = "";
+            t.cursor = 0;
+            t.active = true;
+        });
+    }
+
+    // Return a promise that will resolve when the user has completed typeing a single char
+    public readChar(inputPrompt: string) {
+        let t = this;
+        return new Promise(function (resolve: ResolveInputFunc, reject: RejectInputFunc): void {
+            t.term.write(inputPrompt);
+            t.activeCharPrompt = {
+                inputPrompt,
+                resolve,
+                reject
+            };
+        });
+    }
+
+    // Abort a message and changes line
+    public abortRead(reason: string = "aborted") {
+        if (this.activePrompt != null) {
+            this.term.write("\r\n");
+            this.activePrompt.reject(reason);
+            this.activePrompt = null;
+        } else if (this.activeCharPrompt != null) {
+            this.term.write("\r\n");
+            this.activeCharPrompt.reject(reason);
+            this.activeCharPrompt = null;
+        }
+        this.active = false;
+    }
+
+    // Print a message
+    public print(msg: string) {
+        let normed = msg.replace(/[\r\n]+/g, "\n");
+        this.term.write(normed.replace(/\n/g, "\r\n"));
+    }
+
+    // Print a line
+    public printLine(msg: string) {
+        this.print(msg + "\n");
+    }
+
+    // Prints a list of items using a wide-format
+    public printWide(items: Array<string>, padding: number = 2) {
+        if (items.length === 0) {
+            return this.printLine("");
+        }
+
+        // Compute item sizes and matrix row/cols
+        const itemWidth = items.reduce((width, item) => Math.max(width, item.length), 0) + padding;
+        const wideCols = Math.floor(this.termSize.columns / itemWidth);
+        const wideRows = Math.ceil(items.length / wideCols);
+
+        // Print matrix
+        let i = 0;
+        for (let row = 0; row < wideRows; ++row) {
+            let rowStr = "";
+
+            // Prepare columns
+            for (let col = 0; col < wideCols; ++col) {
+                if (i < items.length) {
+                let item = items[i++];
+                item += " ".repeat(itemWidth - item.length);
+                rowStr += item;
+                }
+            }
+            this.printLine(rowStr);
+        }
     }
 
     // ------------------
@@ -401,93 +435,57 @@ export class TerminalController {
             this.insertAtCursor(data);
         }
     }
+}
 
-
-    // ------------------
-    // Public API
-    // ------------------
-
-    // Return a promise that will resolve when the user has completed typing a single line
-    public read(inputPrompt: string, continuationPrompt: string = "> ") {
-        let t = this;
-        return new Promise(function (resolve: ResolveInputFunc, reject: RejectInputFunc): void {
-            t.term.write(inputPrompt);
-            t.activePrompt = {
-                inputPrompt,
-                continuationPrompt,
-                resolve,
-                reject
-            };
-            t.input = "";
-            t.cursor = 0;
-            t.active = true;
-        });
-    }
-
-    // Return a promise that will resolve when the user has completed typeing a single char
-    public readChar(inputPrompt: string) {
-        let t = this;
-        return new Promise(function (resolve: ResolveInputFunc, reject: RejectInputFunc): void {
-            t.term.write(inputPrompt);
-            t.activeCharPrompt = {
-                inputPrompt,
-                resolve,
-                reject
-            };
-        });
-    }
-
-    // Abort a message and changes line
-    public abortRead(reason: string = "aborted") {
-        if (this.activePrompt != null) {
-            this.term.write("\r\n");
-            this.activePrompt.reject(reason);
-            this.activePrompt = null;
-        } else if (this.activeCharPrompt != null) {
-            this.term.write("\r\n");
-            this.activeCharPrompt.reject(reason);
-            this.activeCharPrompt = null;
+// Detects all the word boundaries on the given input
+function wordBoundaries(input: string, leftSide: boolean = true): Array<number> {
+    let match;
+    let words = new Array<number>();
+    let rx = /\w+/g;
+    while ((match = rx.exec(input))) {
+        if (leftSide) {
+            words.push(match.index);
+        } else {
+            words.push(match.index + match[0].length);
         }
-        this.active = false;
     }
+    return words;
+}
 
-    // Print a message
-    public print(msg: string) {
-        let normed = msg.replace(/[\r\n]+/g, "\n");
-        this.term.write(normed.replace(/\n/g, "\r\n"));
-    }
+// The closest left word boundary of the given input at the given offset
+function closestLeftBoundary(input: string, offset: number) {
+    let found = wordBoundaries(input, true)
+        .reverse()
+        .find(x => x < offset);
+    return found == null ? 0 : found;
+}
 
-    // Print a line
-    public printLine(msg: string) {
-        this.print(msg + "\n");
-    }
+// The closest right word boundary of the given input at the given offset
+function closestRightBoundary(input: string, offset: number) {
+    let found = wordBoundaries(input, false).find(x => x > offset);
+    return found == null ? input.length : found;
+}
 
-    // Prints a list of items using a wide-format
-    public printWide(items: Array<string>, padding: number = 2) {
-        if (items.length === 0) {
-            return this.printLine("");
-        }
-
-        // Compute item sizes and matrix row/cols
-        const itemWidth = items.reduce((width, item) => Math.max(width, item.length), 0) + padding;
-        const wideCols = Math.floor(this.termSize.columns / itemWidth);
-        const wideRows = Math.ceil(items.length / wideCols);
-
-        // Print matrix
-        let i = 0;
-        for (let row = 0; row < wideRows; ++row) {
-            let rowStr = "";
-
-            // Prepare columns
-            for (let col = 0; col < wideCols; ++col) {
-                if (i < items.length) {
-                let item = items[i++];
-                item += " ".repeat(itemWidth - item.length);
-                rowStr += item;
-                }
+// Convert offset at the given input to position.
+// TODO: Maintain the line breaks?
+function offsetToPos(input: string, offset: number, maxCols: number) {
+    let row = 0, col = 0;
+    for (let i = 0; i < offset; ++i) {
+        if (input.charAt(i) === "\n") {
+            col = 0;
+            row += 1;
+        } else {
+            col += 1;
+            if (col > maxCols) {
+                col = 0;
+                row += 1;
             }
-            this.printLine(rowStr);
         }
     }
+    return { row, col };
+}
 
+// Counts the lines in the given input
+function countLines(input: string, maxCols: number) {
+    return offsetToPos(input, input.length, maxCols).row + 1;
 }
