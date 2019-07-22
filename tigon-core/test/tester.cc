4f688bfa2c328070c6334a015c75a3fa073eb258
Andre Kohn <kohna@in.tum.de>
2019-07-22 22:05:45 +0200

    Cleanup

--- tigon-core/test/tester.cc
+++ tigon-core/test/tester.cc
@@ -9,5 +9,4 @@ int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-// ---------------------------------------------------------------------------
 
