93332b2f6dd405ca240b9777b2e2d517205b8b66
Andre Kohn <kohna@in.tum.de>
2019-07-26 18:16:49 +0200

    Add first dummy test

---
+++ tigon-app/src/ctrl/core_ctrl.test.ts
@@ -0,0 +1,9 @@
+const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+
+test('basic', () => {
+  expect(sum()).toBe(0);
+});
+
+test('basic again', () => {
+  expect(sum(1, 2)).toBe(3);
+});
