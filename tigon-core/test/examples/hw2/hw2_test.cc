b945f6a50df22dad4e632bc649f4f7cb94a1632f
Andre Kohn <kohna@in.tum.de>
2019-07-30 23:59:38 +0200

    Add empty test

---
+++ tigon-core/test/examples/hw2/hw2_test.cc
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------
+// Tigon
+// (c) 2019 Andre Kohn
+//---------------------------------------------------------------------------
+
+#include "arrow/api.h"
+#include "arrow/buffer.h"
+#include "arrow/io/api.h"
+#include "arrow/io/memory.h"
+#include "arrow/memory_pool.h"
+#include "gtest/gtest.h"
+#include "parquet/arrow/reader.h"
+#include "parquet/arrow/writer.h"
+#include "parquet/exception.h"
+
+namespace {
+
+TEST(HW2Test, MatchResultToParquet) {
+    
+}
+
+}
+
---
+++ tigon-core/test/examples/hw2/hw2_test_data.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+// Tigon
+// (c) 2019 Andre Kohn
+//---------------------------------------------------------------------------
+
+#ifndef INCLUDE_HW2_TEST_DATA_H_
+#define INCLUDE_HW2_TEST_DATA_H_
+
+#include <stdexcept>
+#include <string>
+
+namespace tigon {
+namespace test {
+
+extern const std::string HW2MatchResult;
+
+}
+} // namespace tigon
+
+#endif // INCLUDE_HW2_TEST_DATA_H_
