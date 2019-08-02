6db5a99ad95cfb755d9eb696ddf14244d3de50c2
Andre Kohn <kohna@in.tum.de>
2019-08-02 18:03:13 +0200

    Build single tigon library

---
+++ tigon-core/include/tigon/extract/csv_extract.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+// Tigon
+// (c) 2019 Andre Kohn
+//---------------------------------------------------------------------------
+
+#ifndef INCLUDE_TIGON_CSV_EXTRACT_H_
+#define INCLUDE_TIGON_CSV_EXTRACT_H_
+
+#include <stdexcept>
+#include <string>
+
+namespace tigon {
+
+} // namespace tigon
+
+#endif // INCLUDE_TIGON_CSV_EXTRACT_H_
---
+++ tigon-core/include/tigon/parser/sql/sql_parser.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+// Tigon
+// (c) 2019 Andre Kohn
+//---------------------------------------------------------------------------
+
+#ifndef INCLUDE_TIGON_PARSER_SQL_SQL_PARSER_H_
+#define INCLUDE_TIGON_PARSER_SQL_SQL_PARSER_H_
+
+#include <stdexcept>
+#include <string>
+
+namespace tigon {
+
+} // namespace tigon
+
+#endif // INCLUDE_TIGON_PARSER_SQL_SQL_PARSER_H_
