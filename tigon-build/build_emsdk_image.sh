5e52b35e96b646ea4cdd09df34906ccc094445df
Andre Kohn <kohna@in.tum.de>
2019-10-26 18:33:50 +0200

    Prepare build image

---
+++ tigon-build/build_emsdk_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+
+DOCKER_IMAGE_NAME="tigon-build"
+DOCKER_IMAGE_NAMESPACE="ankoh"
+DOCKER_IMAGE_TAG="latest"
+
+set -x
+
+cd ${PROJECT_ROOT} && \
+    tar -cvf - ./tigon-build/docker/Dockerfile.emsdk | docker build \
+        -t ${DOCKER_IMAGE_NAMESPACE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
+        -f ./tigon-build/docker/Dockerfile.emsdk \
+        -
