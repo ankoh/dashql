a20555416e4e5e8c5ca08af99f19b5dadd67e3a3
Andre Kohn <kohna@in.tum.de>
2019-11-08 16:15:31 +0100

    Work on rulers

--- tigon-app/src/view/board.tsx
+++ tigon-app/src/view/board.tsx
@@ -4,6 +4,17 @@ import { connect } from 'react-redux';
 import { AutoSizer } from 'react-virtualized';
 import './board.scss';
 
+const TICK_COLOR = "rgb(200, 200, 200)";
+const TICK_WIDTH = 1;
+const LABEL_COLOR = "rgb(180, 180, 180)";
+const LABEL_FONT = "9px Open Sans";
+
+enum RulerOrientation {
+    Horizontal,
+    Vertical
+}
+
+// A tick of the ruler
 class Tick {
     begin: [number, number];
     end: [number, number];
@@ -17,6 +28,7 @@ class Tick {
     }
 }
 
+// A label of the ruler
 class Label {
     position: [number, number];
     text: string;
@@ -29,22 +41,11 @@ class Label {
     }
 };
 
-enum RulerOrientation {
-    Horizontal,
-    Vertical
-}
-
 interface IRulerProps {
     width: number,
     height: number,
     orientation: RulerOrientation,
     scaleFactor: number,
-
-    strokeStyle?: string;
-    fillStyle?: string;
-    fontSize?: string;
-    fontFamily?: string;
-    lineWidth?: number;
 }
 
 interface Iboardtate {
@@ -55,13 +56,6 @@ interface Iboardtate {
 
 class Ruler extends React.Component<IRulerProps, Iboardtate> {
     canvas: React.RefObject<HTMLCanvasElement>;
-
-    get strokeStyle(): string { return this.props.strokeStyle || "rgb(200, 200, 200)"; }
-    get fillStyle(): string { return this.props.strokeStyle || "rgb(180, 180, 180)"; }
-    get fontSize(): string { return this.props.fontSize || "9px"; }
-    get fontFamily(): string { return this.props.fontFamily || "Open Sans"; }
-    get font(): string { return this.fontSize + ' ' + this.fontFamily; }
-    get lineWidth(): number { return this.props.lineWidth || 1; }
 
     constructor(props: IRulerProps) {
         super(props);
@@ -74,73 +68,85 @@ class Ruler extends React.Component<IRulerProps, Iboardtate> {
         }
     }
 
+    // Layout the ruler
     layout(): [Array<Tick>, Array<Label>] {
         if (this.props.orientation == RulerOrientation.Horizontal) {
             return this.layoutImpl(this.props.width, this.props.height, this.props.scaleFactor);
         } else {
-            return this.layoutImpl(this.props.height, this.props.width, this.props.scaleFactor);
+            let [ticks, labels] = this.layoutImpl(this.props.height, this.props.width, this.props.scaleFactor);
+            ticks.forEach(t => t.transpose());
+            labels.forEach(l => l.transpose());
+            return [ticks, labels];
         }
     }
 
+    // Layout implementation for both, the horizontal and the vertical ruler
     layoutImpl(length: number, thickness: number, scaleFactor: number): [Array<Tick>, Array<Label>] {
         let ticks = new Array<Tick>();
         let labels = new Array<Label>();
         let stepLength = 5;
-        let labelOffset = stepLength;
         for (let tickID = 0; tickID < length / 5; ++tickID) {
             const x = tickID * stepLength;
             if (tickID % 10 === 0) {
                 ticks.push(new Tick([x, thickness - 0.5], [x, thickness * 1 / 3 - 0.5]));
-                labels.push(new Label([x + labelOffset, thickness / 4 - 0.5], Math.round(x * scaleFactor).toString()))
+                labels.push(new Label([x, thickness / 4 - 0.5], Math.round(x * scaleFactor).toString()))
             } else {
                 ticks.push(new Tick([x, thickness - 0.5], [x, thickness * 2 / 3 + 0.5]));
             }
         }
-
         return [ticks, labels];
     }
 
+    // Draw the ruler
     draw(canvas: HTMLCanvasElement) {
+        const labelAdjustment = 5;
+
+        // Scale the canvas dimensions
         canvas.width = this.props.width * this.state.dpr;
         canvas.height = this.props.height * this.state.dpr;
 
+        // Prepare the 2D context
         let context = canvas.getContext("2d")!;
-        context.strokeStyle = this.strokeStyle;
-        context.fillStyle = this.fillStyle;
-        context.font = this.font;
-        context.lineWidth = this.lineWidth;
+        context.strokeStyle = TICK_COLOR;
+        context.fillStyle = LABEL_COLOR;
+        context.font = LABEL_FONT;
+        context.lineWidth = TICK_WIDTH;
         context.scale(this.state.dpr, this.state.dpr);
-
         context.textAlign = "left";
         context.textBaseline = "top";
 
-        if (this.props.orientation == RulerOrientation.Vertical) {
-            context.translate(canvas.width / 2, canvas.height / 2);
-            context.rotate(Math.PI / 2);
-            context.translate(-canvas.width / 2, -canvas.height / 2);
-        }
-
+        // Draw the ticks
         context.beginPath();
+        this.state.ticks.forEach(t => {
+            context.moveTo(t.begin[0], t.begin[1]);
+            context.lineTo(t.end[0], t.end[1]);
+        });
 
-        for (let tick of this.state.ticks) {
-            context.moveTo(tick.begin[0], tick.begin[1]);
-            context.lineTo(tick.end[0], tick.end[1]);
+        // Draw the labels
+        if (this.props.orientation == RulerOrientation.Horizontal) {
+            this.state.labels.forEach(l => {
+                context.fillText(l.text, l.position[0] + labelAdjustment, l.position[1]);
+            });
+        } else {
+            this.state.labels.forEach(l => {
+                context.translate(l.position[0], l.position[1]);
+                context.rotate(-0.5 * Math.PI);
+                context.fillText(l.text, 0 + labelAdjustment, 0);
+                context.rotate(0.5 * Math.PI);
+                context.translate(-l.position[0], -l.position[1]);
+            });
         }
-
-        for (let label of this.state.labels) {
-            context.fillText(label.text, label.position[0], label.position[1]);
-        }
-
         context.stroke();
-
     }
 
+    // Draw the ruler after mount
     componentDidMount() {
         if (this.canvas.current) {
             this.draw(this.canvas.current);
         }
     }
 
+    // Draw the ruler after update
     componentDidUpdate(prevProps: IRulerProps) {
         if (this.props != prevProps) {
             let [ticks, labels] = this.layout();
