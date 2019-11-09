c8e4d7b55e291056accc3e377334c13b4e7a6fb7
Andre Kohn <kohna@in.tum.de>
2019-11-09 19:18:20 +0100

    Add method to iterate over statements

--- tigon-app/src/ctrl/tql_interpreter.ts
+++ tigon-app/src/ctrl/tql_interpreter.ts
@@ -3,19 +3,13 @@ import { CoreBuffer } from '../model';
 import { LogController } from './log_ctrl';
 import { TaskID, Task, TaskQueue } from './task_queue';
 
-class LoadFile extends Task {
-};
-
-class LoadHTTP extends Task {
-};
-
-class ExtractCSV extends Task {
-};
-
-class ExtractJSON extends Task {
-};
-
-class RunQuery extends Task {
+function getStatementType<T extends flatbuffers.Table>(s: T): proto.tql.TQLStatement {
+    return s instanceof proto.tql.TQLQueryStatement ? proto.tql.TQLStatement.TQLQueryStatement :
+        s instanceof proto.tql.TQLDisplayStatement ? proto.tql.TQLStatement.TQLDisplayStatement :
+        s instanceof proto.tql.TQLExtractStatement ? proto.tql.TQLStatement.TQLExtractStatement :
+        s instanceof proto.tql.TQLLoadStatement ? proto.tql.TQLStatement.TQLLoadStatement :
+        s instanceof proto.tql.TQLParameterDeclaration ? proto.tql.TQLStatement.TQLParameterDeclaration
+        : proto.tql.TQLStatement.NONE;
 }
 
 
@@ -44,6 +38,18 @@ export class TQLInterpreter {
     public async eval(program: CoreBuffer<proto.tql.TQLProgram>) {
     }
 
+    // Iterate over statements of a certain type
+    public static forEachStatement<T extends flatbuffers.Table>(program: CoreBuffer<proto.tql.TQLProgram>, obj: T, fn: (o: T) => void) {
+        let reader = program.getReader();
+        let filteredType = getStatementType(obj);
+        for (let i = 0; i < reader.statementsLength(); ++i) {
+            if (reader.statementsType(i)! != filteredType) {
+                continue;
+            }
+            reader.statements(i, obj);
+            fn(obj);
+        }
+    }
 }
 
 export default TQLInterpreter;
