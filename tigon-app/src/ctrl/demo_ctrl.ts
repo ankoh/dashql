2029f7c99d03c39e933f4aade05c2b62ef73c001
Andre Kohn <kohna@in.tum.de>
2019-11-17 16:32:46 +0100

    Work on demo data

--- tigon-app/src/ctrl/demo_ctrl.ts
+++ tigon-app/src/ctrl/demo_ctrl.ts
@@ -44,8 +44,52 @@ export class DemoController {
         
     }
 
+    async writeQueryResultColumn(builder: flatbuffers.Builder, typeID: proto.duckdb.RawTypeID, rows: [number]) {
+        proto.duckdb.QueryResultColumn.startQueryResultColumn(builder);
+        proto.duckdb.QueryResultColumn.addTypeId(builder, typeID);
+        let nullMask = new Array<boolean>();
+        nullMask.length = rows.length;
+        nullMask.fill(true);
+        proto.duckdb.QueryResultColumn.createNullMaskVector(builder, nullMask);
+        switch (typeID) {
+            case proto.duckdb.RawTypeID.BOOLEAN:
+                proto.duckdb.QueryResultColumn.createRowsI16Vector(builder, rows);
+                break;
+            case proto.duckdb.RawTypeID.TINYINT:
+                proto.duckdb.QueryResultColumn.createRowsI16Vector(builder, rows);
+                break;
+            case proto.duckdb.RawTypeID.SMALLINT:
+                proto.duckdb.QueryResultColumn.createRowsI32Vector(builder, rows);
+                break;
+            case proto.duckdb.RawTypeID.INTEGER: {
+                let buffer = rows.map(row => new flatbuffers.Long(row, 0));
+                proto.duckdb.QueryResultColumn.createRowsI64Vector(builder, buffer);
+                break;
+            }
+            case proto.duckdb.RawTypeID.BIGINT: {
+                let buffer = rows.map(row => new flatbuffers.Long(row, 0));
+                proto.duckdb.QueryResultColumn.createRowsI64Vector(builder, buffer);
+                break;
+            }
+            case proto.duckdb.RawTypeID.HASH: {
+                let buffer = rows.map(row => new flatbuffers.Long(row, 0));
+                proto.duckdb.QueryResultColumn.createRowsU64Vector(builder, buffer);
+                break;
+            }
+            case proto.duckdb.RawTypeID.POINTER: {
+                let buffer = rows.map(row => new flatbuffers.Long(row, 0));
+                proto.duckdb.QueryResultColumn.createRowsU64Vector(builder, buffer);
+                break;
+            }
+            case proto.duckdb.RawTypeID.FLOAT:
+                proto.duckdb.QueryResultColumn.createRowsF32Vector(builder, rows);
+                break;
+            case proto.duckdb.RawTypeID.DOUBLE:
+                proto.duckdb.QueryResultColumn.createRowsF64Vector(builder, rows);
+                break;
+        }
+    }
+
     async loadTestQueryResults() {
-        let builder = new flatbuffers.Builder();
-        let queryResult = proto.duckdb.QueryResult.startQueryResult(builder);
     }
 };
