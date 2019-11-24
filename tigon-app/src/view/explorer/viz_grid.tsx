37d6fa8dc1dbcb9074292b6499ec89e6fc5f9030
Andre Kohn <kohna@in.tum.de>
2019-11-24 15:01:48 +0100

    Work on layout computation

--- tigon-app/src/view/explorer/viz_grid.tsx
+++ tigon-app/src/view/explorer/viz_grid.tsx
@@ -17,6 +17,7 @@ import {
 
 const ACTION_ICON_WIDTH="16px";
 const ACTION_ICON_HEIGHT="16px";
+const DEFAULT_ROW_HEIGHT = 200;
 
 /// A grid element
 export class GridElement {
@@ -31,12 +32,11 @@ export class GridElement {
         this.rows = rows;
     }
 
-    get columnProp() : string {
-        return this.columns[0].toString() + " / " + this.columns[1].toString();
-    }
-
-    get rowProp() : string {
-        return this.rows[0].toString() + " / " + this.rows[1].toString();
+    get cssArea() : string {
+        return this.columns[0].toString()
+            + " / " + this.rows[0].toString()
+            + " / " + this.columns[1].toString()
+            + " / " + this.rows[1].toString();
     }
 };
 
@@ -116,8 +116,7 @@ function VizCard(props: {
     return (
         <div className={s.viz}
             style={{
-                gridColumn: props.pos.columnProp,
-                gridRow: props.pos.rowProp,
+                gridArea: props.pos.cssArea
             }}
         >
             <div className={s.viz_id}>
@@ -155,8 +154,17 @@ interface IVizGridProps {
     height: number;
 }
 
+/// The size class
+enum SizeClass {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    XLARGE
+}
+
 /// A viz grid state
 interface IVizGridState {
+    sizeClass: SizeClass,
     gridLayout: GridLayout;
     vizStmts: Array<proto.tql.VizStatement>;
     vizPositions: Array<GridElement>;
@@ -171,6 +179,8 @@ export class VizGrid extends React.Component<IVizGridProps, IVizGridState> {
     }
 
     protected static computeLayout(props: IVizGridProps): IVizGridState {
+        let sizeClass = SizeClass.LARGE;
+
         // Get the viz statements
         let vizStmts = mapStatements(
             props.statements,
@@ -179,14 +189,104 @@ export class VizGrid extends React.Component<IVizGridProps, IVizGridState> {
         );
         let vizData = vizStmts.map((v) => props.queryResults.get(v.getQueryId()) || null);
 
-        // Compute the layout
+        // Pick a length value
+        let pickLengthValue = (v: proto.tql.VizLength | undefined, sc: SizeClass) => {
+            if (!v) {
+                return null;
+            } 
+            let lv: proto.tql.VizLengthValue | undefined;
+            switch (sc) {
+                case SizeClass.SMALL:
+                    lv = v.getSmall();
+                    break;
+                case SizeClass.MEDIUM:
+                    lv = v.getMedium();
+                    break;
+                case SizeClass.LARGE:
+                    lv = v.getLarge();
+                    break;
+                case SizeClass.XLARGE:
+                    lv = v.getXlarge();
+                    break;
+            }
+            if (!lv) {
+                lv = v.getWildcard();
+            }
+            return lv || null;
+        }
+
+        // Get the maximum of two row height values
+        let maxRowHeight = (a: proto.tql.VizLengthValue | null, b: proto.tql.VizLengthValue | null) => {
+            if (!a) { return b; }
+            if (!b) { return a; }
+
+            // Same length unit?
+            if (a.getUnit() === b.getUnit()) {
+                return b.getValue() > a.getValue() ? b : a;
+            }
+            // Explicit pixels always win
+            if (a.getUnit() === proto.tql.VizLengthUnit.PIXEL) { return a; }
+            if (b.getUnit() === proto.tql.VizLengthUnit.PIXEL) { return b; }
+            // Percent and spans are not supported for row heights
+            let d = new proto.tql.VizLengthValue();
+            d.setValue(100);
+            d.setUnit(proto.tql.VizLengthUnit.PIXEL);
+            return d;
+        }
+
         let gridLayout = new GridLayout();
-        let vizPositions = new Array<GridElement>();
-        vizPositions.push(new GridElement([1, 6], [1, 1]));
-        vizPositions.push(new GridElement([6, 12], [1, 1]));
+
+        // The allocation state
+        type AllocStateType = { row: number, col: number, height: proto.tql.VizLengthValue | null };
+        let allocState: AllocStateType = {
+            row: 0, col: 0, height: null
+        };
+
+        // Allocate a row
+        let allocRow = () => {
+            let h = allocState.height;
+            if (!h) {
+                gridLayout.rows.push(new GridLength(DEFAULT_ROW_HEIGHT, GridLengthUnit.PIXEL));
+                return;
+            }
+            gridLayout.rows.push(new GridLength(h.getValue(), GridLengthUnit.PIXEL));
+            ++allocState.row;
+            allocState.col = 0;
+            allocState.height = null;
+        };
+
+        // Allocate a span
+        let allocSpan = (span: number, height: proto.tql.VizLengthValue | null = null) => {
+            if (allocState.col + span > gridLayout.columns) {
+                allocRow();
+            }
+            allocState.height = maxRowHeight(allocState.height, height);
+            return new GridElement([allocState.col, allocState.col + span], [allocState.row, allocState.col + 1]);
+        };
+
+        // Compute the viz positions
+        let vizPositions = vizStmts.map((v) => {
+            let layout = v.getLayout();
+            if (!layout) {
+                return allocSpan(1);
+            }
+            let lengthValue = pickLengthValue(layout.getHeight(), sizeClass);
+            let width = layout.getWidth();
+            if (!width) {
+                return allocSpan(1, pickLengthValue(layout.getHeight(), sizeClass));
+            } else {
+                let widthValue = pickLengthValue(width, sizeClass);
+                if (!widthValue || widthValue.getUnit() !== proto.tql.VizLengthUnit.SPAN) {
+                    return allocSpan(1, lengthValue);
+                } else {
+                    return allocSpan(widthValue.getValue(), lengthValue);
+                }
+            }
+        });
 
         // Return state
         return {
+            sizeClass: sizeClass,
             gridLayout: gridLayout,
             vizStmts: vizStmts,
             vizPositions: vizPositions,
