69baf5269c10e85591861962c5f661dc862c1932
Andre Kohn <kohna@in.tum.de>
2020-01-26 15:04:12 +0100

    Versioned dev image using EMSDK commit

--- tigon-dev/build_dev_image.sh
+++ tigon-dev/build_dev_image.sh
@@ -2,14 +2,16 @@
 
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
+EMSDK_COMMIT="a5082b2"
 DOCKER_IMAGE_NAME="tigon-dev"
 DOCKER_IMAGE_NAMESPACE="ankoh"
-DOCKER_IMAGE_TAG="latest"
+DOCKER_IMAGE_TAG="${EMSDK_COMMIT}"
 
 set -x
 
 cd ${PROJECT_ROOT} && \
-    tar -cvf - ./tigon-dev/docker/Dockerfile.dev | docker build \
+    tar -cvf - ./tigon-dev/docker/dev/Dockerfile | docker build \
         -t ${DOCKER_IMAGE_NAMESPACE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
-        -f ./tigon-dev/docker/Dockerfile.dev \
+        -f ./tigon-dev/docker/dev/Dockerfile \
+        --build-arg EMSDK_COMMIT="${EMSDK_COMMIT}" \
         -
