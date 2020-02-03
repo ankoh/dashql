2ac739972e70236f18f535ae0ba4bcfbdef20023
Andre Kohn <kohn.a@outlook.com>
2020-02-03 23:06:38 +0100

    Use versioned image in compile_wasm

--- tigon-dev/compile_wasm.sh
+++ tigon-dev/compile_wasm.sh
@@ -2,11 +2,12 @@
 
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
+IMAGE_TAG="a5082b2"
 CORE_BUILD_DIR="${PROJECT_ROOT}/tigon-core/build/emscripten"
 CORE_SOURCE_DIR="${PROJECT_ROOT}/tigon-core"
 APP_LIB_DIR="${PROJECT_ROOT}/tigon-app/public/lib"
 
-CMD_PREFIX="docker run -it --rm -v${PROJECT_ROOT}:/wd/ -v${PROJECT_ROOT}/.emscripten_cache/:/root/.emscripten_cache/ ankoh/tigon-dev:latest "
+CMD_PREFIX="docker run -it --rm -v${PROJECT_ROOT}:/wd/ -v${PROJECT_ROOT}/.emscripten_cache/:/root/.emscripten_cache/ ankoh/tigon-dev:${IMAGE_TAG} "
 EMCONFIGURE="${CMD_PREFIX} emconfigure"
 EMMAKE="${CMD_PREFIX} emmake"
 
