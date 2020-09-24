3ff094fda135165e742821857ec5113228448371
Andre Kohn <kohn.a@outlook.com>
2020-09-24 08:27:25 +0200

    Improve caching for emsdk install

--- dev/build_dev_image.sh
+++ dev/build_dev_image.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 
-EMSDK_COMMIT="1.40.1"
+EMSDK_VERSION="2.0.4"
 DOCKER_IMAGE_NAME="dashql-dev"
 DOCKER_IMAGE_NAMESPACE="dashql"
-DOCKER_IMAGE_TAG="${EMSDK_COMMIT}"
+DOCKER_IMAGE_TAG="${EMSDK_VERSION}"
 
 set -x
 
@@ -14,5 +14,5 @@ cd ${PROJECT_ROOT} && \
     tar -cvf - ./dev/docker/dev/Dockerfile | docker build \
         -t ${DOCKER_IMAGE_NAMESPACE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
         -f ./dev/docker/dev/Dockerfile \
-        --build-arg EMSDK_COMMIT="${EMSDK_COMMIT}" \
+        --build-arg EMSDK_VERSION="${EMSDK_VERSION}" \
         -
