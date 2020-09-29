aa6ddaeb413ec0c6968bba0a416ae313b6cdc9ae
Andre Kohn <kohn.a@outlook.com>
2020-09-29 18:56:06 +0200

    Fix copyright header

--- webapi/test/duckdb_test.cc
+++ webapi/test/duckdb_test.cc
@@ -1,7 +1,4 @@
-//---------------------------------------------------------------------------
-// Tigon
-// (c) 2019 Andre Kohn
-//---------------------------------------------------------------------------
+// Copyright (c) 2020 The DashQL Authors
 
 #include "duckdb_webapi/api.h"
 #include "duckdb_webapi/proto/query_plan_generated.h"
