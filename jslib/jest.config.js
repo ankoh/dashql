c17de7f0a1ed312187ffe6b03e8d6fe8477464f5
Andre Kohn <kohn.a@outlook.com>
2020-09-30 11:08:20 +0200

    WIP jest

--- jslib/jest.config.js
+++ jslib/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
     roots: ['<rootDir>'],
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.ts$': 'ts-jest',
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-//    setupFiles: ['<rootDir>/test/setup.ts'],
 };
 
