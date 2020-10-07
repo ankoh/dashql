31ce13717b6d64a7e7dba8c13e74cee76c63c4e9
Andre Kohn <kohn.a@outlook.com>
2020-10-07 18:10:14 +0200

    Drop replicated logical type

--- webapi/include/duckdb_webapi/codec.h
+++ webapi/include/duckdb_webapi/codec.h
@@ -25,30 +25,6 @@ flatbuffers::Offset<proto::QueryResultChunk> WriteQueryResultChunk(flatbuffers::
 flatbuffers::Offset<proto::QueryPlan> WriteQueryPlan(flatbuffers::FlatBufferBuilder& builder,
                                                      duckdb::LogicalOperator& plan);
 
-/// A logical type
-struct LogicalType {
-    /// Create type
-    static proto::LogicalType Get();
-    /// Create type
-    static proto::LogicalType Get(proto::LogicalTypeID id);
-    /// Create type
-    static proto::LogicalType Get(proto::LogicalTypeID id, uint8_t width, uint8_t scale);
-    /// Get the physical type
-    static proto::PhysicalTypeID GetPhysicalType(proto::LogicalType& type);
-    /// Logical type to string
-    static const char* ToString(proto::LogicalTypeID typeID);
-    /// Get the maximum logical type
-    static proto::LogicalType MaxType(proto::LogicalType left, proto::LogicalType right);
-    /// Is constant size type?
-    static bool IsConstantSize(proto::PhysicalTypeID type);
-    /// Is integral type?
-    static bool IsIntegral(proto::PhysicalTypeID type);
-    /// Is numeric type?
-    static bool IsNumeric(proto::PhysicalTypeID type);
-    /// Is integer type?
-    static bool IsInteger(proto::PhysicalTypeID type);
-};
-
 }  // namespace duckdb_webapi
 
 #endif  // INCLUDE_DUCKDB_WEBAPI_PROTO_CODEC_H_
