214d8cdd971d14e86e4360d39065b1dae0233b93
Andre Kohn <kohn.a@outlook.com>
2020-10-09 09:49:33 +0200

    Iterator tests

--- webapi/test/iterator_test.cc
+++ webapi/test/iterator_test.cc
@@ -9,6 +9,7 @@
 #include "gtest/gtest.h"
 
 #include "duckdb/common/types/date.hpp"
+#include "duckdb/common/types/timestamp.hpp"
 #include "duckdb/common/operator/numeric_binary_operators.hpp"
 
 using namespace duckdb_webapi;
@@ -154,6 +155,27 @@ TEST(QueryResultIterator, DateColumn) {
         auto txt = std::to_string(i);
         auto v = duckdb::AddOperator::Operation<date_t, interval_t, date_t>(duckdb::Date::FromDate(2020, 10, 9), {0, i, 0});
         ASSERT_EQ(iter.GetValue(0).GetValue<date_t>(), v);
+        iter.Next();
+    }
+    ASSERT_TRUE(iter.IsEnd());
+}
+
+TEST(QueryResultIterator, TimeColumn) {
+    auto db = make_shared<duckdb::DuckDB>();
+    WebAPI::Connection conn{db};
+    auto expected = conn.SendQuery(R"RAW(
+        SELECT TIME '01:00:00' + CAST(CONCAT(MOD(v, 1000)::VARCHAR, ' millisecond') AS INTERVAL) FROM generate_series(0, 10000) AS t(v);
+    )RAW");
+    ASSERT_TRUE(expected.IsOk());
+    auto& result = expected.value();
+    ASSERT_NE(result.column_types(), nullptr);
+    ASSERT_EQ(result.column_types()->size(), 1);
+    ASSERT_EQ(result.column_types()->Get(0)->type_id(), proto::SQLTypeID::TIME);
+    QueryResultIterator iter{conn, result};
+    for (int32_t i = 0; i <= 10000; ++i) {
+        ASSERT_FALSE(iter.IsEnd()) << "i=" << i;
+        auto txt = std::to_string(i);
+        ASSERT_EQ(iter.GetValue(0).GetValue<dtime_t>(), duckdb::Time::FromTime(1, 0, 0, i % 1000)) << "i=" << i;
         iter.Next();
     }
     ASSERT_TRUE(iter.IsEnd());
