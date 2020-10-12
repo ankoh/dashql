3fc222d50fb7fd8a89ca9a9700a705616c63335d
Andre Kohn <kohn.a@outlook.com>
2020-10-12 22:59:38 +0200

    Use dedicated jest typescript config

--- libs/js/jest.config.js
+++ libs/js/jest.config.js
@@ -4,6 +4,11 @@ module.exports = {
         '^.+\\.ts$': 'ts-jest',
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.test.json'
+        }
+    }
 };
 
