3ea54784309945e887cb6e30d1492ddf8e6f33be
Andre Kohn <kohn.a@outlook.com>
2020-10-16 12:19:29 +0200

    Work on rust iterator

--- libs/rs/src/iterator.rs
+++ libs/rs/src/iterator.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 The DashQL Authors
 
 use crate::error::Error;
-use crate::proto::{QueryResult, QueryResultChunk, SQLType, VectorVariant};
+use crate::proto::{Interval, QueryResult, QueryResultChunk, SQLType, VectorVariant};
 use crate::webapi::{Buffer, Connection};
 
 pub struct QueryResultChunkStream<'chunks, 'result: 'chunks, 'conn: 'result> {
@@ -57,26 +57,112 @@ impl<'chunks, 'result: 'chunks, 'conn: 'result> QueryResultChunkStream<'chunks, 
         Ok(self.current_chunk.unwrap().row_count() > 0)
     }
 
-    /// Iterate over a vector
-    fn iterate_raw_vector<V, F: Fn(usize, Option<V>) -> (), X>(
+    fn iterate_raw_vector<A: Copy, B: From<A>, F: Fn(usize, Option<&B>) -> ()>(
         &self,
-        _values: Option<&[X]>,
-        _nulls: Option<&[bool]>,
-        _f: &F,
+        values: Option<&[A]>,
+        nulls: Option<&[bool]>,
+        f: &F,
     ) {
+        match (values, nulls) {
+            (None, _) => return,
+            (Some(vs), None) => {
+                for i in 0..vs.len() {
+                    let v: B = vs[i].into();
+                    f(i, Some(&v));
+                }
+            }
+            (Some(vs), Some(ns)) => {
+                for i in 0..vs.len() {
+                    if ns[i] {
+                        f(i, None);
+                    } else {
+                        let v: B = vs[i].into();
+                        f(i, Some(&v));
+                    };
+                }
+            }
+        }
     }
 
-    /// Iterate over a vector
-    fn iterate_number_vector<V, F: Fn(usize, Option<V>) -> (), X>(
-        &self,
-        _values: Option<flatbuffers::Vector<X>>,
-        _nulls: Option<&[bool]>,
-        _f: &F,
+    fn iterate_number_vector<
+        'vector,
+        A: flatbuffers::Follow<'vector>,
+        B: From<A::Inner>,
+        F: Fn(usize, Option<&B>) -> (),
+    >(
+        &'vector self,
+        values: Option<flatbuffers::Vector<'vector, A>>,
+        nulls: Option<&[bool]>,
+        f: &F,
     ) {
+        match (values, nulls) {
+            (None, _) => return,
+            (Some(vs), None) => {
+                for i in 0..vs.len() {
+                    let v: B = vs.get(i).into();
+                    f(i, Some(&v));
+                }
+            }
+            (Some(vs), Some(ns)) => {
+                for i in 0..vs.len() {
+                    if ns[i] {
+                        f(i, None);
+                    } else {
+                        let v: B = vs.get(i).into();
+                        f(i, Some(&v));
+                    };
+                }
+            }
+        }
     }
 
-    /// Iterate over column
-    pub fn iterate_column<V, F: Fn(usize, Option<V>) -> ()>(&self, cid: usize, f: &F) -> Result<(), Error> {
+    fn iterate_string_vector<'a, V: Default, F: Fn(usize, Option<&V>) -> ()>(
+        &self,
+        values: Option<flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<&'a str>>>,
+        nulls: Option<&[bool]>,
+        f: &F,
+    ) where
+        str: AsRef<V>,
+    {
+        match (values, nulls) {
+            (None, _) => return,
+            (Some(vs), None) => {
+                for i in 0..vs.len() {
+                    f(i, Some(vs.get(i).as_ref()));
+                }
+            }
+            (Some(vs), Some(ns)) => {
+                for i in 0..vs.len() {
+                    f(i, if ns[i] { None } else { Some(vs.get(i).as_ref()) });
+                }
+            }
+        }
+    }
+
+    pub fn iterate_column<
+        V: Default
+            + Clone
+            + From<i8>
+            + From<u8>
+            + From<i16>
+            + From<u16>
+            + From<i32>
+            + From<u32>
+            + From<i64>
+            + From<u64>
+            + From<i128>
+            + From<f32>
+            + From<f64>
+            + From<Interval>,
+        F: Fn(usize, Option<&V>) -> (),
+    >(
+        &self,
+        cid: usize,
+        f: &F,
+    ) -> Result<(), Error>
+    where
+        str: AsRef<V>,
+    {
         let chunk = match self.current_chunk {
             Some(ref c) => c,
             None => return Ok(()),
@@ -97,42 +183,45 @@ impl<'chunks, 'result: 'chunks, 'conn: 'result> QueryResultChunkStream<'chunks, 
             VectorVariant::VectorU8 => {
                 let v = column.variant_as_vector_u8().unwrap();
                 self.iterate_raw_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorI16 => {
                 let v = column.variant_as_vector_i16().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorU16 => {
                 let v = column.variant_as_vector_u16().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorI32 => {
                 let v = column.variant_as_vector_i32().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorU32 => {
                 let v = column.variant_as_vector_u32().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorI64 => {
                 let v = column.variant_as_vector_i64().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorU64 => {
                 let v = column.variant_as_vector_u64().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorF32 => {
                 let v = column.variant_as_vector_f32().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorF64 => {
                 let v = column.variant_as_vector_f64().unwrap();
                 self.iterate_number_vector(v.values(), v.null_mask(), f);
-            },
+            }
             VectorVariant::VectorI128 => (),
             VectorVariant::VectorInterval => (),
-            VectorVariant::VectorString => (),
+            VectorVariant::VectorString => {
+                let v = column.variant_as_vector_string().unwrap();
+                self.iterate_string_vector(v.values(), v.null_mask(), f);
+            }
             VectorVariant::NONE => (),
         };
         Ok(())
