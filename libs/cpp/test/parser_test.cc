ac9ed413dc516ec32084735105ea84cab8fc8824
Andre Kohn <kohn.a@outlook.com>
2020-10-17 20:25:18 +0200

    Add dummy test

---
+++ libs/cpp/test/parser_test.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 The DashQL Authors
+
+#include "dashql/parser/parser.h"
+#include "gtest/gtest.h"
+
+using namespace dashql::parser;
+using namespace std;
+
+namespace {
+
+TEST(ParserTests, Dummy) {
+    ASSERT_EQ(1024, 1024);
+}
+
+}  // namespace
