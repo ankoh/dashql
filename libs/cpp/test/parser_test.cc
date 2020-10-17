026977e7dddf98ef88b15de922a70b75c9c460e7
Andre Kohn <kohn.a@outlook.com>
2020-10-17 20:26:33 +0200

    Add test queries

--- libs/cpp/test/parser_test.cc
+++ libs/cpp/test/parser_test.cc
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 The DashQL Authors
 
-#include "dashql/parser/parser.h"
+#include "dashql/parser/parse_context.h"
 #include "gtest/gtest.h"
 
 using namespace dashql::parser;
@@ -8,8 +8,73 @@ using namespace std;
 
 namespace {
 
-TEST(ParserTests, Dummy) {
-    ASSERT_EQ(1024, 1024);
+TEST(TQLTest, ParameterDeclaration) {
+    auto in = R"RAW(
+    declare parameter days type integer;
+)RAW";
+    ParseContext ctx;
+    auto module = ctx.Parse(in);
+    ASSERT_EQ(module.statements.size(), 1);
+    ASSERT_EQ(module.errors.size(), 0);
+}
+
+TEST(TQLTest, LoadHTTP) {
+    auto in = R"RAW(
+    load raw_data from http (
+        url = 'http://www.google.com',
+        method = get
+    );
+)RAW";
+    ParseContext ctx;
+    auto module = ctx.Parse(in);
+    ASSERT_EQ(module.statements.size(), 1);
+    ASSERT_EQ(module.errors.size(), 0);
+}
+
+TEST(TQLTest, ExtractJsonPath) {
+    auto in = R"RAW(
+    extract weather_data from raw_data using json ();
+)RAW";
+    ParseContext ctx;
+    auto module = ctx.Parse(in);
+    ASSERT_EQ(module.statements.size(), 1);
+    ASSERT_EQ(module.errors.size(), 0);
+}
+
+TEST(TQLTest, Query1) {
+    auto in = R"RAW(
+    select 1;
+)RAW";
+    ParseContext ctx;
+    auto module = ctx.Parse(in);
+    ASSERT_EQ(module.statements.size(), 1);
+    ASSERT_EQ(module.errors.size(), 0);
+}
+
+TEST(TQLTest, Query2) {
+    auto in = R"RAW(
+    query "foo" as select 1;
+)RAW";
+    ParseContext ctx;
+    auto module = ctx.Parse(in);
+    ASSERT_EQ(module.statements.size(), 1);
+    ASSERT_EQ(module.errors.size(), 0);
+}
+
+TEST(TQLTest, SyntaxError) {
+    auto in = "?";
+    ParseContext ctx;
+    auto module = ctx.Parse(in);
+    ASSERT_EQ(module.statements.size(), 0);
+    ASSERT_EQ(module.errors.size(), 1);
+}
+
+TEST(TQLTest, SyntaxErrorRecovery) {
+    auto in = "?select * from foo;";
+    ParseContext ctx;
+    auto module = ctx.Parse(in);
+    ASSERT_EQ(module.statements.size(), 1);
+    ASSERT_EQ(module.errors.size(), 1);
 }
 
 }  // namespace
