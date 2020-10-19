1a5fbebd7a1234e3fc5e677a4a85ffea37da76f8
Andre Kohn <kohn.a@outlook.com>
2020-10-19 10:57:05 +0200

    Add module types

---
+++ libs/js/src/parser/dashql_parser_web.d.ts
@@ -0,0 +1,3 @@
+import { DashQLParserModule } from './dashql_parser_module';
+export function DashQLParser(moduleOverrides?: Partial<DashQLParserModule>,): Promise<DashQLParserModule>;
+export default DashQLParser;
