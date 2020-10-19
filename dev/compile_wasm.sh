60809a8f06777cd8630e3ff541eba66df6421b81
Andre Kohn <kohn.a@outlook.com>
2020-10-19 10:51:45 +0200

    Work on wasm compilation

--- dev/compile_wasm.sh
+++ dev/compile_wasm.sh
@@ -8,7 +8,7 @@ PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 IMAGE_TAG="0.1"
 CPP_BUILD_DIR="${PROJECT_ROOT}/libs/cpp/build/emscripten"
 CPP_SOURCE_DIR="${PROJECT_ROOT}/libs/cpp"
-JS_LIB_DIR="${PROJECT_ROOT}/libs/js/src/duckdb"
+JS_LIB_DIR="${PROJECT_ROOT}/libs/js/src/parser"
 
 CMD_PREFIX="docker run -it --rm -v${PROJECT_ROOT}:/wd/ -v${PROJECT_ROOT}/.emscripten_cache/:/mnt/emscripten_cache/ dashql/dashql-parser-dev:${IMAGE_TAG} "
 EMCONFIGURE="${CMD_PREFIX} emcmake"
@@ -21,16 +21,16 @@ mkdir -p ${CPP_BUILD_DIR}
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 
 ${EMCONFIGURE} cmake \
-    -S/wd/webapi/ \
-    -B/wd/webapi/build/emscripten \
+    -S/wd/libs/cpp/ \
+    -B/wd/libs/cpp/build/emscripten \
     -DCMAKE_BUILD_TYPE=Release
 
 ${EMMAKE} make \
-    -C/wd/webapi/build/emscripten \
+    -C/wd/libs/cpp/build/emscripten \
     -j${CORES} \
-    duckdb_webapi duckdb_nodeapi
+    dashql_parser_web dashql_parser_node
 
 mkdir -p "${JS_LIB_DIR}"
 
-cp ${CPP_SOURCE_DIR}/build/emscripten/dashql_parser_*.{wasm,js,worker.js} "${JS_LIB_DIR}"
+cp ${CPP_SOURCE_DIR}/build/emscripten/dashql_parser_*.{wasm,js} "${JS_LIB_DIR}"
 
