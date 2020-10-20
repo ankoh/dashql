67463c9623c7b5b220dd502fb56f20d23451d498
Andre Kohn <kohn.a@outlook.com>
2020-10-20 18:14:55 +0200

    Add failing tests

--- libs/js/test/parser.test.ts
+++ libs/js/test/parser.test.ts
@@ -8,12 +8,70 @@ beforeAll(async () => {
 });
 
 describe('Parser', () => {
+    describe('errors', () => {
+        test('synatx error', async () => {
+            let result = await parser.parse("?");
+            expect(result.root.errorsLength()).toBe(1);
+        });
 
-    test('simple integer parameter', async () => {
-        let result = await parser.parse(`
-            declare parameter days type integer;
-        `)
-        expect(result.root.errorsLength()).toBe(0);
+        test('error recovery', async () => {
+            let result = await parser.parse(`
+                ?select * from foo;
+            `);
+            let program = result.root;
+            expect(program.statementsLength()).toBe(1);
+            expect(program.errorsLength()).toBe(1);
+        });
+    });
+
+    describe('single statements', () => {
+        test('integer parameter', async () => {
+            let result = await parser.parse(`
+                declare parameter days type integer;
+            `);
+            let program = result.root;
+            expect(program.errorsLength()).toBe(0);
+            expect(program.statementsLength()).toBe(1);
+        });
+
+        test('http load', async () => {
+            let result = await parser.parse(`
+                load raw_data from http (
+                    url = 'http://www.google.com',
+                    method = get
+                );
+            `);
+            let program = result.root;
+            expect(program.errorsLength()).toBe(0);
+            expect(program.statementsLength()).toBe(1);
+        });
+
+        test('extract json', async () => {
+            let result = await parser.parse(`
+                extract weather_data from raw_data using json ();
+            `);
+            let program = result.root;
+            expect(program.errorsLength()).toBe(0);
+            expect(program.statementsLength()).toBe(1);
+        });
+
+        test('select 1', async () => {
+            let result = await parser.parse(`
+                select 1;
+            `);
+            let program = result.root;
+            expect(program.errorsLength()).toBe(0);
+            expect(program.statementsLength()).toBe(1);
+        });
+
+        test('query as select 1', async () => {
+            let result = await parser.parse(`
+                query "foo" as select 1;
+            `);
+            let program = result.root;
+            expect(program.errorsLength()).toBe(0);
+            expect(program.statementsLength()).toBe(1);
+        });
     });
 });
 
