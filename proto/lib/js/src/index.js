b6db2bb0a57fac4a566e28f849eab993644e155f
Andre Kohn <kohn.a@outlook.com>
2020-10-21 11:39:32 +0200

    Remove things

--- proto/lib/js/src/index.js
+++
@@ -1,6 +0,0 @@
-import * as engine from './proto/engine_pb';
-import * as rpath from './proto/rpath_pb';
-import * as tql from './proto/tql_pb';
-import * as web_api from './proto/web_api_pb';
-
-export { engine, rpath, tql, web_api };
