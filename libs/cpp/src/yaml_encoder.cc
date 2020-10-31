286884df2c3939c01a76e95436e64384c62a5021
Andre Kohn <kohn.a@outlook.com>
2020-10-31 01:56:01 +0100

    Add yaml encoder

---
+++ libs/cpp/src/yaml_encoder.cc
@@ -0,0 +1,207 @@
+#include "dashql/parser/yaml_encoder.h"
+
+#include "ryml_std.hpp"
+#include "ryml.hpp"
+#include "c4/yml/emit.hpp"
+#include "c4/yml/std/string.hpp"
+#include "c4/yml/yml.hpp"
+
+#include <cstdint>
+#include <stack>
+#include <unordered_set>
+
+namespace dashql {
+namespace parser {
+
+namespace sx = proto::syntax;
+
+namespace {
+
+void encode(ryml::NodeRef n, proto::syntax::Location loc) {
+    n |= ryml::MAP;
+    n["offset"] << loc.offset();
+    n["length"] << loc.length();
+}
+
+void encode(ryml::NodeRef e, const proto::syntax::Error& err) {
+    e |= ryml::MAP;
+    e["message"] = c4::to_csubstr(err.message()->c_str());
+    encode(e["location"], *err.location());
+}
+
+}  // namespace
+
+/// Encode yaml
+std::string encodeYAML(const proto::syntax::Module& module, bool pretty) {
+    ryml::Tree tree;
+    auto root = tree.rootref();
+    root |= ryml::MAP;
+
+    auto* obj_type_tt = proto::syntax::ObjectTypeTypeTable();
+    auto* attr_key_tt = proto::syntax::AttributeKeyTypeTable();
+
+    auto tmp = root["tmp"];
+    tmp |= ryml::SEQ;
+
+    // Translate document
+    {
+        // Unpack document
+        auto* entries = module.statements()->entries();
+        auto* objs = module.statements()->objects();
+        auto* attrs = module.statements()->attributes();
+        auto* arrays = module.statements()->arrays();
+        auto* values_str = module.statements()->values_string();
+        auto* values_i32 = module.statements()->values_i32();
+
+        auto assert_less_than = [](unsigned v, unsigned size) {
+            assert(v < size);
+            return std::min(size, v);
+        };
+        auto assert_within = [](unsigned& begin, unsigned& end, unsigned size) {
+            assert(begin <= end);
+            assert(end <= size);
+            end = std::min(end, size);
+            begin = std::min(begin, end);
+            return;
+        };
+
+        // Translate objects
+        std::vector<ryml::NodeRef> tmp_values;
+        tmp_values.reserve(module.statements()->objects()->size());
+        for (unsigned oid = 0; oid < objs->size(); ++oid) {
+            auto o = objs->Get(oid);
+
+            // Create object
+            tmp_values.push_back(tmp.append_child());
+            auto v = tmp_values.back();
+            v |= ryml::MAP;
+            v["type"] = c4::to_csubstr(obj_type_tt->names[static_cast<size_t>(o->type())]);
+            encode(v["location"], o->location());
+
+            // Translate attributes
+            auto& attr_span = o->attributes();
+            auto attr_end = attr_span.offset() + attr_span.length();
+            for (unsigned i = attr_span.offset(); i < attr_end; ++i) {
+                // Unpack attribute
+                auto* attr = attrs->Get(i);
+                auto& attr_value = attr->value();
+                auto attr_key = c4::to_csubstr(attr_key_tt->names[static_cast<size_t>(attr->key())]);
+
+                // Check attribute type
+                switch (attr_value.type()) {
+                    case sx::ValueType::NONE:
+                        break;
+                    case sx::ValueType::I32: {
+                        auto n = v[attr_key];
+                        n |= ryml::VAL;
+                        n << attr_value.value();
+                        break;
+                    }
+                    case sx::ValueType::STRING: {
+                        auto n = v[attr_key];
+                        encode(n, attr_value.location());
+                        break;
+                    }
+                    case sx::ValueType::OBJECT: {
+                        auto aid = assert_less_than(attr_value.value(), oid);
+                        tree.move(tmp_values[aid].id(), v.id(), tree.last_child(v.id()));
+                        tmp_values[aid].set_key(attr_key);
+                        break;
+                    }
+                    case sx::ValueType::ARRAY: {
+                        // Translate arrays with a stack since array can be nested
+                        auto n = v[attr_key];
+                        n |= ryml::SEQ;
+                        std::vector<std::tuple<ryml::NodeRef, const sx::Array*>> nested_arrays;
+                        nested_arrays.push_back({n, arrays->Get(attr_value.value())});
+
+                        while (!nested_arrays.empty()) {
+                            auto& [array_node, array_ptr] = nested_arrays.back();
+                            nested_arrays.pop_back();
+
+                            // Unpack the array
+                            auto array_id = nested_arrays.size() - 1;
+                            auto array_type = array_ptr->type();
+                            auto array_begin = array_ptr->offset();
+                            auto array_end = array_begin + array_ptr->length();
+
+                            // Push on next visit
+                            array_ptr = nullptr;
+
+                            switch (array_type) {
+                                case sx::ValueType::NONE:
+                                    break;
+                                case sx::ValueType::ARRAY:
+                                    assert_within(array_begin, array_end, arrays->size());
+                                    for (auto i = array_begin; i < array_end; ++i) {
+                                        auto nested_array = array_node.append_child();
+                                        nested_array |= ryml::SEQ;
+                                        nested_arrays.push_back({nested_array, arrays->Get(i)});
+                                    }
+                                    break;
+                                case sx::ValueType::OBJECT:
+                                    assert_within(array_begin, array_end, oid);
+                                    for (unsigned i = array_begin; i < array_end; ++i) {
+                                        tree.move(tmp_values[i].id(), array_node.id(), tree.last_child(array_node.id()));
+                                    }
+                                    break;
+                                case sx::ValueType::STRING:
+                                    assert_within(array_begin, array_end, values_str->size());
+                                    for (auto i = array_begin; i < array_end; ++i) {
+                                        auto nested_val = array_node.append_child();
+                                        nested_val |= ryml::VAL;
+                                        encode(nested_val, *values_str->Get(i));
+                                    }
+                                    break;
+                                case sx::ValueType::I32:
+                                    assert_within(array_begin, array_end, values_i32->size());
+                                    for (auto i = array_begin; i < array_end; ++i) {
+                                        auto nested_val = array_node.append_child();
+                                        nested_val |= ryml::VAL;
+                                        nested_val << values_i32->Get(i);
+                                    }
+                                    break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Build the document entries
+        auto statements = root["statements"];
+        statements |= ryml::SEQ;
+        for (unsigned id = 0; id < entries->size(); ++id) {
+            assert_less_than(id, entries->size());
+            auto child = tmp_values[entries->Get(id)].id();
+            tree.move(child, statements.id(), tree.last_child(statements.id()));
+        }
+    }
+    tree.remove(tmp.id());
+
+    // Add errors
+    auto errors = root["errors"];
+    errors |= ryml::SEQ;
+    for (auto err : *module.errors())
+        encode(errors.append_child(), *err);
+
+    // Add line breaks
+    auto line_breaks = root["line_breaks"];
+    line_breaks |= ryml::SEQ;
+    for (auto err : *module.line_breaks())
+        encode(line_breaks.append_child(), *err);
+
+    // Add comments
+    auto comments = root["comments"];
+    comments |= ryml::SEQ;
+    for (auto err : *module.comments())
+        encode(comments.append_child(), *err);
+
+    // Write the yaml
+    ryml::emit(tree);
+    return "";
+}
+
+}  // namespace parser
+}  // namespace dashql
--- libs/cpp/test/parser_test.cc
+++ libs/cpp/test/parser_test.cc
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 The DashQL Authors
 
 #include "dashql/parser/parser_driver.h"
-#include "dashql/parser/json.h"
+#include "dashql/parser/yaml_encoder.h"
 #include "gtest/gtest.h"
 #include "flatbuffers/flatbuffers.h"
 
@@ -23,8 +23,8 @@ TEST(ParserTest, SELECT_FCONST) {
     ASSERT_EQ(m->errors()->size(), 0) << m->errors()->Get(0)->message()->c_str();
     ASSERT_EQ(m->statements()->entries()->size(), 1);
 
-    auto s = encodeJSON(*m);
-    ASSERT_EQ(std::string(s.GetString(), s.GetLength()), "");
+    auto s = encodeYAML(*m);
+    ASSERT_EQ(s, "");
 }
 
 // TEST(ParserTest, ParameterDeclaration) {
