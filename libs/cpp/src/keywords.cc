e659c3ae1b6bb2181551f81b1c7553338b8f4242
Andre Kohn <kohn.a@outlook.com>
2020-11-01 20:10:12 +0100

    Determine the maximum keyword length for keyword lowering

--- libs/cpp/src/keywords.cc
+++ libs/cpp/src/keywords.cc
@@ -5,7 +5,7 @@ namespace dashql {
 namespace parser {
 
 /// The keyword map
-static const std::unordered_map<std::string_view, Keyword> keywords = {
+static const std::unordered_map<std::string_view, Keyword> KEYWORDS = {
 #define X(CATEGORY, NAME, TOKEN) { NAME, Keyword{ NAME, Parser::token::DQL_##TOKEN, KeywordCategory::CATEGORY } },
 #include "./grammar/keywords/dashql_keywords.list"
 #include "./grammar/keywords/sql_column_name_keywords.list"
@@ -15,9 +15,28 @@ static const std::unordered_map<std::string_view, Keyword> keywords = {
 #undef X
 };
 
+/// Determine the maximum keyword length
+size_t constexpr length(const char* str) { return *str ? 1 + length(str + 1) : 0; }
+constexpr size_t MAX_KEYWORD_LENGTH = std::max<size_t>({
+#define X(CATEGORY, NAME, TOKEN) length(NAME),
+#include "./grammar/keywords/dashql_keywords.list"
+#include "./grammar/keywords/sql_column_name_keywords.list"
+#include "./grammar/keywords/sql_reserved_keywords.list"
+#include "./grammar/keywords/sql_type_func_keywords.list"
+#include "./grammar/keywords/sql_unreserved_keywords.list"
+#undef X
+});
+
 /// Find a keyword
 const Keyword* Keyword::Find(std::string_view text) {
-    if (auto iter = keywords.find(text); iter != keywords.end())
+    // Convert to lowercase
+    std::array<char, MAX_KEYWORD_LENGTH + 1> buffer;
+    for (unsigned i = 0; i < text.size(); ++i)
+        buffer[i] = ::tolower(text[i]);
+    std::string_view text_lc{buffer.data(), text.size()};
+
+    // Find the keyword
+    if (auto iter = KEYWORDS.find(text_lc); iter != KEYWORDS.end())
         return &iter->second;
     return nullptr;
 }
