fc87739fb5a404ef1ba9042fc68b7cd19005659f
Andre Kohn <kohn.a@outlook.com>
2020-11-15 19:07:28 +0100

    Add script to deploy the app

---
+++ scripts/deploy_app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
+BUILD_DIR="${PROJECT_ROOT}/app/build/release"
+
+BUCKET_URL="s3://dashql-app"
+DISTRIBUTION_ID="E1WT3LVZLA4YZX"
+
+echo "Uploading files to ${BUCKET_URL}..."
+aws s3 sync build ${BUCKET_URL} \
+  --acl public-read \
+  --exclude index.html
+
+# echo "Uploading service-worker.js"
+# aws s3 cp build/service-worker.js ${S3_BUCKET}/service-worker.js \
+#   --metadata-directive REPLACE \
+#   --cache-control max-age=0,no-cache,no-store,must-revalidate \
+#   --content-type application/javascript \
+#   --acl public-read
+
+echo "Uploading index.html"
+aws s3 cp ${BUILD_DIR}/index.html ${BUCKET_URL}/index.html \
+  --metadata-directive REPLACE \
+  --cache-control max-age=0,no-cache,no-store,must-revalidate \
+  --content-type text/html \
+  --acl public-read
+
+echo "Purging the cache for CloudFront"
+aws cloudfront create-invalidation \
+  --distribution-id ${DISTRIBUTION_ID} \
+  --paths /
