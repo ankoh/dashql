de1015568d50c8515a2cfa3cd4393aa46725eac3
Andre Kohn <kohn.a@outlook.com>
2020-11-15 23:13:03 +0100

    Deploy app

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -10,21 +10,6 @@ DISTRIBUTION_ID="E1WT3LVZLA4YZX"
 
 echo "Uploading files to ${BUCKET_URL}..."
 aws s3 sync ${BUILD_DIR} ${BUCKET_URL}/ \
-  --acl public-read \
-  --exclude index.html
-
-# echo "Uploading service-worker.js"
-# aws s3 cp build/service-worker.js ${S3_BUCKET}/service-worker.js \
-#   --metadata-directive REPLACE \
-#   --cache-control max-age=0,no-cache,no-store,must-revalidate \
-#   --content-type application/javascript \
-#   --acl public-read
-
-echo "Uploading index.html"
-aws s3 cp ${BUILD_DIR}/index.html ${BUCKET_URL}/index.html \
-  --metadata-directive REPLACE \
-  --cache-control max-age=0,no-cache,no-store,must-revalidate \
-  --content-type text/html \
   --acl public-read
 
 echo "Purging the cache for CloudFront"
