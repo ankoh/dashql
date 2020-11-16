2a7b0cb425265cf68e2eacd5a6413bfd28dd6676
Andre Kohn <kohn.a@outlook.com>
2020-11-16 12:29:44 +0100

    Move scripts into makefile

--- scripts/reset_duckdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2020 The DashQL Authors
-
-PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
-DUCKDB_ROOT=${PROJECT_ROOT}/submodules/duckdb
-
-rm -rf ${DUCKDB_ROOT}/src/amalgamation/*
-rm -rf ${DUCKDB_ROOT}/build/*
