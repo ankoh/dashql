b96cdfc99c1a1727de2de563cfb4d33898d6d0b4
Andre Kohn <kohn.a@outlook.com>
2020-11-18 22:31:38 +0100

    Add scaffold for module inspector

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,8 +1,10 @@
 export {NavBar, withNavBar} from "./navigation_bar";
 import Board from "./board";
 import EditorLoader from "./editor_loader";
+import ModuleInspector from "./module_inspector";
 
 export {
     Board,
     EditorLoader,
+    ModuleInspector,
 }
