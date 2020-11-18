5f77f6aa920def994a52e9fa8ba2340760a6b354
Andre Kohn <kohn.a@outlook.com>
2020-11-18 10:41:28 +0100

    Use default value

--- app/src/store/demo_data.ts
+++ app/src/store/demo_data.ts
@@ -4,12 +4,14 @@ export const DEMO_SCRIPT =
 
 -- Declare a dynamic input field on top of your dashboard.
 -- Ref: https://docs.dashql.com/grammar/param
-DECLARE PARAMETER country_code AS TEXT;
+DECLARE PARAMETER country AS TEXT (
+    default = 'DE'
+);
 
 -- Load data from external sources like HTTP REST APIs.
 -- Ref: https://docs.dashql.com/grammar/load
 LOAD wheather_csv FROM http (
-    url = 'https://cdn.dashql.com/demo/weather/{{country_code}}'
+    url = 'https://cdn.dashql.com/demo/weather/{{country}}'
 );
 
 -- Interpret the loaded data as relation.
