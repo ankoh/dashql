4431f7776f5f75107337a5e20e76b350498d6221
Andre Kohn <kohn.a@outlook.com>
2020-11-19 17:02:26 +0100

    Center graph

--- app/src/components/program_graph.tsx
+++ app/src/components/program_graph.tsx
@@ -21,7 +21,9 @@ class ProgramGraph extends React.Component<Props> {
     /// Root node
     private svgNode = React.createRef<SVGSVGElement>();
     /// Group node
-    private svgGroupNode = React.createRef<SVGSVGElement>();
+    private svgGraphNode = React.createRef<SVGSVGElement>();
+    /// Zoom node
+    private zoom = d3.zoom();
 
     private renderGraph() {
         if (this.props.program == null) {
@@ -43,20 +45,40 @@ class ProgramGraph extends React.Component<Props> {
             });
         });
 
-        const inner: any = d3.select(this.svgGroupNode.current!);
+        const svgRoot: any = d3.select(this.svgNode.current!);
+        const svgInner: any = d3.select(this.svgGraphNode.current!);
         const render = new dagreD3.render();
+
+        // reset zoom
+        svgRoot.call(this.zoom.transform, d3.zoomIdentity);
 
         // Draw undirected edges
         render.arrows().none = () => {};
 
-        render(inner, g);
+        render(svgInner, g);
+
+        // Setup zooming
+        const svgBB = this.svgNode.current!.getBoundingClientRect();
+        const innerBB = this.svgGraphNode.current!.getBoundingClientRect();
+        const scale = Math.min(svgBB.width / innerBB.width, svgBB.height / (innerBB.height + 60));
+        const xOffset = (svgBB.width - innerBB.width) / 2;
+        const yOffset = (svgBB.height - innerBB.height) / 2;
+
+        this.zoom
+            .scaleExtent([scale, Infinity])
+            .on('zoom', () => {
+                svgInner.attr('transform', d3.event.transform);
+            });
+        svgRoot.call(this.zoom.translateBy, xOffset, yOffset);
+        svgRoot.call(this.zoom.scaleTo, scale);
+
     }
 
     public render() {
         return (
             <div className={classnames(this.props.className)}>
                 <svg ref={this.svgNode} className={styles.svg_root} width="100%" height="100%">
-                    <g ref={this.svgGroupNode} />
+                    <g ref={this.svgGraphNode} />
                 </svg>
             </div>
         );
