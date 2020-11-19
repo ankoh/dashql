8d6ff6c723118ebeeed939d4a92a99335778fda9
Andre Kohn <kohn.a@outlook.com>
2020-11-19 22:57:27 +0100

    Node.js things

--- scripts/edge-lambda.js
+++ scripts/edge-lambda.js
@@ -1,8 +1,8 @@
 "use strict";
-exports.handler = async (e, ctx, cb) => {
+exports.handler = async (e) => {
   const r = e.Records[0].cf.request;
   if (!r.uri.startsWith('/static/')) {
     r.uri = '/';
   }
-  return cb(null, r);
+  return r;
 };
