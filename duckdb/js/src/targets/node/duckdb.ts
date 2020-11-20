e45377313cc980846c56f1839442294bd1e3c638
Andre Kohn <kohn.a@outlook.com>
2020-11-20 11:21:39 +0100

    Rename webapi -> webdb in @dashql/duckdb

--- duckdb/js/src/targets/node/duckdb.ts
+++ duckdb/js/src/targets/node/duckdb.ts
@@ -3,9 +3,9 @@
 import duckdb_api_wasm from '../../wasm/duckdb_node.wasm';
 import duckdb_api_init from '../../wasm/duckdb_node.js';
 import { DuckDBModule } from '../../wasm/duckdb_module';
-import * as webapi from '../../webapi';
+import * as webdb from '../../webdb';
 
-export class DuckDB extends webapi.DuckDBBindings {
+export class DuckDB extends webdb.DuckDBBindings {
     protected path: string;
     constructor(path: string | null = null) {
         super();
@@ -23,4 +23,4 @@ export class DuckDB extends webapi.DuckDBBindings {
     }
 }
 
-export * from '../../webapi';
+export * from '../../webdb';
---
+++ duckdb/js/src/webdb/index.ts
@@ -0,0 +1,4 @@
+export * from "./iterator";
+export * from "./value";
+export * from "./webdb_bindings";
+export * from "./webdb_buffer";
