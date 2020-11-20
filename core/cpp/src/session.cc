4772e3719dcd6115b95dd46c263059879b5f51dc
Andre Kohn <kohn.a@outlook.com>
2020-11-21 00:02:52 +0100

    Work on action graph

--- core/cpp/src/session.cc
+++ core/cpp/src/session.cc
@@ -1,4 +1,5 @@
 #include "dashql/session.h"
+
 #include "dashql/parser/parser_driver.h"
 #include "dashql/proto/session_generated.h"
 
@@ -7,9 +8,14 @@ namespace fb = flatbuffers;
 
 namespace dashql {
 
+fb::Offset<ActionGraph> Session::DeriveActions(fb::FlatBufferBuilder& builder, const ExecutableProgram& prev,
+                                               const Program& next) {
+    proto::action::ActionGraphBuilder graph_builder{builder};
+    return graph_builder.Finish();
+}
+
 /// Constructor
-Session::Session()
-    : database_(), database_connection_(), program_text_(), program_(), action_status_() {
+Session::Session() : database_(), database_connection_(), program_text_(), program_(), action_status_() {
     database_connection_ = database_.Connect();
     program_ = {nullptr, fb::DetachedBuffer()};
 }
@@ -20,11 +26,18 @@ ExpectedBufferRef<proto::session::ExecutableProgram> Session::Evaluate(const cha
     flatbuffers::FlatBufferBuilder builder{text_view.size()};
 
     // Parse the text
-    auto program = parser::ParserDriver::Parse(builder, text_view);
+    auto program_ofs = parser::ParserDriver::Parse(builder, text_view);
+    auto program_ptr = reinterpret_cast<Program*>(builder.GetCurrentBufferPointer() + builder.GetSize() - program_ofs.o);
+    std::optional<fb::Offset<proto::action::ActionGraph>> actions_ofs;
+    if (auto expected = std::get<0>(program_)) {
+        actions_ofs = DeriveActions(builder, *expected, *program_ptr);
+    }
 
     // Build the executable program
     proto::session::ExecutableProgramBuilder prog_builder{builder};
-    prog_builder.add_program(program);
+    prog_builder.add_program(program_ofs);
+    if (actions_ofs)
+        prog_builder.add_action_graph(*actions_ofs);
     auto prog_ofs = prog_builder.Finish();
 
     // Finish the buffer
@@ -35,7 +48,6 @@ ExpectedBufferRef<proto::session::ExecutableProgram> Session::Evaluate(const cha
 }
 
 /// Update the action status
-void Session::UpdateActionStatus(uint32_t id, proto::action::ActionStatus status) {
-}
+void Session::UpdateActionStatus(uint32_t id, proto::action::ActionStatus status) {}
 
-}
+}  // namespace dashql
