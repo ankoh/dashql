d90d14adb9a5e6834c3498b7bcb35fb6831078bd
Andre Kohn <kohn.a@outlook.com>
2020-11-22 21:02:46 +0100

    Similarity tests

--- core/cpp/test/default/program_diff_test.cc
+++ core/cpp/test/default/program_diff_test.cc
@@ -23,18 +23,29 @@ std::pair<const sx::Program*, fb::DetachedBuffer> Parse(std::string_view text) {
     return {program, move(buffer)};
 }
 
-TEST(ProgramDiff, Select1Select2) {
+TEST(ProgramDiff, Similar1) {
+    auto t1 = "SELECT 1";
+    auto t2 = "SELECT 1";
+    auto [p1, pb1] = Parse(t1);
+    auto [p2, pb2] = Parse(t2);
+    ASSERT_EQ(p1->statements()->size(), 1);
+    ASSERT_EQ(p2->statements()->size(), 1);
+    ProgramMatcher matcher{t1, t2, *p1, *p2};
+    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
+    ASSERT_TRUE(sim.Equal());
+}
+
+TEST(ProgramDiff, DifferentConst) {
     auto t1 = "SELECT 1";
     auto t2 = "SELECT 2";
     auto [p1, pb1] = Parse(t1);
     auto [p2, pb2] = Parse(t2);
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
-    auto s1 = p1->statements()->Get(0);
-    auto s2 = p2->statements()->Get(0);
     ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto sim = matcher.ComputeSimilarity(*s1, *s2);
-    ASSERT_EQ(sim.total_nodes - sim.matching_nodes, 1) << "total=" << sim.total_nodes << " matching=" << sim.matching_nodes;
+    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
+    ASSERT_FALSE(sim.Equal());
+    ASSERT_EQ(sim.total_nodes - sim.matching_nodes, 1);
 }
 
 }  // namespace
