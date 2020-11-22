7914f915079cf6f48795e3bda734f596bb561e17
Andre Kohn <kohn.a@outlook.com>
2020-11-22 17:15:27 +0100

    Draft statement similarity metric

--- core/cpp/src/session.cc
+++ core/cpp/src/session.cc
@@ -2,50 +2,22 @@
 
 #include "dashql/parser/parser_driver.h"
 #include "dashql/proto/session_generated.h"
+#include "dashql/proto/syntax_generated.h"
 
 using namespace dashql;
 namespace fb = flatbuffers;
+namespace sx = proto::syntax;
 
 namespace dashql {
 
-
-// In general, the problem of computing the graph-edit distance is NP-complete.
-// However, our problem is simpler since we can assume that the statement names stay the same.
-//
-// Our algorithm works as follows:
-//
-// We traverse the PREVIOUS action graph in topological order and check for every action whether it is applicable.
-//
-// An action is applicable iff:
-//  1) There exists a statement in the new program with the same qualified name as the actions origin.
-//  2) The action itself models precisely what is needed for the new version of the statement.
-//  3) All dependencies are applicable.
-//
-// If an action is applicable, we copy it over to the new action graph and mark it as complete.
-// If it is not, we have to check whether it invalidates any previous table.
-//
-// An action that is not applicable can have two effects:
-//  1) If it is only creating a new table, we emit an action to UNDO the effects (i.e. DROP).
-//  2) If it is modifying an existing table, we have to backtrack all (transitive) dependencies and invalidate them.
-//     We invalidate a previous action by UNDOING its effects and removing the action from the new graph.
-//
-// Example for not applicable actions:
-//  1) SELECT 1 INTO b; DELETE FROM b;
-//     If a user removes the delete statement, we have to backtrack that b (and thus the SELECT statment) cannot be carried over.
-//  2) SELECT 1 INTO b, SELECT * INTO c FROM b;
-//     If a user removes the second statement, we just DROP c.
-//
-// This will produce a new action graph that will UNDO all actions except for those that can be reused as is.
-// Finally, we need to emit new actions for all statements, that were not covered by an applicable action.
-//
-// XXX we're not parsing insert, delete, update at the moment so we can implement the backwards poisoning later.
-//
-// Additional notes:
-//  - The actions within the action graph are encoded in toplogical order.
-//    That allows us to implement the first phase of the algorithm with a linear scan over the previous action array.
-//
+// Drive actions
 fb::Offset<ActionGraph> Session::DeriveActions(fb::FlatBufferBuilder& builder, const ExecutableProgram& prev,
                                                const Program& next) {
+//    auto* prev_program = prev.program();
+//    auto* prev_nodes = prev_program->nodes();
+//    auto* prev_statements = prev_program->statements();
+//    auto* prev_action_graph = prev.action_graph();
+//    auto* prev_actions = prev_action_graph->plan();
 
     proto::action::ActionGraphBuilder graph_builder{builder};
     return graph_builder.Finish();
