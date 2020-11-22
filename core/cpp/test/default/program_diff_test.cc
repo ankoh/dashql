6af80d554526534135f67ffe54aa3a9f39fe8be6
Andre Kohn <kohn.a@outlook.com>
2020-11-22 22:07:02 +0100

    Work on diff

--- core/cpp/test/default/program_diff_test.cc
+++ core/cpp/test/default/program_diff_test.cc
@@ -73,4 +73,16 @@ TEST(ProgramDiff, DifferentColumnRef) {
     ASSERT_EQ(sim.total_nodes - sim.matching_nodes, 1);
 }
 
+TEST(ProgramDiff, DifferentSelect) {
+    auto t1 = "select 1";
+    auto t2 = "select c from b where c = global.d";
+    auto [p1, pb1] = Parse(t1);
+    auto [p2, pb2] = Parse(t2);
+    ASSERT_EQ(p1->statements()->size(), 1);
+    ASSERT_EQ(p2->statements()->size(), 1);
+    ProgramMatcher matcher{t1, t2, *p1, *p2};
+    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
+    ASSERT_FALSE(sim.Equal());
+}
+
 }  // namespace
