a2cbfa6de77dd35e9293d568b0d568f5e79425ec
Andre Kohn <kohn.a@outlook.com>
2020-11-22 17:25:10 +0100

    Add hello world test

---
+++ core/cpp/test/default/program_diff_test.cc
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 The DashQL Authors
+
+#include <sstream>
+
+#include "dashql/program_diff.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+namespace {
+
+TEST(ProgramDiff, HelloWorld) {
+    ASSERT_TRUE(true);
+}
+
+}  // namespace
