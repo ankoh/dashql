dec5cab5a59302c1ea504dab53b5dffba34aa33f
Andre Kohn <kohn.a@outlook.com>
2020-11-23 13:28:01 +0100

    Use parameterized diff tests

--- core/cpp/test/default/program_diff_test.cc
+++ core/cpp/test/default/program_diff_test.cc
@@ -33,98 +33,132 @@ std::pair<const sx::Program*, fb::DetachedBuffer> Parse(std::string_view text) {
     return {program, move(buffer)};
 }
 
-void TestSingleStatements(std::string_view t1, std::string_view t2, bool are_equal, SimilarityEstimate estimate, std::optional<size_t> diff_node_count = std::nullopt) {
-    auto [p1, pb1] = Parse(t1);
-    auto [p2, pb2] = Parse(t2);
+struct SSP {
+    std::string_view t1;
+    std::string_view t2;
+    bool are_equal;
+    SimilarityEstimate estimate;
+    std::optional<size_t> diff_node_count = std::nullopt;
+};
+class SingleStatementTest: public ::testing::TestWithParam<SSP> {};
+
+TEST_P(SingleStatementTest, DeepEquality) {
+    auto& param = GetParam();
+    auto [p1, pb1] = Parse(param.t1);
+    auto [p2, pb2] = Parse(param.t2);
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
-    ProgramMatcherProxy matcher{t1, t2, *p1, *p2};
+    ProgramMatcherProxy matcher{param.t1, param.t2, *p1, *p2};
     auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.CheckDeepEquality(*s1, *s2), param.are_equal);
+}
 
-    ASSERT_EQ(matcher.CheckDeepEquality(*s1, *s2), are_equal);
-    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), estimate);
+TEST_P(SingleStatementTest, SimilarityEstimate) {
+    auto& param = GetParam();
+    auto [p1, pb1] = Parse(param.t1);
+    auto [p2, pb2] = Parse(param.t2);
+    ASSERT_EQ(p1->statements()->size(), 1);
+    ASSERT_EQ(p2->statements()->size(), 1);
+    ProgramMatcherProxy matcher{param.t1, param.t2, *p1, *p2};
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), param.estimate);
+}
 
+TEST_P(SingleStatementTest, StatementDiff) {
+    auto& param = GetParam();
+    auto [p1, pb1] = Parse(param.t1);
+    auto [p2, pb2] = Parse(param.t2);
+    ASSERT_EQ(p1->statements()->size(), 1);
+    ASSERT_EQ(p2->statements()->size(), 1);
+    ProgramMatcherProxy matcher{param.t1, param.t2, *p1, *p2};
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
     auto diff = matcher.ComputeDiff(*s1, *s2);
-    ASSERT_EQ(diff.Equal(), are_equal);
-    if (diff_node_count)
-        ASSERT_EQ(diff.diff_nodes.size(), *diff_node_count);
+    ASSERT_EQ(diff.Equal(), param.are_equal);
+    if (param.diff_node_count)
+        ASSERT_EQ(diff.diff_nodes.size(), *param.diff_node_count);
 }
 
-TEST(ProgramDiff, SingleStatements) {
-    TestSingleStatements("SELECT 1", "SELECT 1", true, SimilarityEstimate::EQUAL, 0);
-    TestSingleStatements("SELECT 1", "SELECT 2", false, SimilarityEstimate::SIMILAR, 1);
-    TestSingleStatements(
+INSTANTIATE_TEST_SUITE_P(ProgramDiff, SingleStatementTest, ::testing::Values(
+    SSP{"SELECT 1", "SELECT 1", true, SimilarityEstimate::EQUAL, 0},
+    SSP{"SELECT 1", "SELECT 2", false, SimilarityEstimate::SIMILAR, 1},
+    SSP{"select c from b where c = global.a",
         "select c from b where c = global.a",
-        "select c from b where c = global.a",
-        true, SimilarityEstimate::EQUAL, 0);
-    TestSingleStatements(
-        "select c from b where c = global.a",
+        true, SimilarityEstimate::EQUAL, 0},
+    SSP{"select c from b where c = global.a",
         "select c from b where c = global.d",
-        false, SimilarityEstimate::SIMILAR, 1);
-    TestSingleStatements(
-        "select 1",
+        false, SimilarityEstimate::SIMILAR, 1},
+    SSP{"select 1",
         "select c from b where c = global.d",
-        false, SimilarityEstimate::SIMILAR);
-    TestSingleStatements(
-        "viz whether_avg using line",
+        false, SimilarityEstimate::SIMILAR},
+    SSP{"viz whether_avg using line",
         "select c from b where c = global.d",
-        false, SimilarityEstimate::NOT_EQUAL);
-}
+        false, SimilarityEstimate::NOT_EQUAL}
+));
 
-void TestUniquePairs(std::string_view t1, std::string_view t2, const std::vector<size_t>& ids0, const std::vector<size_t>& ids1, const std::vector<std::pair<size_t, size_t>>& expected) {
-    auto [p1, pb1] = Parse(t1);
-    auto [p2, pb2] = Parse(t2);
-    ProgramMatcherProxy matcher{t1, t2, *p1, *p2};
+struct UPP {
+    std::string_view t1;
+    std::string_view t2;
+    std::vector<size_t> ids0;
+    std::vector<size_t> ids1;
+    std::vector<std::pair<size_t, size_t>> expected;
+};
+class UniquePairTest: public ::testing::TestWithParam<UPP> {};
+
+TEST_P(UniquePairTest, UniquePairsEqual) {
+    auto& param = GetParam();
+    auto [p1, pb1] = Parse(param.t1);
+    auto [p2, pb2] = Parse(param.t2);
+    ProgramMatcherProxy matcher{param.t1, param.t2, *p1, *p2};
     std::vector<std::pair<size_t, size_t>> unique_pairs;
-    matcher.FindUniquePairs(ids0, ids1, unique_pairs);
-    ASSERT_EQ(unique_pairs, expected);
+    matcher.FindUniquePairs(param.ids0, param.ids1, unique_pairs);
+    ASSERT_EQ(unique_pairs, param.expected);
 }
 
-TEST(ProgramDiff, UniquePairs) {
-    TestUniquePairs(R"DQL(
+INSTANTIATE_TEST_SUITE_P(ProgramDiff, UniquePairTest, ::testing::Values(
+    UPP{R"DQL(
         SELECT 1;
     )DQL", R"DQL(
         SELECT 1;
-    )DQL", {0}, {0}, {{0, 0}});
+    )DQL", {0}, {0}, {{0, 0}}},
 
-    TestUniquePairs(R"DQL(
+    UPP{R"DQL(
         SELECT 2;
         SELECT 1;
     )DQL", R"DQL(
         SELECT 1;
-    )DQL", {0, 1}, {0}, {{1, 0}});
+    )DQL", {0, 1}, {0}, {{1, 0}}},
 
-    TestUniquePairs(R"DQL(
-        SELECT 2;
-        SELECT 1;
-    )DQL", R"DQL(
-        SELECT 3;
-        SELECT 1;
-    )DQL", {0, 1}, {0, 1}, {{1, 1}});
-
-    TestUniquePairs(R"DQL(
+    UPP{R"DQL(
         SELECT 2;
         SELECT 1;
     )DQL", R"DQL(
         SELECT 3;
         SELECT 1;
-    )DQL", {0, 1}, {}, {});
+    )DQL", {0, 1}, {0, 1}, {{1, 1}}},
 
-    TestUniquePairs(R"DQL(
+    UPP{R"DQL(
+        SELECT 2;
+        SELECT 1;
+    )DQL", R"DQL(
+        SELECT 3;
+        SELECT 1;
+    )DQL", {0, 1}, {}, {}},
+
+    UPP{R"DQL(
         SELECT 1;
         SELECT 1;
     )DQL", R"DQL(
         SELECT 3;
         SELECT 1;
-    )DQL", {0, 1}, {0, 1}, {});
+    )DQL", {0, 1}, {0, 1}, {}},
 
-    TestUniquePairs(R"DQL(
+    UPP{R"DQL(
         SELECT 1;
         SELECT 2;
     )DQL", R"DQL(
         SELECT 1;
         SELECT 1;
-    )DQL", {0, 1}, {0, 1}, {});
-}
+    )DQL", {0, 1}, {0, 1}, {}}
+));
 
 }  // namespace
