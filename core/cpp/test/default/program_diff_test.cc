77bfaed41dc8b98802533b538064e22b0d1c0d8a
Andre Kohn <kohn.a@outlook.com>
2020-11-23 08:50:47 +0100

    Use dedicated function to peek the root nodes

--- core/cpp/test/default/program_diff_test.cc
+++ core/cpp/test/default/program_diff_test.cc
@@ -10,6 +10,7 @@
 
 using namespace std;
 using namespace dashql;
+using SimilarityEstimate = ProgramMatcher::SimilarityEstimate;
 namespace fb = flatbuffers;
 
 namespace {
@@ -31,7 +32,9 @@ TEST(ProgramDiff, EqualConst) {
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
     ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::EQUAL);
+    auto sim = matcher.ComputeSimilarity(*s1, *s2);
     ASSERT_TRUE(sim.Equal());
 }
 
@@ -43,7 +46,9 @@ TEST(ProgramDiff, DifferentConst) {
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
     ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::SIMILAR);
+    auto sim = matcher.ComputeSimilarity(*s1, *s2);
     ASSERT_FALSE(sim.Equal());
     ASSERT_EQ(sim.total_nodes - sim.matching_nodes, 1);
 }
@@ -56,7 +61,9 @@ TEST(ProgramDiff, EqualColumnRefs) {
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
     ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::EQUAL);
+    auto sim = matcher.ComputeSimilarity(*s1, *s2);
     ASSERT_TRUE(sim.Equal());
 }
 
@@ -68,8 +75,9 @@ TEST(ProgramDiff, DifferentColumnRef) {
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
     ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
-    ASSERT_FALSE(sim.Equal());
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::SIMILAR);
+    auto sim = matcher.ComputeSimilarity(*s1, *s2);
     ASSERT_EQ(sim.total_nodes - sim.matching_nodes, 1);
 }
 
@@ -81,7 +89,23 @@ TEST(ProgramDiff, DifferentSelect) {
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
     ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto sim = matcher.ComputeSimilarity(*p1->statements()->Get(0), *p2->statements()->Get(0));
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::SIMILAR);
+    auto sim = matcher.ComputeSimilarity(*s1, *s2);
+    ASSERT_FALSE(sim.Equal());
+}
+
+TEST(ProgramDiff, DifferentStatementTypes) {
+    auto t1 = "viz whether_avg using line";
+    auto t2 = "select c from b where c = global.d";
+    auto [p1, pb1] = Parse(t1);
+    auto [p2, pb2] = Parse(t2);
+    ASSERT_EQ(p1->statements()->size(), 1);
+    ASSERT_EQ(p2->statements()->size(), 1);
+    ProgramMatcher matcher{t1, t2, *p1, *p2};
+    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::NOT_EQUAL);
+    auto sim = matcher.ComputeSimilarity(*s1, *s2);
     ASSERT_FALSE(sim.Equal());
 }
 
