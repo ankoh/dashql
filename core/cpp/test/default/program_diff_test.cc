aca177ebc6415830764f6be84afc3c996f65deb1
Andre Kohn <kohn.a@outlook.com>
2020-11-23 13:03:44 +0100

    Simplify diff tests

--- core/cpp/test/default/program_diff_test.cc
+++ core/cpp/test/default/program_diff_test.cc
@@ -15,6 +15,15 @@ namespace fb = flatbuffers;
 
 namespace {
 
+class ProgramMatcherProxy: public ProgramMatcher {
+    public:
+    /// Constructor
+    ProgramMatcherProxy(std::string_view source_text, std::string_view target_text, const sx::Program& source_program, const sx::Program& target_program)
+        : ProgramMatcher(source_text, target_text, source_program, target_program) {}
+
+    using ProgramMatcher::FindUniquePairs;
+};
+
 std::pair<const sx::Program*, fb::DetachedBuffer> Parse(std::string_view text) {
     fb::FlatBufferBuilder builder;
     auto ofs = parser::ParserDriver::Parse(builder, text);
@@ -24,100 +33,59 @@ std::pair<const sx::Program*, fb::DetachedBuffer> Parse(std::string_view text) {
     return {program, move(buffer)};
 }
 
-TEST(ProgramDiff, EqualConst) {
-    auto t1 = "SELECT 1";
-    auto t2 = "SELECT 1";
+void TestSingleStatements(std::string_view t1, std::string_view t2, bool are_equal, SimilarityEstimate estimate, std::optional<size_t> diff_node_count = std::nullopt) {
     auto [p1, pb1] = Parse(t1);
     auto [p2, pb2] = Parse(t2);
     ASSERT_EQ(p1->statements()->size(), 1);
     ASSERT_EQ(p2->statements()->size(), 1);
-    ProgramMatcher matcher{t1, t2, *p1, *p2};
+    ProgramMatcherProxy matcher{t1, t2, *p1, *p2};
     auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
-    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::EQUAL);
-    auto sim = matcher.ComputeSimilarity(*s1, *s2);
-    ASSERT_TRUE(sim.Equal());
-    ASSERT_EQ(sim.diff_nodes.size(), 0);
-    ASSERT_TRUE(matcher.CheckDeepEquality(*s1, *s2));
+
+    ASSERT_EQ(matcher.CheckDeepEquality(*s1, *s2), are_equal);
+    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), estimate);
+
+    auto diff = matcher.ComputeDiff(*s1, *s2);
+    ASSERT_EQ(diff.Equal(), are_equal);
+    if (diff_node_count)
+        ASSERT_EQ(diff.diff_nodes.size(), *diff_node_count);
 }
 
-TEST(ProgramDiff, DifferentConst) {
-    auto t1 = "SELECT 1";
-    auto t2 = "SELECT 2";
-    auto [p1, pb1] = Parse(t1);
-    auto [p2, pb2] = Parse(t2);
-    ASSERT_EQ(p1->statements()->size(), 1);
-    ASSERT_EQ(p2->statements()->size(), 1);
-    ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
-    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::SIMILAR);
-    auto sim = matcher.ComputeSimilarity(*s1, *s2);
-    ASSERT_FALSE(sim.Equal());
-    ASSERT_EQ(sim.total_nodes - sim.matching_nodes, 1);
-    ASSERT_EQ(sim.diff_nodes.size(), 1);
-    ASSERT_FALSE(matcher.CheckDeepEquality(*s1, *s2));
+TEST(ProgramDiff, SingleStatements) {
+    TestSingleStatements("SELECT 1", "SELECT 1", true, SimilarityEstimate::EQUAL, 0);
+    TestSingleStatements("SELECT 1", "SELECT 2", false, SimilarityEstimate::SIMILAR, 1);
+    TestSingleStatements(
+        "select c from b where c = global.a",
+        "select c from b where c = global.a",
+        true, SimilarityEstimate::EQUAL, 0);
+    TestSingleStatements(
+        "select c from b where c = global.a",
+        "select c from b where c = global.d",
+        false, SimilarityEstimate::SIMILAR, 1);
+    TestSingleStatements(
+        "select 1",
+        "select c from b where c = global.d",
+        false, SimilarityEstimate::SIMILAR);
+    TestSingleStatements(
+        "viz whether_avg using line",
+        "select c from b where c = global.d",
+        false, SimilarityEstimate::NOT_EQUAL);
 }
 
-TEST(ProgramDiff, EqualColumnRefs) {
-    auto t1 = "select c from b where c = global.a";
-    auto t2 = "select c from b where c = global.a";
+void TestUniquePairs(std::string_view t1, std::string_view t2, const std::vector<size_t>& ids0, const std::vector<size_t>& ids1, const std::vector<std::pair<size_t, size_t>>& expected) {
     auto [p1, pb1] = Parse(t1);
     auto [p2, pb2] = Parse(t2);
-    ASSERT_EQ(p1->statements()->size(), 1);
-    ASSERT_EQ(p2->statements()->size(), 1);
-    ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
-    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::EQUAL);
-    auto sim = matcher.ComputeSimilarity(*s1, *s2);
-    ASSERT_TRUE(sim.Equal());
-    ASSERT_EQ(sim.diff_nodes.size(), 0);
-    ASSERT_TRUE(matcher.CheckDeepEquality(*s1, *s2));
+    ProgramMatcherProxy matcher{t1, t2, *p1, *p2};
+    std::vector<std::pair<size_t, size_t>> unique_pairs;
+    matcher.FindUniquePairs(ids0, ids1, unique_pairs);
+    ASSERT_EQ(unique_pairs, expected);
 }
 
-TEST(ProgramDiff, DifferentColumnRef) {
-    auto t1 = "select c from b where c = global.a";
-    auto t2 = "select c from b where c = global.d";
-    auto [p1, pb1] = Parse(t1);
-    auto [p2, pb2] = Parse(t2);
-    ASSERT_EQ(p1->statements()->size(), 1);
-    ASSERT_EQ(p2->statements()->size(), 1);
-    ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
-    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::SIMILAR);
-    auto sim = matcher.ComputeSimilarity(*s1, *s2);
-    ASSERT_EQ(sim.total_nodes - sim.matching_nodes, 1);
-    ASSERT_EQ(sim.diff_nodes.size(), 1);
-    ASSERT_FALSE(matcher.CheckDeepEquality(*s1, *s2));
-}
-
-TEST(ProgramDiff, DifferentSelect) {
-    auto t1 = "select 1";
-    auto t2 = "select c from b where c = global.d";
-    auto [p1, pb1] = Parse(t1);
-    auto [p2, pb2] = Parse(t2);
-    ASSERT_EQ(p1->statements()->size(), 1);
-    ASSERT_EQ(p2->statements()->size(), 1);
-    ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
-    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::SIMILAR);
-    auto sim = matcher.ComputeSimilarity(*s1, *s2);
-    ASSERT_FALSE(sim.Equal());
-    ASSERT_FALSE(matcher.CheckDeepEquality(*s1, *s2));
-}
-
-TEST(ProgramDiff, DifferentStatementTypes) {
-    auto t1 = "viz whether_avg using line";
-    auto t2 = "select c from b where c = global.d";
-    auto [p1, pb1] = Parse(t1);
-    auto [p2, pb2] = Parse(t2);
-    ASSERT_EQ(p1->statements()->size(), 1);
-    ASSERT_EQ(p2->statements()->size(), 1);
-    ProgramMatcher matcher{t1, t2, *p1, *p2};
-    auto s1 = p1->statements()->Get(0), s2 = p2->statements()->Get(0);
-    ASSERT_EQ(matcher.EstimateSimilarity(*s1, *s2), SimilarityEstimate::NOT_EQUAL);
-    auto sim = matcher.ComputeSimilarity(*s1, *s2);
-    ASSERT_FALSE(sim.Equal());
-    ASSERT_EQ(sim.diff_nodes.size(), 0);
-    ASSERT_FALSE(matcher.CheckDeepEquality(*s1, *s2));
+TEST(ProgramDiff, UniquePairs) {
+    TestUniquePairs(R"DQL(
+        SELECT 1
+    )DQL", R"DQL(
+        SELECT 1
+    )DQL", {0}, {0}, {{0, 0}});
 }
 
 }  // namespace
