05c5c96b6cefd2ccaf09af90bf2ad8a27bcfdf13
Andre Kohn <kohn.a@outlook.com>
2020-11-26 16:41:50 +0100

    Render statement text via program instance

--- core/cpp/src/program_instance.cc
+++ core/cpp/src/program_instance.cc
@@ -6,4 +6,32 @@ namespace dashql {
 ProgramInstance::ProgramInstance(std::string_view text, const sx::ProgramT& program)
     : program_text_(text), program_(program), parameters_(), patch_() {}
 
+//
+//// Collect the statement options
+//Expected<std::string> ActionPlanner::RenderStatementText(size_t stmt_id) {
+//    // TODO Render the statement text
+//    auto& next = next_program_.program();
+//    auto& stmt = *next.statements[stmt_id];
+//    auto& stmt_root = next.nodes[stmt.root];
+//
+//    // Find all the column refs that occur in the statement
+//    for (auto& dep: next_program_.program().dependencies) {
+//        if (dep.target_statement() != stmt_id || dep.type() != sx::DependencyType::COLUMN_REF) continue;
+//        auto node_id = dep.target_node();
+//        auto& node = next.nodes[node_id];
+//        assert(node.node_type() == sx::NodeType::OBJECT_SQL_COLUMN_REF);
+//
+//        // Map to
+//    }
+//    return std::string{};
+//}
+
+Expected<std::string> ProgramInstance::RenderStatementText(size_t stmt_id) const {
+    auto& stmt = *program_.statements[stmt_id];
+    auto stmt_text = TextAt(program_.nodes[stmt.root].location());
+    // XXX
+    std::string copy{stmt_text.begin(), stmt_text.end()};
+    return copy;
+}
+
 }  // dashql
