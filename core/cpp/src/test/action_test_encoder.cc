e5e389e75ba1b1eabd963c3fbbe012adf174786f
Andre Kohn <kohn.a@outlook.com>
2020-11-27 22:48:01 +0100

    Prepare action tests

--- core/cpp/src/test/action_test_encoder.cc
+++ core/cpp/src/test/action_test_encoder.cc
@@ -1,29 +1,28 @@
 #include "dashql/test/action_test_encoder.h"
 
-#include "dashql/proto/syntax_generated.h"
-#include "dashql/proto/syntax_sql_generated.h"
-#include "dashql/proto/syntax_dashql_generated.h"
-
 #include <cstdint>
+#include <iostream>
 #include <regex>
 #include <sstream>
 #include <stack>
 #include <unordered_set>
-#include <iostream>
 
 #include "c4/yml/std/string.hpp"
 #include "c4/yml/yml.hpp"
+#include "dashql/proto/action_generated.h"
+#include "dashql/proto/syntax_dashql_generated.h"
+#include "dashql/proto/syntax_generated.h"
+#include "dashql/proto/syntax_sql_generated.h"
 #include "ryml.hpp"
 #include "ryml_std.hpp"
 
 namespace dashql {
 namespace parser {
 
-
-namespace {
-
-
-
+void EncodeActionTest(ryml::NodeRef ref, const ProgramInstance& next, const ProgramInstance* prev,
+                      const proto::action::ActionGraphT* prev_graph) {
+    
 }
 
-}}
+}  // namespace parser
+}  // namespace dashql
