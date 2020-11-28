fbdffb784088920bbcd0a4a99a7b252beaf4b417
Andre Kohn <kohn.a@outlook.com>
2020-11-28 21:29:58 +0100

    Work on action test encoder

--- core/cpp/src/test/action_test_encoder.cc
+++ core/cpp/src/test/action_test_encoder.cc
@@ -19,10 +19,50 @@
 namespace dashql {
 namespace parser {
 
-void EncodeActionTest(ryml::NodeRef root, const ProgramInstance& program, const proto::action::ActionGraphT* graph) {
-//    auto& tree = *root.tree();
-//    root |= ryml::MAP;
+void EncodeActionTest(ryml::NodeRef root, const ProgramInstance& program, const proto::action::ActionGraphT& graph) {
+    auto& tree = *root.tree();
+    root |= ryml::MAP;
 
+    auto setup_action_type_tt = proto::action::SetupActionTypeTypeTable();
+    auto program_action_type_tt = proto::action::ProgramActionTypeTypeTable();
+    auto action_status_tt = proto::action::ActionStatusTypeTable();
+
+    root["text"] << std::string{program.program_text()};
+
+    auto setup_actions = root["setup"];
+    setup_actions |= ryml::SEQ;
+    for (auto& action: graph.setup_actions) {
+        auto s = setup_actions.append_child();
+        s |= ryml::MAP;
+        s["action_type"] << c4::to_csubstr(setup_action_type_tt->names[static_cast<uint16_t>(action->action_type)]);
+        s["action_status"] << c4::to_csubstr(action_status_tt->names[static_cast<uint16_t>(action->action_status->status_code())]);
+        s["target_id"] << action->target_id;
+        s["target_name_qualified"] << action->target_name_qualified;
+        s["target_name_short"] << action->target_name_short;
+    }
+
+    auto program_actions = root["program"];
+    program_actions |= ryml::SEQ;
+    for (auto& action: graph.program_actions) {
+        auto p = program_actions.append_child();
+        p["action_type"] << c4::to_csubstr(program_action_type_tt->names[static_cast<uint16_t>(action->action_type)]);
+        p["action_status"] << c4::to_csubstr(action_status_tt->names[static_cast<uint16_t>(action->action_status->status_code())]);
+        auto depends_on = p["depends_on"];
+        depends_on |= ryml::SEQ;
+        for (auto v: action->depends_on) {
+            depends_on.append_child() << v;
+        }
+        auto required_for = p["required_for"];
+        required_for |= ryml::SEQ;
+        for (auto v: action->required_for) {
+            required_for.append_child() << v;
+        }
+        p["origin_statement"] << action->origin_statement;
+        p["target_id"] << action->target_id;
+        p["target_name_qualified"] << action->target_name_qualified;
+        p["target_name_short"] << action->target_name_short;
+        p["script"] << action->script;
+    }
 }
 
 }  // namespace parser
