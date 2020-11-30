66dc032bb40f7572bdcb53c5409484310a3522fd
Andre Kohn <kohn.a@outlook.com>
2020-11-30 23:26:55 +0100

    Add task list

---
+++ app/src/components/task_list.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import * as core from "@dashql/core";
+import { proto } from "@dashql/core";
+import styles from './task_list.module.css';
+
+interface Props {
+    className?: string
+    plan: core.Plan | null;
+}
+
+class TaskList extends React.Component<Props> {
+
+    public renderActions(plan: core.Plan)  {
+        let setup_actions: JSX.Element[] = [];
+        let program_actions: JSX.Element[] = [];
+        plan.iterateSetupActions((i: number, o: proto.action.SetupAction) => {
+            setup_actions.push(
+                <div key={i}>
+                    {o.targetNameShort}
+                </div>
+            );
+        });
+        plan.iterateProgramActions((i: number, o: proto.action.ProgramAction) => {
+            program_actions.push(
+                <div key={i}>
+                    {o.targetNameShort}
+                </div>
+            );
+        });
+        return (
+            <div className={styles.actions}>
+                <div className={styles.setup_actions}>
+                    {setup_actions}
+                </div>
+                <div className={styles.program_actions}>
+                    {program_actions}
+                </div>
+            </div>
+        );
+    }
+
+    public render() {
+        return (
+            <div className={styles.task_list}>
+                {this.props.plan && this.renderActions(this.props.plan)}
+            </div>
+        );
+    }
+
+    componentDidMount() {
+    }
+
+    componentDidUpdate(_prev: Readonly<Props>): void {
+    }
+
+}
+
+export default TaskList;
