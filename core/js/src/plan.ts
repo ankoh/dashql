fc321b45185e3b3d252b68757e4674786da792f9
Andre Kohn <kohn.a@outlook.com>
2020-11-30 23:26:55 +0100

    Add task list

--- core/js/src/plan.ts
+++ core/js/src/plan.ts
@@ -2,16 +2,16 @@
 
 import { FlatBuffer, PlanBuffer } from './bindings';
 import { Program } from './parser/';
-import { session } from './proto/';
+import * as proto from './proto/';
 
 export class Plan {
     /// The program
     _program: Program;
     /// The module
-    _plan: FlatBuffer<session.Plan>;
+    _plan: FlatBuffer<proto.session.Plan>;
 
     /// Constructor
-    public constructor(program: Program, plan: FlatBuffer<session.Plan> = new PlanBuffer()) {
+    public constructor(program: Program, plan: FlatBuffer<proto.session.Plan> = new PlanBuffer()) {
         this._program = program;
         this._plan = plan;
     }
@@ -20,4 +20,25 @@ export class Plan {
     public get program() { return this._program; }
     /// Access the action graph
     public get action_graph() { return this._plan.root.actionGraph(); }
+
+    /// Iterate setup actions
+    public iterateSetupActions(fn: (idx: number, node: proto.action.SetupAction) => void) {
+        const graph = this._plan.root.actionGraph();
+        if (!graph) return;
+        const count = graph.setupActionsLength();
+        const tmp = new proto.action.SetupAction();
+        for (let i = 0; i < count; ++i) {
+            fn(i, graph.setupActions(i, tmp)!);
+        }
+    }
+    /// Iterate program actions
+    public iterateProgramActions(fn: (idx: number, node: proto.action.ProgramAction) => void) {
+        const graph = this._plan.root.actionGraph();
+        if (!graph) return;
+        const count = graph.programActionsLength();
+        const tmp = new proto.action.ProgramAction();
+        for (let i = 0; i < count; ++i) {
+            fn(i, graph.programActions(i, tmp)!);
+        }
+    }
 }
