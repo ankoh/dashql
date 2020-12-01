058db3e650eb3bca25dea64d1e4a757adc7d9ec7
Andre Kohn <kohn.a@outlook.com>
2020-12-01 11:16:06 +0100

    Add redo icon

--- app/src/svg/icons.tsx
+++ app/src/svg/icons.tsx
@@ -700,6 +700,23 @@ export function UndoIcon(props: IIconProps) {
     );
 }
 
+export function RedoIcon(props: IIconProps) {
+    return (
+        <svg
+            className={props.className || 'icon'}
+            width={props.width || '24px'}
+            height={props.height || '24px'}
+            viewBox="0 0 24 24"
+        >
+            <path
+                d="M18.4 10.6C16.55 8.99 14.15 8 11.5 8c-4.65 0-8.58 3.03-9.96 7.22L3.9 16c1.05-3.19 4.05-5.5 7.6-5.5 1.95 0 3.73.72 5.12 1.88L13 16h9V7l-3.6 3.6z"
+                fill={props.fill || '#ffffff'}
+                fillRule="nonzero"
+            />
+        </svg>
+    );
+}
+
 export function AutoRunIcon(props: IIconProps) {
     return (
 
