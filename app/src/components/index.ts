f5d17a0a95306cd5188e7df3838e5f3ee90f097a
Andre Kohn <kohn.a@outlook.com>
2020-12-02 12:23:49 +0100

    Implement floating status bar

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,4 +1,5 @@
 export {NavBar, withNavBar} from "./navigation_bar";
+export {FloatingStatus, withFloatingStatus} from "./floating_status";
 import Board from "./board";
 import EditorLoader from "./editor_loader";
 import ProgramGraph from "./program_graph";
