d4e17c89764bb45cca042b63e8432941d18a6224
Andre Kohn <kohn.a@outlook.com>
2020-12-02 15:22:56 +0100

    Standalone board bar

---
+++ app/src/pages/studio_cmdbar_board.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import {
+    AspectRatioIcon,
+    IIconProps,
+    RulerIcon,
+} from '../svg/icons';
+
+import styles from './studio.module.css';
+
+class ActionProps {}
+
+function createAction(Icon: React.FunctionComponent<IIconProps>): React.FunctionComponent<IIconProps & ActionProps> {
+    return (props: IIconProps & ActionProps) => {
+        return (
+            <div className={styles.cmdbar_board_cmd}>
+                <Icon
+                    className={styles.cmdbar_board_icon}
+                    width={'20px'}
+                    height={'20px'}
+                    {...props}
+                />
+            </div>
+        );
+    };
+}
+
+const RulerAction = createAction(RulerIcon);
+const DeviceAction = createAction(AspectRatioIcon);
+
+export class BoardCommandBar extends React.Component<{}> {
+    public render() {
+        return (
+            <div className={styles.cmdbar_board}>
+                <div className={styles.cmdbar_board_cmdset} />
+                <div className={styles.cmdbar_board_cmdset}>
+                    <RulerAction />
+                    <DeviceAction />
+                </div>
+            </div>
+        );
+    }
+};
