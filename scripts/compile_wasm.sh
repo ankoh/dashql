37f72a5cdeb1a2d71bbc2136af02f8686917df0e
Andre Kohn <kohn.a@outlook.com>
2020-12-04 11:06:13 +0100

    Use explicit wasm-release folder for builds

--- scripts/compile_wasm.sh
+++ scripts/compile_wasm.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 
-CPP_BUILD_DIR="${PROJECT_ROOT}/core/cpp/build/emscripten"
+CPP_BUILD_DIR="${PROJECT_ROOT}/core/cpp/build/wasm-release"
 CPP_SOURCE_DIR="${PROJECT_ROOT}/core/cpp"
 CORE_JS_LIB_DIR="${PROJECT_ROOT}/core/js/src/wasm"
 DUCKDB_JS_LIB_DIR="${PROJECT_ROOT}/duckdb/js/src/wasm"
@@ -17,15 +17,15 @@ CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 
 emcmake cmake \
     -S/wd/core/cpp/ \
-    -B/wd/core/cpp/build/emscripten \
+    -B/wd/core/cpp/build/wasm-release \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -DCMAKE_BUILD_TYPE=Release
 
 emmake make \
-    -C/wd/core/cpp/build/emscripten \
+    -C/wd/core/cpp/build/wasm-release \
     -j${CORES} \
     dashql_core_web dashql_core_node duckdb_web duckdb_node
 
-cp ${CPP_SOURCE_DIR}/build/emscripten/dashql_*.{wasm,js} "${CORE_JS_LIB_DIR}"
-cp ${CPP_SOURCE_DIR}/build/emscripten/duckdb/duckdb_*.{wasm,js} "${DUCKDB_JS_LIB_DIR}"
+cp ${CPP_SOURCE_DIR}/build/wasm-release/dashql_*.{wasm,js} "${CORE_JS_LIB_DIR}"
+cp ${CPP_SOURCE_DIR}/build/wasm-release/duckdb/duckdb_*.{wasm,js} "${DUCKDB_JS_LIB_DIR}"
