3ca5706bc4284bac8b0b87130ca79d0a754f1ba8
Andre Kohn <kohn.a@outlook.com>
2020-12-06 21:59:18 +0100

    WIP CSV parser

--- core/cpp/src/extract/csv_extract.cc
+++ core/cpp/src/extract/csv_extract.cc
@@ -48,20 +48,20 @@ void CSVExtract::Initialize(vector<LogicalType> requested_types) {
     InitParseChunk(sql_types.size());
 }
 
-void CSVExtract::SkipRowsAndReadHeader(size_t skip_rows, bool skip_header) {
-    for (size_t i = 0; i < skip_rows; i++) {
-        // Ignore skip rows
-        string read_line;
-        getline(source, read_line);
-        linenr++;
-    }
-
-    if (skip_header) {
-        // Ignore the first line as a header line
-        InitParseChunk(sql_types.size());
-        ParseCSV(ParserMode::PARSING_HEADER);
-    }
-}
+// void CSVExtract::SkipRowsAndReadHeader(size_t skip_rows, bool skip_header) {
+//     for (size_t i = 0; i < skip_rows; i++) {
+//         // Ignore skip rows
+//         string read_line;
+//         getline(source, read_line);
+//         linenr++;
+//     }
+// 
+//     if (skip_header) {
+//         // Ignore the first line as a header line
+//         InitParseChunk(sql_types.size());
+//         ParseCSV(ParserMode::PARSING_HEADER);
+//     }
+// }
 
 void CSVExtract::ResetBuffer() {
     buffer.reset();
@@ -203,175 +203,6 @@ void CSVExtract::ParseCSV(ParserMode parser_mode, DataChunk &insert_chunk) {
     } else {
         ParseComplexCSV(insert_chunk);
     }
-}
-
-void CSVExtract::AddValue(char *str_val, size_t length, size_t &column, vector<size_t> &escape_positions) {
-    if (sql_types.size() > 0 && column == sql_types.size() && length == 0) {
-        // skip a single trailing delimiter in last column
-        return;
-    }
-    if (mode == ParserMode::SNIFFING_DIALECT) {
-        column++;
-        return;
-    }
-    if (column >= sql_types.size()) {
-        throw InvalidInputException("Error on line %s: expected %lld values per row, but got more. (%s)",
-                                    GetLineNumberStr(linenr, linenr_estimated).c_str(), sql_types.size(),
-                                    options.toString());
-    }
-
-    // insert the line number into the chunk
-    size_t row_entry = parse_chunk.size();
-
-    str_val[length] = '\0';
-
-    // test against null string
-    if (!options.force_not_null[column] && strcmp(options.null_str.c_str(), str_val) == 0) {
-        FlatVector::SetNull(parse_chunk.data[column], row_entry, true);
-    } else {
-        auto &v = parse_chunk.data[column];
-        auto parse_data = FlatVector::GetData<string_t>(v);
-        if (escape_positions.size() > 0) {
-            // remove escape characters (if any)
-            string old_val = str_val;
-            string new_val = "";
-            size_t prev_pos = 0;
-            for (size_t i = 0; i < escape_positions.size(); i++) {
-                size_t next_pos = escape_positions[i];
-                new_val += old_val.substr(prev_pos, next_pos - prev_pos);
-
-                if (options.escape.size() == 0 || options.escape == options.quote) {
-                    prev_pos = next_pos + options.quote.size();
-                } else {
-                    prev_pos = next_pos + options.escape.size();
-                }
-            }
-            new_val += old_val.substr(prev_pos, old_val.size() - prev_pos);
-            escape_positions.clear();
-            parse_data[row_entry] = StringVector::AddStringOrBlob(v, string_t(new_val));
-        } else {
-            parse_data[row_entry] = string_t(str_val, length);
-        }
-    }
-
-    // move to the next column
-    column++;
-}
-
-bool CSVExtract::AddRow(DataChunk &insert_chunk, size_t &column) {
-    linenr++;
-
-    if (column < sql_types.size() && mode != ParserMode::SNIFFING_DIALECT) {
-        throw InvalidInputException("Error on line %s: expected %lld values per row, but got %d. (%s)",
-                                    GetLineNumberStr(linenr, linenr_estimated).c_str(), sql_types.size(), column,
-                                    options.toString());
-    }
-
-    if (mode == ParserMode::SNIFFING_DIALECT) {
-        sniffed_column_counts.push_back(column);
-
-        if (sniffed_column_counts.size() == options.sample_chunk_size) {
-            return true;
-        }
-    } else {
-        parse_chunk.SetCardinality(parse_chunk.size() + 1);
-    }
-
-    if (mode == ParserMode::PARSING_HEADER) {
-        return true;
-    }
-
-    if (mode == ParserMode::SNIFFING_DATATYPES && parse_chunk.size() == options.sample_chunk_size) {
-        return true;
-    }
-
-    if (mode == ParserMode::PARSING && parse_chunk.size() == STANDARD_VECTOR_SIZE) {
-        Flush(insert_chunk);
-        return true;
-    }
-
-    column = 0;
-    return false;
-}
-
-void CSVExtract::Flush(DataChunk &insert_chunk) {
-    if (parse_chunk.size() == 0) {
-        return;
-    }
-
-    // Convert the columns in the parsed chunk to the types of the table
-    insert_chunk.SetCardinality(parse_chunk);
-    for (size_t col_idx = 0; col_idx < sql_types.size(); col_idx++) {
-
-        if (sql_types[col_idx].id() == LogicalTypeId::VARCHAR) {
-            // Target type is VARCHAR: no need to convert
-            // Just test that all strings are valid utf-8 strings
-            auto parse_data = FlatVector::GetData<string_t>(parse_chunk.data[col_idx]);
-            for (size_t i = 0; i < parse_chunk.size(); i++) {
-                if (!FlatVector::IsNull(parse_chunk.data[col_idx], i)) {
-                    auto s = parse_data[i];
-                    auto utf_type = Utf8Proc::Analyze(s.GetDataUnsafe(), s.GetSize());
-                    if (utf_type == UnicodeType::INVALID) {
-                        string col_name = std::to_string(col_idx);
-                        if (col_idx < col_names.size()) {
-                            col_name = "\"" + col_names[col_idx] + "\"";
-                        }
-                        throw InvalidInputException("Error in blob \"%s\" between line %llu and %llu in column \"%s\": "
-                                                    "file is not valid UTF8. Parser options: %s",
-                                                    options.blob_id, linenr - parse_chunk.size(), linenr, col_name,
-                                                    options.toString());
-                    }
-                }
-            }
-            insert_chunk.data[col_idx].Reference(parse_chunk.data[col_idx]);
-        } else {
-            // Target type is not a VARCHAR so we need to convert
-            try {
-                // Need explicit DATE conversion?
-                if (options.has_format[LogicalTypeId::DATE] && sql_types[col_idx].id() == LogicalTypeId::DATE) {
-                    // use the date format to cast the chunk
-                    UnaryExecutor::Execute<string_t, date_t, true>(
-                        parse_chunk.data[col_idx], insert_chunk.data[col_idx], parse_chunk.size(),
-                        [&](string_t input) { return options.date_format[LogicalTypeId::DATE].ParseDate(input); });
-                }
-
-                // Need explicit TIMESTAMP conversion?
-                else if (options.has_format[LogicalTypeId::TIMESTAMP] &&
-                           sql_types[col_idx].id() == LogicalTypeId::TIMESTAMP) {
-                    // use the date format to cast the chunk
-                    UnaryExecutor::Execute<string_t, timestamp_t, true>(
-                        parse_chunk.data[col_idx], insert_chunk.data[col_idx], parse_chunk.size(), [&](string_t input) {
-                            return options.date_format[LogicalTypeId::TIMESTAMP].ParseTimestamp(input);
-                        });
-                }
-
-                // Cast the value
-                else {
-                    // target type is not varchar: perform a cast
-                    VectorOperations::Cast(parse_chunk.data[col_idx], insert_chunk.data[col_idx], parse_chunk.size());
-                }
-            } catch (const Exception &e) {
-                string col_name = std::to_string(col_idx);
-                if (col_idx < col_names.size()) {
-                    col_name = "\"" + col_names[col_idx] + "\"";
-                }
-
-                if (options.auto_detect) {
-                    throw InvalidInputException(
-                        "%s in column %s, between line %llu and %llu."
-                        " Parser options: %s."
-                        "Consider either increasing the sample size (using SAMPLE_SIZE=X) "
-                        "or skipping column conversion (ALL_VARCHAR=1)",
-                        e.what(), col_name, linenr - parse_chunk.size() + 1, linenr, options.toString());
-                } else {
-                    throw InvalidInputException("%s between line %llu and %llu in column %s. Parser options: %s ",
-                                                e.what(), linenr - parse_chunk.size(), linenr, col_name,
-                                                options.toString());
-                }
-            }
-        }
-    }
-    parse_chunk.Reset();
 }
 
 } // namespace duckdb
