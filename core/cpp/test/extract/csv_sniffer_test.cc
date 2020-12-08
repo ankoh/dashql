058d35a20d061e53505f0549b31ba86b2ea2fe26
Andre Kohn <kohn.a@outlook.com>
2020-12-08 17:02:55 +0100

    Work on csv sniffer tests

--- core/cpp/test/extract/csv_sniffer_test.cc
+++ core/cpp/test/extract/csv_sniffer_test.cc
@@ -15,6 +15,37 @@ namespace sx = dashql::proto::syntax;
 
 namespace {
 
-TEST(CSVSniffer, DialectDetection) {}
+class CSVSnifferProxy : public CSVSniffer {
+   public:
+    /// Constructor
+    CSVSnifferProxy(const CSVParserOptions& user_options, CachingBlobStreamBuffer&& streambuf)
+        : CSVSniffer(user_options, move(streambuf)) {}
+
+    using CSVSniffer::DetectTypes;
+};
+
+struct DetectionTest {
+    std::string input;
+    std::vector<CSVSniffer::Dialect> candidates;
+    friend std::ostream& operator<<(std::ostream& out, const DetectionTest& param) {
+        out << param.input;
+        return out;
+    }
+};
+class CSVDialectDetectionTestSuite : public ::testing::TestWithParam<DetectionTest> {};
+
+TEST_P(CSVDialectDetectionTestSuite, CandidatesMatch) {
+    auto& param = GetParam();
+    auto blob_id = test::Blob::Register(test::Blob{param.input});
+    CachingBlobStreamBuffer blob_streambuf(test::Blob::StreamUnderflow, blob_id);
+    std::istream blob_stream{&blob_streambuf};
+
+    CSVParserOptions presets;
+    CSVSnifferProxy sniffer{presets, move(blob_streambuf)};
+    sniffer.DetectTypes();
+}
+
+INSTANTIATE_TEST_SUITE_P(CSVSniffer, CSVDialectDetectionTestSuite,
+                         ::testing::Values(DetectionTest{"1,2,3\n4,5,6\n7,8,9", {{"", ",", ""}}}));
 
 }  // namespace
