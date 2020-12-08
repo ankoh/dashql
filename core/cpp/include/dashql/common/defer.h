57b6382f14aa23b9bcda10e913094ab06d3ca6c8
Andre Kohn <kohn.a@outlook.com>
2020-12-08 11:55:33 +0100

    Share the buffer memory during dialect detection

---
+++ core/cpp/include/dashql/common/defer.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2020 The DashQL Authors
+
+#ifndef INCLUDE_DASHQL_COMMON_DEFER_H_
+#define INCLUDE_DASHQL_COMMON_DEFER_H_
+
+#include <utility>
+
+namespace dashql {
+
+template <typename Fn, typename = std::is_nothrow_invocable<Fn>>
+class DeferredFunctionCall {
+   private:
+   /// The function that is deferred
+   Fn func;
+
+   public:
+   /// Constructor
+   explicit DeferredFunctionCall(Fn&& f) : func(std::forward<Fn>(f)) {}
+   /// Destructor
+   ~DeferredFunctionCall() {
+      func();
+   }
+};
+
+template <typename Function, typename = std::is_nothrow_invocable<Function>>
+inline DeferredFunctionCall<Function> defer(Function&& f) noexcept {
+    return DeferredFunctionCall<Function>(std::forward<Function>(f));
+}
+
+}
+
+#endif // INCLUDE_DASHQL_COMMON_DEFER_H_
