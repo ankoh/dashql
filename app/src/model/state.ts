b6114de65cc604fff076f8f9671c370e0fad6e82
Andre Kohn <kohn.a@outlook.com>
2020-12-10 22:40:38 +0100

    Remove tasks since we have actions now

--- app/src/model/state.ts
+++ app/src/model/state.ts
@@ -1,6 +1,4 @@
-import * as Immutable from "immutable";
 import * as core from "@dashql/core";
-import { TaskID, TaskInfo } from "./task";
 import { LaunchProgress } from "./launch_progress";
 import { AppSettings } from "./settings";
 
@@ -11,15 +9,12 @@ export class AppState {
     public launchProgress: LaunchProgress;
     // The app config
     public appSettings: AppSettings | null;
-    // The tasks
-    public tasks: Immutable.Map<number, TaskInfo>;
 
     /// Constructor
     constructor() {
         this.core = new core.model.State();
         this.launchProgress = new LaunchProgress();
         this.appSettings = null;
-        this.tasks = Immutable.Map<TaskID, TaskInfo>();
     }
 }
 
