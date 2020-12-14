4ed357da6da3db2503af66f74b67ee776bfa84a4
Andre Kohn <kohn.a@outlook.com>
2020-12-14 21:23:48 +0100

    Work on action scheduler

--- core/js/src/action_scheduler.ts
+++ core/js/src/action_scheduler.ts
@@ -1,6 +1,6 @@
 import * as proto from "@dashql/proto";
 import { TopologicalSort, TopoKey, TopoRank } from "./utils";
-import { SetupAction, ProgramAction } from "./actions";
+import { SetupAction, ProgramAction, translateSetupAction, translateProgramAction } from "./actions";
 import { Program } from './model';
 
 export class ActionScheduler {
@@ -20,22 +20,25 @@ export class ActionScheduler {
         // Translate the setup actions
         this._setup_actions = [];
         for (let i = 0; i < action_graph.setupActionsLength(); ++i) {
-            const _a = action_graph.setupActions(i);
-            this._setup_actions.push(new SetupAction());
+            const a = action_graph.setupActions(i)!;
+            this._setup_actions.push(translateSetupAction(a));
         }
 
         // Translate the program actions
+        this._program_actions = [];
+        for (let i = 0; i < action_graph.programActionsLength(); ++i) {
+            const a = action_graph.programActions(i)!;
+            this._program_actions.push(translateProgramAction(this._program, a));
+        }
+
+        // Build the dependency heap
         let program_deps: [TopoKey, TopoRank][] = [];
         this._program_actions = [];
         for (let i = 0; i < action_graph.programActionsLength(); ++i) {
             const a = action_graph.programActions(i)!;
-            const s = program.getStatement(a.originStatement());
-            const p = new ProgramAction(s);
-            this._program_actions.push(p);
             program_deps.push([i, a.dependsOnLength()]);
-        }
 
-        // Collect dependencies
+        }
         this._pending_program_actions = new TopologicalSort(program_deps);
     }
 };
