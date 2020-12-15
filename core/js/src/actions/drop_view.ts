56faf452613f1f7071b17c694a39971b9fcef76f
Andre Kohn <kohn.a@outlook.com>
2020-12-15 18:50:30 +0100

    Construct actions

--- core/js/src/actions/drop_view.ts
+++ core/js/src/actions/drop_view.ts
@@ -1,8 +1,9 @@
+import * as proto from "@dashql/proto";
 import { Program } from "../model";
 import { SetupAction } from "./action";
 
 export class DropViewAction extends SetupAction {
-    constructor(program: Program) {
-        super(program);
+    constructor(action: proto.action.SetupAction, program: Program) {
+        super(action, program);
     }
 }
