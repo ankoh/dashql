b4f49c8b756aa838c1097f67dfd9740d1fd949fa
Andre Kohn <kohn.a@outlook.com>
2020-12-17 14:07:12 +0100

    Work on state serialization

--- core/js/src/platform/platform.ts
+++ core/js/src/platform/platform.ts
@@ -1,4 +1,4 @@
-import { BlobManager } from "./blob_manager";
+import { CacheManager } from "./cache_manager";
 import { CoreWasmAPI } from "../wasm/api";
 import { DatabaseManager } from "./database_manager";
 import { DerivedReduxStore } from "../model";
@@ -7,31 +7,31 @@ import { HTTPManager } from "./http_manager";
 
 export class Platform {
     /// The core wasm api
-    _core_wasm_api: CoreWasmAPI;
+    _coreWasm: CoreWasmAPI;
     /// The global application state
-    _global_state: DerivedReduxStore;
+    _globalState: DerivedReduxStore;
     /// The cache manager
-    _blob_manager: BlobManager;
+    _cacheManager: CacheManager;
     /// The database manager
-    _database_manager: DatabaseManager;
+    _databaseManager: DatabaseManager;
     /// The file manager
-    _file_manager: FileManager;
+    _fileManager: FileManager;
     /// The HTTP manager
-    _http_manager: HTTPManager;
+    _httpManager: HTTPManager;
 
-    constructor(core_wasm_api: CoreWasmAPI, global_state: DerivedReduxStore, blob_manager: BlobManager, database_manager: DatabaseManager, file_manager: FileManager, http_manager: HTTPManager) {
-        this._core_wasm_api = core_wasm_api;
-        this._global_state = global_state;
-        this._blob_manager = blob_manager;
-        this._database_manager = database_manager;
-        this._file_manager = file_manager;
-        this._http_manager = http_manager;
+    constructor(coreWasm: CoreWasmAPI, globalState: DerivedReduxStore, cacheManager: CacheManager, databaseManager: DatabaseManager, fileManager: FileManager, httpManager: HTTPManager) {
+        this._coreWasm = coreWasm;
+        this._globalState = globalState;
+        this._cacheManager = cacheManager;
+        this._databaseManager = databaseManager;
+        this._fileManager = fileManager;
+        this._httpManager = httpManager;
     }
 
-    public get core_wasm() { return this._core_wasm_api; }
-    public get state() { return this._global_state; }
-    public get blobs() { return this._blob_manager; }
-    public get database() { return this._database_manager; }
-    public get file() { return this._file_manager; }
-    public get http() { return this._http_manager; }
+    public get coreWasm() { return this._coreWasm; }
+    public get state() { return this._globalState; }
+    public get cache() { return this._cacheManager; }
+    public get database() { return this._databaseManager; }
+    public get file() { return this._fileManager; }
+    public get http() { return this._httpManager; }
 }
