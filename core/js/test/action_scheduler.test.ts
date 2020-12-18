19083af632f63eb8fc2535e1468e17d5a66d7156
Andre Kohn <kohn.a@outlook.com>
2020-12-18 18:04:40 +0100

    Test core against the unbundled source files

--- core/js/test/action_scheduler.test.ts
+++ core/js/test/action_scheduler.test.ts
@@ -1,18 +1,19 @@
-import { DashQLCoreWasm } from '../';
+import { DashQLCoreWasm, CORE_WASM_RUNTIME_STUBS } from '../src/index_node';
+import * as path from 'path';
 
 var core: DashQLCoreWasm;
 
 beforeAll(async () => {
-    core = new DashQLCoreWasm();
+    core = new DashQLCoreWasm(CORE_WASM_RUNTIME_STUBS, path.resolve(__dirname, "../src/wasm/core_wasm_node.wasm"));
     await core.init();
 });
 
 describe('Action Planner', () => {
    describe('planning', () => {
        test('select 1', async () => {
-           const program = core.parseProgram("select 1");
+           const _program = core.parseProgram("select 1");
            const plan = core.planProgram();
-           const action_graph = plan!.action_graph;
+           const action_graph = plan!.action_graph!;
 
            expect(action_graph.setupActionsLength()).toBe(0);
            expect(action_graph.programActionsLength()).toBe(1);
--- core/js/test/wasm.test.ts
+++ core/js/test/wasm.test.ts
@@ -1,9 +1,10 @@
-import { DashQLCoreWasm } from '../';
+import { DashQLCoreWasm, CORE_WASM_RUNTIME_STUBS } from '../src/index_node';
+import * as path from 'path';
 
 var core: DashQLCoreWasm;
 
 beforeAll(async () => {
-    core = new DashQLCoreWasm();
+    core = new DashQLCoreWasm(CORE_WASM_RUNTIME_STUBS, path.resolve(__dirname, "../src/wasm/core_wasm_node.wasm"));
     await core.init();
 });
 
