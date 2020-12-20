c188c60a2bac0763118c1663788836e6abd83826
Andre Kohn <kohn.a@outlook.com>
2020-12-21 00:28:20 +0100

    Work on platform

--- core/src/platform/file_manager.ts
+++ core/src/platform/file_manager.ts
@@ -1,3 +1,45 @@
-export class FileManager {
+import { DerivedReduxStore } from "../model";
+import { LRUCache } from "../utils/lru_cache";
+import { ObjectURL } from "../model";
 
+const REQUEST_FILE_SIZE = 64;
+
+export interface FileData {
+    /// The cache key
+    key: string;
+    /// The object URL
+    objectURL: ObjectURL;
+}
+
+/// A file cache
+class FileCache extends LRUCache<FileData> {
+    /// Constructor
+    constructor(size: number) {
+        super(size);
+    }
+
+    /// Update handler
+    onEvict(_slot: number, _next: FileData, _evicted: FileData | null): void {
+        // XXX
+    }
+}
+
+export class FileManager {
+    /// The global application state
+    _state: DerivedReduxStore;
+    /// The file cache
+    _file_cache: FileCache;
+
+    /// Constructor
+    constructor(state: DerivedReduxStore) {
+        this._state = state;
+        this._file_cache = new FileCache(REQUEST_FILE_SIZE);
+    }
+
+    public findFile(_key: string): File | null {
+        return null;
+    }
+
+    public addFile(_key: string, _file: File) {
+    }
 }
--- core/src/utils/lru_cache.ts
+++ core/src/utils/lru_cache.ts
@@ -1,4 +1,4 @@
-import { NativeBitmap, NativeMinHeap } from "../utils";
+import { NativeMinHeap } from "../utils";
 
 /// A LRU cache entry
 export interface LRUCacheEntry {
@@ -10,49 +10,46 @@ export interface LRUCacheEntry {
 export abstract class LRUCache<Value extends LRUCacheEntry> {
     /// The slots
     _slots: (Value | null)[];
-    /// The slot mask
-    _slot_mask: NativeBitmap;
+    /// The slot mapping
+    _slotMapping: Map<string, number>;
     /// The LRU queue
-    _lru_queue: NativeMinHeap;
+    _lruQueue: NativeMinHeap;
 
     constructor(size: number) {
         this._slots = [];
         this._slots.length += size;
         this._slots.fill(null, 0, size);
-        this._slot_mask = new NativeBitmap(size);
-        this._lru_queue = new NativeMinHeap();
-        this._lru_queue.buildDefault(size);
+        this._slotMapping = new Map();
+        this._lruQueue = new NativeMinHeap();
+        this._lruQueue.buildDefault(size);
     }
 
     /// Update handler
     protected abstract onEvict(slot: number, newEntry: Value, evictedEntry: Value | null): void;
     /// Use a slot?
     protected use(slot: number) {
-        this._lru_queue.setRank(slot, new Date().getTime());
+        this._lruQueue.setRank(slot, new Date().getTime());
     }
 
     /// Find an entry
     public find(k: string): Value | null {
-        for (let i = 0; i < this._slots.length; ++i) {
-            const entry = this._slots[i];
-            if (!entry) continue;
-            if (entry.key == k) {
-                this.use(i);
-                return entry;
-            }
-        }
-        return null;
+        const i = this._slotMapping.get(k);
+        if (!i) return null;
+        this.use(i);
+        return this._slots[i];
     }
 
     /// Insert an entry
     public insert(v: Value): Value {
-        const i = this._lru_queue.top();
-        if (this._slot_mask.isSet(i)) {
-            const prev = this._slots[i]!;
-            this.onEvict(i, v, prev);
-            this._slots[i] = v;
-            this.use(i);
+        const i = this._lruQueue.top();
+        const e = this._slots[i];
+        if (e) {
+            this._slotMapping.delete(e.key)
         }
+        this.onEvict(i, v, e);
+        this._slots[i] = v;
+        this._slotMapping.set(v.key, i);
+        this.use(i);
         return v;
     }
 }
