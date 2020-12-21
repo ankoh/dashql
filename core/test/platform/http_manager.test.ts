eb4e022c621579fd4f5bf50544cb84efc44b7aab
Andre Kohn <kohn.a@outlook.com>
2020-12-21 13:49:15 +0100

    Test 404 error

--- core/test/platform/http_manager.test.ts
+++ core/test/platform/http_manager.test.ts
@@ -1,5 +1,6 @@
 import { platform } from '../../src/index_node';
 import { mockHTTP, HTTPMock, encodeTextBody, decodeTextBody } from '../mocks/http_mock';
+import { AxiosError } from 'axios';
 
 let httpMock: HTTPMock;
 
@@ -15,15 +16,34 @@ describe('HTTPManager', () => {
     test('init', () => {});
 
     test('fetch', async () => {
-        httpMock.onAny().reply(200, encodeTextBody("foo"));
-
         const http = new platform.HTTPManager();
         await http.init();
-        const r = await http.request({
+
+        // Initial request
+        httpMock.onAny().reply(200, encodeTextBody("foo"));
+        let r = await http.request({
             url: "http://localhost/test1",
             method: "GET",
         });
-        expect(r.response.data.byteLength).toBeGreaterThan(0);
         expect(decodeTextBody(new Uint8Array(r.response.data))).toBe("foo");
+
+        // A different request must fail
+        httpMock.reset();
+        httpMock.onAny().reply(404);
+        expect(async () =>
+            await http.request({
+                url: "http://localhost/test2",
+                method: "GET",
+            })
+        ).rejects.toThrow(new Error("Request failed with status code 404"));
+
+        // // Make sure an identical request hits the cache
+        // httpMock.reset();
+        // httpMock.onAny().reply(404);
+        // r = await http.request({
+        //     url: "http://localhost/test1",
+        //     method: "GET",
+        // });
+        // expect(decodeTextBody(new Uint8Array(r.response.data))).toBe("foo");
     });
 });
