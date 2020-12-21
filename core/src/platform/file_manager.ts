77226c05dd84967e5f333435be22f3d6e7a99c3b
Andre Kohn <kohn.a@outlook.com>
2020-12-21 17:30:37 +0100

    Add rudimentary file cache

--- core/src/platform/file_manager.ts
+++ core/src/platform/file_manager.ts
@@ -1,8 +1,8 @@
-import { DerivedReduxStore } from '../model';
 import { LRUCache } from '../utils/lru_cache';
 import { ObjectURL } from '../model';
+import { DerivedReduxStore, StateMutationType } from '../model';
 
-const REQUEST_FILE_SIZE = 64;
+const FILE_CACHE_SIZE = 64;
 
 export interface FileData {
     /// The cache key
@@ -13,35 +13,54 @@ export interface FileData {
 
 /// A file cache
 class FileCache extends LRUCache<FileData> {
+    /// The redux store
+    _store: DerivedReduxStore;
     /// Constructor
-    constructor(size: number) {
+    constructor(store: DerivedReduxStore, size: number) {
         super(size);
+        this._store = store;
     }
 
     /// Insert handler
-    onInsert(_slot: number, _next: FileData, _evicted: FileData | null): void {
+    onInsert(_slot: number, next: FileData, evicted: FileData | null): void {
+        this._store.dispatch({
+            type: StateMutationType.CACHE_FILE_DATA,
+            data: [next.objectURL, evicted?.key]
+        });
     }
 
     /// Hit handler
-    onHit(_slot: number, _next: FileData): void {
+    onHit(_slot: number, next: FileData): void {
+        this._store.dispatch({
+            type: StateMutationType.HIT_CACHED_FILE_DATA,
+            data: next.key
+        });
     }
 }
 
 export class FileManager {
-    /// The global application state
-    _state: DerivedReduxStore;
+    /// The global application store
+    _store: DerivedReduxStore;
     /// The file cache
     _file_cache: FileCache;
 
     /// Constructor
-    constructor(state: DerivedReduxStore) {
-        this._state = state;
-        this._file_cache = new FileCache(REQUEST_FILE_SIZE);
+    constructor(store: DerivedReduxStore, cache_size: number = FILE_CACHE_SIZE) {
+        this._store = store;
+        this._file_cache = new FileCache(store, cache_size);
     }
 
-    public findFile(_key: string): File | null {
-        return null;
+    /// Find a file
+    public findFile(key: string): ObjectURL | null {
+        const cached = this._file_cache.find(key);
+        return cached?.objectURL || null;
     }
 
-    public addFile(_key: string, _file: File) {}
+    /// Add a file
+    public addFile(key: string, file: File): ObjectURL {
+        return this._file_cache.insert({
+            key: key,
+            objectURL: URL.createObjectURL(file) as string
+        }).objectURL;
+    }
 }
