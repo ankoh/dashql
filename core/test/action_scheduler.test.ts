33321a3749183af9da82dff273c8e882c7190a9f
Andre Kohn <kohn.a@outlook.com>
2020-12-21 21:57:15 +0100

    Test action scheduler

--- core/test/action_scheduler.test.ts
+++ core/test/action_scheduler.test.ts
@@ -1,7 +1,6 @@
 import { DashQLCoreWasm, CORE_WASM_RUNTIME_STUBS, model, actions, ActionScheduler, utils } from '../src/index_node';
 import * as path from 'path';
 import * as proto from '@dashql/proto';
-import { flatbuffers } from 'flatbuffers';
 import { PlatformMock } from './mocks';
 
 var core: DashQLCoreWasm;
@@ -10,6 +9,15 @@ beforeAll(async () => {
     core = new DashQLCoreWasm(CORE_WASM_RUNTIME_STUBS, path.resolve(__dirname, "../src/wasm/core_wasm_node.wasm"));
     await core.init();
 });
+
+function resolveProgramActionLogic(plan: model.Plan) {
+    return plan.mapProgramActions((i, a) => {
+        const aid = model.buildActionID(i, model.ActionClass.ProgramAction);
+        const stmtId = a.originStatement();
+        const stmt = plan.program.getStatement(stmtId);
+        return actions.resolveProgramActionLogic(aid, a, stmt)!;
+    });
+}
 
 describe('Action Scheduler', () => {
    describe('setup actions', () => {
@@ -21,25 +29,15 @@ describe('Action Scheduler', () => {
             const platform = platformMock.getInstance();
 
             const program = core.parseProgram("select 1");
-            expect(program.buffer.statementsLength()).toBe(1);
             const plan = core.planProgram();
             const graph = plan!.buffer.actionGraph()!;
+            expect(program.buffer.statementsLength()).toBe(1);
             expect(graph.setupActionsLength()).toBe(0);
             expect(graph.programActionsLength()).toBe(1);
 
-            let logic = plan!.mapProgramActions((i, a) => {
-                const aid = model.buildActionID(i, model.ActionClass.ProgramAction);
-                const stmtId = a.originStatement();
-                const stmt = program.getStatement(stmtId);
-                return actions.resolveProgramActionLogic(aid, a, stmt)!;
-            });
-
-            let interruptFunc: () => void;
-            let interruptPromise = new Promise((resolve: (value: any) => void, _reject: (reason?: void) => void) => {
-                interruptFunc = () => resolve(null);
-            });
-
-            let scheduler = new ActionScheduler<proto.action.ProgramAction>(interruptPromise);
+            let logic = resolveProgramActionLogic(plan!);
+            let interrupt = new Promise((_resolve: (value: any) => void, _reject: (reason?: void) => void) => {});
+            let scheduler = new ActionScheduler<proto.action.ProgramAction>(interrupt);
             scheduler.reset(logic);
 
             let diff = new utils.NativeStack();
