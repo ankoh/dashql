dc47e473e79a1eb5334ea5239b383d4220b4f593
Andre Kohn <kohn.a@outlook.com>
2020-12-21 21:40:14 +0100

    Test action scheduler

--- core/test/action_scheduler.test.ts
+++ core/test/action_scheduler.test.ts
@@ -1,6 +1,7 @@
-import { DashQLCoreWasm, CORE_WASM_RUNTIME_STUBS, model } from '../src/index_node';
+import { DashQLCoreWasm, CORE_WASM_RUNTIME_STUBS, model, actions, ActionScheduler, utils } from '../src/index_node';
 import * as path from 'path';
 import * as proto from '@dashql/proto';
+import { flatbuffers } from 'flatbuffers';
 import { PlatformMock } from './mocks';
 
 var core: DashQLCoreWasm;
@@ -15,11 +16,35 @@ describe('Action Scheduler', () => {
    });
 
    describe('program actions', () => {
-
-        test('hello world', async () => {
+        test('hello scheduler', async () => {
             const platformMock = new PlatformMock();
             const platform = platformMock.getInstance();
 
+            const program = core.parseProgram("select 1");
+            expect(program.buffer.statementsLength()).toBe(1);
+            const plan = core.planProgram();
+            const graph = plan!.buffer.actionGraph()!;
+            expect(graph.setupActionsLength()).toBe(0);
+            expect(graph.programActionsLength()).toBe(1);
+
+            let logic = plan!.mapProgramActions((i, a) => {
+                const aid = model.buildActionID(i, model.ActionClass.ProgramAction);
+                const stmtId = a.originStatement();
+                const stmt = program.getStatement(stmtId);
+                return actions.resolveProgramActionLogic(aid, a, stmt)!;
+            });
+
+            let interruptFunc: () => void;
+            let interruptPromise = new Promise((resolve: (value: any) => void, _reject: (reason?: void) => void) => {
+                interruptFunc = () => resolve(null);
+            });
+
+            let scheduler = new ActionScheduler<proto.action.ProgramAction>(interruptPromise);
+            scheduler.reset(logic);
+
+            let diff = new utils.NativeStack();
+            let ctx = new actions.ActionContext(platform, plan!);
+            await scheduler.execute(ctx, diff);
         });
    });
 });
