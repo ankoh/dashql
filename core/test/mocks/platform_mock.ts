2fcd05dfa58345b805db79cebeb0935085175871
Andre Kohn <kohn.a@outlook.com>
2020-12-21 18:11:59 +0100

    Work on action scheduler

--- core/test/mocks/platform_mock.ts
+++ core/test/mocks/platform_mock.ts
@@ -3,31 +3,31 @@ import { mock, instance } from 'ts-mockito';
 
 export class PlatformMock {
     /// The global application state
-    public state: model.DerivedReduxStore;
+    public readonly state: model.DerivedReduxStore;
     /// The core wasm api
-    public coreWasm: platform.CoreWasmAPI;
+    public readonly coreWasm: platform.CoreWasmAPI;
     /// The database manager
-    public databaseManager: platform.DatabaseManager;
+    public readonly databaseManager: platform.DatabaseManager;
     /// The file manager
-    public fileManager: platform.FileManager;
+    public readonly fileManager: platform.FileManager;
     /// The HTTP manager
-    public httpManager: platform.HTTPManager;
+    public readonly httpManager: platform.HTTPManager;
 
-    constructor(store: model.DerivedReduxStore) {
+    constructor(store: model.DerivedReduxStore = model.createStore()) {
         this.state = store;
         this.coreWasm = mock({} as platform.CoreWasmAPI);
-        this.databaseManager = mock(platform.DatabaseManager);
-        this.fileManager = mock(platform.FileManager);
-        this.httpManager = mock(platform.HTTPManager);
+        this.databaseManager = new platform.DatabaseManager();
+        this.fileManager = new platform.FileManager(store);
+        this.httpManager = new platform.HTTPManager(store);
     }
 
     getInstance(): platform.Platform {
         return new platform.Platform(
             this.state,
             instance(this.coreWasm),
-            instance(this.databaseManager),
-            instance(this.fileManager),
-            instance(this.httpManager)
+            this.databaseManager,
+            this.fileManager,
+            this.httpManager
         );
     }
 }
