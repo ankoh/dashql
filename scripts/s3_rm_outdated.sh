d66381e2499764cf3229e8f8b54ccf739686ad2e
Andre Kohn <kohn.a@outlook.com>
2020-12-23 15:17:36 +0100

    Cleaup

--- scripts/s3_rm_outdated.sh
+++ scripts/s3_rm_outdated.sh
@@ -19,9 +19,8 @@ aws s3 ls --recursive "$1" | while read -r LINE;
         FILE_TIMESTAMP=$(echo $LINE|awk {'print $1" "$2'})
         FILE_TIMESTAMP=$(${DATE} -d"$FILE_TIMESTAMP" "+%s")
         OLDER_THAN=$(${DATE} -d "$2 ago" "+%s")
-        if [[ $FILE_TIMESTAMP -le $OLDER_THAN ]];
-            then
-            FILE_NAME=`echo $LINE|awk {'print $4'}`
+        if [[ $FILE_TIMESTAMP -le $OLDER_THAN ]]; then
+            FILE_NAME=$(echo $LINE|awk {'print $4'})
             if [ $FILE_NAME != "" ]; then
                 aws s3 rm "$1/$FILE_NAME"
             fi
