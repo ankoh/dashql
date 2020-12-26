3016f2ce76ba38402679d822f1cfad1d55ec7057
Andre Kohn <kohn.a@outlook.com>
2020-12-26 22:50:36 +0100

    Test schema mismatch

--- core/test/model/syntax_schema.test.ts
+++ core/test/model/syntax_schema.test.ts
@@ -43,4 +43,50 @@ describe('Statement schema', () => {
         expect(url.matching).toEqual(schema.Matching.MATCHED);
         expect(url.value).toEqual("'https://localhost/test'");
     });
+
+    test('load url type mismatch', async () => {
+        const program = core.parseProgram(`
+            LOAD weather_csv FROM http (
+                url = 'https://localhost/test'
+            );
+        `);
+        expect(program.buffer.errorsLength()).toEqual(0);
+        expect(program.buffer.statementsLength()).toEqual(1);
+        const stmt = program.getStatement(0);
+
+        const method = schema.enumNode(sx.NodeType.ENUM_DASHQL_LOAD_METHOD_TYPE, 0);
+        const url = schema.numberNode();
+        stmt.matchSchema(schema.objectNode(sx.NodeType.OBJECT_DASHQL_LOAD, {
+            [Key.DASHQL_LOAD_METHOD]: method,
+            [Key.DASHQL_OPTION_URL]: schema.objectNode(sx.NodeType.OBJECT_SQL_CONST, {
+                [Key.SQL_CONST_VALUE]: url
+            }),
+        }));
+
+        expect(method.matching).toEqual(schema.Matching.MATCHED);
+        expect(method.value).toEqual(sxd.LoadMethodType.HTTP);
+        expect(url.matching).toEqual(schema.Matching.TYPE_MISMATCH);
+    });
+
+    test('load url missing', async () => {
+        const program = core.parseProgram(`
+            LOAD weather_csv FROM http;
+        `);
+        expect(program.buffer.errorsLength()).toEqual(0);
+        expect(program.buffer.statementsLength()).toEqual(1);
+        const stmt = program.getStatement(0);
+
+        const method = schema.enumNode(sx.NodeType.ENUM_DASHQL_LOAD_METHOD_TYPE, 0);
+        const url = schema.numberNode();
+        stmt.matchSchema(schema.objectNode(sx.NodeType.OBJECT_DASHQL_LOAD, {
+            [Key.DASHQL_LOAD_METHOD]: method,
+            [Key.DASHQL_OPTION_URL]: schema.objectNode(sx.NodeType.OBJECT_SQL_CONST, {
+                [Key.SQL_CONST_VALUE]: url
+            }),
+        }));
+
+        expect(method.matching).toEqual(schema.Matching.MATCHED);
+        expect(method.value).toEqual(sxd.LoadMethodType.HTTP);
+        expect(url.matching).toEqual(schema.Matching.MISSING);
+    });
 });
