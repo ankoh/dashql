ce6d23fdb56afc3f477ba0cfa2734472a5c52d62
Andre Kohn <kohn.a@outlook.com>
2020-12-26 15:58:50 +0100

    Prepare passing of parameters to planner

--- core/src/model/plan.ts
+++ core/src/model/plan.ts
@@ -3,6 +3,12 @@
 import { Program } from './program';
 import * as proto from '@dashql/proto';
 
+export interface PlanParameter {
+    type: proto.syntax_dashql.ParameterType;
+    origin: number;
+    value: string;
+}
+
 export class Plan {
     /// The program
     _program: Program;
