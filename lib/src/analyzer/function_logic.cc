b0f7c5db2fd30d7551317cce2a8443bf36cdd10d
Andre Kohn <kohn.a@outlook.com>
2020-12-27 23:50:18 +0100

    Resolve function logic with column refs

---
+++ lib/src/analyzer/function_logic.cc
@@ -0,0 +1,15 @@
+#include "dashql/analyzer/function_logic.h"
+
+#include <iostream>
+
+// Use format library that is bundled with DuckDB.
+// https://github.com/cwida/duckdb/blob/abaf6258dd737f397472e911e71af31e01493e00/src/function/scalar/string/printf.cpp
+
+namespace dashql {
+
+std::unique_ptr<FunctionLogic> Resolve(std::string_view name, nonstd::span<proto::syntax::NodeType> args) {
+    std::cout << "resolve function '" << name << "' with " << args.size() << " arguments";
+    return nullptr;
+}
+
+}
