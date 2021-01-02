5aae5b3b43afb6efb9b71f12a3fc315b7b0b8fa1
Andre Kohn <kohn.a@outlook.com>
2021-01-02 18:35:56 +0100

    Formatting

--- app/src/components/spinners.tsx
+++ app/src/components/spinners.tsx
@@ -8,9 +8,7 @@ interface IRectangleWaveSpinnerProps {
     active: boolean;
 }
 
-export class RectangleWaveSpinner extends React.PureComponent<
-    IRectangleWaveSpinnerProps
-> {
+export class RectangleWaveSpinner extends React.PureComponent<IRectangleWaveSpinnerProps> {
     public render() {
         const s = {
             backgroundColor: this.props.color || 'white',
@@ -72,12 +70,12 @@ export function ActionStatusSpinner(props: IActionStatusSpinnerProps): JSX.Eleme
                     height={props.height || '24px'}
                     viewBox="-8 -8 16 16"
                     fill="none"
-                    stroke={props.fill || "white"}
+                    stroke={props.fill || 'white'}
                     strokeWidth="2"
                 >
                     <g fill="none" fillRule="evenodd">
                         <circle cx="0" cy="0" r="7" opacity=".5"></circle>
-                        <circle cx="0" cy="0" r="4" strokeWidth="0" fill={props.fill || "white"}></circle>
+                        <circle cx="0" cy="0" r="4" strokeWidth="0" fill={props.fill || 'white'}></circle>
                         <circle cx="0" cy="0" r="7" strokeDasharray="12, 88" className={styles.status_spinner} />
                     </g>
                 </svg>
@@ -91,11 +89,11 @@ export function ActionStatusSpinner(props: IActionStatusSpinnerProps): JSX.Eleme
                     height={props.height || '24px'}
                     viewBox="-8 -8 16 16"
                     fill="none"
-                    stroke={props.fill || "white"}
+                    stroke={props.fill || 'white'}
                     strokeWidth="2"
                 >
                     <g fill="none" fillRule="evenodd">
-                        <circle cx="0" cy="0" r="4" opacity=".5" strokeWidth="0" fill={props.fill || "white"}></circle>
+                        <circle cx="0" cy="0" r="4" opacity=".5" strokeWidth="0" fill={props.fill || 'white'}></circle>
                     </g>
                 </svg>
             );
@@ -111,7 +109,11 @@ export function ActionStatusSpinner(props: IActionStatusSpinnerProps): JSX.Eleme
                     strokeWidth="2"
                 >
                     <g fill="none" fillRule="evenodd">
-                        <path fill={props.fill || "white"} fill-rule="evenodd" d="M2.343 13.657A8 8 0 1113.657 2.343 8 8 0 012.343 13.657zM6.03 4.97a.75.75 0 00-1.06 1.06L6.94 8 4.97 9.97a.75.75 0 101.06 1.06L8 9.06l1.97 1.97a.75.75 0 101.06-1.06L9.06 8l1.97-1.97a.75.75 0 10-1.06-1.06L8 6.94 6.03 4.97z"></path>
+                        <path
+                            fill={props.fill || 'white'}
+                            fill-rule="evenodd"
+                            d="M2.343 13.657A8 8 0 1113.657 2.343 8 8 0 012.343 13.657zM6.03 4.97a.75.75 0 00-1.06 1.06L6.94 8 4.97 9.97a.75.75 0 101.06 1.06L8 9.06l1.97 1.97a.75.75 0 101.06-1.06L9.06 8l1.97-1.97a.75.75 0 10-1.06-1.06L8 6.94 6.03 4.97z"
+                        ></path>
                     </g>
                 </svg>
             );
@@ -127,7 +129,11 @@ export function ActionStatusSpinner(props: IActionStatusSpinnerProps): JSX.Eleme
                     strokeWidth="2"
                 >
                     <g fill="none" fillRule="evenodd">
-                        <path fill={props.fill || "white"} fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm3.28 5.78a.75.75 0 00-1.06-1.06l-5.5 5.5a.75.75 0 101.06 1.06l5.5-5.5z"></path>
+                        <path
+                            fill={props.fill || 'white'}
+                            fill-rule="evenodd"
+                            d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm3.28 5.78a.75.75 0 00-1.06-1.06l-5.5 5.5a.75.75 0 101.06 1.06l5.5-5.5z"
+                        ></path>
                     </g>
                 </svg>
             );
@@ -143,7 +149,11 @@ export function ActionStatusSpinner(props: IActionStatusSpinnerProps): JSX.Eleme
                     strokeWidth="2"
                 >
                     <g fill="none" fillRule="evenodd">
-                        <path fill={props.fill || "white"} fill-rule="evenodd" d="M8 16A8 8 0 108 0a8 8 0 000 16zm3.78-9.72a.75.75 0 00-1.06-1.06L6.75 9.19 5.28 7.72a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l4.5-4.5z"></path>
+                        <path
+                            fill={props.fill || 'white'}
+                            fill-rule="evenodd"
+                            d="M8 16A8 8 0 108 0a8 8 0 000 16zm3.78-9.72a.75.75 0 00-1.06-1.06L6.75 9.19 5.28 7.72a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l4.5-4.5z"
+                        ></path>
                     </g>
                 </svg>
             );
