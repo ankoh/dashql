8dc63a7444426ecb67ea7609b6890dc4ea832620
Andre Kohn <kohn.a@outlook.com>
2021-01-03 13:27:29 +0100

    Drop async in webdb bindings in preparation for web workers

--- webdb/src/index_web.ts
+++ webdb/src/index_web.ts
@@ -8,22 +8,49 @@ export * from "./webdb_buffer";
 import webdb_api_wasm from './webdb_wasm.wasm';
 import webdb_api_init from './webdb_wasm.js';
 import { WebDBModule } from './webdb_module';
-import { WebDBBindings } from './webdb_bindings';
+import { WebDBBindings, WebDBRuntime } from './webdb_bindings';
 
 export class WebDB extends WebDBBindings {
+    protected runtime: WebDBRuntime;
     protected path: string;
-    constructor(path: string | null = null) {
+
+    public constructor(runtime: WebDBRuntime = {}, path: string | null = null) {
         super();
+        this.runtime = runtime;
         this.path = path ?? webdb_api_wasm;
     }
+
+    protected instantiateWasm(imports: any, success: (module: WebAssembly.Module) => void): Emscripten.WebAssemblyExports {
+        const imports_rt: WebAssembly.Imports = {
+            ...imports,
+            env: {
+                ...imports.env,
+                ...this.runtime
+            }
+        };
+        if (WebAssembly.instantiateStreaming) {
+            WebAssembly.instantiateStreaming(fetch(this.path), imports_rt).then((output) => {
+                success(output.instance);
+            });
+        } else {
+            fetch(this.path)
+                .then(resp => resp.arrayBuffer())
+                .then(bytes =>
+                    WebAssembly.instantiate(bytes, imports_rt).then((output) => {
+                        success(output.instance);
+                    })
+                )
+                .catch((error) => {
+                    console.error('Failed to instantiate WASM:', error);
+                });
+        }
+        return [];
+    }
+
     protected instantiate(moduleOverrides: Partial<WebDBModule>): Promise<WebDBModule> {
         return webdb_api_init({
             ...moduleOverrides,
-            locateFile: (path: string) => {
-                if (path.endsWith('webdb_wasm.wasm'))
-                    return this.path;
-                return path;
-            }
+            instantiateWasm: this.instantiateWasm.bind(this)
         });
     }
 }
