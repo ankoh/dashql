bcaa87b8c39431fa4f6059880e06a4f701b2edf7
Andre Kohn <kohn.a@outlook.com>
2021-01-03 14:24:48 +0100

    Expose synchronous webdb bindings until we have an async iterator

---
+++ webdb/src/webdb_bindings_web.ts
@@ -0,0 +1,57 @@
+// Copyright (c) 2020 The DashQL Authors
+
+import webdb_api_wasm from './webdb_wasm.wasm';
+import webdb_api_init from './webdb_wasm.js';
+import { WebDBModule } from './webdb_module';
+import { WebDBBindings, WebDBRuntime } from './webdb_bindings';
+
+/// WebDB bindings for the browser
+export class WebDB extends WebDBBindings {
+    protected runtime: WebDBRuntime;
+    protected path: string;
+
+    public constructor(runtime: WebDBRuntime = {}, path: string | null = null) {
+        super();
+        this.runtime = runtime;
+        this.path = path ?? webdb_api_wasm;
+    }
+
+    /// Instantiate the wasm module
+    protected instantiateWasm(
+        imports: any,
+        success: (module: WebAssembly.Module) => void,
+    ): Emscripten.WebAssemblyExports {
+        const imports_rt: WebAssembly.Imports = {
+            ...imports,
+            env: {
+                ...imports.env,
+                ...this.runtime,
+            },
+        };
+        if (WebAssembly.instantiateStreaming) {
+            WebAssembly.instantiateStreaming(fetch(this.path), imports_rt).then(output => {
+                success(output.instance);
+            });
+        } else {
+            fetch(this.path)
+                .then(resp => resp.arrayBuffer())
+                .then(bytes =>
+                    WebAssembly.instantiate(bytes, imports_rt).then(output => {
+                        success(output.instance);
+                    }),
+                )
+                .catch(error => {
+                    console.error('Failed to instantiate WASM:', error);
+                });
+        }
+        return [];
+    }
+
+    /// Instantiate the bindings
+    protected instantiate(moduleOverrides: Partial<WebDBModule>): Promise<WebDBModule> {
+        return webdb_api_init({
+            ...moduleOverrides,
+            instantiateWasm: this.instantiateWasm.bind(this),
+        });
+    }
+}
--- webdb/src/worker_web.ts
+++ webdb/src/worker_web.ts
@@ -1,65 +1,8 @@
 // Copyright (c) 2020 The DashQL Authors
 
-export * from './iterator';
-export * from './value';
-export * from './webdb_bindings';
-
-import webdb_api_wasm from './webdb_wasm.wasm';
-import webdb_api_init from './webdb_wasm.js';
 import { WorkerAPI, WorkerAPIRequest, WorkerAPIResponse } from './worker_api';
-import { WebDBModule } from './webdb_module';
-import { WebDBBindings, WebDBRuntime } from './webdb_bindings';
-
-/// WebDB bindings for the browser
-export class WebDB extends WebDBBindings {
-    protected runtime: WebDBRuntime;
-    protected path: string;
-
-    public constructor(runtime: WebDBRuntime = {}, path: string | null = null) {
-        super();
-        this.runtime = runtime;
-        this.path = path ?? webdb_api_wasm;
-    }
-
-    /// Instantiate the wasm module
-    protected instantiateWasm(
-        imports: any,
-        success: (module: WebAssembly.Module) => void,
-    ): Emscripten.WebAssemblyExports {
-        const imports_rt: WebAssembly.Imports = {
-            ...imports,
-            env: {
-                ...imports.env,
-                ...this.runtime,
-            },
-        };
-        if (WebAssembly.instantiateStreaming) {
-            WebAssembly.instantiateStreaming(fetch(this.path), imports_rt).then(output => {
-                success(output.instance);
-            });
-        } else {
-            fetch(this.path)
-                .then(resp => resp.arrayBuffer())
-                .then(bytes =>
-                    WebAssembly.instantiate(bytes, imports_rt).then(output => {
-                        success(output.instance);
-                    }),
-                )
-                .catch(error => {
-                    console.error('Failed to instantiate WASM:', error);
-                });
-        }
-        return [];
-    }
-
-    /// Instantiate the bindings
-    protected instantiate(moduleOverrides: Partial<WebDBModule>): Promise<WebDBModule> {
-        return webdb_api_init({
-            ...moduleOverrides,
-            instantiateWasm: this.instantiateWasm.bind(this),
-        });
-    }
-}
+import { WebDBBindings } from './webdb_bindings';
+import { WebDB } from './webdb_bindings_web';
 
 /// The webdb worker API for web workers
 class WebWorkerAPI extends WorkerAPI {
