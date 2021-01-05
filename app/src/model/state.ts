840ff67597fe8846557a2bcde02ba4cd103e5d65
Andre Kohn <kohn.a@outlook.com>
2021-01-05 16:20:23 +0100

    Add launcher

--- app/src/model/state.ts
+++ app/src/model/state.ts
@@ -5,11 +5,11 @@ import { AppSettings } from "./settings";
 
 export class AppState implements core.model.DerivedState {
     /// The launch is complete?
-    public launchComplete: boolean = true;
+    public launchComplete: boolean = false;
     /// The launch progress
     public launchSteps: Immutable.Map<LaunchStep, LaunchStepInfo> = createLaunchSteps();
-    /// The core
-    public core: core.model.CoreState = new core.model.CoreState();
     // The app config
     public appSettings: AppSettings | null = null;
+    /// The core
+    public core: core.model.CoreState = new core.model.CoreState();
 }
