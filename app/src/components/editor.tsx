a5d0b013d33603fad65edb23b0e4a6f376faab0e
Andre Kohn <kohn.a@outlook.com>
2021-01-05 15:41:28 +0100

    Merge editor controller into component

--- app/src/components/editor.tsx
+++ app/src/components/editor.tsx
@@ -71,7 +71,6 @@ class Editor extends React.Component<Props> {
             });
             this.editor.setPosition({column: 0, lineNumber: 0});
             this.editor.focus();
-            this.props.appContext.controller.editor.registerEditor(this.editor);
 
             // Set theme
             monaco.editor.defineTheme('dashql', monaco_theme);
@@ -132,6 +131,111 @@ class Editor extends React.Component<Props> {
                     }
                 </AutoSizer>
             </div>
+        );
+    }
+
+    public updateMarkers() {
+        const platform = this.props.appContext.controller.platform;
+        const state = platform.store.getState();
+        const program = state.core.program;
+        const data = this.editor?.getModel();
+        if (!data) return;
+
+        if (!program) {
+            monaco.editor.setModelMarkers(data, 'DashQL', []);
+        } else {
+            const markers: monaco.editor.IMarkerData[] = [];
+            for (let i = 0; i < program.buffer.errorsLength(); ++i) {
+                const error = program.buffer.errors(i)!;
+                const location = error.location()!;
+                const begin = data.getPositionAt(location.offset());
+                const startLineNumber = begin.lineNumber;
+                const startColumn = begin.column;
+                const end = data.getPositionAt(location.offset() + location.length());
+                const endLineNumber = end.lineNumber;
+                const endColumn = end.column;
+                if (!startLineNumber || !startColumn || !endLineNumber || !endColumn) {
+                    return undefined;
+                }
+                markers.push({
+                    startLineNumber,
+                    startColumn,
+                    endLineNumber,
+                    endColumn,
+                    message: error.message() ?? '',
+                    severity: monaco.MarkerSeverity.Error,
+                });
+            }
+            monaco.editor.setModelMarkers(data, 'DashQL', markers);
+        }
+    }
+
+    public replace(location: core.proto.syntax.Location, text: string | null) {
+        /// Get monaco editor
+        if (!this.editor) {
+            return;
+        }
+
+        /// Get monaco model
+        const model = this.editor.getModel();
+        if (!model) {
+            return;
+        }
+
+        // Determine edit range
+        const begin = model.getPositionAt(location.offset());
+        const end = model.getPositionAt(location.offset() + location.length());
+        const range = {
+            startLineNumber: begin.lineNumber,
+            startColumn: begin.column,
+            endLineNumber: end.lineNumber,
+            endColumn: end.column,
+        };
+        while (true) {
+            const nextCharacterRange = {
+                startLineNumber: range.endLineNumber,
+                startColumn: range.endColumn,
+                endLineNumber: range.endLineNumber,
+                endColumn: range.endColumn,
+            };
+
+            if (nextCharacterRange.endColumn == (model.getLineLength(nextCharacterRange.endLineNumber) ?? 0) + 1) {
+                nextCharacterRange.endLineNumber += 1;
+                nextCharacterRange.endColumn = 1;
+            } else {
+                nextCharacterRange.endColumn += 1;
+            }
+
+            const nextCharacter = model.getValueInRange(nextCharacterRange);
+            if (nextCharacter != '\n') {
+                break;
+            } else {
+                range.endLineNumber = nextCharacterRange.endLineNumber;
+                range.endColumn = nextCharacterRange.endColumn;
+            }
+        }
+
+        // Pad text with newlines
+        let paddedText = text;
+        if (paddedText != null) {
+            if (!(range.endLineNumber == 1 && range.endColumn == 1)) {
+                paddedText = '\n\n' + paddedText;
+            }
+            if (
+                range.endLineNumber == model.getLineCount() &&
+                range.endColumn == model.getLineLength(range.endLineNumber) + 1
+            ) {
+                paddedText = paddedText + '\n';
+            } else if (range.endColumn == 1 || range.endColumn == model.getLineLength(range.endLineNumber) + 1) {
+                paddedText = paddedText + '\n\n';
+            }
+        }
+
+        // Apply the edits
+        this.editor.executeEdits(
+            '',
+            [{ range: range as monaco.Range, text: paddedText }],
+            [monaco.Selection.fromPositions({ lineNumber: 1, column: 1 }, { lineNumber: 1, column: 1 })],
         );
     }
 }
--- app/src/controller/app.ts
+++ app/src/controller/app.ts
@@ -1,16 +1,10 @@
 import * as core from '@dashql/core';
-import { EditorController } from './editor';
 import { DemoController } from './demo';
-
-/// The worker interval
-const workerIntervalMS = 400;
 
 /// A controller
 export class AppController {
     /// The platform
     protected _platform: core.platform.Platform;
-    /// The editor controller
-    protected _editor: EditorController;
     /// The interpreter controller
     protected _scheduler: core.ActionGraphScheduler;
     /// The script pipeline
@@ -18,40 +12,22 @@ export class AppController {
     /// The demo controller
     protected _demo: DemoController;
 
-    /// The worker timeout
-    protected _workerTimer: number | null;
-
     // Constructor
     constructor(platform: core.platform.Platform) {
         this._platform = platform;
-        this._editor = new EditorController(platform);
         this._scheduler = new core.ActionGraphScheduler(platform);
         this._scriptPipeline = new core.ScriptPipeline(platform, this._scheduler);
         this._demo = new DemoController(platform);
-        this._workerTimer = null;
     }
 
-    /// Get the editor
-    public get editor() { return this._editor; }
+    /// Get the platform
+    public get platform() { return this._platform; }
     /// Get the scheduler
     public get scheduler() { return this._scheduler; }
 
     /// Init the controller
     public async init(): Promise<void> {
-        this._workerTimer = window.setTimeout(this.worker.bind(this), workerIntervalMS);
-
         await this._platform.analyzer.init();
         this._demo.setup();
-    }
-
-    /// The worker function
-    protected worker() {
-        // Clear the worker timer
-        clearTimeout(this._workerTimer || undefined);
-
-        // TODO
-
-        // Reschedule worker
-        this._workerTimer = window.setTimeout(this.worker.bind(this), workerIntervalMS);
     }
 }
