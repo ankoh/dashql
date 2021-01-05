a50f51f7be2648add78665e38308eaa527afd09f
Andre Kohn <kohn.a@outlook.com>
2021-01-05 16:20:23 +0100

    Add launcher

--- app/src/app_launcher.ts
+++ app/src/app_launcher.ts
@@ -36,18 +36,77 @@ SELECT 1 INTO weather_avg FROM weather;
 VIZ weather_avg USING LINE;
 `;
 
-export async function launchApp(ctx: IAppContext) {
-    const dbWorker = webdb.spawnWorker(webdb_worker);
-    const db = new webdb.AsyncWebDB(dbWorker);
-    await db.open(webdb_wasm);
+function startStep(store: model.AppReduxStore, step: model.LaunchStep) {
+    model.mutate(store.dispatch, {
+        type: model.StateMutationType.UPDATE_LAUNCH_STEP,
+        data: [step, model.Status.RUNNING, null]
+    });
+}
 
-    const analyzer = new core.analyzer.Analyzer({}, analyzer_wasm);
-    await analyzer.init();
+function stepSucceeded(store: model.AppReduxStore, step: model.LaunchStep) {
+    model.mutate(store.dispatch, {
+        type: model.StateMutationType.UPDATE_LAUNCH_STEP,
+        data: [step, model.Status.COMPLETED, null]
+    });
+}
 
-    ctx.platform = new platform.BrowserPlatform(ctx.store, db, analyzer);
+function stepFailed(store: model.AppReduxStore, step: model.LaunchStep) {
+    model.mutate(store.dispatch, {
+        type: model.StateMutationType.UPDATE_LAUNCH_STEP,
+        data: [step, model.Status.FAILED, null]
+    });
+}
 
-    model.mutate(ctx.store.dispatch, {
+function configureApp(store: model.AppReduxStore) {
+    stepSucceeded(store, model.LaunchStep.CONFIGURE_APP);
+}
+
+async function initWebDB(store: model.AppReduxStore): Promise<webdb.AsyncWebDB | null> {
+    startStep(store, model.LaunchStep.INIT_WEBDB);
+    try {
+        const dbWorker = webdb.spawnWorker(webdb_worker);
+        const db = new webdb.AsyncWebDB(dbWorker);
+        await db.open(webdb_wasm);
+        stepSucceeded(store, model.LaunchStep.INIT_WEBDB);
+        return db;
+    } catch(e) {
+        stepFailed(store, model.LaunchStep.INIT_WEBDB);
+    }
+    return null;
+}
+
+async function initAnalyzer(store: model.AppReduxStore): Promise<core.analyzer.Analyzer | null> {
+    startStep(store, model.LaunchStep.INIT_ANALYZER);
+    try {
+        const analyzer = new core.analyzer.Analyzer({}, analyzer_wasm);
+        await analyzer.init();
+        stepSucceeded(store, model.LaunchStep.INIT_ANALYZER);
+        return analyzer;
+    } catch(e) {
+        stepFailed(store, model.LaunchStep.INIT_ANALYZER);
+    }
+    return null;
+}
+
+function loadDemo(store: model.AppReduxStore) {
+    model.mutate(store.dispatch, {
         type: core.model.StateMutationType.SET_PROGRAM_TEXT,
         data: [DEMO_SCRIPT, core.utils.countLines(DEMO_SCRIPT)],
     });
+    stepSucceeded(store, model.LaunchStep.LOAD_DEMO);
+}
+
+export async function launchApp(ctx: IAppContext) {
+    configureApp(ctx.store);
+
+    const webdbPromise = initWebDB(ctx.store);
+    const analyzerPromise = initAnalyzer(ctx.store);
+
+    const init = await Promise.all([webdbPromise, analyzerPromise])
+    if (init[0] == null || init[1] == null) return;
+    const webdb = init[0];
+    const analyzer = init[1];
+
+    ctx.platform = new platform.BrowserPlatform(ctx.store, webdb, analyzer);
+    loadDemo(ctx.store);
 }
