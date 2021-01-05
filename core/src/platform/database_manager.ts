ef58193e252a344ab50f0c1d4dbf680a7adacb65
Andre Kohn <kohn.a@outlook.com>
2021-01-05 22:43:12 +0100

    Add semaphore for database manager

--- core/src/platform/database_manager.ts
+++ core/src/platform/database_manager.ts
@@ -1,2 +1,41 @@
+import * as webdb from '@dashql/webdb/dist/webdb_async';
+import { Mutex } from '../utils';
+
 export class DatabaseManager {
+    /// The async webdb
+    _webdb: webdb.AsyncWebDB;
+    /// The connection
+    _connection: webdb.AsyncWebDBConnection | null;
+    /// The mutex
+    _connectionMutex: Mutex;
+
+    constructor(db: webdb.AsyncWebDB) {
+        this._webdb = db;
+        this._connection = null;
+        this._connectionMutex = new Mutex();
+    }
+
+    /// Use the connection
+    public async use<T>(f: (conn: webdb.AsyncWebDBConnection) => Promise<T>): Promise<T | null> {
+        return await this._connectionMutex.useAsync(async () => {
+            if (!this._connection) return null;
+            return await f(this._connection);
+        });
+    }
+
+    /// Disconnect the connection
+    public async disconnect() {
+        return await this._connectionMutex.useAsync(async () => {
+            if (!this._connection) return;
+            await this._connection.disconnect();
+        });
+    }
+
+    /// Create a new connection
+    public async connect() {
+        return await this._connectionMutex.useAsync(async () => {
+            if (this._connection) return null;
+            return await this._webdb.connect();
+        });
+    }
 }
