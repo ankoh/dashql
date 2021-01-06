527a1d05109c8c2f6c8f0e121f23d7dcb5e0587a
Andre Kohn <kohn.a@outlook.com>
2021-01-06 21:48:59 +0100

    Tweak table and viz logic

--- core/src/actions/view_create_logic.ts
+++ core/src/actions/view_create_logic.ts
@@ -1,5 +1,6 @@
 import * as proto from "@dashql/proto";
-import * as utils from "../utils";
+import * as webdb from "@dashql/webdb/dist/webdb_async";
+import * as model from "../model";
 import { ActionID, Statement } from "../model";
 import { ProgramActionLogic } from "./action_logic";
 import { ActionContext } from "./action_context";
@@ -10,8 +11,52 @@ export class ViewCreateActionLogic extends ProgramActionLogic {
         super(action_id, action, statement);
     }
 
-    public async execute(_context: ActionContext): Promise<ActionID> {
-        await utils.sleep(500);
+    public async execute(context: ActionContext): Promise<model.ActionID> {
+        const script = this.script;
+        if (!script) {
+            return this.returnWithStatus(ActionStatusCode.COMPLETED);
+        }
+
+        const db = context.platform.database;
+        const table = await db.use(async (c: webdb.AsyncWebDBConnection) => {
+            /// First run the query
+            const result = await c.runQuery(script);
+
+            // Collect statistics
+            const limit0 = await c.runQuery(`SELECT * FROM ${this.buffer.targetNameShort()} LIMIT 0`);
+            const info: model.RelationInfo = {
+                columnNames: [],
+                columnTypes: [],
+                rowCount: 0
+            };
+            for (let ci = 0; ci < limit0.columnNamesLength(); ++ci) {
+                info.columnNames.push(limit0.columnNames(ci));
+                info.columnTypes.push(webdb.getSQLType(limit0.columnTypes(ci)));
+            }
+
+            // Return plan object
+            const now = new Date();
+            const table: model.DatabaseView = {
+                objectId: this.buffer.objectId(),
+                objectType: model.PlanObjectType.DATABASE_TABLE,
+                timeCreated: now,
+                timeUpdated: now,
+                nameQualified: this.buffer.targetNameQualified() || "",
+                nameShort: this.buffer.targetNameShort() || "",
+                queryResult: result,
+                relationInfo: info
+            };
+            return table;
+        });
+
+        if (table) {
+            const store = context.platform.store;
+            model.mutate(store.dispatch, {
+                type: model.StateMutationType.INSERT_PLAN_OBJECTS,
+                data: [table]
+            });
+        }
+
         return this.returnWithStatus(ActionStatusCode.COMPLETED);
     }
 };
--- core/src/model/index.ts
+++ core/src/model/index.ts
@@ -2,11 +2,13 @@ export * from "./action";
 export * from "./cache";
 export * from "./log";
 export * from "./plan";
+export * from "./plan_object";
 export * from "./program";
 export * from "./state";
 export * from "./state_mutation";
 export * from "./persistent_state";
 export * from "./store";
+export * from "./viz_spec";
 import * as syntax_schema from "./syntax_schema";
 
 import { StateMutationVariant } from "./state_mutation";
