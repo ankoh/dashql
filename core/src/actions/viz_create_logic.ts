381f3ca4c0dc7ff6479157f43f176adf0a8dc6d1
Andre Kohn <kohn.a@outlook.com>
2021-01-06 19:08:51 +0100

    Let the viz query the table

--- core/src/actions/viz_create_logic.ts
+++ core/src/actions/viz_create_logic.ts
@@ -1,5 +1,5 @@
 import * as proto from "@dashql/proto";
-import * as utils from "../utils";
+import * as webdb from "@dashql/webdb/dist/webdb_async";
 import { ActionID, Statement } from "../model";
 import { ProgramActionLogic } from "./action_logic";
 import { ActionContext } from "./action_context";
@@ -10,8 +10,19 @@ export class CreateVizActionLogic extends ProgramActionLogic {
         super(action_id, action, statement);
     }
 
-    public async execute(_context: ActionContext): Promise<ActionID> {
-        await utils.sleep(500);
+    public async execute(context: ActionContext): Promise<ActionID> {
+        const db = context.platform.database;
+        await db.use(async (c: webdb.AsyncWebDBConnection) => {
+            const result = await c.runQuery(`SELECT * FROM ${this.buffer.targetNameShort()}`);
+            const chunkIter = new webdb.QueryResultChunkStream(c, result);
+            while (await chunkIter.next()) {
+                console.log(`rows ${chunkIter.rowCount} columns ${chunkIter.columnCount}`);
+                chunkIter.iterateNumberColumn(0, (row: number, v: number | null) => {
+                    console.log(`[${row}] ${v}`);
+                });
+            }
+        });
+
         return this.returnWithStatus(ActionStatusCode.COMPLETED);
     }
 };
