a4c9098de2dd2fbeed0799d9503ec62d473bc4a6
Andre Kohn <kohn.a@outlook.com>
2021-01-09 14:15:46 +0100

    Add rudimentary drop logic to enable edits

--- core/src/actions/drop_table_logic.ts
+++ core/src/actions/drop_table_logic.ts
@@ -1,4 +1,5 @@
 import * as proto from "@dashql/proto";
+import * as webdb from "@dashql/webdb/dist/webdb_async";
 import { ActionID } from "../model";
 import { SetupActionLogic } from "./action_logic";
 import { ActionContext } from "./action_context";
@@ -9,7 +10,11 @@ export class DropTableActionLogic extends SetupActionLogic {
         super(action_id, action);
     }
 
-    public async execute(_context: ActionContext): Promise<ActionID> {
+    public async execute(context: ActionContext): Promise<ActionID> {
+        const db = context.platform.database;
+        await db.use(async (c: webdb.AsyncWebDBConnection) => {
+            await c.runQuery(`DROP TABLE IF EXISTS ${this.buffer.targetNameShort()}`);
+        });
         return this.returnWithStatus(ActionStatusCode.COMPLETED);
     }
 }
