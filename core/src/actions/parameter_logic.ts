387a3192ba097a8152f79d199d8820b1749134d4
Andre Kohn <kohn.a@outlook.com>
2021-01-09 19:32:09 +0100

    Remove test timeouts

--- core/src/actions/parameter_logic.ts
+++ core/src/actions/parameter_logic.ts
@@ -11,7 +11,6 @@ export class ParameterActionLogic extends ProgramActionLogic {
     }
 
     public async execute(_context: ActionContext): Promise<ActionID> {
-        await utils.sleep(500);
         return this.returnWithStatus(ActionStatusCode.COMPLETED);
     }
 };
