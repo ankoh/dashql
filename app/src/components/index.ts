fd9952b99e346b2eefe1be30a8b61e8a202662fc
Andre Kohn <kohn.a@outlook.com>
2021-01-09 12:10:09 +0100

    Work on data grid

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -2,6 +2,7 @@ export {NavBar, withNavBar} from "./navigation_bar";
 import Board from "./board";
 import EditorLoader from "./editor_loader";
 import ProgramGraph from "./program_graph";
+export { VirtualScrollbars } from "./virtual_scrollbars";
 import { StatusIndicator, ActionStatusIndicator } from "./status";
 
 export {
