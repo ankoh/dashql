c148c4676879ec68dda16d734cad5cc5b7cd2b4d
Andre Kohn <kohn.a@outlook.com>
2021-01-10 12:58:15 +0100

    Test update of viz position in typescript

--- core/test/program_editor.test.ts
+++ core/test/program_editor.test.ts
@@ -13,7 +13,7 @@ beforeEach(async () => {
 });
 
 describe('Program editor', () => {
-    test('rewrite viz position', async () => {
+    test('add viz position', async () => {
         const p = az.parseProgram("VIZ weather_avg USING LINE");
         const pi = az.instantiateProgram();
         expect(p.buffer.statementsLength()).toEqual(1);
@@ -33,6 +33,29 @@ describe('Program editor', () => {
         expect(n).not.toEqual(null);
         expect(n!.program.buffer.statementsLength()).toEqual(1);
         const expected = "VIZ weather_avg USING LINE (\n    pos = (x = 1, y = 2, w = 3, h = 4)\n)";
+        expect(n!.program.text).toEqual(expected);
+    });
+
+    test('update viz position', async () => {
+        const p = az.parseProgram("VIZ weather_avg USING LINE (\n    pos = (x = 1, y = 2, w = 3, h = 4)\n)");
+        const pi = az.instantiateProgram();
+        expect(p.buffer.statementsLength()).toEqual(1);
+        expect(pi).not.toEqual(null);
+        const n = az.editProgram([
+            {
+                type: edit.EditOperationType.VIZ_CHANGE_POSITION,
+                statement_id: 0,
+                data: {
+                    row: 10,
+                    column: 9,
+                    width: 8,
+                    height: 7,
+                }
+            }
+        ]);
+        expect(n).not.toEqual(null);
+        expect(n!.program.buffer.statementsLength()).toEqual(1);
+        const expected = "VIZ weather_avg USING LINE (\n    pos = (x = 10, y = 9, w = 8, h = 7)\n)";
         expect(n!.program.text).toEqual(expected);
     });
 });
