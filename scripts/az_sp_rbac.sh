007b2b47f25b2625840313e6e3b0b8fffd898f14
Andre Kohn <kohn.a@outlook.com>
2021-01-18 21:29:10 +0100

    Add copy script

--- scripts/az_sp_rbac.sh
+++ scripts/az_sp_rbac.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Copyright (c) 2020 The DashQL Authors
 
+set -euo pipefail
+
 STORAGE_RG="rg-dashql"
 STORAGE_ACCOUNT="stdashql"
 SUBSCRIPTION_ID=`az account show --query id --output tsv`
@@ -9,16 +11,16 @@ SCOPE_RG="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${STORAGE_RG}"
 SCOPE_ST="${SCOPE_RG}/providers/Microsoft.Storage/storageAccounts/${STORAGE_ACCOUNT}"
 SCOPE_CONT="${SCOPE_ST}/blobServices/default/containers"
 
-OBJECT_ID=$(az ad sp list --filter "displayname eq 'dashql-app-ci'" --query "[0].objectId" | tr -d '"')
+APP_CI_OID=$(az ad sp list --filter "displayname eq 'dashql-app-ci'" --query "[0].objectId" | tr -d '"')
 
 az role assignment create \
-    --assignee-object-id "${OBJECT_ID}" \
+    --assignee-object-id "${APP_CI_OID}" \
     --assignee-principal-type ServicePrincipal \
     --role "Storage Blob Data Contributor" \
     --scope "${SCOPE_CONT}/dashql-app"
 
 az role assignment create \
-    --assignee-object-id "${OBJECT_ID}" \
+    --assignee-object-id "${APP_CI_OID}" \
     --assignee-principal-type ServicePrincipal \
     --role "Storage Blob Data Contributor" \
     --scope "${SCOPE_CONT}/dashql-app-nightly"
