71eb2056c03bac92c38e262768775591101d7a41
Andre Kohn <kohn.a@outlook.com>
2021-02-03 23:41:13 +0100

    Extract system card component

---
+++ app/src/components/system_card.tsx
@@ -0,0 +1,34 @@
+import * as Immutable from 'immutable';
+import * as React from 'react';
+import * as core from '@dashql/core';
+import { proto } from '@dashql/core';
+import { AppState, Dispatch } from '../model';
+import { connect } from 'react-redux';
+import { ActionStatusIndicator } from './status';
+import { ChevronRightIcon, CloseIcon } from '../svg/icons';
+import styles from './system_card.module.css';
+
+interface Props {
+    className?: string;
+    children?: React.ReactNode;
+    title: string;
+    subtitle: string;
+    onClose?: () => void;
+}
+
+export class SystemCard extends React.Component<Props> {
+    public render() {
+        return (
+            <div className={styles.panel}>
+                <div className={styles.header}>
+                    <div className={styles.header_title}>{this.props.title}</div>
+                    <div className={styles.header_subtitle}>{this.props.subtitle}</div>
+                    <div className={styles.close} onClick={this.props.onClose}>
+                        <CloseIcon width="20px" height="20px" />
+                    </div>
+                </div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
