506671a2273de8dcb1e3f22c18df488e27768af8
Andre Kohn <kohn.a@outlook.com>
2021-02-04 10:18:27 +0100

    Add higher order timer component for log timestamps

---
+++ core/src/utils/time.ts
@@ -0,0 +1,19 @@
+const TIME_UNITS: [Intl.RelativeTimeFormatUnit, number][] = [
+    ['year', 24 * 60 * 60 * 1000 * 365],
+    ['month', (24 * 60 * 60 * 1000 * 365) / 12],
+    ['day', 24 * 60 * 60 * 1000],
+    ['hour', 60 * 60 * 1000],
+    ['minute', 60 * 1000],
+    ['second', 1000],
+];
+
+const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+
+export function getRelativeTime(to: Date, from: Date = new Date()) {
+    const elapsed = to.getTime() - from.getTime();
+    for (const [unitName, unitInMs] of TIME_UNITS) {
+        if (Math.abs(elapsed) > unitInMs || unitName == 'second') {
+            return rtf.format(Math.round(elapsed / unitInMs), unitName);
+        }
+    }
+}
