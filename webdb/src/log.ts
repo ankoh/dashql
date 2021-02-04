3894aa09ea6a2c6e0d6d56d19cd7d672ff31e90d
Andre Kohn <kohn.a@outlook.com>
2021-02-04 11:50:32 +0100

    Add rudimentary origin, topic, event labels to log viewer

--- webdb/src/log.ts
+++ webdb/src/log.ts
@@ -6,31 +6,31 @@ export enum LogLevel {
 }
 
 export enum LogTopic {
-    CONNECT,
-    DISCONNECT,
-    OPEN,
-    QUERY,
-    ITER_NEXT,
-    ITER_REWIND,
+    CONNECT = 1,
+    DISCONNECT = 2,
+    OPEN = 3,
+    QUERY = 4,
+    ITER_NEXT = 5,
+    ITER_REWIND = 6,
 }
 
 export enum LogEvent {
-    OK,
-    ERROR,
-    START
+    OK = 1,
+    ERROR = 2,
+    START = 3
 }
 
 export enum LogOrigin {
-    WEB_WORKER,
-    NODE_WORKER,
-    BINDINGS,
-    CHUNK_BUFFER,
-    CHUNK_STREAM,
-    ROW_ITERATOR,
-    ASYNC_WEBDB,
-    ASYNC_CHUNK_BUFFER,
-    ASYNC_CHUNK_STREAM,
-    ASYNC_ROW_ITERATOR,
+    WEB_WORKER = 1,
+    NODE_WORKER = 2,
+    BINDINGS = 3,
+    CHUNK_BUFFER = 4,
+    CHUNK_STREAM = 5,
+    ROW_ITERATOR = 6,
+    ASYNC_WEBDB = 7,
+    ASYNC_CHUNK_BUFFER = 8,
+    ASYNC_CHUNK_STREAM = 9,
+    ASYNC_ROW_ITERATOR = 10,
 }
 
 export type LogEntry<O, T, E, V> = {
@@ -67,5 +67,64 @@ export class VoidLogger implements Logger {
 export class ConsoleLogger implements Logger {
     public log(entry: LogEntryVariant): void {
         console.log(entry);
+    }
+}
+
+export function getLogEventLabel(event: LogEvent) {
+    switch (event) {
+        case LogEvent.OK:
+            return "OK";
+        case LogEvent.ERROR:
+            return "Error";
+        case LogEvent.START:
+            return "Start";
+        default:
+            return "?";
+    }
+}
+
+export function getLogTopicLabel(topic: LogTopic) {
+    switch (topic) {
+        case LogTopic.CONNECT:
+            return "Connect";
+        case LogTopic.DISCONNECT:
+            return "Disconnect";
+        case LogTopic.OPEN:
+            return "Open";
+        case LogTopic.QUERY:
+            return "Query";
+        case LogTopic.ITER_NEXT:
+            return "Advance Iterator";
+        case LogTopic.ITER_REWIND:
+            return "Rewind Iterator";
+        default:
+            return "?";
+    }
+}
+
+export function getLogOriginLabel(origin: LogOrigin) {
+    switch (origin) {
+        case LogOrigin.WEB_WORKER:
+            return "Web Worker";
+        case LogOrigin.NODE_WORKER:
+            return "Node Worker";
+        case LogOrigin.BINDINGS:
+            return "WebDB Bindings";
+        case LogOrigin.CHUNK_BUFFER:
+            return "Chunk Buffer";
+        case LogOrigin.CHUNK_STREAM:
+            return "Chunk Stream";
+        case LogOrigin.ROW_ITERATOR:
+            return "Row Iterator";
+        case LogOrigin.ASYNC_WEBDB:
+            return "Async WebDB";
+        case LogOrigin.ASYNC_CHUNK_BUFFER:
+            return "Async Chunk Buffer";
+        case LogOrigin.ASYNC_CHUNK_STREAM:
+            return "Async Chunk Stream";
+        case LogOrigin.ASYNC_ROW_ITERATOR :
+            return "Async Row Iterator";
+        default:
+            return "?";
     }
 }
