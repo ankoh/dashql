31aed762a159260409b2f653a3532af726a55207
Andre Kohn <kohn.a@outlook.com>
2021-02-06 18:43:58 +0100

    Add vscode settings

---
+++ lib/cmake/gflags.cmake
@@ -0,0 +1,31 @@
+# Copyright (c) 2020 The DashQL Authors
+
+include(ExternalProject)
+find_package(Git REQUIRED)
+
+# Get gflags
+ExternalProject_Add(
+    gflags_src
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/../submodules/gflags"
+    PREFIX "${CMAKE_BINARY_DIR}/third_party/gflags"
+    INSTALL_DIR "${CMAKE_BINARY_DIR}/third_party/gflags/install"
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/third_party/gflags/install
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+    UPDATE_COMMAND ""
+    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libgflags.a
+)
+
+# Prepare gflags
+ExternalProject_Get_Property(gflags_src install_dir)
+set(GFLAGS_INCLUDE_DIR ${install_dir}/include)
+set(GFLAGS_LIBRARY_PATH ${install_dir}/lib/libgflags.a)
+file(MAKE_DIRECTORY ${GFLAGS_INCLUDE_DIR})
+add_library(gflags STATIC IMPORTED)
+set_property(TARGET gflags PROPERTY IMPORTED_LOCATION ${GFLAGS_LIBRARY_PATH})
+set_property(TARGET gflags APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${GFLAGS_INCLUDE_DIR})
+
+# Dependencies
+add_dependencies(gflags gflags_src)
