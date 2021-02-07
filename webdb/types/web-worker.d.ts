bddb83691138d7c894be6846d7647f386503cd2e
Andre Kohn <kohn.a@outlook.com>
2021-02-07 08:39:37 +0100

    Tweak vscode support

--- webdb/types/web-worker.d.ts
+++ webdb/types/web-worker.d.ts
@@ -1,5 +1,4 @@
 declare module 'web-worker' {
-    type ConstructorOf<C> = { new (...args: any[]): C };
-    const _default: ConstructorOf<Worker>;
+    const _default: any;
     export default _default;
 }
