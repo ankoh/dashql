76725b610365a660fe8451a0520c7aec0ed5f06c
Andre Kohn <kohn.a@outlook.com>
2021-02-12 00:12:30 +0100

    Compile binaryen to wasm

---
+++ lib/include/dashql/table/table.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 The DashQL Authors
+
+#ifndef INCLUDE_DASHQL_TABLE_TABLE_H_
+#define INCLUDE_DASHQL_TABLE_TABLE_H_
+
+#include "dashql/proto_generated.h"
+
+namespace dashql {
+
+}  // namespace dashql
+
+#endif  // INCLUDE_DASHQL_TABLE_TABLE_H_
