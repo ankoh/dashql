40ef5ceca10559481b370b6039b69ae85f03cca5
Andre Kohn <kohn.a@outlook.com>
2021-02-17 12:28:51 +0100

    Rename table to dataframe

---
+++ lib/include/dashql/dataframe/dataframe.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 The DashQL Authors
+
+#ifndef INCLUDE_DASHQL_DATAFRAME_DATAFRAME_H_
+#define INCLUDE_DASHQL_DATAFRAME_DATAFRAME_H_
+
+#include "dashql/proto_generated.h"
+
+namespace dashql {
+
+}  // namespace dashql
+
+#endif  // INCLUDE_DASHQL_DATAFRAME_DATAFRAME_H_
