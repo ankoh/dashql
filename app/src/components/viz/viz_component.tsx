cc9dcca4be8c6856b95b801086196c1961f5a49a
Andre Kohn <kohn.a@outlook.com>
2021-02-24 09:26:03 +0100

    Add component to dispatch to different viz renderers

--- app/src/components/viz/viz_component.tsx
+++ app/src/components/viz/viz_component.tsx
@@ -3,13 +3,56 @@ import * as core from '@dashql/core';
 import TableChart from  './table_chart';
 
 interface Props {
-    spec: core.model.VizInfo;
+    vizInfo: core.model.VizInfo;
+}
+
+enum VizRenderer {
+    VICTORY,
+    TABLE,
 }
 
 export class VizComponent extends React.Component<Props> {
     public render() {
-        const spec = this.props.spec;
-        return <TableChart targetQualified="global.foo" />
+        let renderer: VizRenderer | null = null;
+        const spec = this.props.vizInfo.spec;
+
+        for (const c of spec.components) {
+            switch (c.type) {
+                case core.proto.syntax.VizComponentType.AREA:
+                case core.proto.syntax.VizComponentType.AXIS:
+                case core.proto.syntax.VizComponentType.BAR:
+                case core.proto.syntax.VizComponentType.BOX_PLOT:
+                case core.proto.syntax.VizComponentType.CANDLESTICK:
+                case core.proto.syntax.VizComponentType.ERROR_BAR:
+                case core.proto.syntax.VizComponentType.HISTOGRAM:
+                case core.proto.syntax.VizComponentType.LINE:
+                case core.proto.syntax.VizComponentType.PIE:
+                case core.proto.syntax.VizComponentType.SCATTER:
+                case core.proto.syntax.VizComponentType.VORONOI:
+                    if (renderer == null) {
+                        renderer = VizRenderer.VICTORY;
+                    }
+                    break;
+
+                case core.proto.syntax.VizComponentType.TABLE:
+                    renderer = VizRenderer.TABLE;
+                    break;
+
+                case core.proto.syntax.VizComponentType.NUMBER:
+                case core.proto.syntax.VizComponentType.TEXT:
+                    break;
+            }
+        }
+        if (renderer == null) {
+            return <div />;
+        }
+        switch (renderer) {
+            case VizRenderer.TABLE:
+                return <TableChart targetQualified="global.foo" />;
+            case VizRenderer.VICTORY:
+                return <div>VICTORY</div>;
+            
+        }
     }
 }
 
