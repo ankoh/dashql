3014ce985075eac8f5664fb67fd8c57dd126fbd4
Andre Kohn <kohn.a@outlook.com>
2021-02-24 12:13:44 +0100

    Streamline viz card styling

--- app/src/components/viz/viz_card.tsx
+++ app/src/components/viz/viz_card.tsx
@@ -3,15 +3,21 @@ import classNames from 'classnames';
 import styles from './viz_card.module.css';
 
 interface Props {
-    children?: JSX.Element;
+    children?: JSX.Element[] | JSX.Element;
     className?: string;
+    title?: string;
 }
 
 export class VizCard extends React.Component<Props> {
     public render() {
         return (
-            <div className={classNames(styles.card, this.props.className)}>
-                {this.props.children}
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    {this.props.title}
+                </div>
+                <div className={styles.body}>
+                    {this.props.children}
+                </div>
             </div>
         );
     }
