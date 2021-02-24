c071132a84eb3f273a75824235af221eb617cddf
Andre Kohn <kohn.a@outlook.com>
2021-02-24 14:47:42 +0100

    Add settings for viz cards

--- app/src/components/viz/viz_card.tsx
+++ app/src/components/viz/viz_card.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { SettingsIcon } from '../../svg/icons';
 import styles from './viz_card.module.css';
 
 interface Props {
@@ -14,6 +15,9 @@ export class VizCard extends React.Component<Props> {
                 <div className={styles.header}>
                     <div className={styles.title}>
                         {this.props.title}
+                    </div>
+                    <div className={styles.settings}>
+                        <SettingsIcon fill='rgb(80, 80, 80)' width='14px' height='14px'  />
                     </div>
                 </div>
                 <div className={styles.body}>
