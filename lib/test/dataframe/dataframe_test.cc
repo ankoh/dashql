06c40a56253066ae0e3a3885c033fe51631397c1
Pablo Sichert <mail@pablosichert.com>
2021-02-27 09:14:49 +0100

    Add dataframe scaffold

---
+++ lib/test/dataframe/dataframe_test.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 The DashQL Authors
+
+#include "dashql/dataframe/dataframe.h"
+
+#include "gtest/gtest.h"
+
+using namespace dashql;
+using namespace std;
+
+namespace {
+
+TEST(Dataframe, Constructor) {
+    auto dataframe = dataframe::Dataframe();
+    ASSERT_TRUE(true);
+}
+
+}  // namespace
