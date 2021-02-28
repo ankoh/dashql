6dfa6834b83d63db23f23f247360e75664b8ae27
Andre Kohn <kohn.a@outlook.com>
2021-02-28 22:59:23 +0100

    Dont debounce first instantiation

--- core/src/script_pipeline.ts
+++ core/src/script_pipeline.ts
@@ -3,12 +3,15 @@ import * as model from './model';
 import { Platform } from './platform';
 import { ActionGraphScheduler } from './action_scheduler';
 
+const MIN_INPUT_DELAY = 100;
+const MAX_INPUT_DELAY = 1000;
+const MIN_INSTANTIATION_DELAY = 100;
+
 export class ScriptPipeline {
     /// The store
     _platform: Platform;
     /// The scheduler
     _scheduler: ActionGraphScheduler;
-
     /// The program text
     _programText: string;
     /// The previous program
@@ -17,6 +20,13 @@ export class ScriptPipeline {
     _programParameters: Immutable.List<any>;
     /// The previous program instance
     _programInstance: model.ProgramInstance | null;
+
+    /// The debounce function
+    _debounceFunction = this.instantiateProgram.bind(this);
+    /// The debounce timeout
+    _debounceTimeout: ReturnType<typeof setTimeout> | null;
+    /// The last debounce trigger
+    _lastInputTime: number;
 
     /// Constructor
     constructor(platform: Platform, scheduler: ActionGraphScheduler) {
@@ -28,7 +38,67 @@ export class ScriptPipeline {
         this._program = state.program;
         this._programParameters = state.programParameters;
         this._programInstance = state.programInstance;
+        this._debounceTimeout = null;
+        this._lastInputTime = 0;
         store.subscribe(this.detectChanges.bind(this));
+    }
+
+    /// Debounce instantiation again
+    protected debounceInstantiation(delta: number) {
+        if (this._debounceTimeout != null) {
+            clearTimeout(this._debounceTimeout!);
+        }
+        this._debounceTimeout = setTimeout(this._debounceFunction, delta);
+    }
+
+    /// We parse the program with every keystroke but instantiate programs more carefully.
+    /// If a user is currently writing a SQL statment, we should not rerun the sql script with every keystroke.
+    ///
+    /// We might want to add a heuristic to check wheather a sql query is likely very expensive at the moment.
+    protected instantiateProgram() {
+        if (!this._program) return;
+        const nowMS = new Date().getTime();
+
+        // Has a program been instantiated before?
+        if (this._programInstance) {
+            const lastInstantiation = this._programInstance.createdAt.getTime();
+            const lastErrorCount = this._programInstance.program.buffer.errorsLength();
+
+            // Wait at least MIN_INPUT_DELAY_MS after last input
+            let deltaMS = nowMS - this._lastInputTime;
+            if (deltaMS < MIN_INPUT_DELAY) {
+                this.debounceInstantiation(MIN_INPUT_DELAY - deltaMS);
+                return;
+            }
+            // Wait at least MIN_DEBOUNCE_TIME after last instantiation
+            deltaMS = nowMS - lastInstantiation;
+            if (deltaMS < MIN_INSTANTIATION_DELAY) {
+                this.debounceInstantiation(MIN_INSTANTIATION_DELAY - deltaMS);
+                return;
+            }
+            // Are there more errors than before?
+            if (this._program.buffer.errorsLength() > lastErrorCount) {
+                this.debounceInstantiation(MAX_INPUT_DELAY - deltaMS);
+                return;
+            }
+        }
+
+        // Clear timeout
+        if (this._debounceTimeout != null) {
+            clearTimeout(this._debounceTimeout!);
+        }
+
+        // Instantiate the new program
+        const instance = this._platform.analyzer.instantiateProgram(this._programParameters);
+        // Instantiation failed?
+        // XXX log error
+        if (!instance) return;
+
+        // Otherwise store the new instance in redux
+        model.mutate(this._platform.store.dispatch, {
+            type: model.StateMutationType.SET_PROGRAM_INSTANCE,
+            data: instance,
+        });
     }
 
     /// Handler to detect changes
@@ -41,6 +111,7 @@ export class ScriptPipeline {
 
             // Parse the new program if necessary
             if (next.program && next.program.text == this._programText) {
+                // Program can be replaced directly (might be the result of a rewrite)
                 this._program = next.program;
             } else {
                 const program = this._platform.analyzer.parseProgram(this._programText);
@@ -64,20 +135,11 @@ export class ScriptPipeline {
                 next.programInstance.program == this._program &&
                 next.programInstance.parameters == this._programParameters
             ) {
+                // Instance can be replaced directly (might be the result of a rewrite)
                 this._programInstance = next.programInstance;
             } else {
-                // Instantiate the new program
-                const programInstance = this._platform.analyzer.instantiateProgram(this._programParameters);
-
-                // Instantiation failed?
-                // XXX log error
-                if (!programInstance) return;
-
-                // Otherwise store the new instance in redux
-                model.mutate(this._platform.store.dispatch, {
-                    type: model.StateMutationType.SET_PROGRAM_INSTANCE,
-                    data: programInstance,
-                });
+                // Debounce the program instantiation
+                this.instantiateProgram();
                 return;
             }
         }
