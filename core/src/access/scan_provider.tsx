1c088ec887adeab21548fb64606187441fa76519
Andre Kohn <kohn.a@outlook.com>
2021-03-01 12:12:34 +0100

    Remove logging in scan provider

--- core/src/access/scan_provider.tsx
+++ core/src/access/scan_provider.tsx
@@ -118,7 +118,6 @@ export class ScanProvider extends React.Component<Props, State> {
 
     /// Request a range
     protected requestScan(request: ScanRequest) {
-        console.log(request);
         if (this.state.result && this.state.result.request.includesRequest(request)) {
             return;
         }
@@ -136,7 +135,6 @@ export class ScanProvider extends React.Component<Props, State> {
         if (request.sample > 0) {
             query += ` USING SAMPLE RESERVOIR (${Math.trunc(request.sample)} ROWS)`;
         }
-        console.log(query);
         const result = await this.props.database.use(async conn => {
             return await conn.runQuery(query);
         });
