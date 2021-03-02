c2c8ce5142514f42049f3e94d1a43dbfd2b5f1f7
Andre Kohn <kohn.a@outlook.com>
2021-03-02 12:03:28 +0100

    Test result proxy

--- webdb/src/index_node.ts
+++ webdb/src/index_node.ts
@@ -4,5 +4,6 @@ export * from "./webdb_bindings";
 export * from "./webdb_bindings_node";
 export * from "./iterator";
 export * from "./iterator_base";
+export * from "./result_proxy";
 export * from "./log";
 export * from "./value";
