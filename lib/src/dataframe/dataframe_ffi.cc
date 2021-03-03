b236161f9fd683a7cd12a29221b3b402d6c0b8c1
Pablo Sichert <mail@pablosichert.com>
2021-03-03 05:09:27 +0100

    Prepare tooling for dataframe JS library

--- lib/src/dataframe/dataframe_ffi.cc
+++ lib/src/dataframe/dataframe_ffi.cc
@@ -1,11 +1,22 @@
 // Copyright (c) 2020 The DashQL Authors
 
+#include "dashql/common/ffi_response.h"
 #include "dashql/dataframe/dataframe.h"
 
 using namespace dashql;
+using namespace dashql::dataframe;
 
 extern "C" {
+using AlgebraTreeHandle = uintptr_t;
+using DataframeHandle = uintptr_t;
 
-void dashql_dataframe_ping() {}
+AlgebraTreeHandle dashql_dataframe_algebra_tree() {
+    return reinterpret_cast<AlgebraTreeHandle>(new Dataframe::AlgebraTree());
+}
 
+/// Analyze a query
+DataframeHandle dashql_dataframe(FFIResponse* response, AlgebraTreeHandle algebraTreeHandle) {
+    auto& algebraTree = reinterpret_cast<Dataframe::AlgebraTree&>(algebraTreeHandle);
+    return reinterpret_cast<AlgebraTreeHandle>(new Dataframe(algebraTree));
+}
 }
