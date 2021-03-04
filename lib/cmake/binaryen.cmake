63dc927d9509d8164760ddb4028f8b6773fd6078
Andre Kohn <kohn.a@outlook.com>
2021-03-04 12:30:31 +0100

    Manually install binaryen c-header to resolve conflict with wasm.h

--- lib/cmake/binaryen.cmake
+++ lib/cmake/binaryen.cmake
@@ -6,6 +6,7 @@ ExternalProject_Add(
     binaryen_ep
     SOURCE_DIR "${CMAKE_SOURCE_DIR}/../submodules/binaryen"
     PREFIX "${CMAKE_BINARY_DIR}/third_party/binaryen"
+    INSTALL_DIR "${CMAKE_BINARY_DIR}/third_party/binaryen/install"
     CMAKE_ARGS
         -G${CMAKE_GENERATOR}
         -DCMAKE_CXX_STANDARD=17
@@ -21,15 +22,19 @@ ExternalProject_Add(
     BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} -j${CMAKE_BUILD_PARALLEL_LEVEL} binaryen
     DOWNLOAD_COMMAND ""
     UPDATE_COMMAND ""
-    INSTALL_COMMAND ""
+    INSTALL_COMMAND
+        COMMAND ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/include/ <INSTALL_DIR>/lib/
+        COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/lib/libbinaryen.a <INSTALL_DIR>/lib/
+        COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/binaryen-c.h <SOURCE_DIR>/src/wasm-delegations.h <INSTALL_DIR>/include/
     BUILD_BYPRODUCTS
-        <BINARY_DIR>/lib/libbinaryen.a
+        <INSTALL_DIR>/lib/libbinaryen.a
 )
 
-ExternalProject_Get_Property(binaryen_ep binary_dir)
-ExternalProject_Get_Property(binaryen_ep source_dir)
-set(BINARYEN_LIBRARY_PATH ${binary_dir}/lib/libbinaryen.a)
-set(BINARYEN_INCLUDE_DIR ${source_dir}/src)
+
+ExternalProject_Get_Property(binaryen_ep install_dir)
+set(BINARYEN_LIBRARY_PATH ${install_dir}/lib/libbinaryen.a)
+set(BINARYEN_INCLUDE_DIR ${install_dir}/include)
+file(MAKE_DIRECTORY ${BINARYEN_INCLUDE_DIR})
 
 add_library(binaryen STATIC IMPORTED)
 set_property(TARGET binaryen PROPERTY IMPORTED_LOCATION ${BINARYEN_LIBRARY_PATH})
