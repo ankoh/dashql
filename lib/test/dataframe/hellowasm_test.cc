10a35dd7fe3379c4978f33de76afc202c9afcd08
Andre Kohn <kohn.a@outlook.com>
2021-03-04 12:05:37 +0100

    Use wasmtime c-api

--- lib/test/dataframe/hellowasm_test.cc
+++ lib/test/dataframe/hellowasm_test.cc
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 The DashQL Authors
 
 #include "dashql/test/config.h"
-#include "wasm.hh"
+#include "wasm.h"
 
 #include <v8/v8.h>
 
@@ -18,11 +18,15 @@ TEST(HelloWasm, Foo) {
     auto wasm_path = SOURCE_DIR / "test" / "dataframe" / "hello.wasm";
     (void)wasm_path;
 
-//    // Initialize.
-//    std::cout << "Initializing..." << std::endl;
-//    auto engine = wasm::Engine::make();
-//    auto store_ = wasm::Store::make(engine.get());
-//    auto store = store_.get();
+    // Initialize.
+    printf("Initializing...\n");
+    wasm_engine_t* engine = wasm_engine_new();
+    wasm_store_t* store = wasm_store_new(engine);
+
+    // Shut down.
+    printf("Shutting down...\n");
+    wasm_store_delete(store);
+    wasm_engine_delete(engine);
 }
 
 }
