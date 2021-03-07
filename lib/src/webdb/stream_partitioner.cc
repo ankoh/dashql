e4aa08d88199bcf13917576ecfc8f86f8bce3548
Andre Kohn <kohn.a@outlook.com>
2021-03-07 15:22:03 +0100

    Work on stream partitioner

--- lib/src/webdb/stream_partitioner.cc
+++ lib/src/webdb/stream_partitioner.cc
@@ -1,72 +1,120 @@
 #include "dashql/webdb/stream_partitioner.h" 
+#include "dashql/webdb/codec.h" 
 #include "duckdb/common/types/vector.hpp"
+#include "duckdb/common/types/string_type.hpp"
+#include "duckdb/common/types.hpp"
+#include <string_view>
+#include <vector>
 
 namespace dashql {
 namespace webdb {
 
 StreamPartitioner::StreamPartitioner()
-    : query_result_(nullptr), partition_columns_(), last_chunk_row_() {}
+    : query_result_(nullptr), partition_columns_(), previous_values_() {}
 
 /// Consume a query result
 void StreamPartitioner::prepare(duckdb::QueryResult& result, std::vector<size_t> columns) {
     query_result_ = &result;
     partition_columns_ = {};
-    last_chunk_row_ = {};
+    previous_values_ = {};
 }
 
-/// Iterate over a vector
-template <typename T, bool WITH_NULL, typename OP> void iterVec(duckdb::VectorData &vec, size_t count, OP op) {
-    if (vec.sel) {
-        for (unsigned i = 0; i < count; ++i) {
-            auto s = vec.sel->get_index(i);
-            auto n = false;
-            if constexpr (WITH_NULL) n = (*vec.nullmask)[s];
-            auto d = reinterpret_cast<T *>(vec.data)[s];
-            op(i, d, n);
-        }
-    } else {
-        for (unsigned i = 0; i < count; ++i) {
-            auto d = reinterpret_cast<T *>(vec.data)[i];
-            auto n = false;
-            if constexpr (WITH_NULL) n = (*vec.nullmask)[i];
-            op(i, d, n);
-        }
-    }
-}
-
-/// Scan a duckdb vector and track positions where value change
+/// Scan a duckdb vector and track positions where values change
 template <typename VecType>
 static void partition(duckdb::VectorData& vec, size_t count, duckdb::Value& prev, PartitionMask& out) {
-    auto& prev_value = prev.GetValueUnsafe<VecType>();
-    auto& prev_null = prev.is_null;
-    if (out.size() < count) {
-        out.resize(count, false);
-    }
+    auto prev_value = prev.GetValueUnsafe<VecType>();
+    auto prev_null = prev.is_null;
+    assert(out.size() >= count);
     if (vec.nullmask) {
-        iterVec<VecType, true>(vec, count, [&](unsigned i, VecType value, bool null) {
-            out[i] = (null != prev_null) || (!null && value != prev_value);
+        iterateVector<VecType, true>(vec, count, [&](unsigned i, VecType& value, bool null) {
+            out[i] = out[i] | ((null != prev_null) || (!null && value != prev_value));
             prev_null = null;
             prev_value = value;
         });
     } else {
-        iterVec<VecType, true>(vec, count, [&](unsigned i, VecType value, bool null) {
-            out[i] = value != prev_value;
+        iterateVector<VecType, true>(vec, count, [&](unsigned i, VecType& value, bool null) {
+            out[i] = out[i] | (value != prev_value);
             prev_value = value;
         });
     }
+    prev.GetValueUnsafe<VecType>() = prev_value;
+    prev.is_null = prev_null;
+}
+
+/// Partition strings
+static void partitionStrings(duckdb::VectorData& vec, size_t count, duckdb::Value& prev, PartitionMask& out) {
+    auto prev_value = duckdb::string_t{prev.GetValueUnsafe<std::string>()};
+    auto prev_null = prev.is_null;
+    assert(out.size() >= count);
+    // XXX DuckDB could also just use string_view (polyfill)?
+    auto strv = [](duckdb::string_t& str) {
+        return std::string_view{str.GetDataUnsafe(), static_cast<std::string_view::size_type>(str.GetSize())};
+    };
+    if (vec.nullmask) {
+        iterateVector<duckdb::string_t, true>(vec, count, [&](unsigned i, duckdb::string_t& value, bool null) {
+            out[i] = out[i] | ((null != prev_null) || (!null && strv(value) != strv(prev_value)));
+            prev_null = null;
+            prev_value = value;
+        });
+    } else {
+        iterateVector<duckdb::string_t, true>(vec, count, [&](unsigned i, duckdb::string_t& value, bool null) {
+            out[i] = out[i] | (strv(value) != strv(prev_value));
+            prev_value = value;
+        });
+    }
+    prev.GetValueUnsafe<std::string>() = prev_value.GetString();
+    prev.is_null = prev_null;
 }
 
 /// Consume the next query result chunk 
 void StreamPartitioner::consumeQueryResultChunk(duckdb::DataChunk& chunk, PartitionMask& out) {
     if (!this->query_result_) return;
     if (partition_columns_.empty()) { return; }
+    assert(out.size() >= chunk.size());
+    auto size = chunk.size();
+    auto vectors = chunk.Orrify();
+
+    auto local = out;
 
     for (size_t pi = 0; pi < partition_columns_.size(); ++pi) {
         auto ci = partition_columns_[pi];
         assert(ci < chunk.ColumnCount());
 
-        // XXX compute partition mask of every column and combine them
+        switch (chunk.GetTypes()[ci].InternalType()) {
+            case duckdb::PhysicalType::BOOL:
+                partition<uint8_t>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::INT8:
+                partition<int8_t>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::INT16:
+                partition<int16_t>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::INT32:
+                partition<int32_t>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::INT64:
+                partition<int64_t>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::INT128:
+                partition<duckdb::hugeint_t>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::FLOAT:
+                partition<float>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::DOUBLE:
+                partition<double>(vectors[ci], size, previous_values_[pi], out);
+                break;
+            case duckdb::PhysicalType::VARCHAR:
+            case duckdb::PhysicalType::STRING:
+                partitionStrings(vectors[ci], size, previous_values_[pi], out);
+                break;
+            default:
+                assert(false);
+        }
     }
+
+
 }
 
 }  // namespace webdb
