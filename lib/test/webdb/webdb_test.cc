d72fb32a80639f0d70e0837474c07b902f0039c7
Andre Kohn <kohn.a@outlook.com>
2021-03-08 08:26:40 +0100

    Fix test

--- lib/test/webdb/webdb_test.cc
+++ lib/test/webdb/webdb_test.cc
@@ -18,9 +18,9 @@ namespace {
 TEST(WebDB, InvalidSQL) {
     auto db = make_shared<duckdb::DuckDB>();
     WebDB::Connection conn{db};
-    auto expected = conn.SendQuery({.text = R"RAW(
+    auto expected = conn.SendQuery(R"RAW(
         INVALID SQL
-    )RAW"});
+    )RAW");
     ASSERT_TRUE(expected.IsErr());
 }
 
