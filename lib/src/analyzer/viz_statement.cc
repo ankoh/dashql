e98ff43a7e228a304e87715d8382a358128d4dee
Andre Kohn <kohn.a@outlook.com>
2021-03-09 18:30:45 +0100

    Drop idea to group magically, sampling + sorting will do just fine

--- lib/src/analyzer/viz_statement.cc
+++ lib/src/analyzer/viz_statement.cc
@@ -164,15 +164,15 @@ void VizComponent::ReadFrom(size_t node_id) {
     constexpr size_t ID_POS_COLUMN = 3;
     constexpr size_t ID_POS_WIDTH = 4;
     constexpr size_t ID_POS_HEIGHT = 5;
-    constexpr size_t ID_PIVOT_X = 13;
-    constexpr size_t ID_PIVOT_Y = 29;
+    constexpr size_t ID_CLUSTER = 35;
+    constexpr size_t ID_STACK = 29;
     constexpr size_t ID_ORDER = 33;
     constexpr size_t ID_SAMPLES = 31;
     constexpr size_t ID_X = 6;
     constexpr size_t ID_Y = 7;
     constexpr size_t ID_Y0 = 8;
-    constexpr size_t ID_DATA_PIVOT_X = 12;
-    constexpr size_t ID_DATA_PIVOT_Y = 30;
+    constexpr size_t ID_DATA_CLUSTER = 12;
+    constexpr size_t ID_DATA_STACK = 30;
     constexpr size_t ID_DATA_ORDER = 34;
     constexpr size_t ID_DATA_SAMPLES = 32;
     constexpr size_t ID_DATA_X = 9;
@@ -204,14 +204,15 @@ void VizComponent::ReadFrom(size_t node_id) {
                 .MatchEnum(sx::NodeType::ENUM_DASHQL_VIZ_COMPONENT_TYPE),
             sxm::Attribute(sx::AttributeKey::DASHQL_VIZ_COMPONENT_TYPE_MODIFIERS, ID_TYPE_MODIFIERS)
                 .MatchUI32Bitmap(),
+            sxm::Option(sx::AttributeKey::DASHQL_OPTION_CLUSTER, ID_CLUSTER),
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_COLUMN, ID_COLUMN),
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_DATA, ID_DATA)
                 .MatchOptions()
                 .MatchChildren({
+                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_CLUSTER, ID_DATA_CLUSTER),
                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_ORDER, ID_DATA_ORDER),
-                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_PIVOT_X, ID_DATA_PIVOT_X),
-                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_PIVOT_Y, ID_DATA_PIVOT_Y),
                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_SAMPLES, ID_DATA_SAMPLES),
+                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_STACK, ID_DATA_STACK),
                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_X, ID_DATA_X),
                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_Y, ID_DATA_Y),
                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_Y0, ID_DATA_Y0),
@@ -231,8 +232,6 @@ void VizComponent::ReadFrom(size_t node_id) {
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_FILL, ID_FILL),
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_HEIGHT, ID_HEIGHT),
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_ORDER, ID_ORDER),
-            sxm::Option(sx::AttributeKey::DASHQL_OPTION_PIVOT_X, ID_PIVOT_X),
-            sxm::Option(sx::AttributeKey::DASHQL_OPTION_PIVOT_Y, ID_PIVOT_Y),
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_POSITION)
                 .MatchOptions()
                 .MatchChildren({
@@ -242,6 +241,7 @@ void VizComponent::ReadFrom(size_t node_id) {
                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_WIDTH, ID_POS_WIDTH),
                 }),
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_ROW, ID_ROW),
+            sxm::Option(sx::AttributeKey::DASHQL_OPTION_STACK, ID_STACK),
             sxm::Option(sx::AttributeKey::DASHQL_OPTION_STYLE)
                 .MatchOptions()
                 .MatchChildren({
@@ -266,7 +266,7 @@ void VizComponent::ReadFrom(size_t node_id) {
         });
     // clang-format on
 
-    std::array<NodeMatch, 35> matches;
+    std::array<NodeMatch, 36> matches;
     schema.Match(viz_stmt_.instance(), node_id, matches);
 
     // Read type
@@ -313,16 +313,16 @@ void VizComponent::ReadFrom(size_t node_id) {
     }
 
     /// Get data attributes
-    auto data_pivot_x = SelectAltOption("data.pivot_x", matches[ID_DATA_PIVOT_X].node_id, matches[ID_PIVOT_X].node_id);
-    auto data_pivot_y = SelectAltOption("data.pivot_y", matches[ID_DATA_PIVOT_Y].node_id, matches[ID_PIVOT_Y].node_id);
-    auto data_order = SelectAltOption("data.order", matches[ID_DATA_ORDER].node_id, matches[ID_ORDER].node_id);
+    auto data_cluster = SelectAltOption("data.cluster", matches[ID_DATA_CLUSTER].node_id, matches[ID_CLUSTER].node_id);
+    auto data_stack = SelectAltOption("data.pivot_by", matches[ID_DATA_STACK].node_id, matches[ID_STACK].node_id);
+    auto data_order = SelectAltOption("data.stack", matches[ID_DATA_ORDER].node_id, matches[ID_ORDER].node_id);
     auto data_samples = SelectAltOption("data.samples", matches[ID_DATA_SAMPLES].node_id, matches[ID_SAMPLES].node_id);
     auto data_x = SelectAltOption("data.x", matches[ID_DATA_X].node_id, matches[ID_X].node_id);
     auto data_y = SelectAltOption("data.y", matches[ID_DATA_Y].node_id, matches[ID_Y].node_id);
-    if (AnyOptionSet({data_x, data_y, data_pivot_x, data_pivot_y, data_order, data_samples})) {
+    if (AnyOptionSet({data_x, data_y, data_cluster, data_stack, data_order, data_samples})) {
         data_.emplace();
-        data_->pivot_x = viz_stmt_.instance_.ReadNodeValueOrNull(data_pivot_x).CastAsBool().value_or(false);
-        data_->pivot_y = viz_stmt_.instance_.ReadNodeValueOrNull(data_pivot_x).CastAsBool().value_or(false);
+        data_->cluster = ReadColumnRefs(data_cluster);
+        data_->stack = ReadColumnRefs(data_stack);
         data_->order = ReadColumnRefs(data_order);
         data_->x = ReadColumnRefs(data_x);
         data_->y = ReadColumnRefs(data_y);
@@ -522,14 +522,17 @@ flatbuffers::Offset<proto::viz::VizComponent> VizComponent::Pack(flatbuffers::Fl
     if (data_) {
         auto x = data_->x.empty() ? std::nullopt : std::optional{builder.CreateVectorOfStrings(data_->x)};
         auto y = data_->y.empty() ? std::nullopt : std::optional{builder.CreateVectorOfStrings(data_->y)};
+        auto cluster = data_->cluster.empty() ? std::nullopt : std::optional{builder.CreateVectorOfStrings(data_->cluster)};
+        auto stack = data_->stack.empty() ? std::nullopt : std::optional{builder.CreateVectorOfStrings(data_->stack)};
         auto order = data_->order.empty() ? std::nullopt : std::optional{builder.CreateVectorOfStrings(data_->order)};
 
         pv::VizDataBuilder dataBuilder{builder};
         if (x) dataBuilder.add_x(*x);
         if (y) dataBuilder.add_y(*y);
-        if (data_->pivot_x) dataBuilder.add_pivot_x(data_->pivot_x);
-        if (data_->pivot_y) dataBuilder.add_pivot_y(data_->pivot_y);
-        if (order) dataBuilder.add_x(*order);
+        if (cluster) dataBuilder.add_cluster(*cluster);
+        if (stack) dataBuilder.add_stack(*stack);
+        if (order) dataBuilder.add_order(*order);
+
         //dataBuilder.add_samples(data_->samples);
         data = dataBuilder.Finish();
     }
