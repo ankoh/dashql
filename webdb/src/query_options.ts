0dd2a47281325f79b9066a9254cda74d95710031
Andre Kohn <kohn.a@outlook.com>
2021-03-09 14:15:24 +0100

    First generated query for stacked chart

--- webdb/src/query_options.ts
+++ webdb/src/query_options.ts
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 The DashQL Authors
 
 export interface QueryRunOptions {
-    partitionBoundaries?: number[]
+    partitionBoundaries?: number[];
+}
+
+export function queryOptionsEqual(l?: QueryRunOptions, r?: QueryRunOptions) {
+    if (!l) return !r;
+    if (!r) return !l;
+    let eq = true;
+    if (l.partitionBoundaries) {
+        eq =
+            eq &&
+            !!r.partitionBoundaries &&
+            l.partitionBoundaries.length === r.partitionBoundaries.length &&
+            l.partitionBoundaries.every((v, i) => v === r.partitionBoundaries![i]);
+    }
+    return eq;
 }
