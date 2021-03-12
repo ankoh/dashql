1994dee4f2a8eeaba059372c6ada29d3fc24e0ac
Andre Kohn <kohn.a@outlook.com>
2021-03-12 09:27:00 +0100

    Add dedicated viz composer to streamline spec generation

---
+++ core/src/actions/viz_composer.ts
@@ -0,0 +1,159 @@
+import * as proto from '@dashql/proto';
+import * as model from '../model';
+import * as platform from '../platform';
+import * as v from 'vega';
+import * as vl from 'vega-lite';
+
+export class VizComposer {
+    /// The platform
+    _platform: platform.Platform;
+    /// The plan
+    _plan: model.Plan;
+    /// The table info (when fetched)
+    _table: model.DatabaseTableInfo;
+
+    /// The renderer type
+    _renderer: model.VizRendererType | null = null;
+    /// The required columns
+    _requiredColumnIds: Map<string, number> = new Map();
+    /// The required column list
+    _requiredColumns: string[] = [];
+    /// The vega-lite spec
+    _vegaLiteSpec: vl.TopLevelSpec | null = null;
+    /// The vega spec
+    _vegaSpec: v.Spec | null = null;
+
+    /// The data ordering (if any)
+    _orderBy: string[] | null = null;
+    /// The data partitioning (if any)
+    _partitionBy: string[] | null = null;
+
+    constructor(platform: platform.Platform, plan: model.Plan, table: model.DatabaseTableInfo) {
+        this._platform = platform;
+        this._plan = plan;
+        this._table = table;
+    }
+
+    /// Has a column?
+    protected hasColumn(column: string): boolean {
+        return this._table.columnNameMapping!.has(column);
+    }
+
+    /// Require a column
+    protected requireColumn(name: string) {
+        if (!this._requiredColumnIds.has(name)) {
+            this._requiredColumnIds.set(name, this._requiredColumns.length);
+            this._requiredColumns.push(name);
+        }
+    }
+
+    /// Resolve a column id by name
+    protected resolveColumnId(name: string) {
+        return this._requiredColumnIds.get(name)!;
+    }
+
+    /// Merge the user-provided data into a vega-lite spec
+    protected mergeVegaLiteSpec(spec: any) {
+        // XXX
+        this._vegaLiteSpec = {
+            ...spec,
+            autosize: 'fit',
+            resize: true,
+            title: undefined,
+        };
+    }
+
+    /// Analayze a single viz component
+    public addComponent(component: proto.analyzer.VizComponent) {
+        // Collect type
+        const type = component.type()!;
+        let typeModifiers: Map<proto.syntax.VizComponentTypeModifier, boolean> = new Map();
+        for (let i = 0; i < component.typeModifiersLength(); ++i) {
+            typeModifiers.set(component.typeModifiers(i)!, true);
+        }
+
+        switch (type) {
+            case proto.syntax.VizComponentType.VEGA:
+            case proto.syntax.VizComponentType.AREA:
+            case proto.syntax.VizComponentType.AXIS:
+            case proto.syntax.VizComponentType.BAR:
+            case proto.syntax.VizComponentType.BOX:
+            case proto.syntax.VizComponentType.CANDLESTICK:
+            case proto.syntax.VizComponentType.ERROR_BAR:
+            case proto.syntax.VizComponentType.HISTOGRAM:
+            case proto.syntax.VizComponentType.LINE:
+            case proto.syntax.VizComponentType.PIE:
+            case proto.syntax.VizComponentType.SCATTER:
+            case proto.syntax.VizComponentType.VORONOI: {
+                // XXX conflicts
+                this._renderer = model.VizRendererType.BUILTIN_VEGA;
+                break;
+            }
+            case proto.syntax.VizComponentType.NUMBER:
+            case proto.syntax.VizComponentType.TABLE:
+                // XXX conflicts
+                this._renderer = model.VizRendererType.BUILTIN_TABLE;
+                break;
+        }
+
+        // TODO This is literally doing nothing smart at the moment.
+        //      Let there be fancy vega autogen logic.
+
+        for (const c of this._table.columnNames || []) {
+            this.requireColumn(c);
+        }
+
+        const rawSpec = component.componentSpec();
+        if (rawSpec != null) {
+            let spec = JSON.parse(rawSpec);
+            this.mergeVegaLiteSpec(spec);
+        }
+    }
+
+    // Build the query
+    protected configureQuery(): model.VizDataSource {
+        const colNames = this._table.columnNameMapping;
+        const getColId = this.resolveColumnId.bind(this);
+        return {
+            targetShort: this._table.nameShort,
+            targetQualified: this._table.nameQualified,
+            columns: this._requiredColumns.map(c => colNames.get(c)!),
+            orderBy: this._orderBy?.map(getColId) || [],
+            partitionBy: this._partitionBy?.map(getColId) || [],
+        };
+    }
+
+    // Build the vega spec (if any)
+    protected configureVega(): v.Spec | null {
+        if (this._vegaLiteSpec == null) return null;
+
+        // just a hacky proof-of-concept for now
+        let compiled = vl.compile(this._vegaLiteSpec).spec;
+        return compiled;
+    }
+
+    public build(
+        base: Pick<
+            model.VizInfo,
+            Exclude<
+                keyof model.VizInfo,
+                'timeCreated' | 'timeUpdated' | 'nameQualified' | 'nameShort' | 'renderer' | 'data' | 'vegaLiteSpec' | 'vegaSpec'
+            >
+        >,
+    ): model.VizInfo {
+        const now = new Date();
+        const data = this.configureQuery();
+        const vegaSpec = this._renderer == model.VizRendererType.BUILTIN_VEGA ? this.configureVega() : null;
+        return {
+            ...base,
+            timeCreated: now,
+            timeUpdated: now,
+            nameQualified: this._table.nameQualified || '',
+            nameShort: this._table.nameShort || '',
+            renderer: this._renderer || model.VizRendererType.BUILTIN_TABLE,
+            data,
+            vegaLiteSpec: this._vegaLiteSpec,
+            vegaSpec,
+        };
+    }
+}
--- core/src/actions/viz_logic.ts
+++ core/src/actions/viz_logic.ts
@@ -2,172 +2,54 @@ import * as proto from '@dashql/proto';
 import * as webdb from '@dashql/webdb';
 import * as model from '../model';
 import * as error from '../error';
-import * as v from 'vega';
-import * as vl from 'vega-lite';
+import { VizComposer } from './viz_composer';
 import { ProgramActionLogic, SetupActionLogic } from './action_logic';
 import { ActionContext } from './action_context';
-import { ProgramInstance } from '../model';
 
 import ActionStatusCode = proto.action.ActionStatusCode;
 
 export abstract class VizActionLogic extends ProgramActionLogic {
-    /// The program instance
-    _programInstance: ProgramInstance | null = null;
     /// The viz spec
     _vizSpec: proto.analyzer.VizSpec | null = null;
-    /// The table info (when fetched)
-    _tableInfo: model.DatabaseTableInfo | null = null;
-
-    /// The renderer type
-    _renderer: model.VizRendererType | null = null;
-    /// The required columns
-    _requiredColumnIds: Map<string, number> = new Map();
-    /// The required column list
-    _requiredColumns: string[] = [];
-    /// The vega-lite spec
-    _vegaLiteSpec: vl.TopLevelSpec | null = null;
-
-    /// The data clustering (if any)
-    _clusterBy: string[] | null = null;
-    /// The data stacking (if any)
-    _stackBy: string[] | null = null;
-    /// The data ordering (if any)
-    _orderBy: string[] | null = null;
-    /// The data partitioning (if any)
-    _partitionBy: string[] | null = null;
+    /// The viz composer
+    _vizComposer: VizComposer | null = null;
 
     constructor(action_id: model.ActionHandle, action: proto.action.ProgramAction, statement: model.Statement) {
         super(action_id, action, statement);
     }
 
-    /// Get the qualified target name
+    /// Get the qualified table name
     protected get tableNameQualified() {
         return this.buffer.targetNameQualified()!;
     }
-    /// Has a column?
-    protected hasColumn(column: string): boolean {
-        return this._tableInfo!.columnNameMapping!.has(column);
-    }
 
     /// Read context info
-    public readContext(context: ActionContext) {
-        /// Get the program instance
-        this._programInstance = context.plan.programInstance;
+    public configureVizComposer(context: ActionContext) {
+        // Get the program instance
+        const programInstance = context.plan.programInstance;
         // Get viz spec
-        this._vizSpec = context.plan.programInstance.vizSpecs.get(this.origin.statementId) || null;
+        this._vizSpec = programInstance.vizSpecs.get(this.origin.statementId) || null;
         if (!this._vizSpec) {
-            throw new error.VizLogicError('viz spec does not exist', this._programInstance);
+            throw new error.VizLogicError('viz spec does not exist', programInstance);
         }
         // Get the table info
         const store = context.platform.store;
-        this._tableInfo = store.getState().core.planDatabaseTables.get(this.tableNameQualified) || null;
-        if (!this._tableInfo) {
-            throw new error.VizLogicError('target table does not exist', this._programInstance);
+        let tableInfo = store.getState().core.planDatabaseTables.get(this.tableNameQualified) || null;
+        if (!tableInfo) {
+            throw new error.VizLogicError('target table does not exist', programInstance);
         }
-    }
+        // Build the composer
+        this._vizComposer = new VizComposer(context.platform, context.plan, tableInfo);
 
-    /// Require a column
-    protected requireColumn(name: string) {
-        if (!this._requiredColumnIds.has(name)) {
-            this._requiredColumnIds.set(name, this._requiredColumns.length);
-            this._requiredColumns.push(name);
+        // Read the component specs
+        for (let i = 0; i < this._vizSpec.componentsLength(); ++i) {
+            const c = this._vizSpec.components(i)!;
+            this._vizComposer.addComponent(c)!;
         }
-    }
-
-    /// Resolve a column id by name
-    protected resolveColumnId(name: string) {
-        return this._requiredColumnIds.get(name)!;
-    }
-
-    /// Merge the user-provided data into a vega-lite spec
-    protected mergeVegaLiteSpec(spec: any) {
-        // XXX
-        console.log(spec);
-        this._vegaLiteSpec = {
-            ...spec,
-            autosize: "fit",
-            resize: true,
-            title: undefined
-        };
-    }
-
-    /// Analayze a single viz component
-    protected analyzeVizComponent(component: proto.analyzer.VizComponent) {
-        // Collect type
-        const type = component.type()!;
-        let typeModifiers: Map<proto.syntax.VizComponentTypeModifier, boolean> = new Map();
-        for (let i = 0; i < component.typeModifiersLength(); ++i) {
-            typeModifiers.set(component.typeModifiers(i)!, true);
-        }
-
-        switch (type) {
-            case proto.syntax.VizComponentType.VEGA:
-            case proto.syntax.VizComponentType.AREA:
-            case proto.syntax.VizComponentType.AXIS:
-            case proto.syntax.VizComponentType.BAR:
-            case proto.syntax.VizComponentType.BOX:
-            case proto.syntax.VizComponentType.CANDLESTICK:
-            case proto.syntax.VizComponentType.ERROR_BAR:
-            case proto.syntax.VizComponentType.HISTOGRAM:
-            case proto.syntax.VizComponentType.LINE:
-            case proto.syntax.VizComponentType.PIE:
-            case proto.syntax.VizComponentType.SCATTER:
-            case proto.syntax.VizComponentType.VORONOI: {
-                // XXX conflicts
-                this._renderer = model.VizRendererType.BUILTIN_VEGA;
-                break;
-            }
-            case proto.syntax.VizComponentType.NUMBER:
-            case proto.syntax.VizComponentType.TABLE:
-                // XXX conflicts
-                this._renderer = model.VizRendererType.BUILTIN_TABLE;
-                break;
-        }
-
-        // TODO This is literally doing nothing smart at the moment.
-        //      Let there be fancy vega autogen logic.
-
-        for (const c of this._tableInfo?.columnNames || []) {
-            this.requireColumn(c);
-        }
-
-        const rawSpec = component.componentSpec();
-        if (rawSpec != null) {
-            let spec = JSON.parse(rawSpec);
-            console.log(spec);
-            this.mergeVegaLiteSpec(spec);
-        }
-    }
-
-    // Build the query
-    protected buildQuery(): model.VizDataSource {
-        const colNames = this._tableInfo!.columnNameMapping;
-        const getColId = this.resolveColumnId.bind(this);
-        return {
-            targetShort: this.buffer.targetNameShort()!,
-            targetQualified: this.buffer.targetNameQualified()!,
-            columns: this._requiredColumns.map((c) => colNames.get(c)!),
-            orderBy: this._orderBy?.map(getColId) || [],
-            clusterBy: this._clusterBy?.map(getColId) || [],
-            stackBy: this._stackBy?.map(getColId) || [],
-            partitionBy: this._partitionBy?.map(getColId) || [],
-        }
-    }
-
-    // Build the vega spec (if any)
-    protected buildVegaSpec(): v.Spec | null {
-        if (this._vegaLiteSpec == null) 
-            return null;
-
-        // just a hacky proof-of-concept for now
-        let compiled = vl.compile(this._vegaLiteSpec).spec;
-        console.log(compiled);
-        return compiled;
     }
 
     /// Derive viz renderer
-    protected deriveVizInfo(context: ActionContext, mixin: Partial<model.VizInfo> = {}): model.VizInfo {
-        // Read position
+    protected deriveVizInfo(context: ActionContext): model.VizInfo {
         const posReader = this._vizSpec!.position()!;
         const pos: model.VizPosition = {
             row: posReader.row(),
@@ -175,28 +57,13 @@ export abstract class VizActionLogic extends ProgramActionLogic {
             width: posReader.width(),
             height: posReader.height(),
         };
-
-        // Read the component specs
-        for (let i = 0; i < this._vizSpec!.componentsLength(); ++i) {
-            const c = this._vizSpec!.components(i)!;
-            this.analyzeVizComponent(c)!;
-        }
-
-        const now = new Date();
-        return {
+        return this._vizComposer!.build({
             objectId: this.buffer.objectId(),
             objectType: model.PlanObjectType.VIZ_INFO,
-            timeCreated: now,
-            timeUpdated: now,
-            nameQualified: this.buffer.targetNameQualified() || '',
-            nameShort: this.buffer.targetNameShort() || '',
-            renderer: this._renderer || model.VizRendererType.BUILTIN_TABLE,
             currentStatementId: this.origin.statementId,
             title: this._vizSpec!.title() || null,
             position: pos,
-            data: this.buildQuery(),
-            vegaSpec: this._renderer == model.VizRendererType.BUILTIN_VEGA ? this.buildVegaSpec() : null,
-        };
+        });
     }
 }
 
@@ -209,7 +76,7 @@ export class CreateVizActionLogic extends VizActionLogic {
     }
 
     public prepareExecution(context: ActionContext) {
-        this.readContext(context);
+        this.configureVizComposer(context);
         this._rowCountPromise = context.platform.database.requestTableStatistics(
             this.tableNameQualified,
             model.TableStatisticsType.COUNT_STAR,
