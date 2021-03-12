e48ffe23f79587bfbe07649067c8714deeeec20f
Andre Kohn <kohn.a@outlook.com>
2021-03-12 14:34:51 +0100

    Add empty gallery tab

--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -1,8 +1,10 @@
 import Launcher from "./launcher";
 import Studio from "./studio";
+import Gallery from "./gallery";
 export { NotFound } from "./not_found";
 
 export {
     Launcher,
-    Studio
+    Studio,
+    Gallery
 }
