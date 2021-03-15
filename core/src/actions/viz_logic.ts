6197ac47b4d6a9e4935d6728c3fc617e907ccebd
Andre Kohn <kohn.a@outlook.com>
2021-03-15 08:49:22 +0100

    Rename options json in proto

--- core/src/actions/viz_logic.ts
+++ core/src/actions/viz_logic.ts
@@ -50,7 +50,7 @@ export abstract class VizActionLogic extends ProgramActionLogic {
             for (let i = 0; i < c.typeModifiersLength(); ++i) {
                 mods.set(c.typeModifiers(i)!, true);
             }
-            const optionsJSON = c.componentSpec() || "";
+            const optionsJSON = c.options() || "";
             const options = JSON.parse(optionsJSON);
             this._vizComposer.addComponent(type, mods, options)!;
         }
