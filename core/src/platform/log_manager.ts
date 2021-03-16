8174636e569355623a9b7692d60e13cfd731b3e8
Andre Kohn <kohn.a@outlook.com>
2021-03-16 12:21:53 +0100

    Also log warnings and errors to console

--- core/src/platform/log_manager.ts
+++ core/src/platform/log_manager.ts
@@ -16,8 +16,17 @@ export class LogManager implements Logger {
     }
 
     public log(entry: model.LogEntryVariant) {
-        // XXX Rotate if full
-        // XXX Eventually push to service bus 
+        // Log errors and warnings to console
+        switch (entry.level) {
+            case model.LogLevel.WARNING:
+                console.warn(entry);
+                break;
+            case model.LogLevel.ERROR:
+                console.error(entry);
+                break;
+        }
+
+        // Store in redux
         model.mutate(this._store.dispatch, {
             type: model.StateMutationType.LOG_PUSH_ENTRY,
             data: entry,
