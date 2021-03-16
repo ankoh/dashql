be866b6a7a7b5e1cac1a679f1714d00a3afb99ce
Dakror <mail@dakror.de>
2021-03-16 22:26:14 +0100

    runtime file refactor

--- webdb/src/webdb_bindings.ts
+++ webdb/src/webdb_bindings.ts
@@ -6,58 +6,6 @@ import { flatbuffers } from 'flatbuffers';
 import { Logger } from './log';
 import { QueryRunOptions } from './query_options';
 
-export var DefaultWebDBRuntime = {
-    bindings: null,
-    dashql_blob_stream_underflow: function (blobId: number, buf: number, size: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_read: function (blobId: number, buf: number, bytes: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_write: function (blobId: number, buf: number, bytes: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_directory_exists: function (pathPtr: number, pathLen: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_directory_create: function (pathPtr: number, pathLen: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_directory_remove: function (pathPtr: number, pathLen: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_directory_list_files: function (pathPtr: number, pathLen: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_glob: function (pathPtr: number, pathLen: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_open: function (pathPtr: number, pathLen: number, flags: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_close: function (blobId: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_get_size: function (blobId: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_get_last_modified_time: function (blobId: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_move: function (fromPtr: number, fromLen: number, toPtr: number, toLen: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_set_pointer: function (blobId: number, location: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_exists: function (pathPtr: number, pathLen: number) {
-        throw Error('undefined');
-    },
-    dashql_webdb_fs_file_remove: function (pathPtr: number, pathLen: number) {
-        throw Error('undefined');
-    },
-};
-
 /// Decode a string
 function decodeString(buffer: Uint8Array): string {
     var result = '';
