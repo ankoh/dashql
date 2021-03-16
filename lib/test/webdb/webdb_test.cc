7176cdb0f857f9463095c856c7a77a200bb1144d
Dakror <mail@dakror.de>
2021-03-16 20:10:38 +0100

    parquest extension and test file

--- lib/test/webdb/webdb_test.cc
+++ lib/test/webdb/webdb_test.cc
@@ -9,6 +9,7 @@
 #include "duckdb/common/types/date.hpp"
 #include "duckdb/common/types/timestamp.hpp"
 #include "gtest/gtest.h"
+#include "parquet-extension.hpp"
 
 using namespace dashql::webdb;
 using namespace std;
@@ -24,14 +25,25 @@ TEST(WebDB, InvalidSQL) {
     ASSERT_TRUE(expected.IsErr());
 }
 
-TEST(WebDB, LoadCSV) {
+TEST(WebDB, LoadParquet) {
     auto db = make_shared<duckdb::DuckDB>();
-    WebDB::Connection conn{db};
-    auto expected = conn.RunQuery(R"RAW(
-        SELECT * FROM read_csv_auto('/home/dakror/Desktop/dashql/lib/test/test.csv');
-    )RAW",
-                                  {});
-    ASSERT_TRUE(expected.IsOk());
+    db->LoadExtension<duckdb::ParquetExtension>();
+
+    auto con = duckdb::Connection{*db};
+    auto result = con.Query(R"RAW(
+        SELECT * FROM parquet_scan('/home/dakror/Desktop/dashql/webdb/test/studenten.parquet');
+    )RAW");
+    ASSERT_STREQ(result->ToString().c_str(),
+                 "MatrNr\tName\tSemester\t\nINTEGER\tVARCHAR\tINTEGER\t\n"
+                 "[ Rows: 8]\n"
+                 "24002\tXenokrates\t18\t\n"
+                 "25403\tJonas\t12\t\n"
+                 "26120\tFichte\t10\t\n"
+                 "26830\tAristoxenos\t8\t\n"
+                 "27550\tSchopenhauer\t6\t\n"
+                 "28106\tCarnap\t3\t\n"
+                 "29120\tTheophrastos\t2\t\n"
+                 "29555\tFeuerbach\t2\t\n\n");
 }
 
 }  // namespace
