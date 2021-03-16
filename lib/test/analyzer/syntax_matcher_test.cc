a07cb650b419ef24d9e5194404475a21846589bc
Andre Kohn <kohn.a@outlook.com>
2021-03-16 22:52:45 +0100

    SQLJSON pretty printing

--- lib/test/analyzer/syntax_matcher_test.cc
+++ lib/test/analyzer/syntax_matcher_test.cc
@@ -112,13 +112,13 @@ TEST(SyntaxMatcherTest, VizStatementPositionShort) {
                             sxm::Attribute(sx::AttributeKey::DASHQL_OPTION_POSITION, 9)
                                 .MatchOptions()
                                 .MatchChildren({
-                                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_COLUMN, 0)
-                                        .MatchString(),
-                                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_HEIGHT, 1)
-                                        .MatchString(),
                                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_ROW, 2)
                                         .MatchString(),
+                                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_COLUMN, 0)
+                                        .MatchString(),
                                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_WIDTH, 3)
+                                        .MatchString(),
+                                    sxm::Option(sx::AttributeKey::DASHQL_OPTION_HEIGHT, 1)
                                         .MatchString(),
                                     sxm::Option(sx::AttributeKey::DASHQL_OPTION_X, 4)
                                         .MatchString(),
