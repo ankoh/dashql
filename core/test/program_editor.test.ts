2f50058d2f806127c9a581a9d1155f50d4ed45f8
Andre Kohn <kohn.a@outlook.com>
2021-03-17 11:14:21 +0100

    Update ts program editor test

--- core/test/program_editor.test.ts
+++ core/test/program_editor.test.ts
@@ -33,12 +33,28 @@ describe('Program editor', () => {
         ]);
         expect(n).not.toEqual(null);
         expect(n!.program.buffer.statementsLength()).toEqual(1);
-        const expected = "VIZ weather_avg USING LINE (\n    position = (row = 1, column = 2, width = 3, height = 4)\n)";
+        const expected =
+`VIZ weather_avg USING LINE (
+    position = (
+        row = 1,
+        column = 2,
+        width = 3,
+        height = 4
+    )
+)`;
         expect(n!.program.text).toEqual(expected);
     });
 
     test('update viz position', () => {
-        const p = az.parseProgram("VIZ weather_avg USING LINE (\n    position = (row = 1, column = 2, width = 3, height = 4)\n)");
+        const p = az.parseProgram(
+`VIZ weather_avg USING LINE (
+    position = (
+        row = 1,
+        column = 2,
+        width = 3,
+        height = 4
+    )
+)`);
         const pi = az.instantiateProgram();
         expect(p.buffer.statementsLength()).toEqual(1);
         expect(pi).not.toEqual(null);
@@ -56,7 +72,15 @@ describe('Program editor', () => {
         ]);
         expect(n).not.toEqual(null);
         expect(n!.program.buffer.statementsLength()).toEqual(1);
-        const expected = "VIZ weather_avg USING LINE (\n    position = (row = 10, column = 9, width = 8, height = 7)\n)";
+        const expected =
+`VIZ weather_avg USING LINE (
+    position = (
+        row = 10,
+        column = 9,
+        width = 8,
+        height = 7
+    )
+)`;
         expect(n!.program.text).toEqual(expected);
     });
 });
