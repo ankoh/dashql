5153ac0ad74fa36558690167154a80886c0f66dd
Andre Kohn <kohn.a@outlook.com>
2021-03-17 09:21:08 +0100

    Patching

--- lib/include/dashql/analyzer/json.h
+++ lib/include/dashql/analyzer/json.h
@@ -1,7 +1,10 @@
 #ifndef INCLUDE_DASHQL_ANALYZER_JSON_H_
 #define INCLUDE_DASHQL_ANALYZER_JSON_H_
 
+#include <rapidjson/document.h>
+
 #include "dashql/analyzer/program_instance.h"
+#include "dashql/proto_generated.h"
 #include "rapidjson/writer.h"
 
 namespace dashql {
@@ -9,9 +12,11 @@ namespace dashql {
 /// The writer type
 enum JSONWriterType { JSON, JSON_PRETTY, SQLJSON_PRETTY };
 
+using PatchedOption = std::pair<sx::AttributeKey, rapidjson::Document>;
+
 /// Read all options as JSON
-void writeOptionsAsJSON(ProgramInstance& instance, size_t node_id, std::ostream& out,
-                        JSONWriterType writer = JSONWriterType::JSON);
+void writeOptionsAsJSON(ProgramInstance& instance, size_t node_id, std::ostream& out, JSONWriterType writer,
+                        const std::unordered_map<size_t, PatchedOption>& patches);
 
 }  // namespace dashql
 
