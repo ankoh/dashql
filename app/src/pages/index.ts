0cb0043e5a179192312f8d36e283ec1e418f04e3
Andre Kohn <kohn.a@outlook.com>
2021-03-17 12:51:19 +0100

    Add first viewer page

--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -1,10 +1,12 @@
 import Launcher from "./launcher";
 import Studio from "./studio";
 import Explorer from "./explorer";
+import Viewer from "./viewer";
 export { NotFound } from "./not_found";
 
 export {
     Launcher,
     Studio,
-    Explorer
+    Explorer,
+    Viewer
 }
