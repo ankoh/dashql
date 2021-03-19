bbbc84ab2f669eec3519748711ede7611d7e75e0
Andre Kohn <kohn.a@outlook.com>
2021-03-19 13:44:54 +0100

    Add empty example scripts that must be done for first release

--- app/src/example_scripts.ts
+++ app/src/example_scripts.ts
@@ -2,29 +2,13 @@ import * as core from "@dashql/core";
 import * as model from "./model";
 import axios from 'axios';
 
-import example_helloworld from '../static/examples/helloworld.dashql';
-
-import icon_dashboard from '../static/svg/icons/dashboard.svg';
-import icon_linechart from '../static/svg/icons/line_chart.svg';
-import icon_areachart from '../static/svg/icons/area_chart.svg';
-import icon_barchart from '../static/svg/icons/bar_chart.svg';
-import icon_histogramchart from '../static/svg/icons/histogram_chart.svg';
-import icon_arcchart from '../static/svg/icons/arc_chart.svg';
-import icon_scatterchart from '../static/svg/icons/scatter_chart.svg';
-import icon_streamgraph from '../static/svg/icons/streamgraph.svg';
-import icon_heatmapchart from '../static/svg/icons/heatmap_chart.svg';
-import icon_gesture_tap_hold from '../static/svg/icons/gesture_tap_hold.svg';
-import icon_database_import from '../static/svg/icons/database_import.svg';
-import icon_database_search from '../static/svg/icons/database_search.svg';
-import icon_package_down from '../static/svg/icons/package_down.svg';
-
 export enum ExampleScriptTag {
     HELLOWORLD
 }
 
 export async function loadExampleScript(script: ExampleScriptTag, store: model.AppReduxStore) {
     try {
-        const resp = await axios.get(example_helloworld);
+        const resp = await axios.get(example_demo_helloworld);
         if (resp.status != 200) {
             console.error(`Loading example ${script.toString()} failed with error: ${resp.statusText}`);
             return;
@@ -46,6 +30,44 @@ export async function loadExampleScript(script: ExampleScriptTag, store: model.A
         console.error(`Loading example ${script.toString()} failed with error: ${e}`);
     }
 }
+
+import example_demo_helloworld from '../static/examples/demo_helloworld.dashql';
+import example_demo_unischema from '../static/examples/demo_unischema.dashql';
+import example_extract_csv from '../static/examples/extract_csv.dashql';
+import example_extract_parquet from '../static/examples/extract_parquet.dashql';
+import example_load_http_dynamic from '../static/examples/load_http_dynamic.dashql';
+import example_load_http_static from '../static/examples/load_http_static.dashql';
+import example_sql_approxmedian from '../static/examples/sql_approxmedian.dashql';
+import example_sql_complexjoins from '../static/examples/sql_complexjoins.dashql';
+import example_sql_explicitgrouping from '../static/examples/sql_explicitgrouping.dashql';
+import example_sql_movingavg from '../static/examples/sql_movingavg.dashql';
+import example_sql_patternmatching from '../static/examples/sql_patternmatching.dashql';
+import example_sql_runningsum from '../static/examples/sql_runningsum.dashql';
+import example_sql_sampling from '../static/examples/sql_sampling.dashql';
+import example_viz_area_charts from '../static/examples/viz_area_charts.dashql';
+import example_viz_bar_charts from '../static/examples/viz_bar_charts.dashql';
+import example_viz_heatmaps from '../static/examples/viz_heatmaps.dashql';
+import example_viz_histograms from '../static/examples/viz_histograms.dashql';
+import example_viz_interaction from '../static/examples/viz_interaction.dashql';
+import example_viz_line_charts from '../static/examples/viz_line_charts.dashql';
+import example_viz_pie_charts from '../static/examples/viz_pie_charts.dashql';
+import example_viz_scatter_charts from '../static/examples/viz_scatter_charts.dashql';
+import example_viz_streamgraphs from '../static/examples/viz_streamgraphs.dashql';
+
+import icon_dashboard from '../static/svg/icons/dashboard.svg';
+import icon_linechart from '../static/svg/icons/line_chart.svg';
+import icon_areachart from '../static/svg/icons/area_chart.svg';
+import icon_barchart from '../static/svg/icons/bar_chart.svg';
+import icon_histogramchart from '../static/svg/icons/histogram_chart.svg';
+import icon_arcchart from '../static/svg/icons/arc_chart.svg';
+import icon_scatterchart from '../static/svg/icons/scatter_chart.svg';
+import icon_streamgraph from '../static/svg/icons/streamgraph.svg';
+import icon_heatmapchart from '../static/svg/icons/heatmap_chart.svg';
+import icon_gesture_tap_hold from '../static/svg/icons/gesture_tap_hold.svg';
+import icon_database_import from '../static/svg/icons/database_import.svg';
+import icon_database_search from '../static/svg/icons/database_search.svg';
+import icon_package_down from '../static/svg/icons/package_down.svg';
+
 
 export enum ScriptFeatureTag {
     HTTP_SOURCE,
@@ -77,7 +99,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         icon: icon_dashboard,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_)
             .set(ScriptFeatureTag.DATA_CSV),
-        url: example_helloworld,
+        url: example_demo_helloworld,
         enabled: true
     },
     {
@@ -87,7 +109,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "A third test hello world script",
         icon: icon_dashboard,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_demo_unischema,
         enabled: false
     },
     {
@@ -97,7 +119,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "A test hello world script",
         icon: icon_linechart,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_line_charts,
         enabled: false
     },
     {
@@ -107,7 +129,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "A second test hello world script",
         icon: icon_areachart,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_area_charts,
         enabled: false
     },
     {
@@ -117,7 +139,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "Fooo",
         icon: icon_barchart,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_bar_charts,
         enabled: false
     },
     {
@@ -127,7 +149,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "Fooo",
         icon: icon_arcchart,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_pie_charts,
         enabled: false
     },
     {
@@ -137,7 +159,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "Fooo",
         icon: icon_scatterchart,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_scatter_charts,
         enabled: false
     },
     {
@@ -147,7 +169,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "Fooo",
         icon: icon_histogramchart,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_histograms,
         enabled: false
     },
     {
@@ -157,7 +179,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "Fooo",
         icon: icon_streamgraph,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_streamgraphs,
         enabled: false
     },
     {
@@ -167,7 +189,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "Fooo",
         icon: icon_heatmapchart,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_heatmaps,
         enabled: false
     },
     {
@@ -177,27 +199,27 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_gesture_tap_hold,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_viz_interaction,
         enabled: false
     },
     {
-        key: "load/http",
+        key: "load/http_static",
         collection: "Load",
         title: "Static HTTP",
         description: "FOOOOO",
         icon: icon_package_down,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_load_http_static,
         enabled: false
     },
     {
-        key: "load/httpparam",
+        key: "load/http_dynamic",
         collection: "Load",
         title: "Dynamic HTTP",
         description: "FOOOOO",
         icon: icon_package_down,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_load_http_static,
         enabled: false
     },
     {
@@ -207,7 +229,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_import,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_extract_csv,
         enabled: false
     },
     {
@@ -217,7 +239,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_import,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_extract_parquet,
         enabled: false
     },
     {
@@ -227,7 +249,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_search,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_sql_complexjoins,
         enabled: false
     },
     {
@@ -237,7 +259,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_search,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_sql_explicitgrouping,
         enabled: false
     },
     {
@@ -247,7 +269,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_search,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_sql_movingavg,
         enabled: false
     },
     {
@@ -257,7 +279,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_search,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_sql_runningsum,
         enabled: false
     },
     {
@@ -267,7 +289,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_search,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_sql_approxmedian,
         enabled: false
     },
     {
@@ -277,7 +299,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_search,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_sql_sampling,
         enabled: false
     },
     {
@@ -287,7 +309,7 @@ export const EXAMPLE_SCRIPTS: ExampleScriptMetadata[] = [
         description: "FOOOOO",
         icon: icon_database_search,
         features: new core.utils.NativeBitmap(ScriptFeatureTag._COUNT_),
-        url: example_helloworld,
+        url: example_sql_patternmatching,
         enabled: false
     },
 ];
