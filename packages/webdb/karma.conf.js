332b27ffb2b03bec59083ba688673250c59278b3
Andre Kohn <kohn.a@outlook.com>
2021-03-21 22:39:57 +0100

    Run chrome with --no-sandbox

--- packages/webdb/karma.conf.js
+++ packages/webdb/karma.conf.js
@@ -70,7 +70,13 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeHeadless'],
         singleRun: true,
+        browsers: ['ChromeHeadlessNoSandbox'],
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox'],
+            },
+        },
     });
 };
