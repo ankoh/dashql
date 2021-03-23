7180f24b26847e10cb51912ce6f9a7593a48f8e9
Andre Kohn <kohn.a@outlook.com>
2021-03-23 15:47:34 +0100

    Bundle webdb more carefully

---
+++ packages/webdb/src/targets/sync_browser.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 The DashQL Authors
+
+export * from '../iterator';
+export * from '../iterator_base';
+export * from '../log';
+export * from '../proxy';
+export * from '../query_options';
+export * from '../value';
+export * from '../webdb_runtime';
+export * from '../webdb_bindings';
+export * from '../webdb_bindings_web';
--- packages/webdb/types/web-worker.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'web-worker' {
-    const _default: any;
-    export default _default;
-}
