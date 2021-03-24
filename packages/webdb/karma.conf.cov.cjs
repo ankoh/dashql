9d87d25b6d6611e21135897f2644344aabece647
Andre Kohn <kohn.a@outlook.com>
2021-03-24 23:32:00 +0100

    Track code coverage for core and webdb in test:ci

--- packages/webdb/karma.conf.cov.cjs
+++ packages/webdb/karma.conf.cov.cjs
@@ -1,6 +1,6 @@
-const path = require('path');
+const puppeteer = require('puppeteer');
 
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function (config) {
     config.set({
@@ -11,30 +11,23 @@ module.exports = function (config) {
             'karma-firefox-launcher',
             'karma-sourcemap-loader',
             'karma-spec-reporter',
+            'karma-coverage',
         ],
         frameworks: ['jasmine'],
         files: [
             { pattern: 'dist/tests-browser.js' },
-            { pattern: 'src/analyzer/analyzer_wasm.wasm', included: false, watched: false, served: true },
-            { pattern: '../webdb/dist/webdb.wasm', included: false, watched: false, served: true },
-            {
-                pattern: '../webdb/dist/webdb-browser-parallel.worker.js',
-                included: false,
-                watched: false,
-                served: true,
-            },
+            { pattern: 'dist/*.wasm', included: false, watched: false, served: true },
+            { pattern: 'dist/*.js', included: false, watched: false, served: true },
         ],
         preprocessors: {
-            '**/*.js': ['sourcemap'],
+            '**/*.js': ['sourcemap', 'coverage'],
         },
         proxies: {
-            '/static/analyzer_wasm.wasm': '/base/src/analyzer/analyzer_wasm.wasm',
-            '/static/webdb.wasm': '/absolute' + path.resolve('../webdb/dist/webdb.wasm'),
-            '/static/webdb-browser-parallel.worker.js':
-                '/absolute' + path.resolve('../webdb/dist/webdb-browser-parallel.worker.js'),
+            '/static/webdb.wasm': '/base/dist/webdb.wasm',
+            '/static/webdb-browser-parallel.worker.js': '/base/dist/webdb-browser-parallel.worker.js',
         },
         exclude: [],
-        reporters: ['dots'],
+        reporters: ['dots', 'coverage'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
