4bd230bf5c137c53c1d6a843d8b44a6689a2f3e9
Andre Kohn <kohn.a@outlook.com>
2021-03-24 11:16:53 +0100

    Write webdb wasm to correct directory

--- scripts/wasm_build_lib.sh
+++ scripts/wasm_build_lib.sh
@@ -13,7 +13,7 @@ echo "MODE=${MODE}"
 CPP_BUILD_DIR="${PROJECT_ROOT}/lib/build/wasm/${MODE}"
 CPP_SOURCE_DIR="${PROJECT_ROOT}/lib"
 ANALYZER_LIB_DIR="${PROJECT_ROOT}/packages/core/src/analyzer"
-WEBDB_LIB_DIR="${PROJECT_ROOT}/packages/webdb/src/"
+WEBDB_LIB_DIR="${PROJECT_ROOT}/packages/webdb/src/bindings"
 
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 
