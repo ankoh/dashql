7952b843354b886966e5c70ed4ace8121f8972c4
Andre Kohn <kohn.a@outlook.com>
2021-03-24 22:32:21 +0100

    Code coverage for karma and node

--- packages/webdb/karma.conf.cjs
+++ packages/webdb/karma.conf.cjs
@@ -8,18 +8,29 @@ process.env.CHROME_BIN = puppeteer.executablePath();
 module.exports = function (config) {
     config.set({
         basePath: './',
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-sourcemap-loader',
+            'karma-spec-reporter',
+            'karma-coverage',
+        ],
         frameworks: ['jasmine'],
         files: [
             { pattern: 'dist/tests-browser.js' },
             { pattern: 'dist/*.wasm', included: false, watched: false, served: true },
             { pattern: 'dist/*.js', included: false, watched: false, served: true },
         ],
+        preprocessors: {
+            '**/*.js': ['sourcemap', 'coverage'],
+        },
         proxies: {
             '/static/webdb.wasm': '/base/dist/webdb.wasm',
             '/static/webdb-browser-parallel.worker.js': '/base/dist/webdb-browser-parallel.worker.js',
         },
         exclude: [],
-        reporters: ['dots'],
+        reporters: ['dots', 'coverage'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
@@ -30,6 +41,18 @@ module.exports = function (config) {
             ChromeHeadlessNoSandbox: {
                 base: 'ChromeHeadless',
                 flags: ['--no-sandbox'],
+            },
+        },
+        coverageReporter: {
+            type: 'json',
+            dir: './coverage/',
+            subdir: function (browser) {
+                return browser.toLowerCase().split(/[ /-]/)[0];
+            },
+        },
+        client: {
+            jasmine: {
+                failFast: true,
             },
         },
     });
