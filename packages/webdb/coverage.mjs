746cea3c5c8a9548c27b0ee2231e8b98dbdc2776
Andre Kohn <kohn.a@outlook.com>
2021-03-24 23:07:26 +0100

    Work on coverage reporting

---
+++ packages/webdb/coverage.mjs
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import mkdir from 'make-dir';
+import rimraf from 'rimraf';
+import { spawnSync } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const cov = path.resolve(__dirname, 'coverage');
+const cov_chrome = path.resolve(cov, 'chrome', 'coverage-final.json');
+const cov_firefox = path.resolve(cov, 'firefox', 'coverage-final.json');
+const cov_node = path.resolve(cov, 'node', 'coverage-final.json');
+const cov_all = path.resolve(cov, 'all');
+const cov_out = path.resolve(cov, 'coverage.json');
+const nyc = '../../node_modules/nyc/bin/nyc.js';
+
+let out = spawnSync(nyc, ['report', '-r json', '--report-dir ./coverage/node'], {
+    encoding: 'utf8',
+    shell: true,
+    __dirname,
+});
+console.log(out.stdout);
+if (out.status !== 0) {
+    console.error(out.stderr);
+    process.exit(out.status);
+}
+
+rimraf.sync(cov_all);
+mkdir.sync(cov_all);
+fs.copyFileSync(cov_chrome, path.resolve(cov_all, 'chrome.json'));
+fs.copyFileSync(cov_firefox, path.resolve(cov_all, 'firefox.json'));
+fs.copyFileSync(cov_node, path.resolve(cov_all, 'node.json'));
+
+out = spawnSync(nyc, ['merge', cov_all, cov_out], {
+    encoding: 'utf8',
+    shell: true,
+    cov,
+});
+console.log(out.stdout);
+if (out.status !== 0) {
+    console.error(out.stderr);
+    process.exit(out.status);
+}
