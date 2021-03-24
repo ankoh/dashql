15d86950b8db570ed7b2b04d4c26e1d5b6e543e4
Andre Kohn <kohn.a@outlook.com>
2021-03-24 17:43:59 +0100

    Make tests cross-platform

---
+++ packages/webdb/test/cross/index_node.ts
@@ -0,0 +1,21 @@
+import * as webdb from '../../src/platform/node/index_serial';
+import path from 'path';
+
+let db: webdb.WebDB | null = null;
+
+beforeAll(async () => {
+    const logger = new webdb.ConsoleLogger();
+    const wasm = path.resolve(__dirname, './webdb.wasm');
+    db = new webdb.WebDB(logger, webdb.DefaultWebDBRuntime, wasm);
+    await db.open();
+});
+
+afterAll(() => {});
+
+import { testProxies } from './proxy.test';
+import { testBindings } from './bindings.test';
+import { testIterator } from './iterator.test';
+
+testProxies(() => db!);
+testBindings(() => db!);
+testIterator(() => db!);
