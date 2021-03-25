0dc036dacd331ddae60b2132bf49fd4a9569749b
Dakror <mail@dakror.de>
2021-03-25 08:01:48 +0100

    parquet working

--- packages/webdb/karma.conf.cjs
+++ packages/webdb/karma.conf.cjs
@@ -32,7 +32,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        singleRun: false,
+        singleRun: true,
         browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
