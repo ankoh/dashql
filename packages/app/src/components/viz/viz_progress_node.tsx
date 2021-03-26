1781b6797bdd2622ba467347c550f563cb6288e7
Andre Kohn <kohn.a@outlook.com>
2021-03-26 19:21:39 +0100

    Viz progress component

---
+++ packages/app/src/components/viz/viz_progress_node.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Handle as ReactFlowHandle, Node as NodeData, Position } from 'react-flow-renderer';
+import { ActionStatusIndicator } from '../status';
+import { proto } from '@dashql/core';
+
+import styles from './viz_progress.module.css';
+
+export interface StatementNodeData extends NodeData {
+    statementId: number;
+    data: {
+        statementType: proto.syntax.StatementType;
+        actionStatus: proto.action.ActionStatusCode | null;
+    };
+}
+
+export function StatementNode(props: StatementNodeData) {
+    return (
+        <div className={styles.node}>
+            <div className={styles.node_status}>
+                <ActionStatusIndicator
+                    className={styles.node_status_spinner}
+                    fill="rgb(80, 80, 80)"
+                    width="14px"
+                    height="14px"
+                    status={props.data.actionStatus}
+                />
+            </div>
+            <ReactFlowHandle type="target" position={Position.Top} className={styles.node_handle_top} />
+            <ReactFlowHandle type="source" position={Position.Bottom} className={styles.node_handle_bottom} />
+        </div>
+    );
+}
