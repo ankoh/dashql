131e32abbe9c5a86d2facb51fc650e09c1371b8a
Andre Kohn <kohn.a@outlook.com>
2021-03-26 12:58:22 +0100

    Reproduce ObservableHQ bugs in test

--- packages/webdb/test/async_iterator.test.ts
+++ packages/webdb/test/async_iterator.test.ts
@@ -2,7 +2,7 @@ import * as webdb from '../src/';
 
 const testRows = 3000;
 
-export function testAsyncIterator(db: () => webdb.AsyncWebDB, basedir: string) {
+export function testAsyncIterator(db: () => webdb.AsyncWebDB) {
     let conn: webdb.AsyncWebDBConnection;
 
     beforeEach(async () => {
@@ -34,65 +34,6 @@ export function testAsyncIterator(db: () => webdb.AsyncWebDB, basedir: string) {
                     }
                 }
                 expect(i).toBe(testRows + 1);
-            });
-        });
-
-        describe('uni-schema from parquet', () => {
-            it('single table', async () => {
-                await db().registerURL(`${basedir}/studenten.parquet`);
-
-                let result = await conn.sendQuery(`SELECT MatrNr FROM parquet_scan('${basedir}/studenten.parquet');`);
-                expect(result.columnTypesLength()).toBe(1);
-                let chunks = new webdb.AsyncChunkStreamIterator(conn, result);
-                let vals: number[] = [];
-                while (await chunks.nextAsync()) {
-                    for (const v of chunks.iterateNumberColumn(0)) {
-                        vals.push(v!);
-                    }
-                }
-                expect(vals).toEqual([24002, 25403, 26120, 26830, 27550, 28106, 29120, 29555]);
-            });
-
-            it('simple join', async () => {
-                await db().registerURL(`${basedir}/studenten.parquet`);
-                await db().registerURL(`${basedir}/hoeren.parquet`);
-                await db().registerURL(`${basedir}/vorlesungen.parquet`);
-
-                let result = await conn.sendQuery(`
-                    SELECT studenten.MatrNr, vorlesungen.Titel
-                    FROM parquet_scan('${basedir}/studenten.parquet') studenten
-                    INNER JOIN parquet_scan('${basedir}/hoeren.parquet') hoeren ON (studenten.MatrNr = hoeren.MatrNr)
-                    INNER JOIN parquet_scan('${basedir}/vorlesungen.parquet') vorlesungen ON (vorlesungen.VorlNr = hoeren.VorlNr);
-                `);
-                expect(result.columnTypesLength()).toBe(2);
-                let chunks = new webdb.AsyncChunkStreamIterator(conn, result);
-                interface Row extends webdb.RowProxy {
-                    MatrNr: number | null;
-                    Titel: string | null;
-                }
-                let vals: object[] = [];
-                while (await chunks.nextAsync()) {
-                    for (let row of chunks.collect<Row>()) {
-                        vals.push({
-                            MatrNr: row.MatrNr,
-                            Titel: row.Titel,
-                        });
-                    }
-                }
-                expect(vals).toEqual([
-                    { MatrNr: 26120, Titel: 'Grundzüge' },
-                    { MatrNr: 27550, Titel: 'Grundzüge' },
-                    { MatrNr: 27550, Titel: 'Logik' },
-                    { MatrNr: 28106, Titel: 'Ethik' },
-                    { MatrNr: 28106, Titel: 'Wissenschaftstheorie' },
-                    { MatrNr: 28106, Titel: 'Bioethik' },
-                    { MatrNr: 28106, Titel: 'Der Wieer Kreis' },
-                    { MatrNr: 29120, Titel: 'Grundzüge' },
-                    { MatrNr: 29120, Titel: 'Ethik' },
-                    { MatrNr: 29120, Titel: 'Mäeutik' },
-                    { MatrNr: 29555, Titel: 'Glaube und Wissen' },
-                    { MatrNr: 25403, Titel: 'Glaube und Wissen' },
-                ]);
             });
         });
     });
