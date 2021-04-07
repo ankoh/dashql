2d2059e2c277cb2007832b2fa19475b923429235
Andre Kohn <kohn.a@outlook.com>
2021-04-07 16:10:38 +0200

    Migrate targets and remove proto from duckdb-web

--- packages/duckdb/src/targets/duckdb-browser-serial.ts
+++ packages/duckdb/src/targets/duckdb-browser-serial.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2020 The DashQL Authors
 
-export * from '../common';
+export * from '../log';
+export * from '../status';
 export * from '../bindings';
 export * from '../bindings/bindings_browser';
 export * from '../bindings/runtime_browser';
