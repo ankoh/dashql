e239e9b5e148453af23f6a1be4d410a53bd42dfe
Andre Kohn <kohn.a@outlook.com>
2021-04-07 15:55:58 +0200

    Migrate duckdb-web to arrow

--- packages/duckdb/src/parallel/index.ts
+++ packages/duckdb/src/parallel/index.ts
@@ -1,4 +1,4 @@
-export * from './async_stream_iterator';
-export * from './async_duckdb_dispatcher';
-export * from './async_duckdb_message';
+export * from './async_connection';
 export * from './async_duckdb';
+export * from './worker_dispatcher';
+export * from './worker_request';
