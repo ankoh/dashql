f87044b7c8f536f053a98fb2568b07e39f08a6f8
Andre Kohn <kohn.a@outlook.com>
2021-04-08 11:54:20 +0200

    Migrate filesystem tests

--- packages/duckdb/test/index_node.ts
+++ packages/duckdb/test/index_node.ts
@@ -9,23 +9,19 @@ let adb: duckdb_parallel.AsyncDuckDB | null = null;
 let worker: Worker | null = null;
 
 beforeAll(async () => {
-    db = new duckdb_serial.DuckDB(
-        new duckdb_serial.VoidLogger(),
-        duckdb_serial.DefaultDuckDBRuntime,
-        path.resolve(__dirname, './duckdb.wasm'),
-    );
+    const logger = new duckdb_serial.VoidLogger();
+    db = new duckdb_serial.DuckDB(logger, duckdb_serial.DefaultDuckDBRuntime, path.resolve(__dirname, './duckdb.wasm'));
     await db.open();
 
     worker = new Worker(path.resolve(__dirname, './duckdb-node-parallel.worker.js'));
-    adb = new duckdb_parallel.AsyncDuckDB(new duckdb_parallel.VoidLogger(), worker);
+    adb = new duckdb_parallel.AsyncDuckDB(logger, worker);
     await adb.open(path.resolve(__dirname, './duckdb.wasm'));
 });
 
 afterAll(() => {});
 
-import { testProxies } from './proxy.test';
 import { testBindings } from './bindings.test';
-import { testIterator } from './batch_stream.test';
+import { testBatchStream } from './batch_stream.test';
 import { testFilesystem } from './filesystem.test';
 import { testAsyncBatchStream } from './batch_stream_async.test';
 import { testExtractCSV } from './extract_csv.test';
@@ -33,11 +29,10 @@ import { testExtractCSV } from './extract_csv.test';
 // Loading debug symbols, especially for WASM take insanely long so we just disable the test timeout
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
-//testProxies(() => db!);
-//testBindings(() => db!);
-testIterator(() => db!);
+testBindings(() => db!);
+testBatchStream(() => db!);
 testAsyncBatchStream(() => adb!);
-//testFilesystem(() => adb!, path.resolve(__dirname, '../../../data/uni/out'));
+testFilesystem(() => adb!, path.resolve(__dirname, '../../../data/uni/out'));
 //testExtractCSV(
 //    () => adb!,
 //    (buf: Uint8Array) => tmp.sync(Buffer.from(buf)),
