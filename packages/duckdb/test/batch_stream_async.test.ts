01258fc4037b382aa8feec2aea2eba547ae28638
Andre Kohn <kohn.a@outlook.com>
2021-04-08 11:33:08 +0200

    More async tests

--- packages/duckdb/test/batch_stream_async.test.ts
+++ packages/duckdb/test/batch_stream_async.test.ts
@@ -1,7 +1,7 @@
 import * as duckdb from '../src/';
 import * as arrow from 'apache-arrow';
 
-const testRows = 3000;
+const testRows = 10000;
 
 export function testAsyncBatchStream(db: () => duckdb.AsyncDuckDB) {
     let conn: duckdb.AsyncDuckDBConnection;
@@ -88,6 +88,208 @@ export function testAsyncBatchStream(db: () => duckdb.AsyncDuckDB) {
                     for (const v of batch.getChildAt(0)!) {
                         expect(v).toBe(String(i++));
                     }
+                }
+                expect(i).toBe(testRows + 1);
+            });
+        });
+    });
+
+    describe('Arrow Record-Batches Column-Major', () => {
+        describe('single column', () => {
+            it('TINYINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT (v & 127)::TINYINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                let i = 0;
+                for await (const batch of result) {
+                    expect(batch.numCols).toBe(1);
+                    for (const v of batch.getChildAt(0)!) {
+                        expect(v).toBe(i++ & 127);
+                    }
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('SMALLINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT (v & 32767)::SMALLINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                let i = 0;
+                for await (const batch of result) {
+                    expect(batch.numCols).toBe(1);
+                    for (const v of batch.getChildAt(0)!) {
+                        expect(v).toBe(i++ & 32767);
+                    }
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('INTEGER', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT v::INTEGER AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                let i = 0;
+                for await (const batch of result) {
+                    expect(batch.numCols).toBe(1);
+                    for (const v of batch.getChildAt(0)!) {
+                        expect(v).toBe(i++);
+                    }
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('BIGINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT v::BIGINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                let i = 0;
+                for await (const batch of result) {
+                    expect(batch.numCols).toBe(1);
+                    for (const v of batch.getChildAt(0)!) {
+                        expect(v.valueOf()).toBe(i++);
+                    }
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('STRING', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT v::VARCHAR AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                let i = 0;
+                for await (const batch of result) {
+                    expect(batch.numCols).toBe(1);
+                    for (const v of batch.getChildAt(0)!) {
+                        expect(v).toBe(String(i++));
+                    }
+                }
+                expect(i).toBe(testRows + 1);
+            });
+        });
+    });
+
+    describe('Arrow Table Row-Major', () => {
+        describe('single column', () => {
+            it('TINYINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT (v & 127)::TINYINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const row of table) {
+                    expect(row.v).toBe(i++ & 127);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('SMALLINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT (v & 32767)::SMALLINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const row of table) {
+                    expect(row.v).toBe(i++ & 32767);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('INTEGER', async () => {
+                const result = conn.sendQuery(`
+                    SELECT v::INTEGER AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const row of table) {
+                    expect(row.v).toBe(i++);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('BIGINT', async () => {
+                const result = conn.sendQuery(`
+                    SELECT v::BIGINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const row of table) {
+                    expect(row.v.valueOf()).toBe(i++);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('STRING', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT v::VARCHAR AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const row of table) {
+                    expect(row.v.valueOf()).toBe(String(i++));
+                }
+                expect(i).toBe(testRows + 1);
+            });
+        });
+    });
+
+    describe('Arrow Table Column-Major', () => {
+        describe('single column', () => {
+            it('TINYINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT (v & 127)::TINYINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const v of table.getColumnAt(0)!) {
+                    expect(v).toBe(i++ & 127);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('SMALLINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT (v & 32767)::SMALLINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const v of table.getColumnAt(0)!) {
+                    expect(v).toBe(i++ & 32767);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('INTEGER', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT v::INTEGER AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const v of table.getColumnAt(0)!) {
+                    expect(v).toBe(i++);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('BIGINT', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT v::BIGINT AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const v of table.getColumnAt(0)!) {
+                    expect(v.valueOf()).toBe(i++);
+                }
+                expect(i).toBe(testRows + 1);
+            });
+
+            it('STRING', async () => {
+                const result = await conn.sendQuery(`
+                    SELECT v::VARCHAR AS v FROM generate_series(0, ${testRows}) as t(v);
+                `);
+                const table = await arrow.Table.from(result);
+                let i = 0;
+                for (const v of table.getColumnAt(0)!) {
+                    expect(v.valueOf()).toBe(String(i++));
                 }
                 expect(i).toBe(testRows + 1);
             });
