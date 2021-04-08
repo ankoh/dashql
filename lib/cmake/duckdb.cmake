b1b0900b45fc73c6854b26278c288c864356375d
Andre Kohn <kohn.a@outlook.com>
2021-04-08 19:30:12 +0200

    Stop compiling amalagamated DuckDB

---
+++ lib/cmake/duckdb.cmake
@@ -0,0 +1,59 @@
+# Copyright (c) 2020 The DashQL Authors
+
+include(ExternalProject)
+
+ExternalProject_Add(
+    duckdb_ep
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/../submodules/duckdb"
+    PREFIX "${CMAKE_BINARY_DIR}/third_party/duckdb"
+    INSTALL_DIR "${CMAKE_BINARY_DIR}/third_party/duckdb/install"
+    CMAKE_ARGS
+        -G${CMAKE_GENERATOR}
+        -DCMAKE_CXX_STANDARD=17
+        -DCMAKE_CXX_FLAGS=-std=c++17 -DDUCKDB_NO_THREADS=1
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+        -DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+        -DBUILD_SHELL=FALSE
+        -DBUILD_UNITTESTS=FALSE
+    BUILD_BYPRODUCTS
+        <INSTALL_DIR>/lib/libduckdb_re2.a
+        <INSTALL_DIR>/lib/libduckdb_static.a
+        <INSTALL_DIR>/lib/libfmt.a
+        <INSTALL_DIR>/lib/libhyperloglog.a
+        <INSTALL_DIR>/lib/libminiz.a
+        <INSTALL_DIR>/lib/libpg_query.a
+        <INSTALL_DIR>/lib/libutf8proc.a
+)
+
+ExternalProject_Get_Property(duckdb_ep install_dir)
+
+set(DUCKDB_SOURCE_DIR "${CMAKE_SOURCE_DIR}/../submodules/duckdb")
+set(DUCKDB_INCLUDE_DIR "${install_dir}/include")
+set(DUCKDB_UTF8PROC_INCLUDE_DIR "${DUCKDB_SOURCE_DIR}/third_party/utf8proc/include")
+set(DUCKDB_FMT_INCLUDE_DIR "${DUCKDB_SOURCE_DIR}/third_party/fmt/include")
+set(DUCKDB_LIBRARY_PATH "${install_dir}/lib/libduckdb_static.a")
+
+file(MAKE_DIRECTORY ${DUCKDB_INCLUDE_DIR})
+add_library(duckdb STATIC IMPORTED)
+set_property(TARGET duckdb PROPERTY IMPORTED_LOCATION ${DUCKDB_LIBRARY_PATH})
+
+target_link_libraries(duckdb
+    INTERFACE ${install_dir}/lib/libduckdb_re2.a
+    INTERFACE ${install_dir}/lib/libfmt.a
+    INTERFACE ${install_dir}/lib/libhyperloglog.a
+    INTERFACE ${install_dir}/lib/libminiz.a
+    INTERFACE ${install_dir}/lib/libpg_query.a
+    INTERFACE ${install_dir}/lib/libutf8proc.a
+)
+
+target_include_directories(duckdb
+    INTERFACE ${DUCKDB_INCLUDE_DIR}
+    INTERFACE ${DUCKDB_FMT_INCLUDE_DIR}
+    INTERFACE ${DUCKDB_UTF8PROC_INCLUDE_DIR}
+)
+
+add_dependencies(duckdb duckdb_ep)
