338f921e059981d824806a42f9216d79c3f0d606
Andre Kohn <kohn.a@outlook.com>
2021-04-12 23:39:38 +0200

    Maintain files in buffer manager

--- lib/include/duckdb/web/buffer/buffer_manager.h
+++ lib/include/duckdb/web/buffer/buffer_manager.h
@@ -9,6 +9,7 @@
 #include <memory>
 #include <mutex>
 #include <shared_mutex>
+#include <stack>
 #include <unordered_map>
 #include <vector>
 
@@ -23,8 +24,8 @@ class BufferFrame {
     /// A position in the LRU queue
     using list_position = std::list<BufferFrame*>::iterator;
 
-    /// The page id
-    uint64_t page_id;
+    /// The frame id
+    uint64_t frame_id;
     /// The data buffer
     std::vector<char> buffer;
 
@@ -47,7 +48,7 @@ class BufferFrame {
 
    public:
     /// Constructor
-    BufferFrame(uint64_t page_id, size_t size, list_position fifo_position, list_position lru_position);
+    BufferFrame(uint64_t frame_id, size_t size, list_position fifo_position, list_position lru_position);
     /// Returns a pointer to this page data
     auto* GetData() const { return buffer.data(); }
 };
@@ -55,14 +56,21 @@ class BufferFrame {
 class BufferManager {
    protected:
     /// The page size
-    size_t page_size;
-    /// The page count
-    size_t page_count;
+    const size_t page_size;
 
     /// Maps frame ids to their files
-    std::unordered_map<uint16_t, std::unique_ptr<File>> files;
+    std::unordered_map<uint16_t, std::unique_ptr<File>> files = {};
+    /// The free file ids
+    std::stack<uint16_t> free_file_ids = {};
+    /// The next allocated file ids
+    uint16_t next_file_id = 0;
+    /// The file ids
+    std::unordered_map<std::string_view, uint16_t> file_ids = {};
+    /// The file names
+    std::unordered_map<uint16_t, std::string> file_paths = {};
+
     /// Maps page_ids to BufferFrame objects of all pages that are currently in memory
-    std::map<uint64_t, BufferFrame> frames;
+    std::map<uint64_t, BufferFrame> frames = {};
     /// FIFO list of pages
     std::list<BufferFrame*> fifo;
     /// LRU list of pages
@@ -85,6 +93,11 @@ class BufferManager {
     /// Destructor
     ~BufferManager();
 
+    /// Add a file
+    uint16_t AddFile(std::string_view path);
+    /// Write all pages of a file
+    void FlushFile(uint16_t file_id);
+
     /// Get the page size
     auto GetPageSize() const { return page_size; }
     /// Returns a reference to a `BufferFrame` object for a given page id. When
@@ -95,8 +108,6 @@ class BufferManager {
     /// `fix_page()` and unfixes it. When `is_dirty` is / true, the page is
     /// written back to disk eventually.
     void UnfixPage(BufferFrame& frame, bool is_dirty);
-    /// Write all pages of a file
-    void FlushFile(uint16_t file_id);
 
     /// Returns the page ids of all pages that are in the FIFO list in FIFO order.
     std::vector<uint64_t> get_fifo_list() const;
