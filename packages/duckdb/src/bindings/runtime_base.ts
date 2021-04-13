72e71aa823a21429db7a14e73c0315ac286434fa
Andre Kohn <kohn.a@outlook.com>
2021-04-13 18:44:14 +0200

    Simplify node runtime

--- packages/duckdb/src/bindings/runtime_base.ts
+++ packages/duckdb/src/bindings/runtime_base.ts
@@ -2,20 +2,18 @@ export interface DuckDBRuntime {
     bindings: any;
     duckdb_web_add_handle(url: string, handle: any): void;
     duckdb_web_get_object_url(url: string): string | null;
-    duckdb_web_fs_read(fileId: number, buf: number, bytes: number): number;
-    duckdb_web_fs_write(fileId: number, buf: number, bytes: number): number;
+    duckdb_web_fs_read(fileId: number, buf: number, bytes: number, location: number): number;
+    duckdb_web_fs_write(fileId: number, buf: number, bytes: number, location: number): number;
     duckdb_web_fs_directory_exists(pathPtr: number, pathLen: number): boolean;
     duckdb_web_fs_directory_create(pathPtr: number, pathLen: number): void;
     duckdb_web_fs_directory_remove(pathPtr: number, pathLen: number): void;
     duckdb_web_fs_directory_list_files(pathPtr: number, pathLen: number): boolean;
     duckdb_web_fs_glob(pathPtr: number, pathLen: number): void;
     duckdb_web_fs_file_open(pathPtr: number, pathLen: number, flags: number): number;
-    duckdb_web_fs_file_sync(fileId: number): void;
     duckdb_web_fs_file_close(fileId: number): void;
     duckdb_web_fs_file_get_last_modified_time(fileId: number): number;
     duckdb_web_fs_file_get_size(fileId: number): number;
     duckdb_web_fs_file_move(fromPtr: number, fromLen: number, toPtr: number, toLen: number): void;
-    duckdb_web_fs_file_set_pointer(fileId: number, location: number): void;
     duckdb_web_fs_file_exists(pathPtr: number, pathLen: number): boolean;
     duckdb_web_fs_file_remove(pathPtr: number, pathLen: number): void;
 }
@@ -43,10 +41,10 @@ export const DefaultDuckDBRuntime: DuckDBRuntime = {
     duckdb_web_get_object_url: (url: string): string | null => {
         throw Error('undefined');
     },
-    duckdb_web_fs_read: (fileId: number, buf: number, bytes: number) => {
+    duckdb_web_fs_read: (fileId: number, buf: number, bytes: number, location: number) => {
         throw Error('undefined');
     },
-    duckdb_web_fs_write: (fileId: number, buf: number, bytes: number) => {
+    duckdb_web_fs_write: (fileId: number, buf: number, bytes: number, location: number) => {
         throw Error('undefined');
     },
     duckdb_web_fs_directory_exists: (pathPtr: number, pathLen: number) => {
@@ -70,9 +68,6 @@ export const DefaultDuckDBRuntime: DuckDBRuntime = {
     duckdb_web_fs_file_close: (fileId: number) => {
         throw Error('undefined');
     },
-    duckdb_web_fs_file_sync: (fileId: number) => {
-        throw Error('undefined');
-    },
     duckdb_web_fs_file_get_last_modified_time: (fileId: number) => {
         throw Error('undefined');
     },
@@ -80,9 +75,6 @@ export const DefaultDuckDBRuntime: DuckDBRuntime = {
         throw Error('undefined');
     },
     duckdb_web_fs_file_move: (fromPtr: number, fromLen: number, toPtr: number, toLen: number) => {
-        throw Error('undefined');
-    },
-    duckdb_web_fs_file_set_pointer: (fileId: number, location: number) => {
         throw Error('undefined');
     },
     duckdb_web_fs_file_exists: (pathPtr: number, pathLen: number) => {
