88ffdb8a16a345068121ddf289b28870c79a78a6
Andre Kohn <kohn.a@outlook.com>
2021-04-13 09:55:08 +0200

    Work on buffer manager

--- lib/src/duckdb/web/io/filesystem.cc
+++ lib/src/duckdb/web/io/filesystem.cc
@@ -2,11 +2,13 @@
 
 #include "duckdb/web/io/filesystem.h"
 
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "duckdb/common/file_system.hpp"
+#include "duckdb/web/io/filesystem.h"
 #include "duckdb/web/io/filesystem_api.h"
 
 static const std::function<void(std::string, bool)> *list_files_callback = {};
@@ -14,116 +16,118 @@ static std::vector<std::string> *glob_results = {};
 
 namespace duckdb {
 namespace web {
+namespace io {
 
-SeekableFileSystem::~SeekableFileSystem() {}
+FileSystem::FileSystem(BufferManager &buffer_manager) : buffer_manager_(buffer_manager) {}
 
-void SeekableFileSystem::SetFilePointer(duckdb::FileHandle &handle, size_t pos) {
-    /// This is a hack to seek using the current duckdb filesystem api
-    Read(handle, nullptr, 0, pos);
+std::unique_ptr<duckdb::FileHandle> FileSystem::OpenFile(const char *path, uint8_t flags, duckdb::FileLockType lock) {
+    auto file = buffer_manager_.AddFile(std::string_view{path});
+    return std::make_unique<FileHandle>(*this, std::move(file));
 }
 
-void WebDBFileHandle::Close() { duckdb_web_fs_file_close(blob_id); }
-
-void WebFileSystem::SetFilePointer(duckdb::FileHandle &handle, size_t pos) {
-    duckdb_web_fs_file_set_pointer(((WebDBFileHandle &)handle).blob_id, pos);
+void FileSystem::Read(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes, duckdb::idx_t location) {
+    auto &file = static_cast<FileHandle &>(handle).GetFile();
+    while (nr_bytes > 0) {
+        auto n = std::min<size_t>(buffer_manager_.GetPageSize(), nr_bytes);
+        auto page_id = buffer_manager_.GetPageIDFromOffset(location);
+        auto page = buffer_manager_.FixPage(file, page_id, false);
+        std::memcpy(buffer, page.GetData(), n);
+        nr_bytes -= n;
+        location += n;
+    }
 }
 
-std::unique_ptr<duckdb::FileHandle> WebFileSystem::OpenFile(const char *path, uint8_t flags,
-                                                            duckdb::FileLockType lock) {
-    return std::make_unique<WebDBFileHandle>(*this, std::string(path),
-                                             duckdb_web_fs_file_open(path, strlen(path), flags));
+void FileSystem::Write(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes, duckdb::idx_t location) {
+    auto &file = static_cast<FileHandle &>(handle).GetFile();
+    while (nr_bytes > 0) {
+        auto n = std::min<size_t>(buffer_manager_.GetPageSize(), nr_bytes);
+        auto page_id = buffer_manager_.GetPageIDFromOffset(location);
+        auto page = buffer_manager_.FixPage(file, page_id, true);
+        std::memcpy(page.GetData(), buffer, n);
+        nr_bytes -= n;
+        location += n;
+        buffer_manager_.UnfixPage(std::move(page), true);
+    }
 }
 
-void WebFileSystem::Read(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes, duckdb::idx_t location) {
-    duckdb_web_fs_file_set_pointer(((WebDBFileHandle &)handle).blob_id, location);
-    duckdb_web_fs_read(((WebDBFileHandle &)handle).blob_id, buffer, nr_bytes);
-}
-
-void WebFileSystem::Write(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes, duckdb::idx_t location) {
-    duckdb_web_fs_file_set_pointer(((WebDBFileHandle &)handle).blob_id, location);
-    duckdb_web_fs_write(((WebDBFileHandle &)handle).blob_id, buffer, nr_bytes);
-}
-
-int64_t WebFileSystem::Read(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes) {
+int64_t FileSystem::Read(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes) {
     return duckdb_web_fs_read(((WebDBFileHandle &)handle).blob_id, buffer, nr_bytes);
 }
 
-int64_t WebFileSystem::Write(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes) {
+int64_t FileSystem::Write(duckdb::FileHandle &handle, void *buffer, int64_t nr_bytes) {
     return duckdb_web_fs_write(((WebDBFileHandle &)handle).blob_id, buffer, nr_bytes);
 }
 
-int64_t WebFileSystem::GetFileSize(duckdb::FileHandle &handle) {
+int64_t FileSystem::GetFileSize(duckdb::FileHandle &handle) {
     return duckdb_web_fs_file_get_size(((WebDBFileHandle &)handle).blob_id);
 }
 
-time_t WebFileSystem::GetLastModifiedTime(duckdb::FileHandle &handle) {
+time_t FileSystem::GetLastModifiedTime(duckdb::FileHandle &handle) {
     return duckdb_web_fs_file_get_last_modified_time(((WebDBFileHandle &)handle).blob_id);
 }
 
-void WebFileSystem::Truncate(duckdb::FileHandle &handle, int64_t new_size) {
+void FileSystem::Truncate(duckdb::FileHandle &handle, int64_t new_size) {
     std::cout << "Truncate not implemented" << std::endl;
 }
 
-bool WebFileSystem::DirectoryExists(const std::string &directory) {
+bool FileSystem::DirectoryExists(const std::string &directory) {
     return duckdb_web_fs_directory_exists(directory.c_str(), directory.size());
 }
 
-void WebFileSystem::CreateDirectory(const std::string &directory) {
+void FileSystem::CreateDirectory(const std::string &directory) {
     duckdb_web_fs_directory_create(directory.c_str(), directory.size());
 }
 
-void WebFileSystem::RemoveDirectory(const std::string &directory) {
+void FileSystem::RemoveDirectory(const std::string &directory) {
     return duckdb_web_fs_directory_remove(directory.c_str(), directory.size());
 }
 
-bool WebFileSystem::ListFiles(const std::string &directory, const std::function<void(std::string, bool)> &callback) {
+bool FileSystem::ListFiles(const std::string &directory, const std::function<void(std::string, bool)> &callback) {
     list_files_callback = &callback;
     bool result = duckdb_web_fs_directory_list_files(directory.c_str(), directory.size());
     list_files_callback = {};
     return result;
 }
 
-void WebFileSystem::MoveFile(const std::string &source, const std::string &target) {
+void FileSystem::MoveFile(const std::string &source, const std::string &target) {
     duckdb_web_fs_file_move(source.c_str(), source.size(), target.c_str(), target.size());
 }
 
-bool WebFileSystem::FileExists(const std::string &filename) {
+bool FileSystem::FileExists(const std::string &filename) {
     return duckdb_web_fs_file_exists(filename.c_str(), filename.size());
 }
 
-void WebFileSystem::RemoveFile(const std::string &filename) {
-    std::cout << "WebFileSystem not implemented" << std::endl;
-}
+void FileSystem::RemoveFile(const std::string &filename) { std::cout << "FileSystem not implemented" << std::endl; }
 
-// std::string WebFileSystem::PathSeparator() {
+// std::string FileSystem::PathSeparator() {
 //     std::cout << "PathSeparator not implemented" << std::endl;
 //     return {};
 // }
 //
-// std::string WebFileSystem::JoinPath(const std::string &a, const std::string &path) {
+// std::string FileSystem::JoinPath(const std::string &a, const std::string &path) {
 //     std::cout << "JoinPath not implemented" << std::endl;
 //     return {};
 // }
 
-void WebFileSystem::FileSync(duckdb::FileHandle &handle) {
+void FileSystem::FileSync(duckdb::FileHandle &handle) {
     return duckdb_web_fs_file_sync(((WebDBFileHandle &)handle).blob_id);
 }
 
-void WebFileSystem::SetWorkingDirectory(const std::string &path) {
+void FileSystem::SetWorkingDirectory(const std::string &path) {
     std::cout << "SetWorkingDirectory not implemented" << std::endl;
 }
 
-std::string WebFileSystem::GetWorkingDirectory() {
+std::string FileSystem::GetWorkingDirectory() {
     std::cout << "GetWorkingDirectory not implemented" << std::endl;
     return {};
 }
 
-std::string WebFileSystem::GetHomeDirectory() {
+std::string FileSystem::GetHomeDirectory() {
     std::cout << "GetHomeDirectory not implemented" << std::endl;
     return {};
 }
 
-std::vector<std::string> WebFileSystem::Glob(const std::string &path) {
+std::vector<std::string> FileSystem::Glob(const std::string &path) {
     std::vector<std::string> results;
     glob_results = &results;
     duckdb_web_fs_glob(path.c_str(), path.size());
@@ -132,5 +136,6 @@ std::vector<std::string> WebFileSystem::Glob(const std::string &path) {
     return results;
 }
 
+}  // namespace io
 }  // namespace web
 }  // namespace duckdb
