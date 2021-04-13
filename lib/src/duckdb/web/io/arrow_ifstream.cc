3ede83ee7fb277ecb6db312571351b4605bb2465
Andre Kohn <kohn.a@outlook.com>
2021-04-13 15:52:31 +0200

    Json and csv tests working with buffer manager

--- lib/src/duckdb/web/io/arrow_ifstream.cc
+++ lib/src/duckdb/web/io/arrow_ifstream.cc
@@ -2,6 +2,8 @@
 
 #include "duckdb/web/io/arrow_ifstream.h"
 
+#include <iostream>
+
 #include "arrow/buffer.h"
 #include "arrow/result.h"
 #include "duckdb/common/file_system.hpp"
@@ -34,21 +36,40 @@ arrow::Status InputFileStream::Abort() {
 arrow::Result<int64_t> InputFileStream::Tell() const { return file_position_; }
 
 /// Is the stream closed?
-bool InputFileStream::closed() const { return static_cast<bool>(file_); }
+bool InputFileStream::closed() const { return !static_cast<bool>(file_); }
 
 /// Read at most nbytes bytes from the file
 arrow::Result<int64_t> InputFileStream::Read(int64_t nbytes, void* out) {
-    buffer_manager_.Read(file_, out, nbytes, file_position_);
-    file_position_ += nbytes;
-    return nbytes;
+    auto n = buffer_manager_.Read(file_, out, nbytes, file_position_);
+    file_position_ += n;
+    return n;
 }
+
+namespace {
+
+struct FixedPage : public arrow::Buffer {
+    BufferManager::BufferRef buffer;
+    FixedPage(BufferManager::BufferRef buffer, nonstd::span<char> view)
+        : arrow::Buffer(reinterpret_cast<uint8_t*>(view.data()), view.size()), buffer(std::move(buffer)) {}
+    ~FixedPage() {}
+    FixedPage(const FixedPage& other) = delete;
+};
+
+}  // namespace
 
 /// Read at most nbytes bytes from the file
 arrow::Result<std::shared_ptr<arrow::Buffer>> InputFileStream::Read(int64_t nbytes) {
-    ARROW_ASSIGN_OR_RAISE(auto buffer, arrow::AllocateBuffer(nbytes));
-    buffer_manager_.Read(file_, buffer->mutable_data(), nbytes, file_position_);
-    file_position_ += nbytes;
-    return buffer;
+    // Determine page & offset
+    auto page_id = file_position_ >> buffer_manager_.GetPageSizeShift();
+    auto skip_here = file_position_ - page_id * buffer_manager_.GetPageSize();
+    auto read_here = std::min<size_t>(nbytes, buffer_manager_.GetPageSize() - skip_here);
+
+    // Read page
+    auto page = buffer_manager_.FixPage(file_, page_id, false);
+    assert(skip_here <= page.GetData().size());
+    auto data = page.GetData().subspan(skip_here);
+    file_position_ += data.size();
+    return std::make_shared<FixedPage>(std::move(page), data);
 }
 
 /// Advance the file position by nbytes bytes
@@ -59,10 +80,15 @@ arrow::Status InputFileStream::Advance(int64_t nbytes) {
 
 /// Read at most nbytes bytes from the file without advancing the file position
 arrow::Result<arrow::util::string_view> InputFileStream::Peek(int64_t nbytes) {
-    auto n = std::min<size_t>(nbytes, buffer_manager_.GetPageSize());
+    // Determine page & offset
     auto page_id = file_position_ >> buffer_manager_.GetPageSizeShift();
-    tmp_page_ = std::move(buffer_manager_.FixPage(file_, page_id, false));
-    auto data = tmp_page_->GetData();
+    auto skip_here = file_position_ - page_id * buffer_manager_.GetPageSize();
+    auto read_here = std::min<size_t>(nbytes, buffer_manager_.GetPageSize() - skip_here);
+
+    // Peek page
+    tmp_page_ = buffer_manager_.FixPage(file_, page_id, false);
+    assert(skip_here <= tmp_page_->GetData().size());
+    auto data = tmp_page_->GetData().subspan(skip_here);
     return arrow::util::string_view{data.data(), data.size()};
 }
 
