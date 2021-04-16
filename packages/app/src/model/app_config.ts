60effbaeea18d6ca1c34af5c2f4bc31fd5be706f
Andre Kohn <kohn.a@outlook.com>
2021-04-16 15:26:55 +0200

    Allow empty app configs for now

--- packages/app/src/model/app_config.ts
+++ packages/app/src/model/app_config.ts
@@ -1,8 +1,9 @@
 export interface AppConfig {
-    program: string;
+    program?: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function isAppConfig(object: any): object is AppConfig {
-    return object.program !== undefined;
+    return true;
+    //return object.program !== undefined;
 }
