2142c46e5a43bcd6c308ffdc65742392fdbb480a
Andre Kohn <kohn.a@outlook.com>
2021-04-18 00:21:26 +0200

    Read load and extract statements

--- lib/src/analyzer/stmt/load_stmt.cc
+++ lib/src/analyzer/stmt/load_stmt.cc
@@ -1,3 +1,50 @@
 #include "dashql/analyzer/stmt/load_stmt.h"
 
-namespace dashql {}
+#include "dashql/analyzer/program_instance.h"
+#include "dashql/proto_generated.h"
+
+constexpr size_t SX_LOAD_METHOD = 0;
+constexpr size_t SX_LOAD_FROM_URI = 1;
+namespace fb = flatbuffers;
+namespace ana = dashql::proto::analyzer;
+
+namespace dashql {
+
+std::unique_ptr<LoadStatement> LoadStatement::ReadFrom(ProgramInstance& instance, size_t stmt_id) {
+    // clang-format off
+    auto& program = instance.program();
+    auto& stmt = program.statements[stmt_id];
+    static const auto schema = sxm::Element()
+        .MatchObject(sx::NodeType::OBJECT_DASHQL_LOAD)
+        .MatchChildren({
+            sxm::Attribute(sx::AttributeKey::DASHQL_LOAD_METHOD, SX_LOAD_METHOD)
+                .MatchEnum(sx::NodeType::ENUM_DASHQL_LOAD_METHOD_TYPE),
+            sxm::Attribute(sx::AttributeKey::DASHQL_LOAD_FROM_URI, SX_LOAD_FROM_URI)
+                .MatchString(),
+        });
+    // clang-format on
+
+    // Match root
+    auto ast = schema.Match(instance, stmt->root_node, 2);
+
+    // Get load method
+    if (ast[SX_LOAD_METHOD]) {
+        auto node_id = ast[SX_LOAD_METHOD].node_id;
+        auto& node = program.nodes[node_id];
+    }
+    if (ast[SX_LOAD_FROM_URI]) {
+        auto node_id = ast[SX_LOAD_FROM_URI].node_id;
+        auto& node = program.nodes[node_id];
+    }
+
+    auto load = std::make_unique<LoadStatement>(instance, stmt_id, std::move(ast));
+    return load;
+}
+
+/// Pack the extract statement
+fb::Offset<ana::LoadStatement> LoadStatement::Pack(fb::FlatBufferBuilder& builder) const {
+    proto::analyzer::LoadStatementBuilder eb{builder};
+    return eb.Finish();
+}
+
+}  // namespace dashql
