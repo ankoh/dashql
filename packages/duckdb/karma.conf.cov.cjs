c1ac94639633984d25ec5105c073c9ff2d2c1f36
Andre Kohn <kohn.a@outlook.com>
2021-04-23 22:05:47 +0200

    Further increase karma timeout

--- packages/duckdb/karma.conf.cov.cjs
+++ packages/duckdb/karma.conf.cov.cjs
@@ -54,9 +54,6 @@ module.exports = function (config) {
                 failFast: true,
             },
         },
-        captureTimeout: 60000,
-        browserDisconnectTimeout: 10000,
-        browserDisconnectTolerance: 1,
-        browserNoActivityTimeout: 60000,
+        browserNoActivityTimeout: 200000,
     });
 };
