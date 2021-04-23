e6de11b8c275e9fb7fbdbd0f6e8985bc5accda71
Andre Kohn <kohn.a@outlook.com>
2021-04-23 23:13:40 +0200

    Increase karma thresholds

--- packages/core/karma.conf.cov.cjs
+++ packages/core/karma.conf.cov.cjs
@@ -60,6 +60,9 @@ module.exports = function (config) {
                 failFast: true,
             },
         },
-        browserNoActivityTimeout: 200000,
+        captureTimeout: 300000,
+        browserDisconnectTimeout: 10000,
+        browserDisconnectTolerance: 1,
+        browserNoActivityTimeout: 300000,
     });
 };
