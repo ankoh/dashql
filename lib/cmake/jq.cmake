5f2e09e5e165ac541b5430e7a7d9e742e2192f71
Andre Kohn <kohn.a@outlook.com>
2021-04-23 14:40:13 +0200

    Build jq library

---
+++ lib/cmake/jq.cmake
@@ -0,0 +1,48 @@
+# Copyright (c) 2020 The DashQL Authors
+
+include(ExternalProject)
+
+include(ProcessorCount)
+ProcessorCount(NCORES)
+
+set(PREFIX_CONF)
+set(PREFIX_MAKE CC="ccache ${CC}" CXX="ccache ${CXX}")
+if (EMSCRIPTEN)
+    set(PREFIX_CONF emconfigure)
+    set(PREFIX_MAKE emmake)
+endif ()
+
+ExternalProject_Add(
+    jq_ep
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/../submodules/jq"
+    PREFIX "third_party/jq"
+    INSTALL_DIR "${CMAKE_BINARY_DIR}/third_party/jq/install"
+    CONFIGURE_COMMAND
+        COMMAND cd <SOURCE_DIR> && autoreconf -fi
+        COMMAND cd <BINARY_DIR> && ${EMCONFIGURE} <SOURCE_DIR>/configure --disable-maintainer-mode --with-oniguruma=builtin --prefix=<INSTALL_DIR>
+    BUILD_COMMAND ${EMMAKE} make -j${NCORES}
+    INSTALL_COMMAND ${EMMAKE} make install
+    DOWNLOAD_COMMAND ""
+    UPDATE_COMMAND ""
+    BUILD_BYPRODUCTS
+        <INSTALL_DIR>/lib/libjq.a
+        <INSTALL_DIR>/lib/libonig.a
+)
+
+ExternalProject_Get_Property(jq_ep install_dir)
+ExternalProject_Get_Property(jq_ep binary_dir)
+
+set(JQ_INCLUDE_DIR "${install_dir}/include")
+set(JQ_LIBRARY_PATH "${install_dir}/lib/libjq.a")
+set(JQ_ONIG_LIBRARY_PATH "${install_dir}/lib/libonig.a")
+
+file(MAKE_DIRECTORY ${JQ_INCLUDE_DIR})
+
+add_library(jq STATIC IMPORTED)
+set_property(TARGET jq PROPERTY IMPORTED_LOCATION ${JQ_LIBRARY_PATH})
+
+target_link_libraries(jq
+    INTERFACE ${install_dir}/lib/libonig.a
+)
+
+add_dependencies(jq jq_ep)
