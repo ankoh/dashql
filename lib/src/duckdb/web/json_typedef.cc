b019a364260833bf7754e7d367a62bc585d29be0
Andre Kohn <kohn.a@outlook.com>
2021-05-08 16:21:09 +0200

    Work on json typedef

--- lib/src/duckdb/web/json_typedef.cc
+++ lib/src/duckdb/web/json_typedef.cc
@@ -122,75 +122,64 @@ Result<TimeUnit::type> GetMemberTimeUnit(const RjObject& obj, const std::string&
 }
 
 /// Read an integer type
-Status ReadIntegerType(const rapidjson::Value::ConstObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadIntegerType(const rapidjson::Value::ConstObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const bool is_signed, GetMemberBool(json_type, "isSigned"));
     ARROW_ASSIGN_OR_RAISE(const int bit_width, GetMemberInt<int>(json_type, "bitWidth"));
     switch (bit_width) {
         case 8:
-            *type = is_signed ? int8() : uint8();
-            break;
+            return is_signed ? int8() : uint8();
         case 16:
-            *type = is_signed ? int16() : uint16();
-            break;
+            return is_signed ? int16() : uint16();
         case 32:
-            *type = is_signed ? int32() : uint32();
-            break;
+            return is_signed ? int32() : uint32();
         case 64:
-            *type = is_signed ? int64() : uint64();
-            break;
+            return is_signed ? int64() : uint64();
         default:
             return Status::Invalid("Invalid bit width: ", bit_width);
     }
-    return Status::OK();
 }
 
 /// Read a floating point type
-Status ReadFloatingPointType(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadFloatingPointType(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const auto precision, GetMemberString(json_type, "precision"));
-
     if (precision == "DOUBLE") {
-        *type = float64();
+        return float64();
     } else if (precision == "SINGLE") {
-        *type = float32();
+        return float32();
     } else if (precision == "HALF") {
-        *type = float16();
-    } else {
-        return Status::Invalid("Invalid precision: ", precision);
+        return float16();
     }
-    return Status::OK();
+    return Status::Invalid("Invalid precision: ", precision);
 }
 
 /// Read a map type
-Status ReadMapType(const RjObject& json_type, const std::vector<std::shared_ptr<Field>>& children,
-                   std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadMapType(const RjObject& json_type,
+                                              const std::vector<std::shared_ptr<Field>>& children) {
     if (children.size() != 1) {
         return Status::Invalid("Map must have exactly one child");
     }
-
     ARROW_ASSIGN_OR_RAISE(const bool keys_sorted, GetMemberBool(json_type, "keysSorted"));
-    return MapType::Make(children[0], keys_sorted).Value(type);
+    return MapType::Make(children[0], keys_sorted);
 }
 
 /// Read a fixed size binary type
-Status ReadFixedSizeBinaryType(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadFixedSizeBinaryType(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const int32_t byte_width, GetMemberInt<int32_t>(json_type, "byteWidth"));
-    *type = fixed_size_binary(byte_width);
-    return Status::OK();
+    return fixed_size_binary(byte_width);
 }
 
 /// Read a fixed size list type
-Status ReadFixedSizeListType(const RjObject& json_type, const std::vector<std::shared_ptr<Field>>& children,
-                             std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadFixedSizeListType(const RjObject& json_type,
+                                                        const std::vector<std::shared_ptr<Field>>& children) {
     if (children.size() != 1) {
         return Status::Invalid("FixedSizeList must have exactly one child");
     }
     ARROW_ASSIGN_OR_RAISE(const int32_t list_size, GetMemberInt<int32_t>(json_type, "listSize"));
-    *type = fixed_size_list(children[0], list_size);
-    return Status::OK();
+    return fixed_size_list(children[0], list_size);
 }
 
 /// Read a decimal type
-Status ReadDecimalType(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadDecimalType(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const int32_t precision, GetMemberInt<int32_t>(json_type, "precision"));
     ARROW_ASSIGN_OR_RAISE(const int32_t scale, GetMemberInt<int32_t>(json_type, "scale"));
     int32_t bit_width = 128;
@@ -199,92 +188,79 @@ Status ReadDecimalType(const RjObject& json_type, std::shared_ptr<DataType>* typ
         bit_width = maybe_bit_width.ValueOrDie();
     }
     if (bit_width == 128) {
-        *type = decimal128(precision, scale);
+        return decimal128(precision, scale);
     } else if (bit_width == 256) {
-        *type = decimal256(precision, scale);
-    } else {
-        return Status::Invalid("Only 128 bit and 256 Decimals are supported. Received", bit_width);
+        return decimal256(precision, scale);
     }
-    return Status::OK();
+    return Status::Invalid("Only 128 bit and 256 Decimals are supported. Received", bit_width);
 }
 
 /// Read a date type
-Status GetDateType(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> GetDateType(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const auto unit_str, GetMemberString(json_type, "unit"));
-
     if (unit_str == "DAY") {
-        *type = date32();
+        return date32();
     } else if (unit_str == "MILLISECOND") {
-        *type = date64();
-    } else {
-        return Status::Invalid("Invalid date unit: ", unit_str);
+        return date64();
     }
-    return Status::OK();
+    return Status::Invalid("Invalid date unit: ", unit_str);
 }
 
 /// Read a time type
-Status ReadTimeType(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadTimeType(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const auto unit_str, GetMemberString(json_type, "unit"));
     ARROW_ASSIGN_OR_RAISE(const int bit_width, GetMemberInt<int>(json_type, "bitWidth"));
-
+    std::shared_ptr<DataType> type;
     if (unit_str == "SECOND") {
-        *type = time32(TimeUnit::SECOND);
+        type = time32(TimeUnit::SECOND);
     } else if (unit_str == "MILLISECOND") {
-        *type = time32(TimeUnit::MILLI);
+        type = time32(TimeUnit::MILLI);
     } else if (unit_str == "MICROSECOND") {
-        *type = time64(TimeUnit::MICRO);
+        type = time64(TimeUnit::MICRO);
     } else if (unit_str == "NANOSECOND") {
-        *type = time64(TimeUnit::NANO);
+        type = time64(TimeUnit::NANO);
     } else {
         return Status::Invalid("Invalid time unit: ", unit_str);
     }
-
-    const auto& fw_type = reinterpret_cast<const FixedWidthType&>(**type);
+    const auto& fw_type = reinterpret_cast<const FixedWidthType&>(*type);
     if (bit_width != fw_type.bit_width()) {
         return Status::Invalid("Indicated bit width does not match unit");
     }
-    return Status::OK();
+    return type;
 }
 
 /// Read a duration type
-Status ReadDurationType(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadDurationType(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const TimeUnit::type unit, GetMemberTimeUnit(json_type, "unit"));
-    *type = duration(unit);
-    return Status::OK();
+    return duration(unit);
 }
 
 /// Read a timestamp type
-Status ReadTimeTypestamp(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadTimeTypestamp(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const TimeUnit::type unit, GetMemberTimeUnit(json_type, "unit"));
-
     const auto& it_tz = json_type.FindMember("timezone");
     if (it_tz == json_type.MemberEnd()) {
-        *type = timestamp(unit);
+        return timestamp(unit);
     } else {
         if (!it_tz->value.IsString()) return Status::Invalid("timezone is not a string");
-        *type = timestamp(unit, it_tz->value.GetString());
+        return timestamp(unit, it_tz->value.GetString());
     }
-
-    return Status::OK();
 }
 
 /// Read an interval type
-Status ReadIntervalType(const RjObject& json_type, std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadIntervalType(const RjObject& json_type) {
     ARROW_ASSIGN_OR_RAISE(const auto unit_str, GetMemberString(json_type, "unit"));
-
     if (unit_str == kDayTime) {
-        *type = day_time_interval();
+        return day_time_interval();
     } else if (unit_str == kYearMonth) {
-        *type = month_interval();
-    } else {
-        return Status::Invalid("Invalid interval unit: " + unit_str);
+        return month_interval();
     }
-    return Status::OK();
+    return Status::Invalid("Invalid interval unit: " + unit_str);
 }
 
 /// Read a union type
-Status ReadUnionType(const RjObject& json_type, const std::vector<std::shared_ptr<Field>>& children,
-                     std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<DataType>> ReadUnionType(const RjObject& json_type,
+                                                const std::vector<std::shared_ptr<Field>>& children) {
     ARROW_ASSIGN_OR_RAISE(const auto mode_str, GetMemberString(json_type, "mode"));
 
     UnionMode::type mode;
@@ -308,67 +284,65 @@ Status ReadUnionType(const RjObject& json_type, const std::vector<std::shared_pt
     }
 
     if (mode == UnionMode::SPARSE) {
-        *type = sparse_union(std::move(children), std::move(type_codes));
+        return sparse_union(std::move(children), std::move(type_codes));
     } else {
-        *type = dense_union(std::move(children), std::move(type_codes));
+        return dense_union(std::move(children), std::move(type_codes));
     }
-
-    return Status::OK();
 }
 
 /// Read a type
-Status ReadType(const RjObject& json_type, const std::vector<std::shared_ptr<Field>>& children,
-                std::shared_ptr<DataType>* type) {
+Result<std::shared_ptr<arrow::DataType>> ReadType(const RjObject& json_type,
+                                                  const std::vector<std::shared_ptr<Field>>& children) {
     ARROW_ASSIGN_OR_RAISE(const auto type_name, GetMemberString(json_type, "name"));
 
     if (type_name == "int") {
-        return ReadIntegerType(json_type, type);
+        return ReadIntegerType(json_type);
     } else if (type_name == "floatingpoint") {
-        return ReadFloatingPointType(json_type, type);
+        return ReadFloatingPointType(json_type);
     } else if (type_name == "bool") {
-        *type = boolean();
+        return boolean();
     } else if (type_name == "utf8") {
-        *type = utf8();
+        return utf8();
     } else if (type_name == "binary") {
-        *type = binary();
+        return binary();
     } else if (type_name == "largeutf8") {
-        *type = large_utf8();
+        return large_utf8();
     } else if (type_name == "largebinary") {
-        *type = large_binary();
+        return large_binary();
     } else if (type_name == "fixedsizebinary") {
-        return ReadFixedSizeBinaryType(json_type, type);
+        return ReadFixedSizeBinaryType(json_type);
     } else if (type_name == "decimal") {
-        return ReadDecimalType(json_type, type);
+        return ReadDecimalType(json_type);
     } else if (type_name == "null") {
-        *type = null();
+        return null();
     } else if (type_name == "date") {
-        return GetDateType(json_type, type);
+        return GetDateType(json_type);
     } else if (type_name == "time") {
-        return ReadTimeType(json_type, type);
+        return ReadTimeType(json_type);
     } else if (type_name == "timestamp") {
-        return ReadTimeTypestamp(json_type, type);
+        return ReadTimeTypestamp(json_type);
     } else if (type_name == "interval") {
-        return ReadIntervalType(json_type, type);
+        return ReadIntervalType(json_type);
     } else if (type_name == kDuration) {
-        return ReadDurationType(json_type, type);
+        return ReadDurationType(json_type);
     } else if (type_name == "list") {
         if (children.size() != 1) {
             return Status::Invalid("List must have exactly one child");
         }
-        *type = list(children[0]);
+        return list(children[0]);
     } else if (type_name == "largelist") {
         if (children.size() != 1) {
             return Status::Invalid("Large list must have exactly one child");
         }
-        *type = large_list(children[0]);
+        return large_list(children[0]);
     } else if (type_name == "map") {
-        return ReadMapType(json_type, children, type);
+        return ReadMapType(json_type, children);
     } else if (type_name == "fixedsizelist") {
-        return ReadFixedSizeListType(json_type, children, type);
+        return ReadFixedSizeListType(json_type, children);
     } else if (type_name == "struct") {
-        *type = struct_(children);
+        return struct_(children);
     } else if (type_name == "union") {
-        return ReadUnionType(json_type, children, type);
+        return ReadUnionType(json_type, children);
     } else {
         return Status::Invalid("Unrecognized type name: ", type_name);
     }
@@ -381,7 +355,6 @@ arrow::Result<std::shared_ptr<Field>> ReadField(const rapidjson::Value& obj) {
         return Status::Invalid("Field was not a JSON object");
     }
     const auto& json_field = obj.GetObject();
-    std::shared_ptr<DataType> type;
 
     ARROW_ASSIGN_OR_RAISE(const auto name, GetMemberString(json_field, "name"));
     ARROW_ASSIGN_OR_RAISE(const bool nullable, GetMemberBool(json_field, "nullable"));
@@ -390,7 +363,7 @@ arrow::Result<std::shared_ptr<Field>> ReadField(const rapidjson::Value& obj) {
     ARROW_ASSIGN_OR_RAISE(const auto json_children, GetMemberArray(json_field, "children"));
 
     ARROW_ASSIGN_OR_RAISE(auto children, ReadFields(json_children));
-    RETURN_NOT_OK(ReadType(json_type, children, &type));
+    ARROW_ASSIGN_OR_RAISE(auto type, ReadType(json_type, children));
 
     // Create field
     return arrow::field(name, type, nullable);
