372bcb4b2a2c3cc62d0dcf5b56c38c26bfaef65c
Andre Kohn <kohn.a@outlook.com>
2021-05-11 17:23:15 +0200

    Add memory filesystem test

--- lib/test/duckdb/memory_filesystem_test.cc
+++ lib/test/duckdb/memory_filesystem_test.cc
@@ -16,7 +16,7 @@ using namespace std;
 
 namespace {
 
-TEST(MemoryFilesystem, istreambuf_iterator) {
+TEST(MemoryFilesystem, json) {
     constexpr std::string_view raw_input = R"([
         {"a": 1, "b": 2.0, "c": "foo", "d": false}
         {"a": 4, "b": -5.5, "c": null, "d": true}
@@ -33,6 +33,26 @@ TEST(MemoryFilesystem, istreambuf_iterator) {
     std::istream ifs{input.get()};
     std::string have{std::istreambuf_iterator<char>{ifs}, std::istreambuf_iterator<char>{}};
     ASSERT_EQ(have, std::string{raw_input});
+}
+
+TEST(MemoryFilesystem, integers) {
+    constexpr size_t N = 10000;
+    std::vector<char> input_buffer;
+    input_buffer.resize(N * sizeof(uint64_t));
+    for (size_t i = 0; i < N; ++i) reinterpret_cast<uint64_t*>(input_buffer.data())[i] = i;
+
+    auto memory_filesystem = std::make_unique<io::MemoryFileSystem>();
+    const char* path = "foo";
+    ASSERT_TRUE(memory_filesystem->RegisterFileBuffer(path, std::move(input_buffer)).ok());
+
+    auto filesystem_buffer = std::make_shared<io::FileSystemBuffer>(std::move(memory_filesystem));
+    auto input = std::make_shared<io::InputFileStreamBuffer>(filesystem_buffer, path);
+
+    std::istream ifs{input.get()};
+    std::vector<char> have{std::istreambuf_iterator<char>{ifs}, std::istreambuf_iterator<char>{}};
+    for (size_t i = 0; i < N; ++i) {
+        ASSERT_EQ(reinterpret_cast<uint64_t*>(have.data())[i], i);
+    }
 }
 
 }  // namespace
