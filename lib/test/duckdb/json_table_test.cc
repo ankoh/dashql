411fb9a3df41f919f38f2e2a6c3dcb88dd6abc17
Andre Kohn <kohn.a@outlook.com>
2021-05-12 12:30:20 +0200

    Add test suite for reading json into a single record batch

--- lib/test/duckdb/json_table_test.cc
+++ lib/test/duckdb/json_table_test.cc
@@ -2,6 +2,7 @@
 
 #include <optional>
 
+#include "arrow/record_batch.h"
 #include "duckdb/web/io/memory_filesystem.h"
 #include "duckdb/web/json_analyzer.h"
 #include "duckdb/web/json_parser.h"
@@ -109,34 +110,61 @@ static std::shared_ptr<io::InputFileStreamBuffer> CreateStreamBuf(const char* pa
     return std::make_shared<io::InputFileStreamBuffer>(filesystem_buffer, path);
 }
 
-TEST(TableReader, RowObjectDetect1) {
-    const char* path = "foo.json";
-    constexpr std::string_view raw_input = R"([
-        {"foo": 1},
-        {"foo": 4}
-    ])";
-    std::vector<char> input_buffer{raw_input.data(), raw_input.data() + raw_input.size()};
-
-    json::TableType type = {
-        .shape = json::TableShape::ROW_ARRAY,
-        .type = arrow::struct_({
-            arrow::field("foo", arrow::int32()),
-        }),
+struct TableReaderTest {
+    struct TestPrinter {
+        std::string operator()(const ::testing::TestParamInfo<TableReaderTest>& info) const {
+            return std::string{info.param.name};
+        }
     };
+    std::string_view name;
+    std::string_view input;
+    json::TableType type;
+    std::string_view expected;
+};
+
+struct TableReaderTestSuite : public testing::TestWithParam<TableReaderTest> {};
+
+TEST_P(TableReaderTestSuite, ReadSingleBatch) {
+    constexpr const char* path = "TEST";
+
+    auto& test = GetParam();
+    std::vector<char> input_buffer{test.input.data(), test.input.data() + test.input.size()};
 
     auto fs = std::make_shared<io::MemoryFileSystem>();
     auto fs_buffer = std::make_shared<io::FileSystemBuffer>(fs);
     ASSERT_TRUE(fs->RegisterFileBuffer(path, std::move(input_buffer)).ok());
 
     auto in = std::make_unique<io::InputFileStream>(fs_buffer, path);
-    auto maybe_reader = json::TableReader::Resolve(std::move(in), std::move(type));
+    auto maybe_reader = json::TableReader::Resolve(std::move(in), test.type);
     ASSERT_TRUE(maybe_reader.ok());
-
     auto reader = std::move(maybe_reader.ValueUnsafe());
     ASSERT_TRUE(reader->Prepare().ok());
-
-    //    auto maybe_batch = reader->ReadNextBatch();
-    //    ASSERT_TRUE(maybe_batch.ok());
+    auto maybe_batch = reader->ReadNextBatch();
+    ASSERT_TRUE(maybe_batch.ok());
+    auto& batch = maybe_batch.ValueUnsafe();
+    ASSERT_EQ(batch->ToString(), std::string(test.expected));
 }
+
+// clang-format off
+static std::vector<TableReaderTest> TABLE_READER_TEST = {
+    {
+        .name = "rows_int32",
+        .input = R"JSON([
+            {"foo": 1},
+            {"foo": 4}
+        ])JSON",
+        .type = {
+            .shape = json::TableShape::ROW_ARRAY,
+            .type = arrow::struct_({
+                arrow::field("foo", arrow::int32()),
+            })
+        },
+        .expected = "foo:   [\n    1,\n    4\n  ]\n"
+    }
+};
+// clang-format on
+
+INSTANTIATE_TEST_SUITE_P(TableReaderTest, TableReaderTestSuite, testing::ValuesIn(TABLE_READER_TEST),
+                         TableReaderTest::TestPrinter());
 
 }  // namespace
