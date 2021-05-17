a9c3d455bb8071c588278e79a743144c77cb0c38
Andre Kohn <kohn.a@outlook.com>
2021-05-17 12:15:44 +0200

    Streamline karma configs and add spec reporter for ci

---
+++ packages/core/karma.base.cjs
@@ -0,0 +1,88 @@
+const path = require('path');
+
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+const JS_TIMEOUT = 900000;
+
+module.exports = function (config) {
+    return {
+        basePath: './',
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-sourcemap-loader',
+            'karma-spec-reporter',
+            'karma-coverage',
+            'karma-jasmine-html-reporter',
+        ],
+        frameworks: ['jasmine'],
+        files: [
+            { pattern: 'dist/tests-browser.js' },
+            { pattern: 'src/analyzer/analyzer_wasm.wasm', included: false, watched: false, served: true },
+            { pattern: '../duckdb/dist/duckdb.wasm', included: false, watched: false, served: true },
+            {
+                pattern: '../duckdb/dist/duckdb-browser-parallel.worker.js',
+                included: false,
+                watched: false,
+                served: true,
+            },
+        ],
+        preprocessors: {
+            'src/**/*.js': ['sourcemap'],
+            'test/**/*.js': ['sourcemap'],
+        },
+        proxies: {
+            '/static/analyzer_wasm.wasm': '/base/src/analyzer/analyzer_wasm.wasm',
+            '/static/duckdb.wasm': '/absolute' + path.resolve('../duckdb/dist/duckdb.wasm'),
+            '/static/duckdb-browser-parallel.worker.js':
+                '/absolute' + path.resolve('../duckdb/dist/duckdb-browser-parallel.worker.js'),
+        },
+        exclude: [],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: true,
+        browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox'],
+            },
+        },
+        specReporter: {
+            maxLogLines: 5,
+            suppressErrorSummary: true,
+            suppressFailed: false,
+            suppressPassed: false,
+            suppressSkipped: true,
+            showSpecTiming: true,
+            failFast: true,
+            prefixes: {
+                success: '    OK: ',
+                failure: 'FAILED: ',
+                skipped: 'SKIPPED: ',
+            },
+        },
+        coverageReporter: {
+            type: 'json',
+            dir: './coverage/',
+            subdir: function (browser) {
+                return browser.toLowerCase().split(/[ /-]/)[0];
+            },
+        },
+        client: {
+            jasmine: {
+                failFast: true,
+                timeoutInterval: JS_TIMEOUT,
+            },
+        },
+        captureTimeout: JS_TIMEOUT,
+        browserDisconnectTimeout: JS_TIMEOUT,
+        browserDisconnectTolerance: 1,
+        browserNoActivityTimeout: JS_TIMEOUT,
+        processKillTimeout: JS_TIMEOUT,
+        concurrency: 1,
+    };
+};
--- packages/duckdb/karma.conf.dbg.cjs
+++ packages/duckdb/karma.conf.dbg.cjs
@@ -1,48 +1,5 @@
-const puppeteer = require('puppeteer');
-
-process.env.CHROME_BIN = puppeteer.executablePath();
-
-const JS_TIMEOUT = 900000;
+const base = require('./karma.base.cjs');
 
 module.exports = function (config) {
-    config.set({
-        basePath: '../..',
-        plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-sourcemap-loader', 'karma-jasmine-html-reporter'],
-        frameworks: ['jasmine'],
-        files: [
-            { pattern: 'packages/duckdb/dist/tests-browser.js' },
-            { pattern: 'packages/duckdb/dist/*.wasm', included: false, watched: false, served: true },
-            { pattern: 'packages/duckdb/dist/*.js', included: false, watched: false, served: true },
-            { pattern: 'data/**/*.parquet', included: false, watched: false, served: true },
-            { pattern: 'data/**/*.zip', included: false, watched: false, served: true },
-        ],
-        preprocessors: {
-            'src/**/*.js': ['sourcemap'],
-            'test/**/*.js': ['sourcemap'],
-        },
-        proxies: {
-            '/static/': '/base/packages/duckdb/dist/',
-            '/data/': '/base/data/',
-        },
-        exclude: [],
-        reporters: ['kjhtml'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        singleRun: false,
-        browsers: [],
-        client: {
-            jasmine: {
-                failFast: true,
-                timeoutInterval: JS_TIMEOUT,
-            },
-        },
-        captureTimeout: JS_TIMEOUT,
-        browserDisconnectTimeout: JS_TIMEOUT,
-        browserDisconnectTolerance: 1,
-        browserNoActivityTimeout: JS_TIMEOUT,
-        processKillTimeout: JS_TIMEOUT,
-        concurrency: 1,
-    });
+    config.set({ ...base(config), reporters: ['spec', 'coverage'] });
 };
