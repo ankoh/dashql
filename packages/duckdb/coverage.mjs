78d6260009e19c3130a8d05a25ea57d7707f4f52
Andre Kohn <kohn.a@outlook.com>
2021-05-17 12:56:53 +0200

    Ignore coverage script in eslint

--- packages/duckdb/coverage.mjs
+++ packages/duckdb/coverage.mjs
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import path from 'path';
 import mkdir from 'make-dir';
 import rimraf from 'rimraf';
@@ -9,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const cov = path.resolve(__dirname, 'coverage');
 const cov_chrome = path.resolve(cov, 'chrome', 'coverage-final.json');
-const cov_firefox = path.resolve(cov, 'firefox', 'coverage-final.json');
+//const cov_firefox = path.resolve(cov, 'firefox', 'coverage-final.json');
 //const cov_node = path.resolve(cov, 'node', 'coverage-final.json');
 const cov_all = path.resolve(cov, 'all');
 const cov_out = path.resolve(cov, 'coverage.json');
@@ -18,7 +17,7 @@ const nyc = '../../node_modules/nyc/bin/nyc.js';
 rimraf.sync(cov_all);
 mkdir.sync(cov_all);
 fs.copyFileSync(cov_chrome, path.resolve(cov_all, 'chrome.json'));
-fs.copyFileSync(cov_firefox, path.resolve(cov_all, 'firefox.json'));
+//fs.copyFileSync(cov_firefox, path.resolve(cov_all, 'firefox.json'));
 //fs.copyFileSync(cov_node, path.resolve(cov_all, 'node.json'));
 
 const out = spawnSync(nyc, ['merge', cov_all, cov_out], {
