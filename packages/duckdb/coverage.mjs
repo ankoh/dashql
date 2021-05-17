a65af4d64e36cb7b851212455e04b518d2ecca1a
Andre Kohn <kohn.a@outlook.com>
2021-05-17 13:22:06 +0200

    Reenable firefox and node coverage with worker termination

--- packages/duckdb/coverage.mjs
+++ packages/duckdb/coverage.mjs
@@ -9,8 +9,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const cov = path.resolve(__dirname, 'coverage');
 const cov_chrome = path.resolve(cov, 'chrome', 'coverage-final.json');
-//const cov_firefox = path.resolve(cov, 'firefox', 'coverage-final.json');
-//const cov_node = path.resolve(cov, 'node', 'coverage-final.json');
+const cov_firefox = path.resolve(cov, 'firefox', 'coverage-final.json');
+const cov_node = path.resolve(cov, 'node', 'coverage-final.json');
 const cov_all = path.resolve(cov, 'all');
 const cov_out = path.resolve(cov, 'coverage.json');
 const nyc = '../../node_modules/nyc/bin/nyc.js';
@@ -18,8 +18,8 @@ const nyc = '../../node_modules/nyc/bin/nyc.js';
 rimraf.sync(cov_all);
 mkdir.sync(cov_all);
 fs.copyFileSync(cov_chrome, path.resolve(cov_all, 'chrome.json'));
-//fs.copyFileSync(cov_firefox, path.resolve(cov_all, 'firefox.json'));
-//fs.copyFileSync(cov_node, path.resolve(cov_all, 'node.json'));
+fs.copyFileSync(cov_firefox, path.resolve(cov_all, 'firefox.json'));
+fs.copyFileSync(cov_node, path.resolve(cov_all, 'node.json'));
 
 const out = spawnSync(nyc, ['merge', cov_all, cov_out], {
     encoding: 'utf8',
