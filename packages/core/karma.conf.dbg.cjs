55ed95a17d805592366843ece8d721f1f2a84a51
Andre Kohn <kohn.a@outlook.com>
2021-05-17 10:45:49 +0200

    Further increase js timeouts

--- packages/core/karma.conf.dbg.cjs
+++ packages/core/karma.conf.dbg.cjs
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const JS_TIMEOUT = 900000;
+
 module.exports = function (config) {
     config.set({
         basePath: './',
@@ -43,14 +45,14 @@ module.exports = function (config) {
         client: {
             jasmine: {
                 failFast: true,
-                timeoutInterval: 90000,
+                timeoutInterval: JS_TIMEOUT,
             },
         },
-        captureTimeout: 900000,
-        browserDisconnectTimeout: 900000,
+        captureTimeout: JS_TIMEOUT,
+        browserDisconnectTimeout: JS_TIMEOUT,
         browserDisconnectTolerance: 1,
-        browserNoActivityTimeout: 900000,
-        processKillTimeout: 100000,
+        browserNoActivityTimeout: JS_TIMEOUT,
+        processKillTimeout: JS_TIMEOUT,
         concurrency: 1,
     });
 };
