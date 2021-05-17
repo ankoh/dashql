3742e53d01f539f052735c0e1e8db33563bbbb7a
Andre Kohn <kohn.a@outlook.com>
2021-05-17 13:22:06 +0200

    Reenable firefox and node coverage with worker termination

--- packages/core/coverage.mjs
+++ packages/core/coverage.mjs
@@ -9,7 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const cov = path.resolve(__dirname, 'coverage');
 const cov_chrome = path.resolve(cov, 'chrome', 'coverage-final.json');
-//const cov_firefox = path.resolve(cov, 'firefox', 'coverage-final.json');
+const cov_firefox = path.resolve(cov, 'firefox', 'coverage-final.json');
 const cov_all = path.resolve(cov, 'all');
 const cov_out = path.resolve(cov, 'coverage.json');
 const nyc = '../../node_modules/nyc/bin/nyc.js';
@@ -17,7 +17,7 @@ const nyc = '../../node_modules/nyc/bin/nyc.js';
 rimraf.sync(cov_all);
 mkdir.sync(cov_all);
 fs.copyFileSync(cov_chrome, path.resolve(cov_all, 'chrome.json'));
-//fs.copyFileSync(cov_firefox, path.resolve(cov_all, 'firefox.json'));
+fs.copyFileSync(cov_firefox, path.resolve(cov_all, 'firefox.json'));
 
 const out = spawnSync(nyc, ['merge', cov_all, cov_out], {
     encoding: 'utf8',
