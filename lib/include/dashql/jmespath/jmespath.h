052db43330c74d0995489cb733a3fd3402586e3a
Andre Kohn <kohn.a@outlook.com>
2021-07-14 11:27:54 +0200

    Test jmespath query

--- lib/include/dashql/jmespath/jmespath.h
+++ lib/include/dashql/jmespath/jmespath.h
@@ -12,7 +12,7 @@ namespace dashql {
 
 struct JMESPath {
     /// Evaluate a single expression on a given input
-    static arrow::Result<std::string> Evaluate(const char* input, const char* expression);
+    static arrow::Result<std::string> Evaluate(const char* expression, const char* input);
 };
 
 }  // namespace dashql
