478882e645e110219555de09978ac061347ba8b1
Andre Kohn <kohn.a@outlook.com>
2021-07-15 15:31:57 +0200

    Add alpha banner

---
+++ packages/app/src/components/banner.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+import styles from './banner.module.css';
+
+type Props = Record<string, string>;
+
+class Banner extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <div className={styles.banner_bar}>
+                <span className={styles.banner_text}>
+                    <b>CAUTION:</b>&nbsp;Alpha-quality software in the wild! Talk to us
+                </span>
+                <a
+                    className={styles.banner_link}
+                    target="_blank"
+                    href="https://github.com/dashql/dashql/discussions"
+                    rel="noreferrer"
+                >
+                    here
+                </a>
+            </div>
+        );
+    }
+}
+
+export function withBanner<P>(Component: React.ComponentType<P>): React.FunctionComponent<P> {
+    // eslint-disable-next-line react/display-name
+    return (props: P) => {
+        return (
+            <div className={styles.container}>
+                <Banner />
+                <div className={styles.page}>
+                    <Component {...props} />
+                </div>
+            </div>
+        );
+    };
+}
