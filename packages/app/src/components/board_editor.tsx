78e50ac5c9fd73ad5a3191059b5d1628791c58cd
Andre Kohn <kohn.a@outlook.com>
2021-07-26 20:27:26 +0200

    Fix scroller bug

--- packages/app/src/components/board_editor.tsx
+++ packages/app/src/components/board_editor.tsx
@@ -28,52 +28,53 @@ export class BoardEditor extends React.Component<IBoardEditorProps> {
             <div className={styles.container}>
                 <AutoSizer>
                     {({ height, width }) => (
-                        <div className={styles.content_with_rulers}>
-                            <div className={styles.ruler_corner} />
-                            <Ruler
-                                className={styles.ruler_top}
-                                width={width - rulerThickness}
-                                height={rulerThickness}
-                                orientation={RulerOrientation.Horizontal}
-                                scaleFactor={this.props.scaleFactor}
-                                stepCount={columnCount}
-                                containerPadding={containerPadding[0]}
-                                tickMargin={elementMargin[0]}
-                            />
-                            <Scrollbars
-                                style={{ height: height - rulerThickness, width }}
-                                className={styles.content_scroller}
-                                renderTrackHorizontal={props => (
-                                    <div {...props} style={{ display: 'none' }} className="track-horizontal" />
-                                )}
-                            >
-                                <div className={styles.content_container}>
-                                    <Board
-                                        className={styles.content}
-                                        width={width - rulerThickness}
-                                        editable={true}
-                                        columnCount={columnCount}
-                                        rowHeight={rowHeight}
-                                        containerPadding={containerPadding}
-                                        elementMargin={elementMargin}
-                                    />
-                                    <div className={styles.ruler_left}>
-                                        <AutoSizer disableWidth>
-                                            {s => (
-                                                <Ruler
-                                                    width={rulerThickness}
-                                                    height={s.height}
-                                                    orientation={RulerOrientation.Vertical}
-                                                    scaleFactor={this.props.scaleFactor}
-                                                    stepLength={rowHeight}
-                                                    containerPadding={containerPadding[1]}
-                                                    tickMargin={elementMargin[1]}
-                                                />
-                                            )}
-                                        </AutoSizer>
+                        <div style={{ width, height }}>
+                            <div className={styles.content_with_rulers}>
+                                <div className={styles.ruler_corner} />
+                                <Ruler
+                                    className={styles.ruler_top}
+                                    width={width - rulerThickness}
+                                    height={rulerThickness}
+                                    orientation={RulerOrientation.Horizontal}
+                                    scaleFactor={this.props.scaleFactor}
+                                    stepCount={columnCount}
+                                    containerPadding={containerPadding[0]}
+                                    tickMargin={elementMargin[0]}
+                                />
+                                <div className={styles.content_scroller} style={{ height }}>
+                                    <div
+                                        className={styles.content_container}
+                                        style={{
+                                            gridTemplateRows: `minmax(${height - rulerThickness}px, max-content)`,
+                                        }}
+                                    >
+                                        <Board
+                                            className={styles.content}
+                                            width={width - rulerThickness}
+                                            editable={true}
+                                            columnCount={columnCount}
+                                            rowHeight={rowHeight}
+                                            containerPadding={containerPadding}
+                                            elementMargin={elementMargin}
+                                        />
+                                        <div className={styles.ruler_left}>
+                                            <AutoSizer disableWidth>
+                                                {s => (
+                                                    <Ruler
+                                                        width={rulerThickness}
+                                                        height={s.height}
+                                                        orientation={RulerOrientation.Vertical}
+                                                        scaleFactor={this.props.scaleFactor}
+                                                        stepLength={rowHeight}
+                                                        containerPadding={containerPadding[1]}
+                                                        tickMargin={elementMargin[1]}
+                                                    />
+                                                )}
+                                            </AutoSizer>
+                                        </div>
                                     </div>
                                 </div>
-                            </Scrollbars>
+                            </div>
                         </div>
                     )}
                 </AutoSizer>
