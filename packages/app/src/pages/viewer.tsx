332011c9207ba1ea6cda5a0e4bb956b25b35d28b
Andre Kohn <kohn.a@outlook.com>
2021-08-06 15:30:52 +0200

    Simplify components

--- packages/app/src/pages/viewer.tsx
+++ packages/app/src/pages/viewer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styles from './viewer.module.css';
-import styles_cmd from '../components/cmdbars.module.css';
+import styles_cmd from '../components/cmdbar.module.css';
 import { AppState, Dispatch } from '../model';
 import { AutoSizer } from '../util/autosizer';
 import { Board, ScriptLoader } from '../components';
