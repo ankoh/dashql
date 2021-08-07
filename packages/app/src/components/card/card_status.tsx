e49ee11bbeff224ad2097d3a59722c8a3fb8df82
Andre Kohn <kohn.a@outlook.com>
2021-08-07 17:46:07 +0200

    Migrate to hooks

--- packages/app/src/components/card/card_status.tsx
+++ packages/app/src/components/card/card_status.tsx
@@ -1,16 +1,14 @@
 import * as Immutable from 'immutable';
 import * as React from 'react';
 import * as core from '@dashql/core';
+import * as model from '../../model';
 import classNames from 'classnames';
-import { AppState, Dispatch } from '../../model';
-import { connect } from 'react-redux';
 import { proto } from '@dashql/core';
+import { useSelector } from 'react-redux';
 
 import styles from './card_status.module.css';
 
 interface CardStatusProps {
-    program: core.model.Program | null;
-    programStatus: Immutable.List<core.model.StatementStatus>;
     className?: string;
     card: core.model.CardSpecification;
 }
@@ -24,86 +22,83 @@ interface NodeData {
 
 interface CardStatusState {
     program: core.model.Program | null;
-    programStatus: Immutable.List<core.model.StatementStatus>;
+    status: Immutable.List<core.model.StatementStatus>;
     nodes: NodeData[];
 }
 
-class CardStatus extends React.Component<CardStatusProps, CardStatusState> {
-    constructor(props: CardStatusProps) {
-        super(props);
-        this.state = CardStatus.rebuild(props);
-    }
-
-    protected static rebuild(props: CardStatusProps): CardStatusState {
-        if (!props.program) {
+const updateState = (state: CardStatusState): CardStatusState => ({
+    ...state,
+    nodes: state.nodes.map(n => {
+        const s = state.status.get(n.statementId)!.status;
+        if (n.actionStatus == s) {
+            return n;
+        } else
             return {
-                program: null,
-                programStatus: Immutable.List<core.model.StatementStatus>(),
-                nodes: [],
+                ...n,
+                statementType: n.statementType,
+                actionStatus: s,
             };
-        }
+    }),
+});
 
-        // Collect all transitive dependencies
-        const focus = new core.utils.NativeBitmap(props.program.buffer.statementsLength());
-        const depDFS: number[] = [];
-        const depMapping = props.program.statementDependencies;
-        focus.set(props.card.statementID);
-        depDFS.push(props.card.statementID);
-        while (depDFS.length > 0) {
-            const top = depDFS.pop()!;
-            const deps = depMapping.get(top);
-            if (!deps) continue;
-            for (const dep of deps) {
-                if (!focus.isSet(dep)) {
-                    focus.set(dep);
-                    depDFS.push(dep);
-                }
-            }
-        }
-
-        // We use dagre to do the layouting and render the graph with react-flow afterwards
-        const nodes: NodeData[] = [];
-        props.program.iterateStatements((idx: number, stmt: core.model.Statement) => {
-            if (!focus.isSet(idx)) return;
-            nodes.push({
-                nodeId: idx,
-                statementId: idx,
-                statementType: stmt.statement_type,
-                actionStatus: proto.action.ActionStatusCode.PENDING,
-            });
-        });
-        return CardStatus.updateState(props, {
-            program: props.program,
-            programStatus: props.programStatus,
-            nodes: nodes,
-        });
-    }
-
-    protected static updateState(props: CardStatusProps, state: CardStatusState): CardStatusState {
+function rebuild(
+    props: CardStatusProps,
+    program: core.model.Program | null,
+    status: Immutable.List<core.model.StatementStatus>,
+): CardStatusState {
+    if (!program) {
         return {
-            ...state,
-            nodes: state.nodes.map(n => {
-                const s = props.programStatus.get(n.statementId)!.status;
-                if (n.actionStatus == s) {
-                    return n;
-                } else
-                    return {
-                        ...n,
-                        statementType: n.statementType,
-                        actionStatus: s,
-                    };
-            }),
+            program: null,
+            status: Immutable.List<core.model.StatementStatus>(),
+            nodes: [],
         };
     }
 
-    public static getDerivedStateFromProps(props: CardStatusProps, state: CardStatusState) {
-        if (props.program !== state.program) {
-            return CardStatus.rebuild(props);
+    // Collect all transitive dependencies
+    const focus = new core.utils.NativeBitmap(program.buffer.statementsLength());
+    const depDFS: number[] = [];
+    const depMapping = program.statementDependencies;
+    focus.set(props.card.statementID);
+    depDFS.push(props.card.statementID);
+    while (depDFS.length > 0) {
+        const top = depDFS.pop()!;
+        const deps = depMapping.get(top);
+        if (!deps) continue;
+        for (const dep of deps) {
+            if (!focus.isSet(dep)) {
+                focus.set(dep);
+                depDFS.push(dep);
+            }
         }
-        return CardStatus.updateState(props, state);
     }
 
-    public renderNode(n: NodeData) {
+    // We use dagre to do the layouting and render the graph with react-flow afterwards
+    const nodes: NodeData[] = [];
+    program.iterateStatements((idx: number, stmt: core.model.Statement) => {
+        if (!focus.isSet(idx)) return;
+        nodes.push({
+            nodeId: idx,
+            statementId: idx,
+            statementType: stmt.statement_type,
+            actionStatus: proto.action.ActionStatusCode.PENDING,
+        });
+    });
+    return updateState({
+        program: program,
+        status: status,
+        nodes: nodes,
+    });
+}
+
+export const CardStatus: React.FC<CardStatusProps> = (props: CardStatusProps) => {
+    const [program, status]: [core.model.Program | null, Immutable.List<core.model.StatementStatus>] = useSelector(
+        (state: model.AppState) => [state.core.program, state.core.planState.status],
+    );
+    const [state, setState] = React.useState<CardStatusState>(() => rebuild(props, program, status));
+    if (program !== state.program) {
+        setState(rebuild(props, program, status));
+    }
+    const renderNode = (n: NodeData) => {
         let color = 'transparent';
         switch (n.actionStatus!) {
             case proto.action.ActionStatusCode.BLOCKED:
@@ -123,25 +118,13 @@ class CardStatus extends React.Component<CardStatusProps, CardStatusState> {
                 break;
         }
         return <div key={n.nodeId} className={styles.node} style={{ backgroundColor: color }} />;
+    };
+    if (program == null) {
+        return <div />;
     }
-
-    public render() {
-        if (this.props.program == null) {
-            return <div />;
-        }
-        return (
-            <div className={classNames(styles.container, this.props.className)}>
-                <div className={styles.node_group}>{this.state.nodes.map(n => this.renderNode(n))}</div>
-            </div>
-        );
-    }
-}
-
-const mapStateToProps = (state: AppState) => ({
-    program: state.core.plan?.program || null,
-    programStatus: state.core.planState.status,
-});
-
-const mapDispatchToProps = (_dispatch: Dispatch) => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CardStatus);
+    return (
+        <div className={classNames(styles.container, props.className)}>
+            <div className={styles.node_group}>{state.nodes.map(n => renderNode(n))}</div>
+        </div>
+    );
+};
