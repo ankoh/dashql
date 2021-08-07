54e9bd1f7691e3a473ae4abe4e7051c8d3f74f25
Andre Kohn <kohn.a@outlook.com>
2021-08-07 18:23:25 +0200

    Migrate to hooks

--- packages/app/src/components/blob_loader.tsx
+++ packages/app/src/components/blob_loader.tsx
@@ -20,59 +20,46 @@ interface State {
     buffer: ArrayBuffer | null;
 }
 
-class BlobLoader extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            status: BlobLoaderStatus.PENDING,
-            error: false,
-            buffer: null,
+export const BlobLoader: React.FC<Props> = (props: Props) => {
+    const [state, setState] = React.useState<State>({
+        status: BlobLoaderStatus.PENDING,
+        error: null,
+        buffer: null,
+    });
+
+    React.useEffect(() => {
+        if (state.status != BlobLoaderStatus.PENDING) return;
+        setState({
+            ...state,
+            status: BlobLoaderStatus.IN_FLIGHT,
+        });
+        const loadBlob = async (): Promise<void> => {
+            try {
+                const buffer = await props.blob.arrayBuffer();
+                setState({
+                    status: BlobLoaderStatus.SUCCEEDED,
+                    buffer,
+                    error: null,
+                });
+            } catch (e) {
+                setState({
+                    status: BlobLoaderStatus.FAILED,
+                    buffer: null,
+                    error: e,
+                });
+            }
         };
-    }
+        loadBlob();
+    }, [state.status]);
 
-    async loadBlob(): Promise<void> {
-        try {
-            const buffer = await this.props.blob.arrayBuffer();
-            this.setState({
-                status: BlobLoaderStatus.SUCCEEDED,
-                buffer,
-            });
-        } catch (e) {
-            this.setState({
-                status: BlobLoaderStatus.FAILED,
-                error: e,
-            });
-        }
+    switch (state.status) {
+        case BlobLoaderStatus.PENDING:
+            return <div />;
+        case BlobLoaderStatus.IN_FLIGHT:
+            return props.loadingComponent();
+        case BlobLoaderStatus.FAILED:
+            return props.errorComponent(state.error);
+        case BlobLoaderStatus.SUCCEEDED:
+            return props.children(state.buffer!);
     }
-
-    componentDidMount(): void {
-        if (this.state.status != BlobLoaderStatus.PENDING) return;
-        this.setState({
-            status: BlobLoaderStatus.IN_FLIGHT,
-        });
-        this.loadBlob();
-    }
-
-    componentDidUpdate(prevProps: Props): void {
-        if (this.props.blob == prevProps.blob) return;
-        this.setState({
-            status: BlobLoaderStatus.IN_FLIGHT,
-        });
-        this.loadBlob();
-    }
-
-    public render(): React.ReactElement {
-        switch (this.state.status) {
-            case BlobLoaderStatus.PENDING:
-                return <div />;
-            case BlobLoaderStatus.IN_FLIGHT:
-                return this.props.loadingComponent();
-            case BlobLoaderStatus.FAILED:
-                return this.props.errorComponent(this.state.error);
-            case BlobLoaderStatus.SUCCEEDED:
-                return this.props.children(this.state.buffer!);
-        }
-    }
-}
-
-export default BlobLoader;
+};
