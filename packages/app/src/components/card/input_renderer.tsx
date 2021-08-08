df0277e5eb9ae4c3f93335a03780296daf05b3d2
Andre Kohn <kohn.a@outlook.com>
2021-08-08 16:37:12 +0200

    Migrate to hooks

--- packages/app/src/components/card/input_renderer.tsx
+++ packages/app/src/components/card/input_renderer.tsx
@@ -1,38 +1,23 @@
 import * as React from 'react';
 import * as core from '@dashql/core';
-import * as model from '../../model';
-import { connect } from 'react-redux';
-import { IAppContext, withAppContext } from '../../app_context';
 import { InputGroup, FormControl } from 'react-bootstrap';
 
 import styles from './input_renderer.module.css';
 
 interface Props {
-    appContext: IAppContext;
-    planState: core.model.PlanState;
     card: core.model.CardSpecification;
     editable?: boolean;
 }
 
-export class InputRenderer extends React.Component<Props> {
-    public render(): React.ReactElement {
-        return (
-            <div className={styles.container}>
-                <div className={styles.input_prefix}>{this.props.card.title || ''}</div>
-                <div className={styles.input_group_container}>
-                    <InputGroup size="sm">
-                        <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" />
-                    </InputGroup>
-                </div>
+export const InputRenderer: React.FC<Props> = (props: Props) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.input_prefix}>{props.card.title || ''}</div>
+            <div className={styles.input_group_container}>
+                <InputGroup size="sm">
+                    <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                </InputGroup>
             </div>
-        );
-    }
-}
-
-const mapStateToProps = (state: model.AppState) => ({
-    planState: state.core.planState,
-});
-
-const mapDispatchToProps = (_dispatch: model.Dispatch) => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(withAppContext(InputRenderer));
+        </div>
+    );
+};
--- packages/app/src/components/card/json_renderer.tsx
+++ packages/app/src/components/card/json_renderer.tsx
@@ -1,46 +1,29 @@
 import * as React from 'react';
 import * as core from '@dashql/core';
 import * as model from '../../model';
-import { connect } from 'react-redux';
-import { IAppContext, withAppContext } from '../../app_context';
+import { useSelector } from 'react-redux';
 import { CardFrame } from './card_frame';
 import { JSONViewer } from './json_viewer';
 import { BlobLoader } from '../blob_loader';
 
 interface Props {
-    appContext: IAppContext;
-    planState: core.model.PlanState;
     card: core.model.CardSpecification;
     editable?: boolean;
 }
 
-export class DumpRenderer extends React.Component<Props> {
-    constructor(props: Props) {
-        super(props);
-    }
-
-    /// Render the table
-    public render(): React.ReactElement {
-        const target = this.props.card.dataSource!.targetQualified;
-        const obj = core.model.resolveBlobByName(this.props.planState, target)!;
-        return (
-            <CardFrame title={this.props.card.title || target} controls={this.props.editable}>
-                <BlobLoader
-                    blob={obj.blob}
-                    loadingComponent={() => <div>loading...</div>}
-                    errorComponent={e => <div>Error: {e}</div>}
-                >
-                    {buffer => <JSONViewer buffer={buffer} />}
-                </BlobLoader>
-            </CardFrame>
-        );
-    }
-}
-
-const mapStateToProps = (state: model.AppState) => ({
-    planState: state.core.planState,
-});
-
-const mapDispatchToProps = (_dispatch: model.Dispatch) => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(withAppContext(DumpRenderer));
+export const JsonRenderer: React.FC<Props> = (props: Props) => {
+    const planState = useSelector((state: model.AppState) => state.core.planState);
+    const target = props.card.dataSource!.targetQualified;
+    const obj = core.model.resolveBlobByName(planState, target)!;
+    return (
+        <CardFrame title={props.card.title || target} controls={props.editable}>
+            <BlobLoader
+                blob={obj.blob}
+                loadingComponent={() => <div>loading...</div>}
+                errorComponent={e => <div>Error: {e}</div>}
+            >
+                {buffer => <JSONViewer buffer={buffer} />}
+            </BlobLoader>
+        </CardFrame>
+    );
+};
