ef77e61f52793623933e927156d305829f4c8351
Andre Kohn <kohn.a@outlook.com>
2021-08-08 11:33:20 +0200

    Migrate to hooks

--- packages/app/src/components/current_time.tsx
+++ packages/app/src/components/current_time.tsx
@@ -2,61 +2,21 @@ import * as React from 'react';
 
 interface Props {
     refreshRate: number;
-    children: (time: Date, updateTime: () => void) => React.ReactNode;
+    children: (time: Date, updateTime: () => void) => React.ReactElement;
 }
 
-interface State {
-    currentTime: Date;
-}
-
-class CurrentTime extends React.Component<Props, State> {
-    protected _timer: any | null;
-    protected _updateTime = this.updateTime.bind(this);
-
-    constructor(props: Props) {
-        super(props);
-        this._timer = null;
-        this.state = {
-            currentTime: new Date(),
-        };
-    }
-
-    protected updateTime() {
-        this.setState({
-            currentTime: new Date(),
-        });
-    }
-
-    public shouldComponentUpdate(nextProps: Props) {
-        return nextProps.refreshRate != this.props.refreshRate || nextProps.children != this.props.children;
-    }
-
-    public componentDidMount() {
-        this._timer = setInterval(this.updateTime.bind(this), this.props.refreshRate);
-    }
-
-    public componentDidUpdate(_prevProps: Props) {
-        if (this._timer) {
-            clearInterval(this._timer);
-        }
-        this._timer = setInterval(this.updateTime.bind(this), this.props.refreshRate);
-    }
-
-    public componentWillUnmount() {
-        if (this._timer) {
-            clearInterval(this._timer);
-        }
-        this._timer = null;
-    }
-
-    public render() {
-        return this.props.children(this.state.currentTime, this._updateTime);
-    }
-}
+export const CurrentTime: React.FC<Props> = (props: Props) => {
+    const [currentTime, setCurrentTime] = React.useState<Date>(new Date());
+    React.useEffect(() => {
+        const timer = setInterval(() => setCurrentTime(new Date()), props.refreshRate);
+        return () => clearInterval(timer);
+    }, [props.refreshRate]);
+    return props.children(currentTime, () => setCurrentTime(new Date()));
+};
 
 export function withCurrentTime<
     OUT_PROPS extends { currentTime?: Date; updateCurrentTime: () => void },
-    IN_PROPS = Pick<OUT_PROPS, Exclude<keyof OUT_PROPS, 'currentTime' | 'updateCurrentTime'>>
+    IN_PROPS = Pick<OUT_PROPS, Exclude<keyof OUT_PROPS, 'currentTime' | 'updateCurrentTime'>>,
 >(Component: React.ComponentType<OUT_PROPS>, refreshRate: number): React.FunctionComponent<IN_PROPS> {
     // eslint-disable-next-line react/display-name
     return (props: IN_PROPS) => {
