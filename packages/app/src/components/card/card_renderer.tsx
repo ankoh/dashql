9bb336b8b0e21303538363e15f0d195ff8a2a51a
Andre Kohn <kohn.a@outlook.com>
2021-08-08 16:37:12 +0200

    Migrate to hooks

--- packages/app/src/components/card/card_renderer.tsx
+++ packages/app/src/components/card/card_renderer.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as core from '@dashql/core';
 import { CardStatus } from './card_status';
-import HexRenderer from './hex_renderer';
-import JsonRenderer from './json_renderer';
-import InputRenderer from './input_renderer';
+import { HexRenderer } from './hex_renderer';
+import { JsonRenderer } from './json_renderer';
+import { InputRenderer } from './input_renderer';
 import TableRenderer from './table_renderer';
 import { VegaRenderer } from './vega_renderer';
 
--- packages/app/src/components/card/hex_renderer.tsx
+++ packages/app/src/components/card/hex_renderer.tsx
@@ -2,50 +2,33 @@ import * as React from 'react';
 import * as core from '@dashql/core';
 import * as model from '../../model';
 import { AutoSizer } from 'react-virtualized';
-import { connect } from 'react-redux';
-import { IAppContext, withAppContext } from '../../app_context';
+import { useSelector } from 'react-redux';
 import { CardFrame } from './card_frame';
 import { HexViewer } from './hex_viewer';
 import { BlobLoader } from '../blob_loader';
 
 interface Props {
-    appContext: IAppContext;
-    planState: core.model.PlanState;
     card: core.model.CardSpecification;
     editable?: boolean;
 }
 
-export class DumpRenderer extends React.Component<Props> {
-    constructor(props: Props) {
-        super(props);
-    }
-
-    /// Render the table
-    public render(): React.ReactElement {
-        const target = this.props.card.dataSource!.targetQualified;
-        const obj = core.model.resolveBlobByName(this.props.planState, target)!;
-        return (
-            <CardFrame title={this.props.card.title || target} controls={this.props.editable}>
-                <AutoSizer>
-                    {({ width, height }) => (
-                        <BlobLoader
-                            blob={obj.blob}
-                            loadingComponent={() => <div>loading...</div>}
-                            errorComponent={e => <div>Error: {e}</div>}
-                        >
-                            {buffer => <HexViewer buffer={buffer} width={width} height={height} />}
-                        </BlobLoader>
-                    )}
-                </AutoSizer>
-            </CardFrame>
-        );
-    }
-}
-
-const mapStateToProps = (state: model.AppState) => ({
-    planState: state.core.planState,
-});
-
-const mapDispatchToProps = (_dispatch: model.Dispatch) => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(withAppContext(DumpRenderer));
+export const HexRenderer: React.FC<Props> = (props: Props) => {
+    const planState = useSelector((state: model.AppState) => state.core.planState);
+    const target = props.card.dataSource!.targetQualified;
+    const obj = core.model.resolveBlobByName(planState, target)!;
+    return (
+        <CardFrame title={props.card.title || target} controls={props.editable}>
+            <AutoSizer>
+                {({ width, height }) => (
+                    <BlobLoader
+                        blob={obj.blob}
+                        loadingComponent={() => <div>loading...</div>}
+                        errorComponent={e => <div>Error: {e}</div>}
+                    >
+                        {buffer => <HexViewer buffer={buffer} width={width} height={height} />}
+                    </BlobLoader>
+                )}
+            </AutoSizer>
+        </CardFrame>
+    );
+};
