dae3f42fb02b94c0e49fc488999ed1367f7705a4
Andre Kohn <kohn.a@outlook.com>
2021-08-12 22:39:36 +0200

    Work on migrated tests

--- packages/app/karma.base.cjs
+++ packages/app/karma.base.cjs
@@ -18,10 +18,10 @@ module.exports = function (config) {
         ],
         frameworks: ['jasmine'],
         files: [
-            { pattern: 'packages/core/dist/tests-browser.js' },
-            { pattern: 'packages/core/dist/tests-browser.js.map', included: false },
-            { pattern: 'packages/core/src/analyzer/analyzer_wasm.wasm', included: false, watched: false, served: true },
-            { pattern: 'packages/core/src/jmespath/jmespath_wasm.wasm', included: false, watched: false, served: true },
+            { pattern: 'packages/app/build/libs/tests-browser.js' },
+            { pattern: 'packages/app/build/libs/tests-browser.js.map', included: false },
+            { pattern: 'packages/app/src/analyzer/analyzer_wasm.wasm', included: false, watched: false, served: true },
+            { pattern: 'packages/app/src/jmespath/jmespath_wasm.wasm', included: false, watched: false, served: true },
             { pattern: 'node_modules/@dashql/duckdb/dist/*.js', included: false, watched: false, served: true },
             { pattern: 'node_modules/@dashql/duckdb/dist/*.wasm', included: false, watched: false, served: true },
         ],
@@ -29,8 +29,8 @@ module.exports = function (config) {
             'tests-**/*.js': ['sourcemap', 'coverage'],
         },
         proxies: {
-            '/static/jmespath_wasm.wasm': '/base/packages/core/src/jmespath/jmespath_wasm.wasm',
-            '/static/analyzer_wasm.wasm': '/base/packages/core/src/analyzer/analyzer_wasm.wasm',
+            '/static/jmespath_wasm.wasm': '/base/packages/app/src/jmespath/jmespath_wasm.wasm',
+            '/static/analyzer_wasm.wasm': '/base/packages/app/src/analyzer/analyzer_wasm.wasm',
             '/static/duckdb/': '/base/node_modules/@dashql/duckdb/dist/',
             '/data/': '/base/data/',
         },
@@ -63,7 +63,7 @@ module.exports = function (config) {
         },
         coverageReporter: {
             type: 'json',
-            dir: './packages/core/coverage/',
+            dir: './packages/app/coverage/',
             subdir: function (browser) {
                 return browser.toLowerCase().split(/[ /-]/)[0];
             },
