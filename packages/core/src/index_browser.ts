205776b826c9907bb8f3b1041d29f33b4e2f71f2
Andre Kohn <kohn.a@outlook.com>
2021-08-13 09:51:41 +0200

    Fix script commands

--- packages/core/src/index_browser.ts
+++ packages/core/src/index_browser.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2021 The DashQL Authors
 
-export * from './';
+export * from '.';
 export * from './analyzer/bindings_browser';
 export * from './jmespath/bindings_browser';
--- packages/core/src/index_node.ts
+++ packages/core/src/index_node.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2021 The DashQL Authors
 
-export * from './';
+export * from '.';
 export * from './analyzer/bindings_node';
 export * from './jmespath/bindings_node';
