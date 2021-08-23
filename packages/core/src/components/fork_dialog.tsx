f10b20d1ff84fa6dbc8372d256c96fc8a5288a4f
Andre Kohn <kohn.a@outlook.com>
2021-08-23 13:43:57 +0200

    Fork dialog

--- packages/core/src/components/fork_dialog.tsx
+++ packages/core/src/components/fork_dialog.tsx
@@ -1,32 +1,100 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Form, Button, Dropdown, DropdownButton, ButtonGroup } from 'react-bootstrap';
 
 import styles from './fork_dialog.module.css';
 
 import icon_github from '../../static/svg/icons/github.svg';
 import icon_copy from '../../static/svg/icons/file_multiple.svg';
 
-interface Props {
+enum ForkTargetType {
+    GITHUB_GIST,
+    LOCAL_FILE,
+}
+
+interface FormProps {
+    type: ForkTargetType;
+    className?: string;
+}
+
+const ForkDetailForm: React.FC<FormProps> = (props: FormProps) => {
+    switch (props.type) {
+        case ForkTargetType.GITHUB_GIST:
+            return (
+                <Form className={props.className}>
+                    <Form.Group className="mb-2" controlId="formFileName">
+                        <Form.Control type="text" placeholder="Enter file name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Enable DashQL Analytics" defaultChecked={true} disabled />
+                        <Form.Check
+                            type="checkbox"
+                            label="Public Dashboard Statistics"
+                            defaultChecked={true}
+                            disabled
+                        />
+                    </Form.Group>
+                    <ButtonGroup>
+                        <Button>Create secret gist</Button>
+                        <DropdownButton as={ButtonGroup} title="" id="bg-nested-dropdown">
+                            <Dropdown.Item eventKey="1">Create secret gist</Dropdown.Item>
+                            <Dropdown.Item eventKey="2">Create public gist</Dropdown.Item>
+                        </DropdownButton>
+                    </ButtonGroup>
+                </Form>
+            );
+        case ForkTargetType.LOCAL_FILE:
+            return (
+                <Form className={props.className}>
+                    <Form.Group className="mb-2" controlId="formFileName">
+                        <Form.Control type="text" placeholder="Enter file name" />
+                    </Form.Group>
+                    <Button>Create file</Button>
+                </Form>
+            );
+    }
+};
+
+interface DialogProps {
     onClose: () => void;
 }
 
-export const ForkDialog: React.FC<Props> = (_props: Props) => {
+export const ForkDialog: React.FC<DialogProps> = (_props: DialogProps) => {
+    const [forkTarget, setForkTarget] = React.useState<ForkTargetType | null>(null);
+
     return (
         <AnimatePresence>
             <motion.div className={styles.container} initial={{ translateY: 20 }} animate={{ translateY: 0 }}>
                 <div className={styles.header}>Fork Script</div>
                 <div className={styles.body}>
-                    <div className={styles.fork_target_list}>
-                        <div className={styles.fork_target}>
-                            <svg width="28px" height="28px">
-                                <use xlinkHref={`${icon_github}#sym`} />
-                            </svg>
-                        </div>
-                        <div className={styles.fork_target}>
-                            <svg width="20px" height="20px">
-                                <use xlinkHref={`${icon_copy}#sym`} />
-                            </svg>
-                        </div>
+                    <div className={styles.fork_target_config}>
+                        {(forkTarget == null || forkTarget == ForkTargetType.GITHUB_GIST) && (
+                            <div
+                                key="gist"
+                                className={styles.fork_target_type}
+                                onClick={() => setForkTarget(s => (s != null ? null : ForkTargetType.GITHUB_GIST))}
+                            >
+                                <svg width="28px" height="28px">
+                                    <use xlinkHref={`${icon_github}#sym`} />
+                                </svg>
+                            </div>
+                        )}
+                        {(forkTarget == null || forkTarget == ForkTargetType.LOCAL_FILE) && (
+                            <div
+                                key="local"
+                                className={styles.fork_target_type}
+                                onClick={() => setForkTarget(s => (s != null ? null : ForkTargetType.LOCAL_FILE))}
+                            >
+                                <svg width="20px" height="20px">
+                                    <use xlinkHref={`${icon_copy}#sym`} />
+                                </svg>
+                            </div>
+                        )}
+                        {forkTarget != null && (
+                            <div className={styles.fork_target_detail}>
+                                <ForkDetailForm type={forkTarget} />
+                            </div>
+                        )}
                     </div>
                 </div>
             </motion.div>
