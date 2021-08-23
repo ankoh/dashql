f8c525896d234448fe2fbd62cf4222f390d6ca23
Andre Kohn <kohn.a@outlook.com>
2021-08-23 17:24:33 +0200

    Work on share dialog

--- packages/core/src/components/share_dialog.tsx
+++ packages/core/src/components/share_dialog.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Form, InputGroup, Button } from 'react-bootstrap';
 
+import icon_clipboard from '../../static/svg/icons/clipboard_text.svg';
 import styles from './share_dialog.module.css';
 
 interface Props {
@@ -10,12 +12,47 @@ interface Props {
 export const ShareDialog: React.FC<Props> = (_props: Props) => {
     return (
         <AnimatePresence>
-            <motion.div className={styles.container} initial={{ translateY: 20 }} animate={{ translateY: 0 }}>
-                <div className={styles.header}>Share Script</div>
+            <motion.div
+                className={styles.container}
+                initial={{ translateY: 20 }}
+                animate={{ translateY: 0 }}
+                onClick={e => e.stopPropagation()}
+            >
+                <div className={styles.header}>Share Dashboard</div>
                 <div className={styles.body}>
-                    <div className={styles.fork_target_list}>
-                        <div className={styles.fork_target}></div>
-                        <div className={styles.fork_target}></div>
+                    <div className={styles.share_url_table}>
+                        <div className={styles.share_url_type}>Viewer</div>
+                        <div className={styles.share_url_text}>
+                            <InputGroup>
+                                <Form.Control
+                                    size="sm"
+                                    type="text"
+                                    placeholder="https://app.dashql.com/viewer?foo"
+                                    readOnly
+                                />
+                                <Button>
+                                    <svg width="16px" height="16px">
+                                        <use xlinkHref={`${icon_clipboard}#sym`} />
+                                    </svg>
+                                </Button>
+                            </InputGroup>
+                        </div>
+                        <div className={styles.share_url_type}>Explorer</div>
+                        <div className={styles.share_url_text}>
+                            <InputGroup>
+                                <Form.Control
+                                    size="sm"
+                                    type="text"
+                                    placeholder="https://app.dashql.com/explorer?foo"
+                                    readOnly
+                                />
+                                <Button>
+                                    <svg width="16px" height="16px">
+                                        <use xlinkHref={`${icon_clipboard}#sym`} />
+                                    </svg>
+                                </Button>
+                            </InputGroup>
+                        </div>
                     </div>
                 </div>
             </motion.div>
