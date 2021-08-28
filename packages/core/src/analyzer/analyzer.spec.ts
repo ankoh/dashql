d46d8a17a3247c2cc4f1bbe3e79fbfbf821abc4a
Andre Kohn <kohn.a@outlook.com>
2021-08-28 15:40:54 +0200

    Merge tests

--- packages/core/src/analyzer/analyzer.spec.ts
+++ packages/core/src/analyzer/analyzer.spec.ts
@@ -1,9 +1,9 @@
-import * as analyzer from '.';
+import * as analyzer from './analyzer_node';
 import * as edit from '../edit';
 import * as test from '../test';
 
 describe('Program editor', () => {
-    let az: analyzer.AnalyzerBindings | null = null;
+    let az: analyzer.Analyzer | null = null;
 
     beforeAll(async () => {
         az = await test.initAnalyzer();
@@ -83,5 +83,70 @@ describe('Program editor', () => {
     )
 )`;
         expect(n!.program.text).toEqual(expected);
+    });
+});
+
+describe('Parser', () => {
+    let az: analyzer.Analyzer | null = null;
+
+    beforeAll(async () => {
+        az = await test.initAnalyzer();
+    });
+    afterEach(async () => {
+        az.reset();
+    });
+
+    describe('errors', () => {
+        it('syntax error', async () => {
+            const r = az.parseProgram('?');
+            const p = r.buffer;
+            expect(p.statementsLength()).toEqual(0);
+            expect(p.errorsLength()).toEqual(1);
+        });
+    });
+
+    describe('single statements', () => {
+        it('select 1', async () => {
+            const r = az.parseProgram(`
+            select 1;
+        `);
+            const p = r.buffer;
+            expect(p.errorsLength()).toEqual(0);
+            expect(p.statementsLength()).toEqual(1);
+        });
+
+        it('fetch http from url', async () => {
+            const r = az.parseProgram(`
+                FETCH weather_csv FROM http (
+                    url = 'https://localhost/test'
+                );
+        `);
+            const p = r.buffer;
+            expect(p.errorsLength()).toEqual(0);
+            expect(p.statementsLength()).toEqual(1);
+        });
+    });
+});
+
+describe('Task Graph', () => {
+    let az: analyzer.Analyzer | null = null;
+
+    beforeAll(async () => {
+        az = await test.initAnalyzer();
+    });
+    afterEach(async () => {
+        az.reset();
+    });
+
+    describe('program tasks', () => {
+        it('select 1', async () => {
+            const program = az.parseProgram('create table foo as select 1');
+            az.instantiateProgram();
+            expect(program.buffer.statementsLength()).toBe(1);
+            const plan = az.planProgram();
+            const graph = plan!.buffer.taskGraph()!;
+            expect(graph.setupTasksLength()).toBe(0);
+            expect(graph.programTasksLength()).toBe(1);
+        });
     });
 });
