1e1bc67625835eb509c74ed164e440f0bfb78cdf
Andre Kohn <kohn.a@outlook.com>
2021-08-28 14:23:12 +0200

    Experiment with effect testing

--- packages/core/src/components/current_time.spec.tsx
+++ packages/core/src/components/current_time.spec.tsx
@@ -6,33 +6,66 @@ import { CurrentTime } from './current_time';
 interface Props {
     time: Date;
     update: () => void;
+    trigger: boolean;
 }
 
 const Bounce: React.FC<Props> = (props: Props) => {
     React.useEffect(() => {
-        props.update();
-    });
+        if (props.trigger) props.update();
+    }, [props.trigger]);
     return <div>{props.time.toUTCString()}</div>;
 };
 
 describe('CurrentTime', () => {
     beforeAll(() => {
-        jest.useFakeTimers('modern');
-        jest.setSystemTime(Date.UTC(2020, 3, 1));
+        jest.useFakeTimers();
     });
 
     afterAll(() => {
         jest.useRealTimers();
     });
 
-    it('renders correctly', () => {
-        const tree = renderer
-            .create(
-                <CurrentTime refreshRate={1000}>
-                    {(time, update) => <Bounce time={time} update={update} />}
+    it('rerenders with refresh-rate', () => {
+        jest.setSystemTime(Date.UTC(2020, 3, 1, 0, 0, 0));
+
+        let root: renderer.ReactTestRenderer;
+        renderer.act(() => {
+            root = renderer.create(
+                <CurrentTime refreshRate={1000}>{(time, _update) => <div>{time.toUTCString()}</div>}</CurrentTime>,
+            );
+        });
+        expect(root.toJSON()).toMatchSnapshot();
+
+        renderer.act(() => jest.advanceTimersByTime(500));
+        expect(root.toJSON()).toMatchSnapshot();
+
+        renderer.act(() => jest.advanceTimersByTime(500));
+        expect(root.toJSON()).toMatchSnapshot();
+    });
+
+    it('updates when requested', () => {
+        jest.setSystemTime(Date.UTC(2020, 3, 1, 0, 0, 0));
+
+        let root: renderer.ReactTestRenderer;
+        renderer.act(() => {
+            root = renderer.create(
+                <CurrentTime refreshRate={10000}>
+                    {(time, update) => <Bounce time={time} update={update} trigger={false} />}
                 </CurrentTime>,
-            )
-            .toJSON();
-        expect(tree).toMatchSnapshot();
+            );
+        });
+        expect(root.toJSON()).toMatchSnapshot();
+
+        renderer.act(() => jest.advanceTimersByTime(1000));
+        expect(root.toJSON()).toMatchSnapshot();
+
+        renderer.act(() => {
+            root.update(
+                <CurrentTime refreshRate={10000}>
+                    {(time, update) => <Bounce time={time} update={update} trigger={true} />}
+                </CurrentTime>,
+            );
+        });
+        expect(root.toJSON()).toMatchSnapshot();
     });
 });
