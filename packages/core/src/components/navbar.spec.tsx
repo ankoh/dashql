bfe1dd9b9ea1a02f41b9c0ae2a3288784056d561
Andre Kohn <kohn.a@outlook.com>
2021-08-28 16:45:03 +0200

    Remove obsolete comments

--- packages/core/src/components/navbar.spec.tsx
+++ packages/core/src/components/navbar.spec.tsx
@@ -6,28 +6,7 @@ import { NavBar } from './navbar';
 import { MemoryRouter } from 'react-router-dom/index';
 
 describe('NavBar', () => {
-    //let windowSpy: any;
-    //beforeEach(() => {
-    //    windowSpy = jest.spyOn(window, 'window', 'get');
-    //});
-    //afterEach(() => {
-    //    windowSpy.mockRestore();
-    //});
-
     it('renders correctly', () => {
-        //const map = {};
-        //const windowAddEventListener = jest.fn();
-        //const windowRemoveEventListener = jest.fn();
-        //windowAddEventListener.mockImplementationOnce((key: any, cb: () => void) => {
-        //    map[key] = cb;
-        //});
-        //windowSpy.mockImplementation(() => ({
-        //    addEventListener: windowAddEventListener,
-        //    removeEventListener: windowRemoveEventListener,
-        //}));
-        //expect(windowSpy).toHaveBeenCalled();
-        //expect(windowAddEventListener).toHaveBeenCalledTimes(1);
-        //expect(windowRemoveEventListener).toHaveBeenCalledTimes(1);
         const tree = renderer
             .create(
                 <MemoryRouter>
