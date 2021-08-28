9dcb9df2279e6961c1c902aaf590e81c1dca0e9b
Andre Kohn <kohn.a@outlook.com>
2021-08-28 20:07:11 +0200

    Add snapshot of example page

---
+++ packages/core/src/pages/examples.spec.tsx
@@ -0,0 +1,52 @@
+import { jest } from '@jest/globals';
+import * as analyzer from '../analyzer/analyzer_node';
+import * as test_env from '../test';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom/index';
+import { ProgramContextProvider } from '../model/';
+import { AnalyzerProvider } from '../analyzer/';
+import { Examples } from './examples';
+
+describe('NavBar', () => {
+    let az: analyzer.Analyzer | null = null;
+    let windowMock: any;
+
+    beforeAll(async () => {
+        az = await test_env.initAnalyzer();
+        windowMock = jest.spyOn(window, 'window', 'get');
+        windowMock.mockImplementation(() => ({
+            matchMedia: jest.fn().mockImplementation(query => ({
+                matches: false,
+                media: query,
+                onchange: null,
+                addEventListener: jest.fn(),
+                removeEventListener: jest.fn(),
+                dispatchEvent: jest.fn(),
+            })),
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
+            setTimeout: setTimeout,
+        }));
+    });
+    afterEach(async () => {
+        await az.reset();
+        windowMock.mockRestore();
+    });
+
+    it('renders correctly with an empty state', () => {
+        const tree = renderer
+            .create(
+                <MemoryRouter>
+                    <ProgramContextProvider>
+                        <AnalyzerProvider analyzer={az!}>
+                            <Examples />
+                        </AnalyzerProvider>
+                    </ProgramContextProvider>
+                </MemoryRouter>,
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
