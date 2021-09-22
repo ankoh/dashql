37c78ba8ffd3573b1331c1b1ee34e5b4f99c7596
Andre Kohn <kohn.a@outlook.com>
2021-09-22 11:40:07 +0200

    Extract common program header

---
+++ packages/core/src/components/program_header.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Script, getScriptName, getScriptNamespace, getScriptBeans } from '../model';
+
+import logo from '../../static/svg/logo/logo.svg';
+
+import styles from './program_header.module.css';
+
+interface Props {
+    script: Script;
+}
+
+export const ProgramHeader: React.FC<Props> = (props: Props) => (
+    <div className={styles.container}>
+        <div className={styles.avatar}>
+            <div className={styles.avatar_icon}>
+                <svg width="24px" height="24px">
+                    <use xlinkHref={`${logo}#sym`} />
+                </svg>
+            </div>
+        </div>
+        <div className={styles.title}>
+            <span className={styles.title_namespace}>{getScriptNamespace(props.script)}</span>/
+            <span className={styles.title_name}>{getScriptName(props.script)}</span>
+        </div>
+        <div className={styles.description}>{props.script.description}</div>
+        <div className={styles.beans}>
+            {getScriptBeans(props.script).map(b => (
+                <div key={b} className={styles.bean}>
+                    {b}
+                </div>
+            ))}
+        </div>
+    </div>
+);
