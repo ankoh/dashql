3f9275bbce1b23621ef37333c54f213a42734459
Andre Kohn <kohn.a@outlook.com>
2021-09-23 11:32:52 +0200

    Work on theme

--- packages/core/src/components/system_bar.tsx
+++ packages/core/src/components/system_bar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { RefreshingLogViewer } from './log_viewer';
 import { DatabaseViewer } from './database_viewer';
-import classNames from 'classnames';
 
 import styles from './system_bar.module.css';
 
@@ -14,19 +13,13 @@ interface SystemPanelProps {
     children: JSX.Element;
     systemID: number;
     expandedPanel: number | null;
-    light?: boolean;
     onClick: (tab: number) => void;
 }
 
 const SystemPanel: React.FC<SystemPanelProps> = (props: SystemPanelProps) => {
     const expanded = props.systemID == props.expandedPanel;
     return (
-        <div
-            className={classNames(styles.system_button, {
-                [styles.system_button_light]: props.light || false,
-                [styles.active]: expanded,
-            })}
-        >
+        <div className={styles.system_button}>
             <div className={styles.system_toggle} onClick={() => props.onClick(props.systemID)}>
                 <svg className={styles.system_icon} width="20px" height="20px">
                     <use xlinkHref={`${props.icon}#sym`} />
@@ -39,7 +32,6 @@ const SystemPanel: React.FC<SystemPanelProps> = (props: SystemPanelProps) => {
 
 interface Props {
     className?: string;
-    light?: boolean;
 }
 
 export const SystemBar: React.FC<Props> = (props: Props) => {
@@ -48,31 +40,13 @@ export const SystemBar: React.FC<Props> = (props: Props) => {
     const toggleTab = (tab: number): void => setExpanded(expanded == tab ? null : tab);
     return (
         <div className={props.className}>
-            <SystemPanel
-                systemID={0}
-                expandedPanel={expanded}
-                onClick={toggleTab.bind(this)}
-                icon={icon_database}
-                light={props.light}
-            >
+            <SystemPanel systemID={0} expandedPanel={expanded} onClick={toggleTab.bind(this)} icon={icon_database}>
                 <DatabaseViewer onClose={() => toggleTab(0)} />
             </SystemPanel>
-            <SystemPanel
-                systemID={1}
-                expandedPanel={expanded}
-                onClick={toggleTab.bind(this)}
-                icon={icon_log}
-                light={props.light}
-            >
+            <SystemPanel systemID={1} expandedPanel={expanded} onClick={toggleTab.bind(this)} icon={icon_log}>
                 <RefreshingLogViewer onClose={() => toggleTab(1)} />
             </SystemPanel>
-            <SystemPanel
-                systemID={2}
-                expandedPanel={expanded}
-                onClick={toggleTab.bind(this)}
-                icon={icon_info}
-                light={props.light}
-            >
+            <SystemPanel systemID={2} expandedPanel={expanded} onClick={toggleTab.bind(this)} icon={icon_info}>
                 <div />
             </SystemPanel>
         </div>
