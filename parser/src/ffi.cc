60b35021cdc8b6c73aa66fadd8396b84cff609c8
Andre Kohn <kohn.a@outlook.com>
2022-04-01 14:08:40 +0200

    only add executable for wasm

--- parser/src/ffi.cc
+++ parser/src/ffi.cc
@@ -6,7 +6,7 @@
 using namespace dashql::parser;
 namespace sx = dashql::proto::syntax;
 
-extern "C" void parse(FFIResponse* response, const char* text, int length) {
+extern "C" void dashql_parse(FFIResponse* response, const char* text, int length) {
     // Parse the program
     auto program = ParserDriver::Parse(std::string_view{text, static_cast<size_t>(length)});
 
@@ -20,4 +20,6 @@ extern "C" void parse(FFIResponse* response, const char* text, int length) {
     FFIResponseBuffer::Get().Store(*response, std::move(program_buffer));
 }
 
+#ifdef WASM
 extern "C" int main() { return 0; }
+#endif
