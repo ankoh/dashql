3b75bf46c1b22465d409efc6cbfeb8c4e3d3a851
Andre Kohn <kohn.a@outlook.com>
2022-04-06 00:11:33 +0200

    ast printing

--- dvms/src/grammar/syntax/ast_printing.rs
+++ dvms/src/grammar/syntax/ast_printing.rs
@@ -1,11 +1,13 @@
 use super::node::*;
 use super::sql_nodes::*;
+use crate::grammar::syntax::enums::get_enum_text;
 use crate::proto::syntax as sx;
 use quick_xml::events::attributes::Attribute;
 use quick_xml::events::BytesEnd;
 use quick_xml::events::BytesStart;
 use quick_xml::events::Event;
 use quick_xml::Writer;
+use std::error::Error;
 use std::io::Cursor;
 use sx::AttributeKey as Key;
 
@@ -43,75 +45,87 @@ fn encode_location<'writer, 'text>(
         writer: &mut Writer<W>,
         text: &'text str,
         ast: sx::Program<'ast>,
-    ) where
+    ) -> Result<(), Box<dyn Error + Send + Sync>>
+    where
         W: std::io::Write,
     {
         let nodes = ast.nodes().unwrap_or_default();
-        let mut writer = Writer::new(Cursor::new(Vec::new()));
 
-        let mut stmts = BytesStart::borrowed_name(b"statements");
+        // Start statements
+        writer.write_event(Event::Start(BytesStart::borrowed_name(b"statements")))?;
         for s in ast.statements().unwrap_or_default().iter() {
+            // Start statement
             let mut stmt = BytesStart::borrowed_name(b"statement");
             stmt.push_attribute((
                 "type",
                 s.statement_type().variant_name().unwrap_or_default(),
             ));
-            writer.write_event(Event::Start(stmt));
+            writer.write_event(Event::Start(stmt))?;
 
             // Do a post-order DFS traversal
             let mut pending = Vec::new();
             pending.push((false, s.root_node()));
             while !pending.is_empty() {
                 let (visited, node_id) = pending.last().copied().unwrap();
-                let mut node = nodes[node_id as usize];
+                let n = nodes[node_id as usize];
 
                 if !visited {
                     pending.last_mut().unwrap().0 = true;
-                    let mut el = BytesStart::borrowed_name(b"node");
-                    match node.node_type() {
+                    let mut node = BytesStart::borrowed_name(b"node");
+                    match n.node_type() {
                         sx::NodeType::NONE => {}
                         sx::NodeType::BOOL => {
-                            el.push_attribute((
+                            node.push_attribute((
                                 "value",
-                                format!("{}", node.children_begin_or_value() != 0).as_str(),
+                                format!("{}", n.children_begin_or_value() != 0).as_str(),
                             ));
                         }
                         sx::NodeType::UI32_BITMAP | sx::NodeType::UI32 => {
-                            el.push_attribute((
+                            node.push_attribute((
                                 "value",
-                                format!("{}", node.children_begin_or_value()).as_str(),
+                                format!("{}", n.children_begin_or_value()).as_str(),
                             ));
                         }
                         sx::NodeType::STRING_REF => {
-                            encode_location(&mut el, *node.location(), text);
+                            encode_location(&mut node, *n.location(), text);
                         }
                         sx::NodeType::ARRAY => {
-                            let begin = node.children_begin_or_value();
-                            for i in begin..(begin + node.children_count()) {
+                            let begin = n.children_begin_or_value();
+                            for i in begin..(begin + n.children_count()) {
                                 pending.push((false, i));
                             }
                             break;
                         }
                         _ => {
-                            let node_type_id = node.node_type();
+                            let node_type_id = n.node_type();
                             if node_type_id.0 > sx::NodeType::OBJECT_KEYS_.0 {
+                                node.push_attribute((
+                                    "type",
+                                    n.node_type().variant_name().unwrap_or_default(),
+                                ));
+                                encode_location(&mut node, *n.location(), text);
+                                let begin = n.children_begin_or_value();
+                                for i in begin..(begin + n.children_count()) {
+                                    pending.push((false, i));
+                                }
                             } else if node_type_id.0 > sx::NodeType::ENUM_KEYS_.0 {
-                                el.push_attribute((
-                                    "value",
-                                    format!("{}", node.children_begin_or_value()).as_str(),
-                                ))
+                                node.push_attribute(("value", get_enum_text(&n)))
                             } else {
-                                el.push_attribute((
+                                node.push_attribute((
                                     "value",
-                                    format!("{}", node.children_begin_or_value()).as_str(),
+                                    format!("{}", n.children_begin_or_value()).as_str(),
                                 ))
                             }
                         }
                     }
+                    writer.write_event(Event::Start(node))?;
+                    continue;
                 }
+                writer.write_event(Event::End(BytesEnd::borrowed(b"node")))?;
             }
-
-            writer.write_event(Event::End(BytesEnd::borrowed(b"statement")));
+            writer.write_event(Event::End(BytesEnd::borrowed(b"statement")))?;
         }
+        writer.write_event(Event::End(BytesEnd::borrowed(b"statements")))?;
+        Ok(())
     }
 }
