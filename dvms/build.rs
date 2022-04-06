9e5ffca27658e3423469c669b0902e75089503b7
Andre Kohn <kohn.a@outlook.com>
2022-04-06 20:26:22 +0200

    link c++

--- dvms/build.rs
+++ dvms/build.rs
@@ -1,11 +1,21 @@
 extern crate cmake;
 
+use std::env;
+
 fn main() {
     let dst = cmake::Config::new("../parser")
         .build_target("dashql_parser")
         .always_configure(true)
         .build();
     println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-lib=static=stdc++");
+    let target_os = env::var("CARGO_CFG_TARGET_OS");
+    match target_os.as_ref().map(|x| &**x) {
+        Ok("macos") => {
+            println!("cargo:rustc-flags=-l dylib=c++");
+        }
+        _ => {
+            println!("cargo:rustc-link-lib=static=stdc++");
+        }
+    }
     println!("cargo:rustc-link-lib=static=dashql_parser");
 }
