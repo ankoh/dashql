e2af20b29b1d8d2d87028b21a7d35829642d1201
Andre Kohn <kohn.a@outlook.com>
2022-04-19 12:29:59 +0200

    separate ast printing tests

---
+++ dvms/src/grammar/syntax/ast_printing_tests.rs
@@ -0,0 +1,33 @@
+use super::ast_printing;
+use quick_xml::Writer;
+use std::error::Error;
+use std::io::Cursor;
+
+fn test_grammar(stmt: &str, expected: &str) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let program = crate::grammar::parse(stmt)?;
+    let mut writer = Writer::new_with_indent(Cursor::new(Vec::new()), ' ' as u8, 4);
+    ast_printing::print_ast(&mut writer, stmt, program.read())?;
+    let xml_buffer = writer.into_inner().into_inner();
+    let xml_str = std::str::from_utf8(&xml_buffer)?;
+    assert_eq!(xml_str, expected.trim());
+    Ok(())
+}
+
+#[test]
+fn test_select_1() -> Result<(), Box<dyn Error + Send + Sync>> {
+    test_grammar(
+        "select 1;",
+        r#"
+<statements>
+    <statement type="SELECT">
+        <node type="OBJECT_SQL_SELECT" loc="0..8" text="select 1">
+            <node key="SQL_SELECT_TARGETS" type="ARRAY">
+                <node type="OBJECT_SQL_RESULT_TARGET" loc="7..8" text="1">
+                    <node key="SQL_RESULT_TARGET_VALUE" type="STRING_REF" loc="7..8" text="1"/>
+                </node>
+            </node>
+        </node>
+    </statement>
+</statements>"#,
+    )
+}
--- dvms/src/grammar/syntax/mod.rs
+++ dvms/src/grammar/syntax/mod.rs
@@ -1,5 +1,7 @@
 mod ast_node;
 mod ast_printing;
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod ast_printing_tests;
 mod ast_translation;
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod ast_translation_tests;
