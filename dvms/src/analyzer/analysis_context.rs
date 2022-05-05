eb700dc4ad6acfe6c6a3f7b185cb5539d5db9e0e
Andre Kohn <kohn.a@outlook.com>
2022-05-05 13:32:40 +0200

    work on analyzer

--- dvms/src/analyzer/analysis_context.rs
+++ dvms/src/analyzer/analysis_context.rs
@@ -1,5 +1,6 @@
-use super::super::grammar::Program;
+use super::super::grammar::*;
 use dashql_proto::syntax as sx;
+use std::collections::HashMap;
 use std::rc::Rc;
 
 pub struct ProgramAnalysisContext<'arena, 'text, 'ast> {
@@ -8,6 +9,8 @@ pub struct ProgramAnalysisContext<'arena, 'text, 'ast> {
     pub program_flat: sx::Program<'ast>,
     pub program_translated: Rc<Program<'text, 'arena>>,
     pub subtree_sizes: Vec<usize>,
+    pub statement_names: Vec<NamePath<'text, 'arena>>,
+    pub statement_by_name: HashMap<NamePath<'text, 'arena>, usize>,
     pub statement_deps: Vec<sx::DependencyT>,
 }
 
@@ -24,6 +27,8 @@ impl<'arena, 'text, 'ast> ProgramAnalysisContext<'arena, 'text, 'ast> {
             program_flat,
             program_translated,
             subtree_sizes: Vec::new(),
+            statement_names: Vec::new(),
+            statement_by_name: HashMap::default(),
             statement_deps: Vec::new(),
         }
     }
