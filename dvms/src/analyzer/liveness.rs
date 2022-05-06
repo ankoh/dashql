9d1ee5a137182c73e9dfb2c0081d8a82fc1d7841
Andre Kohn <kohn.a@outlook.com>
2022-05-06 10:56:19 +0200

    propagate liveness information

--- dvms/src/analyzer/liveness.rs
+++ dvms/src/analyzer/liveness.rs
@@ -1,8 +1,33 @@
+use crate::grammar::Statement;
+
 use super::analysis_context::ProgramAnalysisContext;
+use std::collections::HashSet;
 
 pub fn identify_dead_statements<'a>(ctx: &mut ProgramAnalysisContext<'a>) {
     ctx.statement_liveness
         .resize(ctx.program_translated.statements.len(), false);
 
-    // Collect dependencies
+    // Prepare DFSs starting from viz and input statements
+    let mut pending = Vec::new();
+    let mut visited = HashSet::new();
+    for (stmt_id, stmt) in ctx.program_translated.statements.iter().enumerate() {
+        match stmt {
+            Statement::Viz(_) | Statement::Input(_) => pending.push(stmt_id),
+            _ => {}
+        }
+    }
+
+    // Propagate dependencies
+    while pending.is_empty() {
+        let next = *pending.last().unwrap();
+        pending.pop();
+        ctx.statement_liveness[next] = true;
+        if visited.contains(&next) {
+            continue;
+        }
+        visited.insert(next);
+        for ((_, dep), _) in ctx.statement_depends_on.range((next, 0)..(next + 1, 0)) {
+            pending.push(*dep);
+        }
+    }
 }
