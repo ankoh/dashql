1f9c30538e2ee672e9f53d890c8f1670121ddae2
Andre Kohn <kohn.a@outlook.com>
2022-05-06 13:38:43 +0200

    start with dson helpers

--- dvms/src/grammar/syntax/dson.rs
+++ dvms/src/grammar/syntax/dson.rs
@@ -1,4 +1,5 @@
 use super::ast_nodes_sql::*;
+use dashql_proto::syntax as sx;
 
 #[derive(Debug, Clone, Default)]
 pub struct DsonField<'arena> {
@@ -16,5 +17,44 @@ pub enum DsonValue<'arena> {
 impl<'arena> Default for DsonValue<'arena> {
     fn default() -> Self {
         DsonValue::Expression(Expression::Null)
+    }
+}
+
+impl<'arena> std::ops::Index<sx::AttributeKey> for DsonValue<'arena> {
+    type Output = Option<DsonValue<'arena>>;
+
+    fn index(&self, index: sx::AttributeKey) -> &Self::Output {
+        match self {
+            DsonValue::Object(o) => (),
+            DsonValue::Array(a) => (),
+            DsonValue::Expression(e) => (),
+        }
+        todo!()
+    }
+}
+
+impl<'arena> std::ops::Index<usize> for DsonValue<'arena> {
+    type Output = Option<DsonValue<'arena>>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match self {
+            DsonValue::Object(o) => (),
+            DsonValue::Array(a) => (),
+            DsonValue::Expression(e) => (),
+        }
+        todo!()
+    }
+}
+
+impl<'arena> std::ops::Index<&str> for DsonValue<'arena> {
+    type Output = Option<DsonValue<'arena>>;
+
+    fn index(&self, index: &str) -> &Self::Output {
+        match self {
+            DsonValue::Object(o) => (),
+            DsonValue::Array(a) => (),
+            DsonValue::Expression(e) => (),
+        }
+        todo!()
     }
 }
