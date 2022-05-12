e5bc642d2b8f441537f534e4eced33ecc21a1eb3
Andre Kohn <kohn.a@outlook.com>
2022-05-13 00:29:54 +0200

    test value serialization

--- dvms/src/analyzer/sql_value.rs
+++ dvms/src/analyzer/sql_value.rs
@@ -8,16 +8,16 @@ pub struct LogicalDecimalType {
 
 #[derive(Debug, Clone, Serialize, PartialEq)]
 pub struct LogicalStructType {
-    fields: Vec<(String, LogicalType)>,
+    fields: Vec<(String, SQLType)>,
 }
 
 #[derive(Debug, Clone, Serialize, PartialEq)]
 pub struct LogicalListType {
-    value: Box<LogicalType>,
+    value: Box<SQLType>,
 }
 
 #[derive(Debug, Clone, Serialize, PartialEq)]
-pub enum LogicalType {
+pub enum SQLType {
     Invalid,
     Null,
     Unknown,
@@ -46,17 +46,35 @@ pub enum LogicalType {
 }
 
 #[derive(Debug, Clone, Serialize, PartialEq)]
-pub enum PhysicalData {
+pub enum SQLValueData {
     Null,
     I64(i64),
     F64(f64),
     String(String),
-    List(Vec<PhysicalData>),
-    Struct(Vec<PhysicalData>),
+    List(Vec<SQLValueData>),
+    Struct(Vec<SQLValueData>),
 }
 
 #[derive(Debug, Clone, Serialize, PartialEq)]
 pub struct SQLValue {
-    pub logical_type: LogicalType,
-    pub physical_data: PhysicalData,
+    #[serde(rename = "type")]
+    pub sql_type: SQLType,
+    pub data: SQLValueData,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::error::Error;
+
+    #[test]
+    pub fn test_42() -> Result<(), Box<dyn Error + Send + Sync>> {
+        let value = SQLValue {
+            sql_type: SQLType::Int64,
+            data: SQLValueData::I64(0),
+        };
+        let vs = serde_json::to_string(&value)?;
+        assert_eq!(vs, r#"{"type":"Int64","data":{"I64":0}}"#);
+        Ok(())
+    }
 }
