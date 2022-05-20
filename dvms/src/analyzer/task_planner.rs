a5668101ca6282e50d9393c87bc2f38fa6f7f67a
Andre Kohn <kohn.a@outlook.com>
2022-05-20 12:34:21 +0200

    work on task planner

--- dvms/src/analyzer/task_planner.rs
+++ dvms/src/analyzer/task_planner.rs
@@ -1,5 +1,6 @@
 use super::{program_analysis::ProgramAnalysis, program_diff::DiffOp};
 use serde::Serialize;
+use std::collections::HashSet;
 use std::error::Error;
 
 use crate::grammar::{
@@ -42,10 +43,10 @@ pub enum SetupTaskType {
     None,
     DropBlob,
     DropInput,
-    DropSet,
     DropTable,
     DropView,
     DropViz,
+    Unset,
 }
 
 impl Default for SetupTaskType {
@@ -75,7 +76,6 @@ pub enum ProgramTaskType {
     Load,
     ModifyTable,
     Set,
-    Transform,
     UpdateViz,
 }
 
@@ -103,29 +103,6 @@ pub struct TaskGraph {
     pub setup_tasks: Vec<SetupTask>,
     pub program_tasks: Vec<ProgramTask>,
     pub program_task_mapping: Vec<usize>,
-}
-
-#[derive(Debug, Clone)]
-pub struct TaskPlannerContext<'a> {
-    /// The next program
-    pub next_program: &'a ProgramAnalysis<'a>,
-    /// The previous program
-    pub prev_program: Option<(&'a ProgramAnalysis<'a>, &'a TaskGraph)>,
-    /// The diff between the programs
-    pub diff: Vec<DiffOp>,
-    /// The reverse task mapping.
-    /// Maps an task to the corresponding previous task if the diff was either KEEP, MOVE or UPDATE.
-    /// We use this to figure out, whether the set of dependencies changed.
-    pub reverse_task_mapping: Vec<Option<usize>>,
-    /// The applicability of tasks in the previous task graph.
-    /// An task is applicable iff:
-    ///  1) The diff is either KEEP or MOVE
-    ///  2) The task is not affected by a parmeter update
-    ///  3) The dependency set stayed the same
-    ///  4) All dependencies are applicable
-    pub task_applicability: Vec<bool>,
-    /// The new task graph
-    pub task_graph: Box<TaskGraph>,
 }
 
 fn translate_statements<'a>(
@@ -230,7 +207,103 @@ fn translate_statements<'a>(
     })
 }
 
-fn identify_applicable_tasks<'a>(ctx: &mut ProgramAnalysis<'a>) -> Result<(), Box<dyn Error + Send + Sync>> {
+#[derive(Debug, Clone)]
+pub struct TaskPlannerContext<'a> {
+    /// The next program
+    pub next_program: &'a ProgramAnalysis<'a>,
+    /// The previous program
+    pub prev_program: Option<(&'a ProgramAnalysis<'a>, &'a TaskGraph)>,
+    /// The diff between the programs
+    pub diff: Vec<DiffOp>,
+    /// The reverse task mapping.
+    /// Maps an task to the corresponding previous task if the diff was either KEEP, MOVE or UPDATE.
+    /// We use this to figure out, whether the set of dependencies changed.
+    pub reverse_task_mapping: Vec<Option<usize>>,
+    /// The applicability of tasks in the previous task graph.
+    /// An task is applicable iff:
+    ///  1) The diff is either KEEP or MOVE
+    ///  2) The task is not affected by a parmeter update
+    ///  3) The dependency set stayed the same
+    ///  4) All dependencies are applicable
+    pub program_task_applicability: Vec<bool>,
+    /// The new task graph
+    pub task_graph: Box<TaskGraph>,
+}
+
+fn identify_applicable_tasks<'a>(ctx: &mut TaskPlannerContext<'a>) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let (prev_program, prev_tasks) = match ctx.prev_program {
+        Some((prev_program, prev_tasks)) => (prev_program, prev_tasks),
+        None => return Ok(()),
+    };
+    ctx.program_task_applicability
+        .resize(prev_tasks.program_tasks.len(), false);
+
+    // Invalidate a task.
+    // If a task is invalidated, we might have to propagate the invalidation to the tasks before us.
+    // We are very pessimistic here and invalidate all our incoming dependencies to make sure everything is clean.
+    // (Except for the cases where it's trivial to see that nobody else is affected)
+    let invalidate = |task_id: usize| {
+        let mut visited = HashSet::new();
+        let mut pending = Vec::new();
+        pending.push(task_id);
+
+        while !pending.is_empty() {
+            let top = pending.pop().unwrap();
+
+            // Already visited?
+            if visited.contains(&top) {
+                continue;
+            }
+            visited.insert(top);
+
+            // Get invalidation info
+            assert!(task_id != usize::MAX);
+            let task = &prev_tasks.program_tasks[task_id];
+            // let (drop_task, update_task, propagates) = match task.task_type {
+            //     ProgramTaskType::None => (SetupTaskType::None, ProgramTaskType::None, false),
+            //     ProgramTaskType::CreateTable => (SetupTaskType::DropTable, ProgramTaskType::None, true),
+            //     ProgramTaskType::CreateView => (SetupTaskType::DropView, ProgramTaskType::None, true),
+            //     ProgramTaskType::CreateViz => (SetupTaskType::DropViz, ProgramTaskType::UpdateViz, false),
+            //     ProgramTaskType::Fetch => (SetupTaskType::DropBlob, ProgramTaskType::None, false),
+            //     ProgramTaskType::Input => (SetupTaskType::DropInput, ProgramTaskType::None, false),
+            //     ProgramTaskType::Load => (SetupTaskType::DropTable, ProgramTaskType::None, false),
+            //     ProgramTaskType::ModifyTable => (SetupTaskType::DropTable, ProgramTaskType::None, true),
+            //     ProgramTaskType::Set => (SetupTaskType::Unset, ProgramTaskType::None, false),
+            //     ProgramTaskType::UpdateViz => (SetupTaskType::DropViz, ProgramTaskType::UpdateViz, false),
+            // };
+
+            // Propagates invalidation?
+            let propagates = match task.task_type {
+                ProgramTaskType::None => false,
+                ProgramTaskType::CreateTable => true,
+                ProgramTaskType::CreateView => true,
+                ProgramTaskType::CreateViz => false,
+                ProgramTaskType::Fetch => false,
+                ProgramTaskType::Input => false,
+                ProgramTaskType::Load => false,
+                ProgramTaskType::ModifyTable => true,
+                ProgramTaskType::Set => false,
+                ProgramTaskType::UpdateViz => false,
+            };
+            if propagates {
+                for dep in task.depends_on.iter() {
+                    pending.push(*dep);
+                }
+            }
+
+            // Task is not applicable
+            ctx.program_task_applicability[top] = false;
+        }
+    };
+
+    // We traverse the previous task graph in topological order.
+    // That reduces the applicability check to the direct dependencies.
+    let mut deps: Vec<(usize, usize)> = Vec::new();
+    deps.reserve(prev_tasks.program_tasks.len());
+    for (i, t) in prev_tasks.program_tasks.iter().enumerate() {
+        deps.push((i, t.depends_on.len()));
+    }
+
     Ok(())
 }
 
