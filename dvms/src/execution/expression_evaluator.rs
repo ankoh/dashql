dd0531045a7b777e5042895f6e9779815bc7d96a
Andre Kohn <kohn.a@outlook.com>
2022-05-30 14:49:27 +0200

    work on expression evaluation

--- dvms/src/execution/expression_evaluator.rs
+++ dvms/src/execution/expression_evaluator.rs
@@ -4,11 +4,8 @@ use crate::error::RawError;
 use crate::grammar::Expression;
 use crate::grammar::FunctionName;
 use crate::grammar::NamePath;
-use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::error::Error;
-use std::hash::Hasher;
-use std::ptr;
 
 /// Context for evaluating expressions
 #[derive(Default)]
@@ -53,33 +50,5 @@ impl<'a> Evaluatable<'a> for Expression<'a> {
             _ => return Err(Box::new(RawError::from(format!("cannot evaluate: {:?}", self)))),
         };
         Ok(value)
-    }
-}
-
-impl<'a> Expression<'a> {
-    pub fn get_id(&self) -> u64 {
-        let mut hasher = DefaultHasher::new();
-        match &self {
-            Expression::Null => ptr::hash(&self, &mut hasher),
-            Expression::Uint32(v) => ptr::hash(v, &mut hasher),
-            Expression::Boolean(v) => ptr::hash(v, &mut hasher),
-            Expression::Array(v) => ptr::hash(v, &mut hasher),
-            Expression::Case(v) => ptr::hash(v, &mut hasher),
-            Expression::ColumnRef(v) => ptr::hash(v, &mut hasher),
-            Expression::Conjunction(v) => ptr::hash(v, &mut hasher),
-            Expression::ConstCast(v) => ptr::hash(v, &mut hasher),
-            Expression::Disjunction(v) => ptr::hash(v, &mut hasher),
-            Expression::Exists(v) => ptr::hash(v, &mut hasher),
-            Expression::FunctionCall(v) => ptr::hash(v, &mut hasher),
-            Expression::Indirection(v) => ptr::hash(v, &mut hasher),
-            Expression::Nary(v) => ptr::hash(v, &mut hasher),
-            Expression::ParameterRef(v) => ptr::hash(v, &mut hasher),
-            Expression::SelectStatement(v) => ptr::hash(v, &mut hasher),
-            Expression::StringRef(v) => ptr::hash(v, &mut hasher),
-            Expression::Subquery(v) => ptr::hash(v, &mut hasher),
-            Expression::TypeCast(v) => ptr::hash(v, &mut hasher),
-            Expression::TypeTest(v) => ptr::hash(v, &mut hasher),
-        };
-        hasher.finish()
     }
 }
