9f520ba243482da6c8fb431a034744b94c21a4f2
Andre Kohn <kohn.a@outlook.com>
2022-06-23 16:07:27 +0200

    work towards browser tests

--- libs/duckdbx-api/js/browser_runtime.mjs
+++ libs/duckdbx-api/js/browser_runtime.mjs
@@ -1,6 +1,46 @@
-export async function createClient() {}
-export async function openDatabase(client, path) {}
-export async function closeDatabase(db) {}
-export async function createConnection(db) {}
-export async function closeConnection(db) {}
-export async function runQuery(conn, text) {}
+import * as duckdb from '@duckdb/duckdb-wasm';
+import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm';
+import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm';
+
+const DUCKDB_BUNDLES = {
+    mvp: {
+        mainModule: duckdb_wasm,
+        mainWorker: new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js', import.meta.url).toString(),
+    },
+    eh: {
+        mainModule: duckdb_wasm_eh,
+        mainWorker: new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js', import.meta.url).toString(),
+    },
+};
+let duckdbBundle = null;
+
+export async function createClient() {
+    if (duckdbBundle != null) {
+        duckdbBundle = await duckdb.selectBundle(DUCKDB_BUNDLES);
+    }
+    const logger = new duckdb.ConsoleLogger(duckdb.LogLevel.WARNING);
+    const worker = new Worker(duckdbBundle.mainWorker);
+    return new duckdb.AsyncDuckDB(logger, worker);
+}
+
+export async function openDatabase(db, path) {
+    if (path !== undefined && path !== null) {
+        await db.open({
+            path: path,
+        });
+    }
+    return db;
+}
+export async function closeDatabase(db) {
+    return await db.terminate();
+}
+export async function createConnection(db) {
+    return await db.connect();
+}
+export async function closeConnection(conn) {
+    return await conn.close();
+}
+export async function runQuery(conn, text) {
+    const id = conn.useUnsafe((_, c) => c);
+    return await conn.bindings.runQuery(id, text);
+}
--- libs/duckdbx-api/tests/wasm_tests.rs
+++ libs/duckdbx-api/tests/wasm_tests.rs
@@ -3,11 +3,21 @@
 #[cfg(feature = "browser")]
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
+use duckdbx_api;
 use std::assert_eq;
-
+use std::error::Error;
 use wasm_bindgen_test::*;
 
 #[wasm_bindgen_test]
 fn pass() {
     assert_eq!(1 + 1, 2);
+}
+
+#[wasm_bindgen_test(async)]
+pub async fn hello_duckdb() {
+    let client = duckdbx_api::DatabaseClient::create().await.unwrap();
+    let db = client.open_transient().await.unwrap();
+    let conn = db.connect().await.unwrap();
+    let result = conn.run_query("select 1;").await.unwrap();
+    assert_eq!(result.len(), 1);
 }
