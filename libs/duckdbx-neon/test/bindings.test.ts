ff82ddb2ad62e9171e3e8b8baf8151c3f9afcdee
Andre Kohn <kohn.a@outlook.com>
2022-06-26 14:09:30 +0200

    hello duckdb test

--- libs/duckdbx-neon/test/bindings.test.ts
+++ libs/duckdbx-neon/test/bindings.test.ts
@@ -1,13 +1,24 @@
 import * as duckdbx from '../src';
+import * as arrow from 'apache-arrow';
 
 export function testBindings() {
     describe('Bindings', () => {
         it('hello duckdb', async () => {
             const db = await duckdbx.openInMemory();
             const conn = await db.connect();
-            const result = await conn.runQuery('select 1');
+            const result = await conn.runQuery('select 42::integer as a');
             const buffer = result.access();
             expect(buffer.length).toBeGreaterThan(0);
+            const reader = arrow.RecordBatchReader.from<{
+                a: arrow.Int32;
+            }>(buffer);
+            expect(reader.isSync()).toBeTrue();
+            expect(reader.isFile()).toBeTrue();
+            const table = new arrow.Table(reader);
+            expect(table.numCols).toEqual(1);
+            expect(table.numRows).toEqual(1);
+            const rows = table.toArray();
+            expect(rows[0].a).toEqual(42);
         });
     });
 }
