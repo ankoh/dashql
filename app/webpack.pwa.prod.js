70072d07917e756a9e83f4ad5dd445d63a172112
Andre Kohn <kohn.a@outlook.com>
2022-06-28 00:24:15 +0200

    work on electron config

--- app/webpack.pwa.prod.js
+++ app/webpack.pwa.prod.js
@@ -5,22 +5,24 @@ import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const buildDir = path.resolve(__dirname, './dist/pwa');
 
-export default {
-    ...configure({
-        buildDir,
-        tsLoaderOptions: {
-            compilerOptions: {
-                configFile: './tsconfig.json',
-                sourceMap: false,
-            },
+const base = configure({
+    buildDir,
+    tsLoaderOptions: {
+        compilerOptions: {
+            configFile: './tsconfig.json',
+            sourceMap: false,
         },
-        extractCss: true,
-        cssIdentifier: '[hash:base64]',
-        dashqlAPP: 'https://app.dashql.com',
-        dashqlAPI: 'https://api.dashql.com',
-        githubOAuthClientID: '286d19fc45d2e4e826d6',
-        githubOAuthCallback: `https://api.dashql.com/static/html/github_oauth.${GITHUB_OAUTH_VERSION}.html`,
-    }),
+    },
+    extractCss: true,
+    cssIdentifier: '[hash:base64]',
+    dashqlAPP: 'https://app.dashql.com',
+    dashqlAPI: 'https://api.dashql.com',
+    githubOAuthClientID: '286d19fc45d2e4e826d6',
+    githubOAuthCallback: `https://api.dashql.com/static/html/github_oauth.${GITHUB_OAUTH_VERSION}.html`,
+});
+
+export default {
+    ...base,
     mode: 'production',
     devtool: false,
 };
