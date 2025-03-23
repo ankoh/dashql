877da528daaa430b55a62cb9e903886cee4ad1ab
André Kohn <andre.kohn@salesforce.com>
2025-03-23 16:13:40 +0100

    Update progress state during file loading

--- packages/dashql-app/src/view/file_loader.tsx
+++ packages/dashql-app/src/view/file_loader.tsx
@@ -1,27 +1,89 @@
 import * as React from 'react';
-import * as core from '@ankoh/dashql-core';
 import * as pb from '@ankoh/dashql-protobuf';
 import * as zstd from '../utils/zstd.js';
 import Immutable from 'immutable';
 
 import { CATALOG_DEFAULT_DESCRIPTOR_POOL } from '../connection/catalog_update_state.js';
-import { ConnectionAllocator } from '../connection/connection_registry.js';
+import { ConnectionAllocator, useConnectionStateAllocator } from '../connection/connection_registry.js';
 import { ConnectionStateWithoutId } from '../connection/connection_state.js';
 import { PlatformFile } from "../platform/file.js";
 import { ScriptData, WorkbookEntry } from '../workbook/workbook_state.js';
 import { ScriptLoadingStatus } from '../workbook/script_loader.js';
-import { WorkbookAllocator, WorkbookStateWithoutId } from '../workbook/workbook_state_registry.js';
+import { useWorkbookStateAllocator, WorkbookAllocator, WorkbookStateWithoutId } from '../workbook/workbook_state_registry.js';
 import { createConnectionParamsSignature, createConnectionStateFromParams, readConnectionParamsFromProto } from '../connection/connection_params.js';
 import { decodeCatalogFileFromProto } from '../connection/catalog_import.js';
 import { ScriptOriginType, ScriptType } from '../workbook/script_metadata.js';
 import { DashQLSetupFn, useDashQLCoreSetup } from '../core_provider.js';
 
+interface ProgressState {
+    // The time when the file reading started
+    fileReadingStartedAt: Date | null;
+    // The time when the file finished at
+    fileReadingFinishedAt: Date | null;
+    // The file size
+    fileByteCount: number | null;
+
+    // The time when the file decompressing started
+    fileDecompressingStartedAt: Date | null;
+    // The time when the file decompressing finished
+    fileDecompressingFinishedAt: Date | null;
+    // The decompressed file size
+    fileDecompressedByteCount: number | null;
+
+    // The number of catalogs
+    catalogCount: number | null;
+    // The number of loaded catalogs
+    catalogsLoaded: number;
+    // The time when the loading of the first catalog started
+    catalogLoadingStartedAt: Date | null;
+    // The time when the loading of the last catalog finished
+    catalogLoadingFinishedAt: Date | null;
+
+    // The number of workbooks
+    workbookCount: number | null;
+    // The number of loaded workbooks
+    workbooksLoaded: number;
+    // The time when the loading of the first workbook started
+    workbookLoadingStartedAt: Date | null;
+    // The time when the loading of the last workbook finished
+    workbookLoadingFinishedAt: Date | null;
+
+    // The time when the loading finished
+    fileLoadingFinishedAt: Date | null;
+}
+
+type UpdateProgressFn = (state: ProgressState) => void;
+
 interface Props {
     file: PlatformFile;
 }
 
-async function loadDashQLFile(file: PlatformFile, dqlSetup: DashQLSetupFn, allocateConn: ConnectionAllocator, allocateWorkbook: WorkbookAllocator, signal: AbortSignal) {
+async function loadDashQLFile(file: PlatformFile, dqlSetup: DashQLSetupFn, allocateConn: ConnectionAllocator, allocateWorkbook: WorkbookAllocator, updateProgress: UpdateProgressFn, signal: AbortSignal) {
+    const progress: ProgressState = {
+        fileReadingStartedAt: null,
+        fileReadingFinishedAt: null,
+        fileByteCount: null,
+
+        fileDecompressingStartedAt: null,
+        fileDecompressingFinishedAt: null,
+        fileDecompressedByteCount: null,
+
+        catalogCount: null,
+        catalogsLoaded: 0,
+        catalogLoadingStartedAt: null,
+        catalogLoadingFinishedAt: null,
+
+        workbookCount: null,
+        workbooksLoaded: 0,
+        workbookLoadingStartedAt: null,
+        workbookLoadingFinishedAt: null,
+
+        fileLoadingFinishedAt: null,
+    };
     try {
+        progress.fileReadingStartedAt = new Date();
+        updateProgress({ ...progress });
+
         // Setup DashQL
         const dql = await dqlSetup("file_loader");
         signal.throwIfAborted();
@@ -30,11 +92,23 @@ async function loadDashQLFile(file: PlatformFile, dqlSetup: DashQLSetupFn, alloc
         const fileBuffer = await file.readAsArrayBuffer();
         signal.throwIfAborted();
 
+        progress.fileReadingFinishedAt = new Date();
+        progress.fileByteCount = fileBuffer.byteLength;
+        progress.fileDecompressingStartedAt = progress.fileReadingFinishedAt;
+        updateProgress({ ...progress });
+
         // Decompress the file buffer
         await zstd.init();
         signal.throwIfAborted();
         const fileDecompressed = zstd.decompress(fileBuffer);
         const fileProto = pb.dashql.file.File.fromBinary(fileDecompressed);
+
+        progress.fileDecompressingFinishedAt = new Date();
+        progress.fileDecompressedByteCount = fileDecompressed.byteLength;
+        progress.catalogLoadingStartedAt = progress.fileDecompressingFinishedAt;
+        progress.catalogCount = fileProto.catalogs.length;
+        progress.workbookCount = fileProto.workbooks.length;
+        updateProgress({ ...progress });
 
         // The connection map
         const connMap = new Map<string, [number, ConnectionStateWithoutId]>();
@@ -62,7 +136,6 @@ async function loadDashQLFile(file: PlatformFile, dqlSetup: DashQLSetupFn, alloc
                 connState = createConnectionStateFromParams(dql, params);
                 connId = allocateConn(connState);
                 connMap.set(paramsSig, [connId, connState]);
-                continue;
             } else {
                 [connId, connState] = prevConn;
             }
@@ -70,7 +143,14 @@ async function loadDashQLFile(file: PlatformFile, dqlSetup: DashQLSetupFn, alloc
             // Add schema descriptors
             const catalogProto = decodeCatalogFileFromProto(fileCatalog);
             connState!.catalog.addSchemaDescriptorsT(CATALOG_DEFAULT_DESCRIPTOR_POOL, catalogProto);
+
+            progress.catalogLoadingFinishedAt = new Date();
+            progress.catalogsLoaded += 1;
+            updateProgress({ ...progress });
         }
+
+        progress.workbookLoadingStartedAt = new Date();
+        updateProgress({ ...progress });
 
         // Setup workbook connections that are not covered by catalogs
         for (const workbook of fileProto.workbooks) {
@@ -94,7 +174,6 @@ async function loadDashQLFile(file: PlatformFile, dqlSetup: DashQLSetupFn, alloc
                 connState = createConnectionStateFromParams(dql, params);
                 connId = allocateConn(connState);
                 connMap.set(paramsSig, [connId, connState]);
-                continue;
             } else {
                 [connId, connState] = prevConn;
             }
@@ -186,23 +265,37 @@ async function loadDashQLFile(file: PlatformFile, dqlSetup: DashQLSetupFn, alloc
             workbookIds.push(workbookId);
 
             console.log(workbookState);
+
+            progress.workbookLoadingFinishedAt = new Date();
+            progress.workbooksLoaded += 1;
+            updateProgress({ ...progress });
         }
 
-        // XXX Switch current workbook
+        progress.fileLoadingFinishedAt = new Date();
+        updateProgress({ ...progress });
 
-        console.log(`read bytes: ${fileBuffer.byteLength}, decompressed: ${fileDecompressed.byteLength}`);
-        console.log(fileProto);
     } catch (e: any) {
         console.log(e);
     }
 }
 
 export function FileLoader(props: Props) {
-    const dql = useDashQLCoreSetup();
+    const dqlSetup = useDashQLCoreSetup();
+    const allocateConnection = useConnectionStateAllocator();
+    const allocateWorkbook = useWorkbookStateAllocator();
+    const [_progress, setProgress] = React.useState<ProgressState | null>(null);
 
     React.useEffect(() => {
+        const proxiedSetProgress = (value: ProgressState | null) => {
+            console.log(value);
+            setProgress(value);
+        };
 
-    });
+        const abort = new AbortController();
+        loadDashQLFile(props.file, dqlSetup, allocateConnection, allocateWorkbook, proxiedSetProgress, abort.signal);
+        return () => abort.abort();
+
+    }, [dqlSetup, allocateWorkbook, allocateConnection, props.file]);
 
     return <div />;
 }
