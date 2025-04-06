e3c307eef679ed87ca8bb545e7e4b641a8e3d6e8
André Kohn <kohn.a@outlook.com>
2025-04-06 10:43:41 +0200

    Update gtest to v1.16.0

--- packages/dashql-core/cmake/gtest.cmake
+++ packages/dashql-core/cmake/gtest.cmake
@@ -1,9 +1,10 @@
 include(ExternalProject)
 
+# 6910c9d -> v1.16.0
 ExternalProject_Add(
     gtest_ep
     GIT_REPOSITORY "https://github.com/google/googletest.git"
-    GIT_TAG b796f7d
+    GIT_TAG 6910c9d
     TIMEOUT 10
     PREFIX "external_gtest"
     INSTALL_DIR "external_gtest/install"
