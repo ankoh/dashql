fa04ecd1d0eeda973193af6672df42224eefffac
André Kohn <andre.kohn@salesforce.com>
2025-04-15 15:09:12 +0200

    Refactor murmur3 hashing

--- packages/dashql-app/src/utils/hash_murmur3.test.ts
+++ packages/dashql-app/src/utils/hash_murmur3.test.ts
@@ -1,7 +1,12 @@
-import { Murmur3_128 } from './hash_murmur3.js';
+import { Murmur3_x86_128 } from './hash_murmur3.js';
 
 describe('Murmur3_128', () => {
-    it("hashes", () => {
-        expect(Murmur3_128.withSeed(128).add("Hello, world!").asString()).toEqual("a548a5a0a6ab7834b355131677149730");
+    // https://github.com/PeterScott/murmur3/blob/master/test.c
+    // https://github.com/m2osw/murmur3/blob/main/tests/catch_basic.cpp
+    it("examples", () => {
+        expect(Murmur3_x86_128.withSeed(123).add("Hello, world!").asString()).toEqual("61c9129e5a1aacd7a41621629e37c886");
+        expect(Murmur3_x86_128.withSeed(321).add("Hello, world!").asString()).toEqual("d5fbdcb3c26c4193045880c5a7170f0f");
+        expect(Murmur3_x86_128.withSeed(123).add("xxxxxxxxxxxxxxxxxxxxxxxxxxxx").asString()).toEqual("5e40bab278825a164cf929d31fec6047");
+        expect(Murmur3_x86_128.withSeed(123).add("").asString()).toEqual("fedc524526f3e79926f3e79926f3e799");
     });
 });
