952b254ac41d86fbbd88924a7bc49c6a33495b3b
André Dirnberger <kohn.a@outlook.com>
2025-09-05 14:31:00 +0200

    Skip qualification hint test for now

--- packages/dashql-app/src/view/workbook/dashql_completion_hint.test.ts
+++ packages/dashql-app/src/view/workbook/dashql_completion_hint.test.ts
@@ -9,6 +9,7 @@ declare const DASHQL_PRECOMPILED: (stubs: WebAssembly.Imports) => PromiseLike<We
 
 let dql: dashql.DashQL | null = null;
 beforeAll(async () => {
+    expect(DASHQL_PRECOMPILED).toBeDefined();
     dql = await dashql.DashQL.create(DASHQL_PRECOMPILED);
     expect(dql).not.toBeNull();
 });
@@ -36,9 +37,12 @@ describe('Completion Hint', () => {
         const candidate = completionReader.candidates(0);
         expect(candidate?.completionText()).toEqual("\"attrA\"");
 
+        // Compute completion hints
         const textBuffer = Text.of([text]);
         const hints = computeCompletionHints(completionPtr, 0, textBuffer);
         expect(hints).not.toBeNull();
+
+        // Check candidate hint
         expect(hints!.candidate.hintPrefix).not.toBeNull();
         expect(hints!.candidate.hintPrefix!.text).toEqual("\"");
         expect(hints!.candidate.hintPrefix!.at).toEqual(text.length - "attr".length);
@@ -46,10 +50,61 @@ describe('Completion Hint', () => {
         expect(hints!.candidate.hintSuffix!.text).toEqual("A\"");
         expect(hints!.candidate.hintSuffix!.at).toEqual(text.length);
 
-        cursorPtr.destroy();
         completionPtr.destroy();
+        cursorPtr.destroy();
         scriptPtr.destroy();
         schemaScriptPtr.destroy();
+        registry.destroy();
+        catalog.destroy();
+    });
+
+    it.skip('candidate qualification', async () => {
+        const catalog = dql!.createCatalog();
+        const registry = dql!.createScriptRegistry();
+        const schemaScriptPtr = dql!.createScript(catalog, 1);
+        const scriptPtr = dql!.createScript(catalog, 2);
+
+        schemaScriptPtr.insertTextAt(0, "create table db0.schema0.\"tableA\"(\"attrA\" int)")
+        schemaScriptPtr.analyze();
+        registry.addScript(schemaScriptPtr);
+        catalog.loadScript(schemaScriptPtr, 0);
+
+        const text = "select * from tab";
+        scriptPtr.insertTextAt(0, text);
+        scriptPtr.analyze();
+        const cursorPtr = scriptPtr.moveCursor(text.search(" tab") + 4);
+        const completionPtr = scriptPtr.completeAtCursor(10, registry);
+
+        const completionReader = completionPtr.read()
+        expect(completionReader.candidatesLength()).toEqual(10);
+        const candidate = completionReader.candidates(0);
+        expect(candidate?.completionText()).toEqual("\"tableA\"");
+
+        // Compute completion hints
+        const textBuffer = Text.of([text]);
+        const hints = computeCompletionHints(completionPtr, 0, textBuffer);
+        expect(hints).not.toBeNull();
+
+        // Check candidate hint
+        expect(hints!.candidate.hintPrefix).not.toBeNull();
+        expect(hints!.candidate.hintPrefix!.text).toEqual("\"");
+        expect(hints!.candidate.hintPrefix!.at).toEqual(text.length - "tab".length);
+        expect(hints!.candidate.hintSuffix).not.toBeNull();
+        expect(hints!.candidate.hintSuffix!.text).toEqual("leA\"");
+        expect(hints!.candidate.hintSuffix!.at).toEqual(text.length);
+
+        // Check qualification hint
+        expect(hints!.candidateQualification).not.toBeNull();
+        expect(hints!.candidateQualification!.hintPrefix).not.toBeNull();
+        expect(hints!.candidateQualification!.hintPrefix!.text).toEqual("db0.schema0.");
+        expect(hints!.candidateQualification!.hintSuffix).toBeNull();
+
+        completionPtr.destroy();
+        cursorPtr.destroy();
+        scriptPtr.destroy();
+        schemaScriptPtr.destroy();
+        registry.destroy();
+        catalog.destroy();
     });
 });
 
