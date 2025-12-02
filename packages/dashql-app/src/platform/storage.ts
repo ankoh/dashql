a7c39c350b82541fab786eb59cbe5557b9ddcd50
André Dirnberger <kohn.a@outlook.com>
2025-12-02 21:49:12 +0100

    Simplify idb storage model

--- packages/dashql-app/src/platform/storage.ts
+++ packages/dashql-app/src/platform/storage.ts
@@ -1,7 +1,7 @@
 import * as dexie from 'dexie';
 import { type EntityTable } from 'dexie';
 
-import { StoredConnection, StoredConnectionAuth, StoredConnectionCatalog, StoredWorkbook, StoredWorkbookEntry, StoredWorkbookScript } from './storage_model.js';
+import { StoredConnection, StoredConnectionCatalog, StoredWorkbook, StoredWorkbookScript } from './storage_model.js';
 
 const DATABASE_NAME = "DASHQL";
 const DATABASE_VERSION_1 = 1;
@@ -10,30 +10,22 @@ export const DB = (new dexie.Dexie(DATABASE_NAME)) as dexie.Dexie & {
     /// The connections
     connections: EntityTable<StoredConnection, "id">,
     /// The connection auth state
-    connectionAuth: EntityTable<StoredConnectionAuth, "connectionId">,
-    /// The connection auth state
     connectionCatalogs: EntityTable<StoredConnectionCatalog, "connectionId">,
     /// The workbooks
     workbooks: EntityTable<StoredWorkbook, "id">,
     /// The workbook scripts
     workbookScripts: EntityTable<StoredWorkbookScript, "id">,
-    /// The workbook scripts
-    workbookEntries: EntityTable<StoredWorkbookEntry, "id">,
 };
 
 DB.version(DATABASE_VERSION_1).stores({
     /// The connection config
     connections: "++id",
-    /// The connection auth params
-    connectionAuth: "connectionId",
     /// The connection catalogs
     connectionCatalogs: "connectionId",
     /// The workbooks
     workbooks: "++id,connectionId",
     /// The workbook scripts
     workbookScripts: "++id,workbookId",
-    /// The workbook entries
-    workbookEntries: "++id,workbookId,scriptId",
 });
 
 
--- packages/dashql-app/src/platform/storage_model.ts
+++ packages/dashql-app/src/platform/storage_model.ts
@@ -1,15 +1,19 @@
+// The storage model of DashQL is held deliberately simple.
+//
+// We store:
+//  - Connections: These are the exported connection states of all connections in the app.
+//  - Connection Catalogs: Catalog refreshes can be very costly (e.g. Trino) and we want to save an exported catalog on disk if we have one.
+//  - Workbooks: These are the exported workbook states minus anything stored separately
+//               (Connection params, Workbook scripts)
+//  - Workbook Scripts: These are the actual script texts
+
 
 export interface StoredConnection {
     /// The connection id
     id: number;
-    /// The connection params as protobuf `dashql.ConnectionParams`
-    paramsBuffer: Uint8Array;
+    /// The connection details as protobuf `dashql.SalesforceConnectionDetails`
+    connectionBuffer: Uint8Array;
 };
-
-export interface StoredConnectionAuth {
-    /// The connection id
-    connectionId: number;
-}
 
 export interface StoredConnectionCatalog {
     /// The connection id
@@ -21,28 +25,23 @@ export interface StoredConnectionCatalog {
 export interface StoredWorkbook {
     /// The workbook id
     id: number;
-    /// The connection id
+    /// The connection id.
+    /// Immutable, doesn't change.
     connectionId: number;
-    /// The workbook as protobuf `dashql.Workbook`
+    /// The workbook as protobuf `dashql.Workbook`.
+    /// Workbook scripts are not stored as part of the protobuf.
+    /// They are instead stored separately as `StoredWorkbookScript`.
     workbookBuffer: Uint8Array;
 }
 
+/// We deliberately store the workbook script separately.
+/// We'll update this entry very regularly whenever the script text changes.
+/// We don't want to re-serialize the entire workbook state over and over again.
 export interface StoredWorkbookScript {
     /// The script id
     id: number;
     /// The workbook id
     workbookId: number;
-    /// The script buffer
-    scriptBuffer: Uint8Array;
-}
-
-export interface StoredWorkbookEntry {
-    /// The entry id
-    id: number;
-    /// The workbook id
-    workbookId: number;
-    /// The script id
-    scriptId: number;
-    /// The entry index
-    entryIndex: number;
+    /// The script text
+    scriptText: string;
 }
