2a63e0198c85fe0aaea223ad4ec7cea6f30353db
André Dirnberger <kohn.a@outlook.com>
2026-03-08 13:48:30 +0100

    Inject dependency paths into vite config template

--- packages/dashql-app/vite.config.template.ts
+++ packages/dashql-app/vite.config.template.ts
@@ -2,30 +2,37 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import * as path from "node:path";
 
-const PROTOBUF_PATH = "__PROTOBUF_PATH__";
-const COMPUTE_PATH = "__COMPUTE_PATH__";
-const CORE_API_PATH = "__CORE_API_PATH__";
-const CORE_WASM_PATH = "__CORE_WASM_PATH__";
+const PROTOBUF_PATH = path.resolve(__dirname, "__PROTOBUF_PATH__");
+const COMPUTE_PATH = path.resolve(__dirname, "__COMPUTE_PATH__");
+const CORE_API_PATH = path.resolve(__dirname, "__CORE_API_PATH__");
+const CORE_WASM_PATH = path.resolve(__dirname, "__CORE_WASM_PATH__");
+const ZSTD_WASM_PATH = path.resolve(__dirname, "__ZSTD_WASM_PATH__");
 
 export default defineConfig({
   plugins: [react()],
   root: ".",
   build: {
     rollupOptions: {
-      input: "./oauth.html",
+      input: {
+        app: path.resolve(__dirname, "index.html"),
+        oauth_redirect: path.resolve(__dirname, "oauth.html"),
+      },
     },
     outDir: "dist",
   },
   resolve: {
     alias: [
+      { find: "@ankoh/dashql-protobuf", replacement: PROTOBUF_PATH },
+      { find: "@ankoh/dashql-compute", replacement: COMPUTE_PATH },
+      { find: "@ankoh/dashql-core", replacement: CORE_API_PATH },
       {
-        // Matches "@ankoh/dashql-core-wasm" AND "@ankoh/dashql-core-wasm?url" and preserves the query string
         find: /^@ankoh\/dashql-core-wasm(\?.*)?$/,
-        replacement: path.resolve(__dirname, CORE_WASM_PATH) + "$1",
+        replacement: CORE_WASM_PATH + "$1",
       },
-      { find: "@ankoh/dashql-protobuf", replacement: path.resolve(__dirname, PROTOBUF_PATH) },
-      { find: "@ankoh/dashql-compute", replacement: path.resolve(__dirname, COMPUTE_PATH) },
-      { find: "@ankoh/dashql-core", replacement: path.resolve(__dirname, CORE_API_PATH) },
+      {
+        find: /^@bokuweb\/zstd-wasm\/dist\/web\/zstd.wasm(\?.*)?$/,
+        replacement: ZSTD_WASM_PATH + "$1",
+      },
     ],
   },
 });
