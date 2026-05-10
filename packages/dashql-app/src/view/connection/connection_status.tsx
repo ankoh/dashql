cd6d3e21280298c99ba93791078c01d989d84e87
André Dirnberger <kohn.a@outlook.com>
2026-05-10 15:24:37 +0200

    Refine connection status for demo connector

--- packages/dashql-app/src/view/connection/connection_status.tsx
+++ packages/dashql-app/src/view/connection/connection_status.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import symbols from '@ankoh/dashql-svg-symbols';
 
 import { ConnectionState } from '../../connection/connection_state.js';
+import { ConnectorType } from '../../connection/connector_info.js';
+import { DatalessConnectionStateDetails, isDemoConnector } from '../../connection/dataless/dataless_connection_state.js';
 import { Button, ButtonVariant } from '../../view/foundations/button.js';
 
 interface ButtonProps {
@@ -29,9 +31,13 @@ export const CONNECTION_HEALTH_COLORS: string[] = [
 
 export const ConnectionStatus = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
     const health = props.conn.connectionHealth ?? 0;
-    const connStatusText = CONNECTION_HEALTH_NAMES[health];
-    const connStatusColor = CONNECTION_HEALTH_COLORS[health];
     const connectorIcon = props.conn.connectorInfo.icons.colored;
+
+    const isDatalessNonDemo =
+        props.conn.connectorInfo.connectorType === ConnectorType.DATALESS &&
+        !isDemoConnector(props.conn.details.value as DatalessConnectionStateDetails);
+    const connStatusText = isDatalessNonDemo ? "Dataless" : "Demo";
+    const connStatusColor = CONNECTION_HEALTH_COLORS[health];
 
     const handleClick = () => {
         if (props.onClick) {
@@ -55,7 +61,7 @@ export const ConnectionStatus = React.forwardRef<HTMLButtonElement, ButtonProps>
             ref={ref}
             variant={ButtonVariant.Default}
             leadingVisual={ConnectorIconVisual}
-            trailingVisual={StatusDotVisual}
+            trailingVisual={isDatalessNonDemo ? undefined : StatusDotVisual}
             onClick={handleClick}
             aria-label={props.compact ? connStatusText : undefined}
         >
