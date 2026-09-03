78458d30452fcebf03a5a5ce752f246007f6d4e6
André Dirnberger <kohn.a@outlook.com>
2026-09-03 13:24:56 +0200

    Add example notebook with TPCH queries

---
+++ examples/notebooks/tpch/dashql-functions.sql
@@ -0,0 +1,6 @@
+-- DashQL Connection Functions.
+-- This file is auto-generated and can only be updated through a catalog refresh.
+--
+-- Catalog Source: -
+-- Last Refresh: 2026-09-03T06:52:18.177Z
+
